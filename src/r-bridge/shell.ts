e860d00f4f3e1e934a251c1c30bae5a4dff096dd
Florian Sihler <florian.sihler@uni-ulm.de>
2023-04-08 21:58:57 +0200

    refactor: remove unnecessary function onLine within shell

--- src/r-bridge/shell.ts
+++ src/r-bridge/shell.ts
@@ -286,10 +286,6 @@ class RShellSession {
     this.write(`${data}${this.options.eol}`)
   }
 
-  public onLine(from: OutputStreamSelector, listener: (line: string) => void): void {
-    this.on(from, 'line', listener)
-  }
-
   private on(from: OutputStreamSelector, event: string, listener: (...data: any[]) => void): void {
     const both = from === 'both'
     if (both || from === 'stdout') {

5a4e5fce05612e483895f15d740e61386700e96c
Florian Sihler <florian.sihler@uni-ulm.de>
2023-04-22 13:09:11 +0200

    test-fix: startAndEndsWith had wrong initial guard

--- src/util/strings.ts
+++ src/util/strings.ts
@@ -2,7 +2,7 @@
 import { guard } from './assert'
 
 export function startAndEndsWith (str: string, letter: string): boolean {
-  guard(str.length !== 1, `startAndEndsWith: letter must be a single character  ${letter}`)
+  guard(letter.length === 1, `startAndEndsWith: letter must be a single character  ${letter}`)
   guard(str.length >= 2, `startAndEndsWith: str can not be empty|one character to start and end with the same letter ${letter}`)
 
   return str[0] === letter && str[str.length - 1] === letter

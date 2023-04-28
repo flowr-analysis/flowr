5ae92aaf5951736d66b6cc3954433a1d30e32948
Florian Sihler <florian.sihler@uni-ulm.de>
2023-04-28 18:04:20 +0200

    feat: permutation function and tests for range merge

--- test/util/range.ts
+++ test/util/range.ts
@@ -1,10 +1,11 @@
 import { assert } from 'chai'
 import { mergeRanges, rangeFrom, SourceRange } from '../../src/util/range'
+import { allPermutations } from '../../src/util/arrays'
 
 describe('Ranges', () => {
   describe('rangeFrom', () => {
     it('correct parameters', () => {
-      const pool = [-1, 0, 1, 2, 3, 99]
+      const pool = [-1, 0, 1, 2, 99]
       for(const startLine of pool) {
         for(const startColumn of pool) {
           for (const endLine of pool) {
@@ -20,13 +21,14 @@ describe('Ranges', () => {
     })
   })
   describe('mergeRanges', () => {
-    function assertMerged (a: SourceRange, b: SourceRange, expected: SourceRange) {
-      assert.deepStrictEqual(mergeRanges(a, b), expected, `mergeRanges(${JSON.stringify(a)}, ${JSON.stringify(b)})`)
+    function assertMerged (expected: SourceRange, ...a: SourceRange[]) {
+      assert.deepStrictEqual(mergeRanges(...a), expected, `mergeRanges(${JSON.stringify(a)})`)
     }
 
-    const assertIndependentOfOrder= (a: SourceRange, b: SourceRange, expected: SourceRange): void => {
-      assertMerged(a, b, expected)
-      assertMerged(b, a, expected)
+    const assertIndependentOfOrder= (expected: SourceRange, ...a: SourceRange[]): void => {
+      for(const permutation of allPermutations(a)) {
+        assertMerged(expected, ...permutation)
+      }
     }
     it('throw on no range', () => {
       assert.throws(() => mergeRanges(), Error, undefined, 'no range to merge')
@@ -37,12 +39,16 @@ describe('Ranges', () => {
       }
     })
     it('overlapping ranges', () => {
-      assertIndependentOfOrder(rangeFrom(1, 1, 1, 2), rangeFrom(1, 2, 1, 3), rangeFrom(1, 1, 1, 3))
-      assertIndependentOfOrder(rangeFrom(1, 2, 1, 3), rangeFrom(1, 1, 1, 3), rangeFrom(1, 1, 1, 3))
-      assertIndependentOfOrder(rangeFrom(2, 1, 2, 3), rangeFrom(1, 2, 2, 4), rangeFrom(1, 2, 2, 4))
+      assertIndependentOfOrder(rangeFrom(1, 1, 1, 3), rangeFrom(1, 1, 1, 2), rangeFrom(1, 2, 1, 3))
+      assertIndependentOfOrder(rangeFrom(1, 1, 1, 3), rangeFrom(1, 2, 1, 3), rangeFrom(1, 1, 1, 3))
+      assertIndependentOfOrder(rangeFrom(1, 2, 2, 4), rangeFrom(2, 1, 2, 3), rangeFrom(1, 2, 2, 4))
     })
     it('non-overlapping ranges', () => {
-      assertIndependentOfOrder(rangeFrom(1, 1, 1, 2), rangeFrom(1, 3, 1, 4), rangeFrom(1, 1, 1, 4))
+      assertIndependentOfOrder(rangeFrom(1, 1, 1, 4), rangeFrom(1, 1, 1, 2), rangeFrom(1, 3, 1, 4))
+      assertIndependentOfOrder(rangeFrom(1, 1, 4, 4), rangeFrom(1, 1, 1, 1), rangeFrom(4, 4, 4, 4))
+    })
+    it('more than two ranges', () => {
+      assertIndependentOfOrder(rangeFrom(1, 1, 3, 3), rangeFrom(1, 1, 1, 1), rangeFrom(2, 2, 2, 2), rangeFrom(3, 3, 3, 3))
     })
   })
 })

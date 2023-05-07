2b0ceddc8aea5280f3d28470b45f417cf1fbf422
Florian Sihler <florian.sihler@uni-ulm.de>
2023-05-07 20:01:23 +0200

    feat: support nested operator assignments

--- src/statistics/features/assignments.ts
+++ src/statistics/features/assignments.ts
@@ -2,19 +2,26 @@ import { Feature, formatMap, Query } from '../feature'
 import { MergeableRecord } from '../../util/objects'
 import * as xpath from 'xpath-ts2'
 import { groupCount } from '../../util/arrays'
-import { RNumHexFloatRegex } from '../../r-bridge/lang:4.x/values'
 
 export interface AssignmentInfo extends MergeableRecord {
-  assignmentOperator: string[]
+  assignmentOperator:       string[]
+  nestedOperatorAssignment: number
 }
 
 // TODO: integers, constants, etc.
 export const initialAssignmentInfo = (): AssignmentInfo => ({
-  assignmentOperator: []
+  assignmentOperator:       [],
+  nestedOperatorAssignment: 0
 })
 
 const defaultOperatorAssignmentQuery: Query = xpath.parse(`//EQ_ASSIGN|//LEFT_ASSIGN|//RIGHT_ASSIGN`)
-
+// either <-/<<-/=, with a nested rhs, or ->/->> with a nested lhs
+const nestedOperatorAssignmentQuery: Query = xpath.parse(`//*[
+  (self::LEFT_ASSIGN or self::EQ_ASSIGN) and following-sibling::expr//*[self::LEFT_ASSIGN or self::EQ_ASSIGN or self::RIGHT_ASSIGN]
+ ]
+ |
+ //RIGHT_ASSIGN[preceding-sibling::expr//*[self::LEFT_ASSIGN or self::EQ_ASSIGN or self::RIGHT_ASSIGN]]
+`)
 
 export const assignments: Feature<AssignmentInfo> = {
   name:        'assignments',
@@ -22,6 +29,10 @@ export const assignments: Feature<AssignmentInfo> = {
 
   append(existing: AssignmentInfo, input: Document): AssignmentInfo {
     const assignmentOperators = defaultOperatorAssignmentQuery.select({ node: input }).map(n => n.textContent ?? '<unknown>')
+    const nestedOperators = nestedOperatorAssignmentQuery.select({ node: input }).length
+
+    existing.nestedOperatorAssignment += nestedOperators
+
     existing.assignmentOperator.push(...assignmentOperators)
     return existing
   },
@@ -30,6 +41,7 @@ export const assignments: Feature<AssignmentInfo> = {
     // TODO: separate between unique and total count
     return `---assignments-------------
 \toperator assignments (${data.assignmentOperator.length} times)${formatMap(groupCount(data.assignmentOperator), details)}
+\tnested operator assignments: ${data.nestedOperatorAssignment}
     `
   }
 }

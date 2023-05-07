e91049716356f38fb17c7ae886d9c598fac273f2
Florian Sihler <florian.sihler@uni-ulm.de>
2023-05-07 15:47:36 +0200

    feat: deal with parameter names

--- src/statistics/features/definedFunctions.ts
+++ src/statistics/features/definedFunctions.ts
@@ -1,4 +1,4 @@
-import { Feature, formatMap } from '../feature'
+import { append, Feature, formatMap } from '../feature'
 import * as xpath from 'xpath-ts2'
 import { MergeableRecord } from '../../util/objects'
 import { groupCount } from '../../util/arrays'
@@ -9,17 +9,19 @@ export interface FunctionDefinitionInfo extends MergeableRecord {
   // TODO: scoping/namespaces?
   // TODO: local/global functions etc.
   /** all, anonymous, assigned, non-assigned, ... */
-  total:             number
+  total:              number
   /** how many are really using OP-Lambda? */
-  lambdasOnly:       number
+  lambdasOnly:        number
   /** using `<<-`, `<-`, `=`, `->` `->>` */
-  assignedFunctions: FunctionNameInfo[]
+  assignedFunctions:  FunctionNameInfo[]
+  usedParameterNames: string[]
 }
 
 export const initialFunctionDefinitionInfo = (): FunctionDefinitionInfo => ({
-  total:             0,
-  lambdasOnly:       0,
-  assignedFunctions: []
+  total:              0,
+  lambdasOnly:        0,
+  assignedFunctions:  [],
+  usedParameterNames: []
 })
 
 // TODO: note that this can not work with assign, setGeneric and so on for now
@@ -36,6 +38,10 @@ export const queryAssignedFunctionDefinitions = xpath.parse(`
   //RIGHT_ASSIGN[preceding-sibling::expr/*[self::FUNCTION or self::OP-LAMBDA]]/following-sibling::expr[count(*)=1]/SYMBOL
 `)
 
+export const queryUsedParameterNames = xpath.parse(`
+  //FUNCTION/../SYMBOL_FORMALS
+`)
+
 export const definedFunctions: Feature<FunctionDefinitionInfo> = {
   name:        'defined functions',
   description: 'all functions defined within the document',
@@ -47,6 +53,9 @@ export const definedFunctions: Feature<FunctionDefinitionInfo> = {
     existing.total += allFunctions.length + allLambdas.length
     existing.lambdasOnly += allLambdas.length
 
+    const usedParameterNames = queryUsedParameterNames.select({ node: input })
+    append(existing, 'usedParameterNames', usedParameterNames)
+
     const assignedFunctions = queryAssignedFunctionDefinitions.select({ node: input })
     existing.assignedFunctions.push(...new Set(assignedFunctions.map(node => node.textContent ?? '<unknown>')))
     return existing
@@ -55,10 +64,12 @@ export const definedFunctions: Feature<FunctionDefinitionInfo> = {
   toString(data: FunctionDefinitionInfo): string {
     const groupedAssignedFunctions = groupCount(data.assignedFunctions)
     const startingWithDot = [...groupedAssignedFunctions.keys()].filter(key => key.startsWith('.')).length
+    const groupedUsedParameterNames = groupCount(data.usedParameterNames)
 
     return `---defined functions------------
-\ttotal: ${data.total} (of which ${data.lambdasOnly} are lambdas)
+\ttotal: ${data.total} (of which ${data.lambdasOnly} are using OP-LAMBDA)
 \tfunctions defined: ${groupedAssignedFunctions.size} (of which ${startingWithDot} start with dot)${formatMap(groupedAssignedFunctions)}
+\tused parameter names: ${groupedUsedParameterNames.size} ${formatMap(groupedUsedParameterNames)}
 `
   }
 

66f8efa3eea6ab2ec937375687fc6c6599a48387
Florian Sihler <florian.sihler@uni-ulm.de>
2023-05-12 14:50:51 +0200

    feat: track if-then and if-then-else

--- src/statistics/features/controlflow.ts
+++ src/statistics/features/controlflow.ts
@@ -1,9 +1,11 @@
-import { Feature, FeatureInfo } from '../feature'
+import { Feature, FeatureInfo, Query } from '../feature'
 import * as xpath from 'xpath-ts2'
+import { append } from '../output/statisticsFile'
 
 export interface ControlflowInfo extends FeatureInfo {
   ifThen:                   number
   ifThenElse:               number
+  /** can be nested with if-s or if-then-else's */
   nestedIfThen:             number
   nestedIfThenElse:         number
   /** if(TRUE), ... */
@@ -28,13 +30,59 @@ export const initialControlflowInfo = (): ControlflowInfo => ({
   switchCase:               0
 })
 
-const ifThenQuery = xpath.parse(`//IF[not(.//ELSE)]`)
+const ifThenQuery: Query = xpath.parse(`//IF[not(following-sibling::ELSE)]`)
+const ifThenElseQuery: Query = xpath.parse(`//IF[following-sibling::ELSE]`)
+
+const selectCondition: Query = xpath.parse(`..//expr[preceding-sibling::OP-LEFT-PAREN][1]`)
+const constantCondition: Query = xpath.parse(`
+  ./NUM_CONST
+  |
+  ./NULL_CONST
+  |
+  ./STR_CONST
+  |
+  ./SYMBOL[text() = 'T' or text() = 'F']`)
+const singleVariableCondition: Query = xpath.parse(`./SYMBOL[text() != 'T' and text() != 'F']`)
+
+const nestedIfThenQuery: Query = xpath.parse(`..//expr/IF`)
+
+function collectForIfThenOptionalElse(existing: ControlflowInfo, name: 'IfThen' | 'IfThenElse',  ifThenOptionalElse: Node, filepath: string | undefined) {
+  // select when condition to check if constant, ...
+  const conditions = selectCondition.select({ node: ifThenOptionalElse })
+
+  append(controlflow.name, name, conditions, filepath)
+
+  const constantKey = `constant${name}`
+  const constantConditions = conditions.flatMap(c => constantCondition.select({ node: c }))
+
+  existing[constantKey] += constantConditions.length
+  append(controlflow.name, constantKey, constantConditions, filepath)
+
+  const singleVariableKey = `singleVariable${name}`
+  const singleVariableConditions = conditions.flatMap(c => singleVariableCondition.select({ node: c }))
+  existing[singleVariableKey] += singleVariableConditions.length
+  append(controlflow.name, singleVariableKey, singleVariableConditions, filepath)
+
+  const nestedKey = `nested${name}`
+  const nestedIfThens = nestedIfThenQuery.select({ node: ifThenOptionalElse })
+
+  existing[nestedKey] += nestedIfThens.length
+}
 
 export const controlflow: Feature<ControlflowInfo> = {
   name:        'Controlflow',
   description: 'Deals with if-then-else and switch-case',
 
   process(existing: ControlflowInfo, input: Document, filepath: string | undefined): ControlflowInfo {
+
+    const ifThen = ifThenQuery.select({ node: input })
+    const ifThenElse = ifThenElseQuery.select({ node: input })
+
+    existing.ifThen += ifThen.length
+    existing.ifThenElse += ifThenElse.length
+
+    ifThen.forEach(ifThen => { collectForIfThenOptionalElse(existing, 'IfThen', ifThen, filepath) })
+    ifThenElse.forEach(ifThenElse => { collectForIfThenOptionalElse(existing, 'IfThenElse', ifThenElse, filepath) })
 
     return existing
   }

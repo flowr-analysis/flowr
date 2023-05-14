a33ece643f869f2d4c94efad7689a17db11d3fbe
Florian Sihler <florian.sihler@uni-ulm.de>
2023-05-14 15:06:59 +0200

    feat: basic module documentations with extended tsdoc tag support

--- src/r-bridge/index.ts
+++ src/r-bridge/index.ts
@@ -1,3 +1,11 @@
+/**
+ * The `r-bridge` module provides the interface to the R-backend.
+ * It is responsible to retrieve the normalized version of an R-AST from the R-backend.
+ *
+ * Using a {@link RShell} connection, you can use the {@link retrieveAstFromRCode} function to retrieve the normalized AST.
+ *
+ * @module
+ */
 export * from './lang:4.x'
 export * from './shell'
 export * from './retriever'

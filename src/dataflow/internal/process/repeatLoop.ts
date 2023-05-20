dfbc4ac0bfbe49eaf251e75a834deb31bc2b69d8
Florian Sihler <florian.sihler@uni-ulm.de>
2023-05-20 15:23:04 +0200

    feat: naive processing of repeat loops in data flow

--- src/dataflow/internal/process/repeatLoop.ts
+++ src/dataflow/internal/process/repeatLoop.ts
@@ -1,15 +1,14 @@
-import { log } from '../../../util/log'
 import { DataflowInformation } from '../info'
 import { DataflowProcessorDown } from '../../processor'
 
-export function processRepeatLoop<OtherInfo>(loop: unknown, body: DataflowInformation<OtherInfo>, _down: DataflowProcessorDown<OtherInfo>): DataflowInformation<OtherInfo> {
-  // TODO
-  log.error('repeat loop not implemented')
-  return body
-  /* {
-    activeNodes: [],
-    in:          [...body.in, ...body.activeNodes.map(id => ({attribute: 'maybe' as const, id: id.id}))],
-    out:         body.out,
-    graph:       body.graph
-  } */
+export function processRepeatLoop<OtherInfo>(loop: unknown, body: DataflowInformation<OtherInfo>, down: DataflowProcessorDown<OtherInfo>): DataflowInformation<OtherInfo> {
+  return {
+    activeNodes:  [],
+    in:           [...body.in, ...body.activeNodes],
+    out:          body.out,
+    environments: body.environments,
+    ast:          down.ast,
+    scope:        down.scope,
+    graph:        body.graph
+  }
 }

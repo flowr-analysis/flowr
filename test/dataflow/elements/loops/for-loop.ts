6607b1a8467a05bf4b0e5c4d7fe2806626e14d9f
Florian Sihler <florian.sihler@uni-ulm.de>
2023-06-18 23:58:27 +0200

    test: testing multiple redefinitions in loop to validate that cyclic linking only uses the last one

--- test/dataflow/elements/loops/for-loop.ts
+++ test/dataflow/elements/loops/for-loop.ts
@@ -48,4 +48,27 @@ describe('for', withShell(shell => {
       .addEdge("7", "8", "defined-by", "always")
       .addEdge("0", "7", "same-def-def", "maybe")
   )
+
+  assertDataflow(`Redefinition within loop`,
+    shell,
+    `x <- 9\nfor(i in 1:10) { x <- x; x <- x }\n x`,
+    new DataflowGraph()
+      .addNode("0", "x", initializeCleanEnvironments(), LocalScope)
+      .addNode("7", "x", initializeCleanEnvironments(), LocalScope)
+      .addNode("10", "x", initializeCleanEnvironments(), LocalScope)
+      .addNode("8", "x", initializeCleanEnvironments())
+      .addNode("11", "x", initializeCleanEnvironments())
+      .addNode("15", "x", initializeCleanEnvironments())
+      .addNode("3", "i", initializeCleanEnvironments(), LocalScope)
+      .addEdge("11", "7", "read", "always")// second x <- *x* always reads first *x* <- x
+      .addEdge("8", "0", "read", "maybe")
+      .addEdge("8", "10", "read", "maybe")
+      .addEdge("15", "0", "read", "maybe")
+      .addEdge("15", "10", "read", "maybe")
+      .addEdge("7", "8", "defined-by", "always")
+      .addEdge("10", "11", "defined-by", "always")
+      .addEdge("0", "7", "same-def-def", "maybe")
+      .addEdge("0", "10", "same-def-def", "maybe")
+      .addEdge("7", "10", "same-def-def", "always") // both in same loop execution
+  )
 }))

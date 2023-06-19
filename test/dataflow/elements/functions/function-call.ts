314049c9981c278c6750fd7a531f412b71b2f5db
Florian Sihler <florian.sihler@uni-ulm.de>
2023-06-20 00:36:25 +0200

    test: another test for function definition

--- test/dataflow/elements/functions/function-call.ts
+++ test/dataflow/elements/functions/function-call.ts
@@ -37,8 +37,46 @@ describe('Function Call', withShell(shell => {
         .addEdge('9', '10', 'argument', 'always')
         .addEdge('9', '3', 'read', 'always')
     )
-    assertDataflow(`Calling function a with constant function`, shell, `i <- 4
-a <- function(x) { x <- 2; 1 }
-a(i)`, new DataflowGraph())
+    const envWithXConstDefined = define(
+      {nodeId: '4', scope: 'local', name: 'x', used: 'always', kind: 'argument', definedAt: '5' },
+      LocalScope,
+      pushLocalEnvironment(initializeCleanEnvironments()))
+    assertDataflow(`Calling with a constant function`, shell, `i <- 4
+a <- function(x) { x <- x; x <- 3; 1 }
+a(i)`, new DataflowGraph()
+      .addNode({ tag: 'variable-definition', id: '0', name: 'i', scope: LocalScope })
+      .addNode({ tag: 'use', id: '17', name: 'i'})
+      .addEdge('17', '0', 'read', 'always')
+      .addNode({ tag: 'variable-definition', id: '3', name: 'a', scope: LocalScope })
+      .addNode({ tag:  'function-call', id:   '16', name: 'a', args: [{
+        nodeId: '17', name: 'i', scope: LocalScope, used: 'always'
+      }]})
+      .addNode({
+        tag:     'function-definition',
+        id:      '14',
+        name:    '14',
+        scope:   LocalScope,
+        subflow: {
+          out:          [],
+          in:           [],
+          activeNodes:  [],
+          scope:        LocalScope,
+          environments: envWithXConstDefined,
+          graph:        new DataflowGraph()
+            .addNode({ tag: 'variable-definition', id: '4', name: 'x', scope: LocalScope, environment: envWithXConstDefined })
+            .addNode({ tag: 'variable-definition', id: '6', name: 'x', scope: LocalScope, environment: envWithXConstDefined })
+            .addNode({ tag: 'variable-definition', id: '9', name: 'x', scope: LocalScope, environment: envWithXConstDefined })
+            .addNode({ tag: 'use', id: '7', name: 'x', environment: envWithXConstDefined })
+            .addNode({ tag: 'use', id: '6', name: 'x', environment: envWithXConstDefined})
+            .addEdge('6', '7', 'defined-by', 'always')
+            .addEdge('7', '4', 'read', 'always')
+            .addEdge('6', '9', 'same-def-def', 'always')
+            .addEdge('4', '9', 'same-def-def', 'always')
+            .addEdge('4', '6', 'same-def-def', 'always')
+        }})
+      .addEdge('3', '14', 'defined-by', 'always')
+      .addEdge('16', '3', 'read', 'always')
+      .addEdge('16', '17', 'argument', 'always')
+    )
   })
 }))

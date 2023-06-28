f84fc5397d9c3cfa4cf8cbbac66c6aa0e07f2e19
Florian Sihler <florian.sihler@uni-ulm.de>
2023-06-28 10:39:53 +0200

    [skip ci] wip: remove await async layer for `xml2jsonObject`

--- src/r-bridge/lang:4.x/ast/parser/xml/internal/xml2json.ts
+++ src/r-bridge/lang:4.x/ast/parser/xml/internal/xml2json.ts
@@ -3,13 +3,13 @@ import { XmlParserConfig } from "../config"
 import { XmlBasedJson } from "../input-format"
 
 /**
- * parse the xml presented by R into a json object that will be used for conversion
+ * Parse the xml presented by R into a json object that will be used for conversion
  *
- * @param config    - the configuration to use (i.e., what names should be used for the attributes, children, ...)
- * @param xmlString - the xml input to parse
+ * @param config    - The configuration to use (i.e., what names should be used for the attributes, children, ...)
+ * @param xmlString - The xml input to parse
  */
-export async function xlm2jsonObject(config: XmlParserConfig, xmlString: string): Promise<XmlBasedJson> {
-  return await xml2js.parseStringPromise(xmlString, {
+export function xlm2jsonObject(config: XmlParserConfig, xmlString: string): Promise<XmlBasedJson> {
+  return xml2js.parseStringPromise(xmlString, {
     attrkey:               config.attributeName,
     charkey:               config.contentName,
     childkey:              config.childrenName,
@@ -19,5 +19,5 @@ export async function xlm2jsonObject(config: XmlParserConfig, xmlString: string)
     preserveChildrenOrder: true,
     normalize:             true,
     strict:                true
-  }) as XmlBasedJson
+  }) as Promise<XmlBasedJson>
 }

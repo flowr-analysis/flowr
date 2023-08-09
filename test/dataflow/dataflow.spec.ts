823503b0e2bae6858824beee6fc04398dfe849f1
Florian Sihler <florian.sihler@uni-ulm.de>
2023-08-09 15:59:55 +0200

    fix: remove demo test

--- test/dataflow/dataflow.spec.ts
+++ test/dataflow/dataflow.spec.ts
@@ -1,7 +1,3 @@
-import { assertDataflow, withShell } from '../helper/shell'
-import { DataflowGraph } from '../../src/dataflow'
-import { log, LogLevel } from '../../src/util/log'
-
 describe("Dataflow", () => {
   require('./environments/environments')
 
@@ -12,13 +8,5 @@ describe("Dataflow", () => {
       require('./elements/functions/function-definition')
       require('./elements/functions/function-call')
     })
-    describe('x', withShell(shell => {
-      before(() => {
-        log.updateSettings(logger => {
-          logger.settings.minLevel = LogLevel.error
-        })
-      })
-      assertDataflow('u', shell, 'file:///home/limerent/Documents/SocialScience/1-Zenodo_Output.csv-output/586/Paper_script.R', new DataflowGraph())
-    }))
   })
 })

4a20f253a0a00b5dcef22d95fb2ede8c2f251793
Florian Sihler <florian.sihler@uni-ulm.de>
2023-08-23 00:26:51 +0200

    test, refactor: move commutativity test within the same test to not unnecessarily inflate the counter

--- test/dataflow/graph/equal.ts
+++ test/dataflow/graph/equal.ts
@@ -1,37 +1,37 @@
 import { DataflowGraph, diffGraphsToMermaidUrl } from '../../../src/dataflow'
 import { assert } from 'chai'
 
+function test(cmp: (x: boolean) => void, a: DataflowGraph, b: DataflowGraph, text: string) {
+	try {
+		cmp(a.equals(b))
+	} catch (e) {
+		// only calculate the dataflow graphs if it fials
+		const diff = diffGraphsToMermaidUrl({ label: 'left', graph: a }, { label: 'right', graph: b }, undefined, '')
+		console.error(text + '; diff:\n', diff)
+		throw e
+	}
+}
+
 describe('Graph Equality', () => {
 	const raw = (name: string, a: DataflowGraph, b: DataflowGraph, text: string, cmp: (x: boolean) => void) => {
 		return it(name, () => {
-			// only calculate the dataflow graphs if not
-			try {
-				cmp(a.equals(b))
-			} catch (e) {
-				const diff = diffGraphsToMermaidUrl({ label: 'left', graph: a }, { label: 'right', graph: b }, undefined, '')
-				console.error(text + '; diff:\n', diff)
-				throw e
-			}
+			// as the comparison is relatively quick, we allow explicit checks for commutativity
+			test(cmp, a, b, 'a;b' + text)
+			test(cmp, b, a, 'b;a' + text)
 		})
 	}
 
 	describe('positive', () => {
-	// as the comparison is really quick, we allow explicit checks for commutativity
 		const eq = (name: string, a: DataflowGraph, b: DataflowGraph) => {
-			describe(name, () => {
-				raw('a==b', a, b, 'should be equal', x => assert.isTrue(x))
-				raw('b==a', b, a, 'should be equal', x => assert.isTrue(x))
-			})
+			raw(name, a, b, 'should be equal', x => assert.isTrue(x))
 		}
 
 		eq('Empty graphs', new DataflowGraph(), new DataflowGraph())
+		eq('Same vertex', new DataflowGraph().addVertex({ id: '0', name: 'x', tag: 'use' }), new DataflowGraph().addVertex({ id: '0', name: 'x', tag: 'use' }))
 	})
 	describe('negative', () => {
 		const neq = (name: string, a: DataflowGraph, b: DataflowGraph) => {
-			describe(name, () => {
-				raw('a!=b', a, b, 'should differ', x => assert.isFalse(x))
-				raw('b!=a', b, a, 'should differ', x => assert.isFalse(x))
-			})
+			raw(name, a, b, 'should differ', x => assert.isFalse(x))
 		}
 		neq('Additional root vertex', new DataflowGraph(), new DataflowGraph().addVertex({ id: '0', name: 'x', tag: 'use' }))
 		neq('Additional non-root vertex', new DataflowGraph(), new DataflowGraph().addVertex({ id: '0', name: 'x', tag: 'use' }, false))

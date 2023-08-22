94e2b465ddc5a446f272e4eb38b0d20cc7726aff
Florian Sihler <florian.sihler@uni-ulm.de>
2023-08-22 21:59:27 +0200

    test: adding first negative tests for df analysis

--- test/dataflow/graph/equal.ts
+++ test/dataflow/graph/equal.ts
@@ -2,18 +2,37 @@ import { DataflowGraph, diffGraphsToMermaidUrl } from '../../../src/dataflow'
 import { assert } from 'chai'
 
 describe('Graph Equality', () => {
-	const assertsEquals = (name: string, a: DataflowGraph, b: DataflowGraph) => {
+	const raw = (name: string, a: DataflowGraph, b: DataflowGraph, text: string, cmp: (x: boolean) => void) => {
 		return it(name, () => {
 			// only calculate the dataflow graphs if not
 			try {
-				assert.isTrue(a.equals(b))
+				cmp(a.equals(b))
 			} catch (e) {
 				const diff = diffGraphsToMermaidUrl({ label: 'left', graph: a }, { label: 'right', graph: b }, undefined, '')
-				console.error('diff:\n', diff)
+				console.error(text + '; diff:\n', diff)
 				throw e
 			}
 		})
 	}
 
-	assertsEquals('Empty graphs', new DataflowGraph(), new DataflowGraph())
+	describe('positive', () => {
+	// as the comparison is really quick, we allow explicit checks for commutativity
+		const eq = (name: string, a: DataflowGraph, b: DataflowGraph) => {
+			describe(name, () => {
+				raw('a==b', a, b, 'should be equal', x => assert.isTrue(x))
+				raw('b==a', b, a, 'should be equal', x => assert.isTrue(x))
+			})
+		}
+
+		eq('Empty graphs', new DataflowGraph(), new DataflowGraph())
+	})
+	describe('negative', () => {
+		const neq = (name: string, a: DataflowGraph, b: DataflowGraph) => {
+			describe(name, () => {
+				raw('a!=b', a, b, 'should differ', x => assert.isFalse(x))
+				raw('b!=a', b, a, 'should differ', x => assert.isFalse(x))
+			})
+		}
+		neq('Additional vertex', new DataflowGraph(), new DataflowGraph().addVertex({ id: '0', name: 'x', tag: 'use' }))
+	})
 })

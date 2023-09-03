5e79b0f7f914a3c0c1cffad581be0d7cb0f95493
Florian Sihler <florian.sihler@uni-ulm.de>
2023-09-03 11:38:42 +0200

    doc: repl messages

--- src/cli/repl/server/messages/repl.ts
+++ src/cli/repl/server/messages/repl.ts
@@ -1,6 +1,14 @@
 import { FlowrBaseMessage, RequestMessageDefinition } from './messages'
 import Joi from 'joi'
 
+/**
+ * Request the execution of the given expression as a REPL statement.
+ * We strongly recommend that you make use of a unique {@link FlowrBaseMessage#id}
+ * in case the message responses happen in parallel.
+ *
+ * @see ExecuteReplExpressionIntermediateMessage
+ * @see ExecuteReplExpressionEndMessage
+ */
 export interface ExecuteReplExpressionRequestMessage extends FlowrBaseMessage {
 	type:       'request-repl-execution',
 	/** Should ansi formatting be enabled for the response? Is `false` by default.  */
@@ -19,15 +27,20 @@ export const requestExecuteReplExpressionMessage: RequestMessageDefinition<Execu
 	})
 }
 
-
-// TODO: document intermediate, ended by end message
+/**
+ * This message may be sent multiple times, triggered for every "output" performed by the execution.
+ * {@link ExecuteReplExpressionEndMessage} marks the end of these messages.
+ */
 export interface ExecuteReplExpressionIntermediateMessage extends FlowrBaseMessage {
 	type:   'response-repl-execution'
 	stream: 'stdout' | 'stderr'
 	result: string
 }
 
-// TODO: note that tcp should ensure the ordering
+/**
+ * Marks the end of the execution of the respective {@link ExecuteReplExpressionRequestMessage}.
+ * The underlying TCP connection should ensure the ordering so that this message is the last.
+ */
 export interface ExecuteReplExpressionEndMessage extends FlowrBaseMessage {
 	type: 'end-repl-execution'
 }

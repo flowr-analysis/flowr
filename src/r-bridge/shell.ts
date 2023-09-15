760d5f725ea73dc27b370158ad6cbdb4ad5304b8
Florian Sihler <florian.sihler@uni-ulm.de>
2023-09-15 20:16:43 +0200

    refactor: remove suffix again, does not work :/

--- src/r-bridge/shell.ts
+++ src/r-bridge/shell.ts
@@ -272,8 +272,6 @@ export class RShell {
 
 		const successfulDone = new RegExp(`.*DONE *\\(${packageName}\\)`)
 
-		const suffix = getPlatform() === 'windows' ? `,type="win.binary"` : ''
-
 		await this.session.collectLinesUntil('both', {
 			predicate:       data => successfulDone.test(data),
 			includeInResult: false
@@ -282,7 +280,7 @@ export class RShell {
 			resetOnNewData: true
 		}, () => {
 			// the else branch is a cheesy way to work even if the package is already installed!
-			this.sendCommand(`install.packages(${ts2r(packageName)},repos="https://cloud.r-project.org/", quiet=FALSE, lib=temp${suffix})`)
+			this.sendCommand(`install.packages(${ts2r(packageName)},repos="https://cloud.r-project.org/", quiet=FALSE, lib=temp)`)
 		})
 		if(autoload) {
 			this.sendCommand(`library(${ts2r(packageName)}, lib.loc=${ts2r(tempdir)})`)

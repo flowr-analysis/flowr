195656cb558b44fc0ebbe076e98c8e7e73aa0719
Florian Sihler <florian.sihler@uni-ulm.de>
2023-09-17 21:28:03 +0200

    refactor: restructure functionality tests

---
+++ test/functionality/flowr/server.spec.ts
@@ -0,0 +1,92 @@
+import { defaultTokenMap, withShell } from '../helper/shell'
+import { fakeSend, withSocket } from '../helper/net'
+import { retrieveVersionInformation } from '../../../src/cli/repl/commands/version'
+import { FlowrHelloResponseMessage } from '../../../src/cli/repl/server/messages/hello'
+import { assert } from 'chai'
+import { FileAnalysisRequestMessage, FileAnalysisResponseMessage } from '../../../src/cli/repl/server/messages/analysis'
+import { DecoratedAstMap, ParentInformation, requestFromInput } from '../../../src/r-bridge'
+import { LAST_PER_FILE_STEP, SteppingSlicer } from '../../../src/core'
+import { jsonReplacer } from '../../../src/util/json'
+import {
+	ExecuteEndMessage,
+	ExecuteIntermediateResponseMessage,
+	ExecuteRequestMessage
+} from '../../../src/cli/repl/server/messages/repl'
+
+describe('FlowR Server', withShell(shell => {
+	it('Correct Hello Message', withSocket(shell,async socket => {
+		const messages = socket.getMessages()
+		assert.strictEqual(messages.length, 1, 'Expected exactly one message to hello the client')
+
+		const hello = messages[0] as FlowrHelloResponseMessage
+		const knownVersion = await retrieveVersionInformation(shell)
+
+		assert.deepStrictEqual(hello, {
+			type:       'hello',
+			clientName: 'client-0',
+			versions:   {
+				r:     knownVersion.r,
+				flowr: knownVersion.flowr
+			}
+		}, "Expected hello message to have the predefined format")
+	}))
+
+	it('Process simple REPL Message', withSocket(shell, async socket => {
+		fakeSend<ExecuteRequestMessage>(socket, {
+			type:       'request-repl-execution',
+			ansi:       false,
+			id:         '0',
+			expression: '1 + 1'
+		})
+
+		await socket.waitForMessage('end-repl-execution')
+
+		const messages = socket.getMessages()
+
+		assert.deepStrictEqual(messages[1] as ExecuteIntermediateResponseMessage, {
+			type:   'response-repl-execution',
+			id:     '0',
+			stream: 'stdout',
+			result: '[1] 2\n'
+		}, 'response message should contain request id, result should be not in standard error (no failure), and it should be the correct result')
+
+		assert.deepStrictEqual(messages[2] as ExecuteEndMessage, {
+			type: 'end-repl-execution',
+			id:   '0',
+		}, 'the end message should have the same id as the response (and come after the response)')
+
+	}))
+
+
+	it('Allow to analyze a simple expression', withSocket(shell, async socket => {
+		fakeSend<FileAnalysisRequestMessage>(socket, {
+			type:      "request-file-analysis",
+			id:        "42",
+			filetoken: "super-token",
+			filename:  "x",
+			content:   "1 + 1"
+		})
+		await socket.waitForMessage('response-file-analysis')
+		const messages = socket.getMessages(['hello', 'response-file-analysis'])
+
+		const response = messages[1] as FileAnalysisResponseMessage
+
+		// we are testing the server and not the slicer here!
+		const results = await new SteppingSlicer({
+			stepOfInterest: LAST_PER_FILE_STEP,
+			shell,
+			tokenMap:       await defaultTokenMap(),
+			request:        requestFromInput('1 + 1'),
+		}).allRemainingSteps()
+		// hideous
+		results.normalize.idMap = undefined as unknown as DecoratedAstMap<ParentInformation>
+
+		assert.strictEqual(response.id, '42', 'Expected the second message to have the same id as the request')
+		// this is hideous and only to unify the ids
+		assert.deepStrictEqual(JSON.stringify(response.results, jsonReplacer)
+			.replace(/\.GlobalEnv","id":"\d+"/, ".GlobalEnv\","),
+		JSON.stringify(results, jsonReplacer)
+			.replace(/\.GlobalEnv","id":"\d+"/, ".GlobalEnv\","), 'Expected the second message to have the same results as the slicer')
+
+	}))
+}))
---
+++ test/functionality/r-bridge/lang/ast/parse-access.ts
@@ -0,0 +1,466 @@
+import { assertAst, withShell } from "../../../helper/shell"
+import { exprList, numVal } from '../../../helper/ast-builder'
+import { rangeFrom } from '../../../../../src/util/range'
+import { RType } from '../../../../../src/r-bridge'
+
+describe("Parse value access", withShell(shell => {
+	describe('Single bracket', () => {
+		assertAst("Empty", shell, "a[]", exprList({
+			type:     RType.Access,
+			location: rangeFrom(1, 2, 1, 2),
+			lexeme:   '[',
+			operator: '[',
+			info:     {},
+			accessed: {
+				type:      RType.Symbol,
+				location:  rangeFrom(1, 1, 1, 1),
+				namespace: undefined,
+				lexeme:    "a",
+				content:   "a",
+				info:      {}
+			},
+			access: []
+		}))
+		assertAst("One value", shell, "a[1]", exprList({
+			type:     RType.Access,
+			location: rangeFrom(1, 2, 1, 2),
+			lexeme:   '[',
+			operator: '[',
+			info:     {},
+			accessed: {
+				type:      RType.Symbol,
+				location:  rangeFrom(1, 1, 1, 1),
+				namespace: undefined,
+				lexeme:    "a",
+				content:   "a",
+				info:      {}
+			},
+			access: [{
+				type:     RType.Argument,
+				location: rangeFrom(1, 3, 1, 3),
+				lexeme:   "1",
+				name:     undefined,
+				info:     {},
+				value:    {
+					type:     RType.Number,
+					location: rangeFrom(1, 3, 1, 3),
+					lexeme:   "1",
+					content:  numVal(1),
+					info:     {}
+				}
+			}]
+		}))
+		assertAst("One variable", shell, "a[x]", exprList({
+			type:     RType.Access,
+			location: rangeFrom(1, 2, 1, 2),
+			lexeme:   '[',
+			operator: '[',
+			info:     {},
+			accessed: {
+				type:      RType.Symbol,
+				location:  rangeFrom(1, 1, 1, 1),
+				namespace: undefined,
+				lexeme:    "a",
+				content:   "a",
+				info:      {}
+			},
+			access: [{
+				type:     RType.Argument,
+				location: rangeFrom(1, 3, 1, 3),
+				lexeme:   "x",
+				name:     undefined,
+				info:     {},
+				value:    {
+					type:      RType.Symbol,
+					location:  rangeFrom(1, 3, 1, 3),
+					namespace: undefined,
+					lexeme:    "x",
+					content:   "x",
+					info:      {}
+				}
+			}]
+		}))
+		assertAst("One expression", shell, "a[x + 3]", exprList({
+			type:     RType.Access,
+			location: rangeFrom(1, 2, 1, 2),
+			lexeme:   '[',
+			operator: '[',
+			info:     {},
+			accessed: {
+				type:      RType.Symbol,
+				location:  rangeFrom(1, 1, 1, 1),
+				namespace: undefined,
+				lexeme:    "a",
+				content:   "a",
+				info:      {}
+			},
+			access: [{
+				type:     RType.Argument,
+				location: rangeFrom(1, 3, 1, 7),
+				lexeme:   "x + 3",
+				name:     undefined,
+				info:     {},
+				value:    {
+					type:     RType.BinaryOp,
+					location: rangeFrom(1, 5, 1, 5),
+					flavor:   'arithmetic',
+					operator: '+',
+					lexeme:   "+",
+					info:     {},
+					lhs:      {
+						type:      RType.Symbol,
+						location:  rangeFrom(1, 3, 1, 3),
+						namespace: undefined,
+						lexeme:    "x",
+						content:   "x",
+						info:      {}
+					},
+					rhs: {
+						type:     RType.Number,
+						location: rangeFrom(1, 7, 1, 7),
+						lexeme:   "3",
+						content:  numVal(3),
+						info:     {}
+					}
+				}
+			}]
+		}))
+		assertAst("Multiple", shell, "a[3,2]", exprList({
+			type:     RType.Access,
+			location: rangeFrom(1, 2, 1, 2),
+			lexeme:   '[',
+			operator: '[',
+			info:     {},
+			accessed: {
+				type:      RType.Symbol,
+				location:  rangeFrom(1, 1, 1, 1),
+				namespace: undefined,
+				lexeme:    "a",
+				content:   "a",
+				info:      {}
+			},
+			access: [{
+				type:     RType.Argument,
+				location: rangeFrom(1, 3, 1, 3),
+				lexeme:   "3",
+				name:     undefined,
+				info:     {},
+				value:    {
+					type:     RType.Number,
+					location: rangeFrom(1, 3, 1, 3),
+					lexeme:   "3",
+					content:  numVal(3),
+					info:     {}
+				}
+			}, {
+				type:     RType.Argument,
+				location: rangeFrom(1, 5, 1, 5),
+				lexeme:   "2",
+				name:     undefined,
+				info:     {},
+				value:    {
+					type:     RType.Number,
+					location: rangeFrom(1, 5, 1, 5),
+					lexeme:   "2",
+					content:  numVal(2),
+					info:     {}
+				}
+			}]
+		}))
+		assertAst("Multiple with empty", shell, "a[,2,4]", exprList({
+			type:     RType.Access,
+			location: rangeFrom(1, 2, 1, 2),
+			lexeme:   '[',
+			operator: '[',
+			info:     {},
+			accessed: {
+				type:      RType.Symbol,
+				location:  rangeFrom(1, 1, 1, 1),
+				namespace: undefined,
+				lexeme:    "a",
+				content:   "a",
+				info:      {}
+			},
+			access: [null, {
+				type:     RType.Argument,
+				location: rangeFrom(1, 4, 1, 4),
+				lexeme:   "2",
+				name:     undefined,
+				info:     {},
+				value:    {
+					type:     RType.Number,
+					location: rangeFrom(1, 4, 1, 4),
+					lexeme:   "2",
+					content:  numVal(2),
+					info:     {}
+				}
+			}, {
+				type:     RType.Argument,
+				location: rangeFrom(1, 6, 1, 6),
+				lexeme:   "4",
+				name:     undefined,
+				info:     {},
+				value:    {
+					type:     RType.Number,
+					location: rangeFrom(1, 6, 1, 6),
+					lexeme:   "4",
+					content:  numVal(4),
+					info:     {}
+				}
+			}]
+		}))
+		assertAst("Named argument", shell, "a[1,super=4]", exprList({
+			type:     RType.Access,
+			location: rangeFrom(1, 2, 1, 2),
+			lexeme:   '[',
+			operator: '[',
+			info:     {},
+			accessed: {
+				type:      RType.Symbol,
+				location:  rangeFrom(1, 1, 1, 1),
+				namespace: undefined,
+				lexeme:    "a",
+				content:   "a",
+				info:      {}
+			},
+			access: [{
+				type:     RType.Argument,
+				location: rangeFrom(1, 3, 1, 3),
+				lexeme:   "1",
+				name:     undefined,
+				info:     {},
+				value:    {
+					type:     RType.Number,
+					location: rangeFrom(1, 3, 1, 3),
+					lexeme:   "1",
+					content:  numVal(1),
+					info:     {}
+				}
+			}, {
+				type:     RType.Argument,
+				location: rangeFrom(1, 5, 1, 9),
+				lexeme:   "super",
+				name:     {
+					type:      RType.Symbol,
+					location:  rangeFrom(1, 5, 1, 9),
+					namespace: undefined,
+					lexeme:    "super",
+					content:   "super",
+					info:      {}
+				},
+				info:  {},
+				value: {
+					type:     RType.Number,
+					location: rangeFrom(1, 11, 1, 11),
+					lexeme:   "4",
+					content:  numVal(4),
+					info:     {}
+				}
+			}]
+		}))
+		assertAst("Chained", shell, "a[1][4]", exprList({
+			type:     RType.Access,
+			location: rangeFrom(1, 5, 1, 5),
+			lexeme:   '[',
+			operator: '[',
+			info:     {},
+			accessed: {
+				type:     RType.Access,
+				location: rangeFrom(1, 2, 1, 2),
+				lexeme:   '[',
+				operator: '[',
+				info:     {},
+				accessed: {
+					type:      RType.Symbol,
+					location:  rangeFrom(1, 1, 1, 1),
+					namespace: undefined,
+					lexeme:    "a",
+					content:   "a",
+					info:      {}
+				},
+				access: [{
+					type:     RType.Argument,
+					location: rangeFrom(1, 3, 1, 3),
+					lexeme:   "1",
+					name:     undefined,
+					info:     {},
+					value:    {
+						type:     RType.Number,
+						location: rangeFrom(1, 3, 1, 3),
+						lexeme:   "1",
+						content:  numVal(1),
+						info:     {}
+					}
+				}]
+			},
+			access: [{
+				type:     RType.Argument,
+				location: rangeFrom(1, 6, 1, 6),
+				lexeme:   "4",
+				name:     undefined,
+				info:     {},
+				value:    {
+					type:     RType.Number,
+					location: rangeFrom(1, 6, 1, 6),
+					lexeme:   "4",
+					content:  numVal(4),
+					info:     {}
+				}
+			}]
+		}))
+	})
+	describe('Double bracket', () => {
+		assertAst("Empty", shell, "b[[]]", exprList({
+			type:     RType.Access,
+			location: rangeFrom(1, 2, 1, 3),
+			lexeme:   '[[',
+			operator: '[[',
+			info:     {},
+			accessed: {
+				type:      RType.Symbol,
+				location:  rangeFrom(1, 1, 1, 1),
+				namespace: undefined,
+				lexeme:    "b",
+				content:   "b",
+				info:      {}
+			},
+			access: []
+		}))
+		assertAst("One element", shell, "b[[5]]", exprList({
+			type:     RType.Access,
+			location: rangeFrom(1, 2, 1, 3),
+			lexeme:   '[[',
+			operator: '[[',
+			info:     {},
+			accessed: {
+				type:      RType.Symbol,
+				location:  rangeFrom(1, 1, 1, 1),
+				namespace: undefined,
+				lexeme:    "b",
+				content:   "b",
+				info:      {}
+			},
+			access: [{
+				type:     RType.Argument,
+				location: rangeFrom(1, 4, 1, 4),
+				lexeme:   "5",
+				name:     undefined,
+				info:     {},
+				value:    {
+					type:     RType.Number,
+					location: rangeFrom(1, 4, 1, 4),
+					lexeme:   "5",
+					content:  numVal(5),
+					info:     {}
+				}
+			}]
+		}))
+		assertAst("Multiple", shell, "b[[5,3]]", exprList({
+			type:     RType.Access,
+			location: rangeFrom(1, 2, 1, 3),
+			lexeme:   '[[',
+			operator: '[[',
+			info:     {},
+			accessed: {
+				type:      RType.Symbol,
+				location:  rangeFrom(1, 1, 1, 1),
+				namespace: undefined,
+				lexeme:    "b",
+				content:   "b",
+				info:      {}
+			},
+			access: [{
+				type:     RType.Argument,
+				location: rangeFrom(1, 4, 1, 4),
+				lexeme:   "5",
+				name:     undefined,
+				info:     {},
+				value:    {
+					type:     RType.Number,
+					location: rangeFrom(1, 4, 1, 4),
+					lexeme:   "5",
+					content:  numVal(5),
+					info:     {}
+				}
+			}, {
+				type:     RType.Argument,
+				location: rangeFrom(1, 6, 1, 6),
+				lexeme:   "3",
+				name:     undefined,
+				info:     {},
+				value:    {
+					type:     RType.Number,
+					location: rangeFrom(1, 6, 1, 6),
+					lexeme:   "3",
+					content:  numVal(3),
+					info:     {}
+				}
+			}]
+		}))
+		assertAst("Multiple with empty", shell, "b[[5,,]]", exprList({
+			type:     RType.Access,
+			location: rangeFrom(1, 2, 1, 3),
+			lexeme:   '[[',
+			operator: '[[',
+			info:     {},
+			accessed: {
+				type:      RType.Symbol,
+				location:  rangeFrom(1, 1, 1, 1),
+				namespace: undefined,
+				lexeme:    "b",
+				content:   "b",
+				info:      {}
+			},
+			access: [{
+
+				type:     RType.Argument,
+				location: rangeFrom(1, 4, 1, 4),
+				lexeme:   "5",
+				name:     undefined,
+				info:     {},
+				value:    {
+					type:     RType.Number,
+					location: rangeFrom(1, 4, 1, 4),
+					lexeme:   "5",
+					content:  numVal(5),
+					info:     {}
+				}
+			},null,null]
+		}))
+	})
+	describe('Dollar and Slot', () => {
+		assertAst("Dollar access", shell, "c$x", exprList({
+			type:     RType.Access,
+			location: rangeFrom(1, 2, 1, 2),
+			lexeme:   '$',
+			operator: '$',
+			info:     {},
+			accessed: {
+				type:      RType.Symbol,
+				location:  rangeFrom(1, 1, 1, 1),
+				namespace: undefined,
+				lexeme:    "c",
+				content:   "c",
+				info:      {}
+			},
+			access: 'x'
+		}))
+		assertAst("Slot based access", shell, "d@y", exprList({
+			type:     RType.Access,
+			location: rangeFrom(1, 2, 1, 2),
+			lexeme:   '@',
+			operator: '@',
+			info:     {},
+			accessed: {
+				type:      RType.Symbol,
+				location:  rangeFrom(1, 1, 1, 1),
+				namespace: undefined,
+				lexeme:    "d",
+				content:   "d",
+				info:      {}
+			},
+			access: 'y'
+		}))
+	})
+}))
+

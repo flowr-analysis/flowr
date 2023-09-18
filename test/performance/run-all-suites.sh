9170968043986519000c9817c454831e721b6033
Florian Sihler <florian.sihler@uni-ulm.de>
2023-09-18 21:23:09 +0200

    ci, wip: try to run benchmark suites in parallel

--- test/performance/run-all-suites.sh
+++ test/performance/run-all-suites.sh
@@ -4,6 +4,7 @@ set -eu
 
 # default to 1 parallel processes
 PARALLEL="${1-1}"
+ONLY_SUITE="${2-}"
 
 
 SUITE_PREFIX="suite-"
@@ -15,6 +16,10 @@ mkdir -p "${OUTPUT_DIR}"
 readarray -t SUITES < <(find . -maxdepth 1 -type d -name "${SUITE_PREFIX}*" | sed "s|^\./${SUITE_PREFIX}||" | sort)
 
 echo "Found ${#SUITES[*]} suite(s)"
+
+if [[ -n "${ONLY_SUITE}" ]]; then
+  SUITES=("${ONLY_SUITE}")
+fi
 
 for SUITE in "${SUITES[@]}"; do
   mkdir -p "${OUTPUT_DIR}/${SUITE}"

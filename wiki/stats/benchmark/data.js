e1d7118de0f600211ed780e5ba1005935a01bd60
github-action-benchmark <github@users.noreply.github.com>
2023-09-21 00:13:08 +0000

    add "social-science" Benchmark Suite (customSmallerIsBetter) benchmark result for 20612c4734312e5bbd8963132eb9d25802d2f8a8

--- wiki/stats/benchmark/data.js
+++ wiki/stats/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1695255188383,
+  "lastUpdate": 1695255188623,
   "repoUrl": "https://github.com/Code-Inspect/flowr",
   "entries": {
     "\"artificial\" Benchmark Suite": [
@@ -192,6 +192,104 @@ window.BENCHMARK_DATA = {
             "value": 0.720988345209971,
             "unit": "#",
             "extra": "std: 0.13987413604166843"
+          }
+        ]
+      }
+    ],
+    "\"social-science\" Benchmark Suite": [
+      {
+        "commit": {
+          "author": {
+            "email": "florian.sihler@uni-ulm.de",
+            "name": "Florian Sihler",
+            "username": "EagleoutIce"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "20612c4734312e5bbd8963132eb9d25802d2f8a8",
+          "message": "Fine tune benchmark plots with new colors, labels, and more (#338)\n\n* ci: cycle colors\r\n\r\n* ci: show commit message name start in benchmarks as well\r\n\r\n* ci-fix: npm script `performance-test` should be able to read arguments :D\r\n\r\n* ci, typo: fix wrong name for uploading of benchmark results\r\n\r\n* ci-fix: add `--` to separatae arguments in `qa.yaml`\r\n\r\n* ci: reset current benchmark data",
+          "timestamp": "2023-09-21T01:44:43+02:00",
+          "tree_id": "9f4b37285d7d1eb59c97e553f8fd0766c9bb1b06",
+          "url": "https://github.com/Code-Inspect/flowr/commit/20612c4734312e5bbd8963132eb9d25802d2f8a8"
+        },
+        "date": 1695255188618,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Total per-file",
+            "value": 10924.603258219999,
+            "unit": "ms",
+            "range": 8162.605362097769,
+            "extra": "median: 7633.67ms"
+          },
+          {
+            "name": "Retrieve AST from R code",
+            "value": 2088.2099376,
+            "unit": "ms",
+            "range": 95.54303478866264,
+            "extra": "median: 2068.94ms"
+          },
+          {
+            "name": "Normalize R AST",
+            "value": 149.64170438,
+            "unit": "ms",
+            "range": 98.76134983441513,
+            "extra": "median: 111.52ms"
+          },
+          {
+            "name": "Produce dataflow information",
+            "value": 226.55017604,
+            "unit": "ms",
+            "range": 388.9695823485092,
+            "extra": "median: 61.86ms"
+          },
+          {
+            "name": "Total per-slice",
+            "value": 11.635431291893523,
+            "unit": "ms",
+            "range": 19.87223607979893,
+            "extra": "median: 8.83ms"
+          },
+          {
+            "name": "Static slicing",
+            "value": 10.975662455647656,
+            "unit": "ms",
+            "range": 19.77659599605963,
+            "extra": "median: 8.17ms"
+          },
+          {
+            "name": "Reconstruct code",
+            "value": 0.6472877066595938,
+            "unit": "ms",
+            "range": 0.4622639696255693,
+            "extra": "median: 0.57ms"
+          },
+          {
+            "name": "failed to reconstruct/re-parse",
+            "value": 9,
+            "unit": "#",
+            "extra": "out of 11160 slices"
+          },
+          {
+            "name": "times hit threshold",
+            "value": 967,
+            "unit": "#"
+          },
+          {
+            "name": "reduction (characters)",
+            "value": 0.8973961730207582,
+            "unit": "#",
+            "extra": "std: 0.08572960788217562"
+          },
+          {
+            "name": "reduction (normalized tokens)",
+            "value": 0.8564460343281113,
+            "unit": "#",
+            "extra": "std: 0.11535970153362557"
           }
         ]
       }

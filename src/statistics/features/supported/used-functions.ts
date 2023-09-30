70efce940c5167d8c0b8efb5a304b4f04570274a
Florian Sihler <florian.sihler@uni-ulm.de>
2023-09-30 23:17:50 +0200

    refactor, test: nested function definition information, switch to the processing with a visitor

--- src/statistics/features/supported/used-functions.ts
+++ src/statistics/features/supported/used-functions.ts
@@ -1,14 +1,7 @@
-import { SinglePackageInfo } from './used-packages'
-import { FunctionNameInfo } from './defined-functions'
-import { Feature, FeatureProcessorInput, Query } from '../feature'
-import * as xpath from 'xpath-ts2'
-import { appendStatisticsFile, extractNodeContent } from '../../output'
+import { Feature, FeatureProcessorInput } from '../feature'
+import { appendStatisticsFile } from '../../output'
 import { Writable } from 'ts-essentials'
-
-export interface UsedFunction {
-	package:  SinglePackageInfo,
-	function: FunctionNameInfo
-}
+import { RNodeWithParent, RType, visitAst } from '../../../r-bridge'
 
 const initialFunctionUsageInfo = {
 	allFunctionCalls:           0,
@@ -22,8 +15,6 @@ const initialFunctionUsageInfo = {
 	primitiveFunctions:         0,
 	/** e.g. do not evaluate part of functions, `quote`, ... */
 	specialPrimitiveFunctions:  0,
-	/** `.Primitive`, `.Internal`, `lazyLoadDBfetch`, ... */
-	internalFunctions:          0,
 	/** `body`, `environment`, `formals` */
 	metaFunctions:              0,
 	/** return */
@@ -33,7 +24,11 @@ const initialFunctionUsageInfo = {
 	assignFunctions:            0,
 	getFunctions:               0,
 	helpFunctions:              0,
-	optionFunctions:            0
+	optionFunctions:            0,
+	/** `a(b(), c(3, d()))` has 3 (`b`, `c`, `d`) */
+	nestedFunctionCalls:        0,
+	deepestNesting:             0,
+	unnamedCalls:               0
 }
 
 export type FunctionUsageInfo = Writable<typeof initialFunctionUsageInfo>
@@ -43,39 +38,14 @@ function from(...names: string[]): RegExp {
 	return new RegExp(names.join('|'))
 }
 
-/** from R internals */
-const mathFunctions = from('abs', 'sign', 'sqrt', 'floor', 'ceiling',
-	'exp', 'expm1', 'log2', 'log10', 'log1p', 'cos', 'sin', 'tan', 'acos', 'asin', 'atan',
-	'cosh', 'sinh', 'tanh', 'acosh', 'asinh', 'atanh', 'cospi', 'sinpi', 'tanpi', 'gamma',
-	'lgamma', 'digamma', 'trigamma', 'cumsum', 'cumprod', 'cummax', 'cummin',
-	'Im', 'Re', 'Arg', 'Conj', 'Mod')
-
-const programmingFunctions = from('nargs', 'missing', 'on.exit', 'interactive',
-	'as.call', 'as.character', 'as.complex', 'as.double', 'as.environment', 'as.integer', 'as.logical', 'as.raw',
-	'is.array', 'is.atomic', 'is.call', 'is.character', 'is.complex', 'is.double', 'is.environment', 'is.expression',
-	'is.finite', 'is.function', 'is.infinite', 'is.integer', 'is.language', 'is.list', 'is.logical', 'is.matrix',
-	'is.na', 'is.name', 'is.nan', 'is.null', 'is.numeric', 'is.object', 'is.pairlist', 'is.raw',
-	'is.real', 'is.recursive', 'is.single', 'is.symbol', 'baseenv', 'emptyenv', 'globalenv', 'pos.to.env',
-	'unclass', 'invisible', 'seq_along', 'seq_len')
-
+/* from R internals */
+const mathFunctions = from('abs', 'sign', 'sqrt', 'floor', 'ceiling', 'exp', 'expm1', 'log2', 'log10', 'log1p', 'cos', 'sin', 'tan', 'acos', 'asin', 'atan', 'cosh', 'sinh', 'tanh', 'acosh', 'asinh', 'atanh', 'cospi', 'sinpi', 'tanpi', 'gamma', 'lgamma', 'digamma', 'trigamma', 'cumsum', 'cumprod', 'cummax', 'cummin', 'Im', 'Re', 'Arg', 'Conj', 'Mod')
+const programmingFunctions = from('nargs', 'missing', 'on.exit', 'interactive', 'as.call', 'as.character', 'as.complex', 'as.double', 'as.environment', 'as.integer', 'as.logical', 'as.raw', 'is.array', 'is.atomic', 'is.call', 'is.character', 'is.complex', 'is.double', 'is.environment', 'is.expression', 'is.finite', 'is.function', 'is.infinite', 'is.integer', 'is.language', 'is.list', 'is.logical', 'is.matrix', 'is.na', 'is.name', 'is.nan', 'is.null', 'is.numeric', 'is.object', 'is.pairlist', 'is.raw', 'is.real', 'is.recursive', 'is.single', 'is.symbol', 'baseenv', 'emptyenv', 'globalenv', 'pos.to.env', 'unclass', 'invisible', 'seq_along', 'seq_len')
 const sessionManagementFunctions = from('browser', 'proc.time', 'gc.time', 'tracemem', 'retracemem', 'untracemem')
-
-const markedAsInternalOnly = ['.Primitive', '.Internal',
-	'.Call.graphics', '.External.graphics', '.subset', '.subset2',
-	'.primTrace', '.primUntrace', 'lazyLoadDBfetch']
-
-const primitiveFunctions = from(':', '~', 'c', 'list', 'call', 'switch', 'expression', 'substitute',
-	'UseMethod', 'standardGeneric', '.C', '.Fortran', '.Call', '.External',
-	'round', 'signif', 'rep', 'seq.int', ...markedAsInternalOnly)
-
+const primitiveFunctions = from(':', '~', 'c', 'list', 'call', 'switch', 'expression', 'substitute', 'UseMethod', 'standardGeneric', '.C', '.Fortran', '.Call', '.External', 'round', 'signif', 'rep', 'seq.int', '.Primitive', '.Internal', '.Call.graphics', '.External.graphics', '.subset', '.subset2', '.primTrace', '.primUntrace', 'lazyLoadDBfetch')
 const specialPrimitiveFunctions = from('quote', 'substitute', 'missing', 'on.exit', 'call', 'expression', '.Internal')
-
-const internalOnlyFunctions = from(...markedAsInternalOnly)
-
 const metaFunctions = from('body', 'environment', 'formals')
-
-const returnFunction = from('return')
-
+const returnFunction = /return/
 const parsingFunctions = from('parse', 'deparse', 'substitute', 'quote', 'bquote', 'call', 'eval', 'evalq', 'eval.parent')
 const editFunctions = from('edit', 'vi', 'emacs', 'pico', 'xemacs', 'xedit', 'fix', 'fixInNamespace')
 const assignFunctions = from('assign',  'assignInNamespace', 'assignInMyNamespace')
@@ -83,45 +53,73 @@ const getFunctions = from('get', 'exists', 'getFromNamespace')
 const helpFunctions = from('help', 'help.search', 'prompt')
 const optionFunctions = from('options', 'getOption', '.Option')
 
-function collectFunctionByName(names: string[], info: FunctionUsageInfo, field: keyof FunctionUsageInfo, name: RegExp): void {
-	collectFunctionByPredicate(names, info, field, n => name.test(n))
+function collectFunctionByName(name: string, info: FunctionUsageInfo, field: keyof FunctionUsageInfo, regex: RegExp): void {
+	if(regex.test(name)) {
+		info[field]++
+	}
 }
 
-function collectFunctionByPredicate(names: string[], info: FunctionUsageInfo, field: keyof FunctionUsageInfo, pred: (name: string) => boolean): void {
-	const matchingNames = names.filter(pred)
-	info[field] += matchingNames.length
-	// as they all are recorded as part of the allFunctionCalls, we do not need to append them separately
+function analyzeFunctionName(name: string, info: FunctionUsageInfo) {
+	collectFunctionByName(name, info, 'mathFunctions', mathFunctions)
+	collectFunctionByName(name, info, 'programmingFunctions', programmingFunctions)
+	collectFunctionByName(name, info, 'sessionManagementFunctions', sessionManagementFunctions)
+	collectFunctionByName(name, info, 'primitiveFunctions', primitiveFunctions)
+	collectFunctionByName(name, info, 'specialPrimitiveFunctions', specialPrimitiveFunctions)
+	collectFunctionByName(name, info, 'metaFunctions', metaFunctions)
+	collectFunctionByName(name, info, 'returnFunction', returnFunction)
+	collectFunctionByName(name, info, 'parsingFunctions', parsingFunctions)
+	collectFunctionByName(name, info, 'editFunctions', editFunctions)
+	collectFunctionByName(name, info, 'assignFunctions', assignFunctions)
+	collectFunctionByName(name, info, 'getFunctions', getFunctions)
+	collectFunctionByName(name, info, 'helpFunctions', helpFunctions)
+	collectFunctionByName(name, info, 'optionFunctions', optionFunctions)
 }
 
-const functionCallQuery: Query = xpath.parse('//SYMBOL_FUNCTION_CALL')
+function visitCalls(info: FunctionUsageInfo, input: FeatureProcessorInput): void {
+	const calls: RNodeWithParent[] = []
+	const allCalls: string[] = []
+
+	visitAst(input.normalizedRAst.ast,
+		node => {
+			if(node.type !== RType.FunctionCall) {
+				return
+			}
+
+			if(calls.length > 0) {
+				info.nestedFunctionCalls++
+				appendStatisticsFile(usedFunctions.name, 'nested-calls', [node.lexeme], input.filepath)
+				info.deepestNesting = Math.max(info.deepestNesting, calls.length)
+			}
+
+			if(node.flavor === 'unnamed') {
+				info.unnamedCalls++
+				appendStatisticsFile(usedFunctions.name, 'unnamed-calls', [node.lexeme], input.filepath)
+				allCalls.push(node.calledFunction.lexeme ?? '<unknown>')
+			} else {
+				analyzeFunctionName(node.functionName.lexeme, info)
+				allCalls.push(node.functionName.lexeme)
+			}
+
+			calls.push(node)
+		}, node => {
+			// drop again :D
+			if(node.type === RType.FunctionCall) {
+				calls.pop()
+			}
+		}
+	)
+
+	info.allFunctionCalls += allCalls.length
+	appendStatisticsFile(usedFunctions.name, 'all-calls', allCalls, input.filepath)
+}
+
 
 export const usedFunctions: Feature<FunctionUsageInfo> = {
 	name:        'Used Functions',
 	description: 'All functions called, split into various sub-categories',
 
 	process(existing: FunctionUsageInfo, input: FeatureProcessorInput): FunctionUsageInfo {
-		const allFunctionCalls = functionCallQuery.select({ node: input.parsedRAst })
-
-		existing.allFunctionCalls += allFunctionCalls.length
-		appendStatisticsFile(this.name, 'allFunctionCalls', allFunctionCalls, input.filepath)
-
-		const names = allFunctionCalls.map(extractNodeContent)
-
-		collectFunctionByName(names, existing, 'mathFunctions', mathFunctions)
-		collectFunctionByName(names, existing, 'programmingFunctions', programmingFunctions)
-		collectFunctionByName(names, existing, 'sessionManagementFunctions', sessionManagementFunctions)
-		collectFunctionByName(names, existing, 'primitiveFunctions', primitiveFunctions)
-		collectFunctionByName(names, existing, 'specialPrimitiveFunctions', specialPrimitiveFunctions)
-		collectFunctionByName(names, existing, 'internalFunctions', internalOnlyFunctions)
-		collectFunctionByName(names, existing, 'metaFunctions', metaFunctions)
-		collectFunctionByName(names, existing, 'returnFunction', returnFunction)
-		collectFunctionByName(names, existing, 'parsingFunctions', parsingFunctions)
-		collectFunctionByName(names, existing, 'editFunctions', editFunctions)
-		collectFunctionByName(names, existing, 'assignFunctions', assignFunctions)
-		collectFunctionByName(names, existing, 'getFunctions', getFunctions)
-		collectFunctionByName(names, existing, 'helpFunctions', helpFunctions)
-		collectFunctionByName(names, existing, 'optionFunctions', optionFunctions)
-
+		visitCalls(existing, input)
 		return existing
 	},
 
--- test/functionality/statistics/features/used-functions.ts
+++ test/functionality/statistics/features/used-functions.ts
@@ -15,6 +15,29 @@ describe('Used Function Calls', withShell(shell => {
 			expected: {
 				allFunctionCalls: 1
 			}
+		},
+		{
+			name:     'calling a math function',
+			code:     'sin(3)',
+			expected: {
+				allFunctionCalls: 1,
+				mathFunctions: 	  1
+			}
+		},
+		{
+			name: 'nested function calls',
+			code: `
+				a(b(), c(3, d()))
+				if(a(b(), d(e))) {
+					f()
+				}
+			`,
+			expected: {
+				allFunctionCalls:    8,
+				nestedFunctionCalls: 5,
+				deepestNesting:      2,
+				primitiveFunctions:  1 /* c is correctly classified ^^ */
+			}
 		}
 	])
 }))

b19bf0084fa732741de233336aa2136a00be5076
Florian Sihler <florian.sihler@uni-ulm.de>
2023-10-10 19:26:16 +0200

    feat, wip: working on variable statistics

--- src/statistics/features/supported/variables.ts
+++ src/statistics/features/supported/variables.ts
@@ -1,85 +1,50 @@
-import { Feature, FeatureProcessorInput, Query } from '../feature'
-import * as xpath from 'xpath-ts2'
-import { appendStatisticsFile } from '../../output'
+import { Feature, FeatureProcessorInput } from '../feature'
 import { Writable } from 'ts-essentials'
+import { isSpecialSymbol, RNodeWithParent, RoleInParent, RType, visitAst } from '../../../r-bridge'
+import { guard, isNotUndefined } from '../../../util/assert'
+import { appendStatisticsFile } from '../../output'
+import { EdgeType } from '../../../dataflow'
+import { definedFunctions } from './defined-functions'
 
 
-const initialAssignmentInfo = {
-	assignmentOperator:               0,
-	specialAssignmentOps:             0,
-	nestedOperatorAssignment:         0,
-	directlyNestedOperatorAssignment: 0
+const initialVariableInfo = {
+	numberOfVariableUses:  0,
+	numberOfDefinitions:   0,
+	numberOfRedefinitions: 0
 }
 
-export type AssignmentInfo = Writable<typeof initialAssignmentInfo>
+export type VariableInfo = Writable<typeof initialVariableInfo>
 
 
-const defaultOperatorAssignmentQuery: Query = xpath.parse('//EQ_ASSIGN|//LEFT_ASSIGN|//RIGHT_ASSIGN')
-// either <-/<<-/=, with a nested rhs, or ->/->> with a nested lhs
-const nestedOperatorAssignmentQuery: Query = xpath.parse(`//*[
-  (self::LEFT_ASSIGN or self::EQ_ASSIGN) and following-sibling::expr//*[self::LEFT_ASSIGN or self::EQ_ASSIGN or self::RIGHT_ASSIGN]
- ]
- |
- //RIGHT_ASSIGN[preceding-sibling::expr//*[self::LEFT_ASSIGN or self::EQ_ASSIGN or self::RIGHT_ASSIGN]]
-`)
+function visitVariables(info: VariableInfo, input: FeatureProcessorInput): void {
 
-// the rhs must be an assignment directly
-const directlyNestedOperatorAssignmentQuery: Query = xpath.parse(`//*[
-  (self::LEFT_ASSIGN or self::EQ_ASSIGN) and following-sibling::expr/*[self::LEFT_ASSIGN or self::EQ_ASSIGN or self::RIGHT_ASSIGN]
- ]
- |
- //RIGHT_ASSIGN[preceding-sibling::expr/*[self::LEFT_ASSIGN or self::EQ_ASSIGN or self::RIGHT_ASSIGN]]
-`)
+	visitAst(input.normalizedRAst.ast,
+		node => {
+			if(node.type !== RType.Symbol || isSpecialSymbol(node)) {
+				return
+			}
 
-// LBB for double '[[<-', OP-LEFT-BRACKET for '[<-', 'OP-DOLLAR' for '$<-', OP-AT for '@<-' (similar with EQ_ASSIGN, swapped for RIGHT_ASSIGN)
-const bracketAssignQuery: Query = xpath.parse(`
-  //expr/*[(self::LBB or self::OP-LEFT-BRACKET or self::OP-DOLLAR or self::OP-AT) 
-        and (
-          ../following-sibling::LEFT_ASSIGN or ../following-sibling::EQ_ASSIGN
-          or
-          ../preceding-sibling::RIGHT_ASSIGN
-     )]
- `)
+			if(node.info.role === RoleInParent.AssignmentBinaryOperationLhs) {
 
-function enrichOpForBracketAssign(node: Node): string {
-	let operator: string | null = null
-	const siblings = node.parentNode?.parentNode?.childNodes
-	if(siblings == null) {
-		return `${node.textContent ?? '<unknown>'}??`
-	}
+			}
 
-	// next and previous sibling do not work to our liking (they are not entertained by the xpath-ts chain)
-	for(let i = 0; i < siblings.length; i++) {
-		const child = siblings.item(i)
-		if(child.nodeName === 'LEFT_ASSIGN' || child.nodeName === 'EQ_ASSIGN' || child.nodeName === 'RIGHT_ASSIGN') {
-			operator = child.textContent
-			break
+			info.numberOfVariableUses++
+
 		}
-	}
+	)
 
-	return `${node.textContent ?? '<unknown>'}${operator ?? '??'}`
+	appendStatisticsFile(definedFunctions.name, 'all-definitions', allDefinitions.map(s => JSON.stringify(s)), input.filepath)
 }
 
-export const assignments: Feature<AssignmentInfo> = {
-	name:        'Assignments',
-	description: 'all ways to assign something in R',
 
-	process(existing: AssignmentInfo, input: FeatureProcessorInput): AssignmentInfo {
-		const assignmentOperators = defaultOperatorAssignmentQuery.select({ node: input.parsedRAst })
-		const nestedOperators = nestedOperatorAssignmentQuery.select({ node: input.parsedRAst })
-		const directlyNestedOperators = directlyNestedOperatorAssignmentQuery.select({ node: input.parsedRAst })
-		const specialAssignmentOps = bracketAssignQuery.select({ node: input.parsedRAst }).map(enrichOpForBracketAssign)
+export const variables: Feature<VariableInfo> = {
+	name:        'Variables',
+	description: 'Variable Usage, Assignments, and Redefinitions',
 
-		existing.nestedOperatorAssignment += nestedOperators.length
-		existing.directlyNestedOperatorAssignment += directlyNestedOperators.length
-		existing.assignmentOperator += assignmentOperators.length
-		existing.specialAssignmentOps += specialAssignmentOps.length
-
-		appendStatisticsFile(this.name, 'assignmentOperator', assignmentOperators, input.filepath)
-		appendStatisticsFile(this.name, 'specialAssignmentOps', specialAssignmentOps, input.filepath)
+	process(existing: VariableInfo, input: FeatureProcessorInput): VariableInfo {
 
 		return existing
 	},
 
-	initialValue: initialAssignmentInfo
+	initialValue: initialVariableInfo
 }
--- test/functionality/statistics/features/variables.ts
+++ test/functionality/statistics/features/variables.ts
@@ -2,152 +2,38 @@ import { withShell } from '../../helper/shell'
 import { testForFeatureForInput } from '../statistics.spec'
 
 
-describe('Used Function Calls', withShell(shell => {
-	testForFeatureForInput(shell, 'usedFunctions', [
+describe('Variables', withShell(shell => {
+	testForFeatureForInput(shell, 'variables', [
 		{
-			name:     'no calls',
-			code:     'a <- 1',
+			name:     'no variables',
+			code:     '2 + 3',
 			expected: {},
 			written:  'nothing'
 		},
 		{
-			name:     'one call',
-			code:     'b()',
+			name:     'one variable use',
+			code:     'a',
+			expected: {
+				allFunctionCalls: 1
+			},
+			written: []
+		},
+		{
+			name:     'one variable definition',
+			code:     'a <- 3',
 			expected: {
 				allFunctionCalls: 1
 			},
 			written: [
-				['all-calls', [{ value: JSON.stringify({
-					name:              'b',
-					location:          { line: 1, column: 1 },
-					numberOfArguments: 0,
-					namespace:         undefined
-				}) }]],
-			]
-		},
-		{
-			name:     'one call with multiple arguments and namespace',
-			code:     'a::b(3, 4)',
-			expected: {
-				allFunctionCalls: 1
-			},
-			written: [
-				['all-calls', [{ value: JSON.stringify({
-					name:              'b',
-					location:          { line: 1, column: 1 },
-					numberOfArguments: 2,
-					namespace:         'a'
-				}) }]],
-			]
-		},
-		{
-			name:     'unnamed function call',
-			code:     '(function(x) { x })(3)',
-			expected: {
-				allFunctionCalls: 1,
-				unnamedCalls:     1
-			},
-			written: [
-				['unnamed-calls', [
-					{ value: '(function(x) { x })' }
-				]],
-				['all-calls', [{ value: JSON.stringify({
-					name:              undefined,
-					location:          { line: 1, column: 1 },
-					numberOfArguments: 1,
-					namespace:         undefined
-				}) }]],
-			]
-		},
-		{
-			name:     'calling a math function',
-			code:     'sin(3)',
-			expected: {
-				allFunctionCalls: 1,
-				mathFunctions: 	  1
-			},
-			written: [
-				['all-calls', [{ value: JSON.stringify({
-					name:              'sin',
-					location:          { line: 1, column: 1 },
-					numberOfArguments: 1,
-					namespace:         undefined
-				}) }]],
-			]
-		},
-		{
-			name: 'nested function calls',
-			code: `a(b(), c(3, d()))
-				if(a(b(), d(e))) {
-					f()
-				}
-			`,
-			expected: {
-				allFunctionCalls:    8,
-				nestedFunctionCalls: 5,
-				deepestNesting:      2,
-				primitiveFunctions:  1 /* c is correctly classified ^^ */
-			},
-			written: [
-				['nested-calls', [
-					{ value: 'b' },
-					{ value: 'c' },
-					{ value: 'd' },
-					{ value: 'b' },
-					{ value: 'd' }
-				]],
-				['all-calls', [
-					{ value: JSON.stringify({
-						name:              'a',
+				['all-calls', [{
+					value: JSON.stringify({
+						name:              'b',
 						location:          { line: 1, column: 1 },
 						numberOfArguments: 2,
-						namespace:         undefined
-					}) },
-					{ value: JSON.stringify({
-						name:              'b',
-						location:          { line: 1, column: 3 },
-						numberOfArguments: 0,
-						namespace:         undefined
-					}) },
-					{ value: JSON.stringify({
-						name:              'c',
-						location:          { line: 1, column: 8 },
-						numberOfArguments: 2,
-						namespace:         undefined
-					}) },
-					{ value: JSON.stringify({
-						name:              'd',
-						location:          { line: 1, column: 13 },
-						numberOfArguments: 0,
-						namespace:         undefined
-					}) },
-					{ value: JSON.stringify({
-						name:              'a',
-						// atm, links columns from the start of the input :C
-						location:          { line: 2, column: 36 },
-						numberOfArguments: 2,
-						namespace:         undefined
-					}) },
-					{ value: JSON.stringify({
-						name:              'b',
-						location:          { line: 2, column: 38 },
-						numberOfArguments: 0,
-						namespace:         undefined
-					}) },
-					{ value: JSON.stringify({
-						name:              'd',
-						location:          { line: 2, column: 43 },
-						numberOfArguments: 1,
-						namespace:         undefined
-					}) },
-					{ value: JSON.stringify({
-						name:              'f',
-						location:          { line: 3, column: 41 },
-						numberOfArguments: 0,
-						namespace:         undefined
-					}) }
-				]]]
+						namespace:         'a'
+					})
+				}]],
+			]
 		}
 	])
 }))
-

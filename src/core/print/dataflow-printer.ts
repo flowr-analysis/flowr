06cf9df979a344997cb4fb9998ae69a6b1e25c2c
Florian Sihler <florian.sihler@uni-ulm.de>
2023-10-12 12:07:15 +0200

    feat: more memory-robust serialization of graph

--- src/core/print/dataflow-printer.ts
+++ src/core/print/dataflow-printer.ts
@@ -1,8 +1,24 @@
 import { jsonReplacer } from '../../util/json'
 import { DataflowInformation } from '../../dataflow/internal/info'
+import { guard } from '../../util/assert'
+
+function dfGraphReconstruct(df: object): string {
+	const elems: [string, string][] = []
+
+	for(const [key, value] of Object.entries(df)) {
+		if(key === 'graph') {
+			guard(typeof value === 'object', 'graph must be an object')
+			elems.push([key, dfGraphReconstruct(value as object)])
+		} else {
+			elems.push([key, JSON.stringify(value, jsonReplacer)])
+		}
+	}
+
+	return `{${elems.map(([key, value]) => `"${key}":${value}`).join(',')}}`
+}
 
 /** Should work with larger things as well */
 // eslint-disable-next-line @typescript-eslint/require-await
-export async function dataflowGraphToJson(ast: DataflowInformation): Promise<string> {
-	return JSON.stringify(ast, jsonReplacer)
+export async function dataflowGraphToJson(df: DataflowInformation): Promise<string> {
+	return dfGraphReconstruct(df)
 }

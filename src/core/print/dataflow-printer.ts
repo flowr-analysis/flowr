f378a6e2a8138b16df01b8f0a7f0a9101fd11534
Florian Sihler <florian.sihler@uni-ulm.de>
2023-10-12 12:27:30 +0200

    feat-fix: try to improve object recovery

--- src/core/print/dataflow-printer.ts
+++ src/core/print/dataflow-printer.ts
@@ -1,16 +1,31 @@
 import { jsonReplacer } from '../../util/json'
 import { DataflowInformation } from '../../dataflow/internal/info'
-import { guard } from '../../util/assert'
 
-function dfGraphReconstruct(df: object): string {
+// TODO: make this better
+function objectJson(df: object): string {
 	const elems: [string, string][] = []
 
 	for(const [key, value] of Object.entries(df)) {
-		if(key === 'graph') {
-			guard(typeof value === 'object', 'graph must be an object')
-			elems.push([key, dfGraphReconstruct(value as object)])
-		} else {
-			elems.push([key, JSON.stringify(value, jsonReplacer)])
+		switch(typeof value) {
+			case 'undefined':
+			case 'function':
+				continue
+			case 'object':
+				if(Array.isArray(value)) {
+					elems.push([key, `[${value.map(x => JSON.stringify(x, jsonReplacer)).join(',')}]`])
+				} else if(value instanceof Set) {
+					elems.push([key, `[${[...value].map(x => JSON.stringify(x, jsonReplacer)).join(',')}]`])
+				} else if(value instanceof Map) {
+					elems.push([key, `[${[...value].map(([k, v]) => `[${JSON.stringify(k, jsonReplacer)},${JSON.stringify(v, jsonReplacer)}]`).join(',')}]`])
+				} else {
+					elems.push([key, objectJson(value as object)])
+				}
+				break
+			case 'bigint':
+				elems.push([key, `${value.toString()}n`])
+				break
+			default:
+				elems.push([key, JSON.stringify(value, jsonReplacer)])
 		}
 	}
 
@@ -20,5 +35,5 @@ function dfGraphReconstruct(df: object): string {
 /** Should work with larger things as well */
 // eslint-disable-next-line @typescript-eslint/require-await
 export async function dataflowGraphToJson(df: DataflowInformation): Promise<string> {
-	return dfGraphReconstruct(df)
+	return objectJson(df)
 }

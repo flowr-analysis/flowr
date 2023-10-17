051cd871c58ad035560fbac4a6a314d0a9a1a5e7
Florian Sihler <florian.sihler@uni-ulm.de>
2023-10-17 21:51:04 +0200

    refactor: remove call classification with function calls

--- src/statistics/features/supported/used-functions.ts
+++ src/statistics/features/supported/used-functions.ts
@@ -6,76 +6,14 @@ import { SourcePosition } from '../../../util/range'
 import { MergeableRecord } from '../../../util/objects'
 
 const initialFunctionUsageInfo = {
-	allFunctionCalls:           0,
-	/** abs, expm1, tanpi, ... */
-	mathFunctions:              0,
-	/** nargs, missing, is.character, ... */
-	programmingFunctions:       0,
-	/** browser, proc.time, gc.time, ... */
-	sessionManagementFunctions: 0,
-	/** `:`, `~`, `c`, `UseMethod`, `.C`, ... */
-	primitiveFunctions:         0,
-	/** e.g. do not evaluate part of functions, `quote`, ... */
-	specialPrimitiveFunctions:  0,
-	/** `body`, `environment`, `formals` */
-	metaFunctions:              0,
-	/** return */
-	returnFunction:             0,
-	parsingFunctions:           0,
-	editFunctions:              0,
-	assignFunctions:            0,
-	getFunctions:               0,
-	helpFunctions:              0,
-	optionFunctions:            0,
+	allFunctionCalls:    0,
 	/** `a(b(), c(3, d()))` has 3 (`b`, `c`, `d`) */
-	nestedFunctionCalls:        0,
-	deepestNesting:             0,
-	unnamedCalls:               0
+	nestedFunctionCalls: 0,
+	deepestNesting:      0,
+	unnamedCalls:        0
 }
 
 export type FunctionUsageInfo = Writable<typeof initialFunctionUsageInfo>
-
-
-function from(...names: string[]): RegExp {
-	return new RegExp(names.join('|'))
-}
-
-/* from R internals */
-const mathFunctions = from('abs', 'sign', 'sqrt', 'floor', 'ceiling', 'exp', 'expm1', 'log2', 'log10', 'log1p', 'cos', 'sin', 'tan', 'acos', 'asin', 'atan', 'cosh', 'sinh', 'tanh', 'acosh', 'asinh', 'atanh', 'cospi', 'sinpi', 'tanpi', 'gamma', 'lgamma', 'digamma', 'trigamma', 'cumsum', 'cumprod', 'cummax', 'cummin', 'Im', 'Re', 'Arg', 'Conj', 'Mod')
-const programmingFunctions = from('nargs', 'missing', 'on.exit', 'interactive', 'as.call', 'as.character', 'as.complex', 'as.double', 'as.environment', 'as.integer', 'as.logical', 'as.raw', 'is.array', 'is.atomic', 'is.call', 'is.character', 'is.complex', 'is.double', 'is.environment', 'is.expression', 'is.finite', 'is.function', 'is.infinite', 'is.integer', 'is.language', 'is.list', 'is.logical', 'is.matrix', 'is.na', 'is.name', 'is.nan', 'is.null', 'is.numeric', 'is.object', 'is.pairlist', 'is.raw', 'is.real', 'is.recursive', 'is.single', 'is.symbol', 'baseenv', 'emptyenv', 'globalenv', 'pos.to.env', 'unclass', 'invisible', 'seq_along', 'seq_len')
-const sessionManagementFunctions = from('browser', 'proc.time', 'gc.time', 'tracemem', 'retracemem', 'untracemem')
-const primitiveFunctions = from(':', '~', 'c', 'list', 'call', 'switch', 'expression', 'substitute', 'UseMethod', 'standardGeneric', '.C', '.Fortran', '.Call', '.External', 'round', 'signif', 'rep', 'seq.int', '.Primitive', '.Internal', '.Call.graphics', '.External.graphics', '.subset', '.subset2', '.primTrace', '.primUntrace', 'lazyLoadDBfetch')
-const specialPrimitiveFunctions = from('quote', 'substitute', 'missing', 'on.exit', 'call', 'expression', '.Internal')
-const metaFunctions = from('body', 'environment', 'formals')
-const returnFunction = /return/
-const parsingFunctions = from('parse', 'deparse', 'substitute', 'quote', 'bquote', 'call', 'eval', 'evalq', 'eval.parent')
-const editFunctions = from('edit', 'vi', 'emacs', 'pico', 'xemacs', 'xedit', 'fix', 'fixInNamespace')
-const assignFunctions = from('assign',  'assignInNamespace', 'assignInMyNamespace')
-const getFunctions = from('get', 'exists', 'getFromNamespace')
-const helpFunctions = from('help', 'help.search', 'prompt')
-const optionFunctions = from('options', 'getOption', '.Option')
-
-function collectFunctionByName(name: string, info: FunctionUsageInfo, field: keyof FunctionUsageInfo, regex: RegExp): void {
-	if(regex.test(name)) {
-		info[field]++
-	}
-}
-
-function analyzeFunctionName(name: string, info: FunctionUsageInfo) {
-	collectFunctionByName(name, info, 'mathFunctions', mathFunctions)
-	collectFunctionByName(name, info, 'programmingFunctions', programmingFunctions)
-	collectFunctionByName(name, info, 'sessionManagementFunctions', sessionManagementFunctions)
-	collectFunctionByName(name, info, 'primitiveFunctions', primitiveFunctions)
-	collectFunctionByName(name, info, 'specialPrimitiveFunctions', specialPrimitiveFunctions)
-	collectFunctionByName(name, info, 'metaFunctions', metaFunctions)
-	collectFunctionByName(name, info, 'returnFunction', returnFunction)
-	collectFunctionByName(name, info, 'parsingFunctions', parsingFunctions)
-	collectFunctionByName(name, info, 'editFunctions', editFunctions)
-	collectFunctionByName(name, info, 'assignFunctions', assignFunctions)
-	collectFunctionByName(name, info, 'getFunctions', getFunctions)
-	collectFunctionByName(name, info, 'helpFunctions', helpFunctions)
-	collectFunctionByName(name, info, 'optionFunctions', optionFunctions)
-}
 
 export interface FunctionCallInformation extends MergeableRecord {
 	/** the name of the called function, or undefined if this was an unnamed function call */
@@ -112,7 +50,6 @@ function visitCalls(info: FunctionUsageInfo, input: FeatureProcessorInput): void
 					namespace:         undefined
 				})
 			} else {
-				analyzeFunctionName(node.functionName.lexeme, info)
 				allCalls.push({
 					name:              node.functionName.lexeme,
 					location:          node.location.start,

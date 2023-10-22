3bddf3405f55032ec305f05c76643c57fa400ab9
Florian Sihler <florian.sihler@uni-ulm.de>
2023-10-22 15:02:43 +0200

    refactor, wip: fighting with encoding errors

--- src/util/summarizer/statistics/summarizer.ts
+++ src/util/summarizer/statistics/summarizer.ts
@@ -66,17 +66,19 @@ function identifyCommonPrefix(files: Map<string,string>): string {
 /** returns the target path */
 async function extractArchive(f: string): Promise<Map<string,string>> {
 	const files = await retrieveAllFilesInArchive(f)
-	const commonRoot  = identifyCommonPrefix(files)
+	const commonRoot = identifyCommonPrefix(files)
 	// post process until we find the '<filename>.(r|R)' suffix. otherwise, if there are no features and only the meta folder, the meta folder will be removed, resulting in a write
 	// to the toplevel!
 	const fname = path.basename(f).replace(/\.tar\.gz$/, '')
-	const commonPart = commonRoot.substring(0, commonRoot.indexOf(fname) + fname.length)
+	const commonPart = new RegExp('^' + fname, 'u')[Symbol.search](commonRoot) + fname.length
+	console.log(`    Common root: ${commonRoot}, `, fname,',', new RegExp('^' + fname, 'u')[Symbol.search](commonRoot),fname.length, commonPart)
 
 
 	// transform all map keys by removing the common root
 	const transformed = new Map<string, string>()
 	for(const [key, value] of files.entries()) {
-		transformed.set(key.replace(commonPart, ''), value)
+		console.log(`    Transforming ${key}`)
+		transformed.set(key.slice(commonPart), value)
 	}
 	return transformed
 }

eedcf81d9c57cd8206908f4a97c419055f44aee9
Florian Sihler <florian.sihler@uni-ulm.de>
2023-10-23 20:24:00 +0200

    refactor, wip: collecting single and double bracket arguments

--- src/statistics/features/supported/data-access/post-process.ts
+++ src/statistics/features/supported/data-access/post-process.ts
@@ -2,9 +2,14 @@ import { FeatureStatisticsWithMeta } from '../../feature'
 import { StatisticsSummarizerConfiguration } from '../../../../util/summarizer/statistics/summarizer'
 import { SummarizedMeasurement } from '../../../../util/summarizer/benchmark/data'
 import { MergeableRecord } from '../../../../util/objects'
-import { CommonSyntaxTypeCounts } from '../../common-syntax-probability'
+import {
+	appendCommonSyntaxTypeCounter,
+	CommonSyntaxTypeCounts,
+	emptyCommonSyntaxTypeCounts
+} from '../../common-syntax-probability'
 import { emptySummarizedWithProject, recordFilePath, SummarizedWithProject } from '../../post-processing'
 import { DataAccessInfo } from './data-access'
+import { bigint2number } from '../../../../util/numbers'
 
 interface DataAccessMetaPostProcessing<Measurement=SummarizedMeasurement> extends MergeableRecord {
 	singleBracket:         Map<number, Measurement | CommonSyntaxTypeCounts<Measurement>>,
@@ -23,6 +28,26 @@ function addToList(data: SummarizedWithProject, dataAccess: number, filepath: st
 	}
 }
 
+function summarizeForBracket(dataAccess: Record<number, bigint | CommonSyntaxTypeCounts>, data: DataAccessMetaPostProcessing<SummarizedWithProject>['singleBracket' | 'doubleBracket'], filepath: string, config: StatisticsSummarizerConfiguration) {
+	for(const [key, val] of Object.entries(dataAccess) as [string, bigint | string | CommonSyntaxTypeCounts][]) {
+		const numericKey = Number(key)
+		const get = data.get(numericKey) ?? emptyCommonSyntaxTypeCounts(() => [])
+		if(typeof val === 'bigint' || typeof val === 'string') {
+			// it is for argument 0
+			const sumGet = get as SummarizedWithProject
+			const numericVal = bigint2number(val)
+			sumGet.count.push()
+			if(numericVal > 0) {
+				recordFilePath(sumGet, filepath, config)
+			}
+		} else {
+			appendCommonSyntaxTypeCounter(get as CommonSyntaxTypeCounts<number[][]>, val)
+		}
+		// TODO: update similar to other sets?
+		data.set(numericKey, get as SummarizedWithProject)
+	}
+}
+
 export function postProcess(featureRoot: string, info: Map<string, FeatureStatisticsWithMeta>, outputPath: string, config: StatisticsSummarizerConfiguration): void {
 	const data: DataAccessMetaPostProcessing<SummarizedWithProject> = {
 		singleBracket:         new Map(),
@@ -33,6 +58,9 @@ export function postProcess(featureRoot: string, info: Map<string, FeatureStatis
 		byName:                emptySummarizedWithProject(),
 		bySlot:                emptySummarizedWithProject()
 	}
+	// initialize the special 0
+	data.singleBracket.set(0, emptySummarizedWithProject())
+	data.doubleBracket.set(0, emptySummarizedWithProject())
 
 	for(const [filepath, value] of info.entries()) {
 		const dataAccess = value.dataAccess as DataAccessInfo
@@ -42,6 +70,7 @@ export function postProcess(featureRoot: string, info: Map<string, FeatureStatis
 		addToList(data.byName, dataAccess.byName, filepath, config)
 		// TODO: can we get used names etc?
 		addToList(data.bySlot, dataAccess.bySlot, filepath, config)
-
+		summarizeForBracket(dataAccess.singleBracket, data.singleBracket, filepath, config)
+		summarizeForBracket(dataAccess.doubleBracket, data.doubleBracket, filepath, config)
 	}
 }

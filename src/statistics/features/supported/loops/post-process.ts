99e4eba455961be759d069a6938fd396c800cf09
Florian Sihler <florian.sihler@uni-ulm.de>
2023-10-23 10:36:40 +0200

    refactor: summarize loops to csv as well

--- src/statistics/features/supported/loops/post-process.ts
+++ src/statistics/features/supported/loops/post-process.ts
@@ -6,12 +6,14 @@ import { MergeableRecord } from '../../../../util/objects'
 import {
 	appendCommonSyntaxTypeCounter,
 	CommonSyntaxTypeCounts,
-	emptyCommonSyntaxTypeCounts
+	emptyCommonSyntaxTypeCounts, summarizeCommonSyntaxTypeCounter
 } from '../../common-syntax-probability'
 import { bigint2number } from '../../../../util/numbers'
 import path from 'path'
 import fs from 'node:fs'
 import { jsonReplacer } from '../../../../util/json'
+import { summarizedMeasurement2Csv, summarizedMeasurement2CsvHeader } from '../../../../util/summarizer/benchmark/data'
+import { summarizeMeasurement } from '../../../../util/summarizer/benchmark/first-phase/process'
 
 type LoopInfoPostProcess = MergeableRecord & {
 	[k in keyof LoopInfo]: LoopInfo[k] extends number | bigint ? SummarizedWithProject : CommonSyntaxTypeCounts<number[][]>
@@ -48,9 +50,28 @@ export function postProcess(featureRoot: string, info: Map<string, FeatureStatis
 		}
 	}
 
-	// TODO: summarize!
-
-	// TODO: write this line by line to avoid to write too much data!
-	fs.writeFileSync(path.join(outputPath, 'loop.json'), JSON.stringify(collected, jsonReplacer))
-
+	const metaOut = fs.createWriteStream(path.join(outputPath, 'loops-meta.csv'))
+	metaOut.write(`kind,unique-projects,unique-files,${summarizedMeasurement2CsvHeader()}\n`)
+	for(const [key, val] of Object.entries(collected)) {
+		const data = val as SummarizedWithProject | CommonSyntaxTypeCounts<number[][]>
+		if('uniqueProjects' in data) {
+			metaOut.write(`${JSON.stringify(key)},${data.uniqueProjects.size},${data.uniqueFiles.size},${summarizedMeasurement2Csv(summarizeMeasurement(data.count))}\n`)
+		} else {
+			// new file for each :D
+			const out = fs.createWriteStream(path.join(outputPath, `loops-type-${key}.csv`))
+			// name is for fields like number etc to allow to group multiple entries
+			out.write(`kind,name,${summarizedMeasurement2CsvHeader()}\n`)
+			for(const [name, vals] of Object.entries(data) as [string, number[][] | Record<string, number[][]>][]) {
+				if(Array.isArray(vals)) {
+					out.write(`${JSON.stringify(name)},"",${summarizedMeasurement2Csv(summarizeMeasurement(vals.flat()))}\n`)
+				} else {
+					for(const [keyName, keyValue] of Object.entries(vals)) {
+						out.write(`${JSON.stringify(name)},${JSON.stringify(keyName)},${summarizedMeasurement2Csv(summarizeMeasurement(keyValue.flat()))}\n`)
+					}
+				}
+			}
+			out.close()
+		}
+	}
+	metaOut.close()
 }

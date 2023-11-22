996e91d960fd6c0f4f5a95ffd21ce58211089133
Florian Sihler <florian.sihler@uni-ulm.de>
2023-11-22 12:16:01 +0100

    refactor: enforce non-recursive nature of `requireAllTestsInFolder`

--- test/functionality/_helper/collect-tests.ts
+++ test/functionality/_helper/collect-tests.ts
@@ -2,12 +2,13 @@ import fs from 'node:fs'
 import path from 'path'
 
 /**
- * Require all files in a folder that end with a suffix
+ * Require all files in a folder that end with a suffix, will never recurse into subfolders
  * @param folder - the folder to require all files from
  * @param suffix - the suffix which the files of interest must have
  */
 export function requireAllTestsInFolder(folder: string, suffix = '-tests.ts'): void {
-	for(const file of fs.readdirSync(folder)) {
+	for(const fileBuff of fs.readdirSync(folder, { recursive: false })) {
+		const file = fileBuff.toString()
 		if(file.endsWith(suffix)) {
 			require(path.join(folder,file))
 		}

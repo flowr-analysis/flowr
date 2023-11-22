591396e926e4f7ff639a3ca77fa3c64754515174
Florian Sihler <florian.sihler@uni-ulm.de>
2023-11-22 23:00:50 +0100

    test: simple dependency ordering in pipeline

--- test/functionality/pipelines/create/dependency-check-tests.ts
+++ test/functionality/pipelines/create/dependency-check-tests.ts
@@ -1,5 +1,5 @@
 import { createPipeline } from '../../../../src/core/steps/pipeline'
-import { IStep } from '../../../../src/core/steps'
+import { IStep, NameOfStep } from '../../../../src/core/steps'
 import { expect } from 'chai'
 import { PARSE_WITH_R_SHELL_STEP } from '../../../../src/core/steps/all/00-parse'
 
@@ -20,5 +20,20 @@ describe('dependency check', () => {
 				{ ...PARSE_WITH_R_SHELL_STEP, name: 'parse-v1', dependencies: ['parse-v2'] },
 				{ ...PARSE_WITH_R_SHELL_STEP, name: 'parse-v2', dependencies: ['parse-v1'] }
 			], /cycle/)
+	})
+	describe('default behavior', () => {
+		function positive(name: string, steps: IStep[], expected: NameOfStep[]) {
+			it(name, () => {
+				const pipeline = createPipeline(steps)
+				expect([...pipeline.steps.keys()]).to.have.members(expected)
+				expect(pipeline.order).to.have.ordered.members(expected)
+			})
+		}
+
+		positive('should work on a single step', [PARSE_WITH_R_SHELL_STEP], ['parse'])
+		positive('should work on a single step with dependencies', [
+			PARSE_WITH_R_SHELL_STEP,
+			{ ...PARSE_WITH_R_SHELL_STEP, name: 'parse-v2', dependencies: ['parse'] }
+		], ['parse', 'parse-v2'])
 	})
 })

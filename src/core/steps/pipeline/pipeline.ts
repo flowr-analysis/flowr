5837668eb94b7beb044a716f7afa8835e4794d56
Florian Sihler <florian.sihler@uni-ulm.de>
2023-11-24 11:04:18 +0100

    doc: document the pipeline interface and friends

--- src/core/steps/pipeline/pipeline.ts
+++ src/core/steps/pipeline/pipeline.ts
@@ -1,11 +1,19 @@
 import { IStep, NameOfStep } from '../step'
 import { verifyAndBuildPipeline } from './dependency-checker'
 
+/**
+ * A pipeline is a collection of {@link Pipeline#steps|steps} that are executed in a certain {@link Pipeline#order|order}.
+ * It is to be created {@link createPipeline}.
+ */
 export interface Pipeline {
 	readonly steps: ReadonlyMap<NameOfStep, IStep>
 	readonly order: NameOfStep[]
 }
 
+/**
+ * Creates a pipeline from the given steps.
+ * Refer to {@link verifyAndBuildPipeline} for details and constraints on the steps.
+ */
 export function createPipeline(steps: IStep[]): Pipeline {
 	return verifyAndBuildPipeline(steps)
 }

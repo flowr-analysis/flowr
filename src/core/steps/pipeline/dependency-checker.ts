e22fca137f4533a213e15cb00d9f1010cc914b8e
Florian Sihler <florian.sihler@uni-ulm.de>
2023-11-24 12:14:22 +0100

    refactor(pipeline-dep): add rule invalidation numbers to the dependency checker

--- src/core/steps/pipeline/dependency-checker.ts
+++ src/core/steps/pipeline/dependency-checker.ts
@@ -5,25 +5,28 @@ import { Pipeline } from './pipeline'
 
 /**
  * Given a set of {@link IStep|steps} with their dependencies, this function verifies that
+ * 0) the pipeline is not empty
  * 1) all names of steps are unique for the given pipeline
- * 2) all dependencies of steps are valid (i.e., refer to existing steps)
+ * 2) all {@link IStepOrder#dependencies|dependencies} of steps are valid (i.e., refer to existing steps)
  * 3) there are no cycles in the dependency graph
+ * 4) the target of a {@link IStepOrder#decorates|decoration} exists
+ * 5) the target of a {@link IStepOrder#decorates|decoration} is not part of the {@link IStepOrder#dependencies|dependencies}
  * If successful, it returns the topologically sorted list of steps in order of desired execution.
  * @throws InvalidPipelineError if any of the above conditions are not met
  */
 export function verifyAndBuildPipeline(steps: IStep[]): Pipeline {
 	if(steps.length === 0) {
-		throw new InvalidPipelineError('Pipeline is empty')
+		throw new InvalidPipelineError('0) Pipeline is empty')
 	}
 
 	// we construct a map linking each name to its respective step
 	const stepMap = new Map<NameOfStep, IStep>()
-	// we track all elements without dependencies, i.e. those that start the pipeline
+	// we track all elements without dependencies, i.e., those that start the pipeline
 	const inits: NameOfStep[] = []
 	initializeSteps(steps, stepMap, inits)
 
 	if(inits.length === 0) {
-		throw new InvalidPipelineError('Pipeline has no initial steps (i.e., it contains no step without dependencies)')
+		throw new InvalidPipelineError('3) Pipeline has no initial steps (i.e., it contains no step without dependencies)')
 	}
 	const sorted = topologicalSort(inits, stepMap)
 
@@ -31,7 +34,7 @@ export function verifyAndBuildPipeline(steps: IStep[]): Pipeline {
 		// check if any of the dependencies in the map are invalid
 		checkForInvalidDependency(steps, stepMap)
 		// otherwise, we assume a cycle
-		throw new InvalidPipelineError('Pipeline contains at least one cycle')
+		throw new InvalidPipelineError('3) Pipeline contains at least one cycle')
 	}
 
 	return {
@@ -62,8 +65,11 @@ function checkForInvalidDependency(steps: IStep[], stepMap: Map<NameOfStep, ISte
 	for(const step of steps) {
 		for(const dep of step.dependencies) {
 			if(!stepMap.has(dep)) {
-				throw new InvalidPipelineError(`Step "${step.name}" depends on step "${dep}" which does not exist`)
+				throw new InvalidPipelineError(`2) Step "${step.name}" depends on step "${dep}" which does not exist`)
 			}
+		}
+		if(step.decorates && !stepMap.has(step.decorates)) {
+			throw new InvalidPipelineError(`4) Step "${step.name}" decorates step "${step.decorates}" which does not exist`)
 		}
 	}
 }
@@ -73,7 +79,7 @@ function initializeSteps(steps: IStep[], stepMap: Map<NameOfStep, IStep>, inits:
 		const name = step.name
 		// if the name is already in the map we have a duplicate
 		if(stepMap.has(name)) {
-			throw new InvalidPipelineError(`Step name "${name}" is not unique in the pipeline`)
+			throw new InvalidPipelineError(`1) Step name "${name}" is not unique in the pipeline`)
 		}
 		stepMap.set(name, step)
 		if(step.dependencies.length === 0) {

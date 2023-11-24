338dbb229ab5608db201e52f44538dbf7fa87b72
Florian Sihler <florian.sihler@uni-ulm.de>
2023-11-24 16:36:51 +0100

    refactor(pipeline): allow to acess step types

--- src/core/steps/pipeline/pipeline.ts
+++ src/core/steps/pipeline/pipeline.ts
@@ -4,11 +4,20 @@ import { verifyAndBuildPipeline } from './dependency-checker'
 /**
  * A pipeline is a collection of {@link Pipeline#steps|steps} that are executed in a certain {@link Pipeline#order|order}.
  * It is to be created {@link createPipeline}.
+ *
+ * If you want to get the type of all steps in the pipeline (given they are created canonically using const step names), refer to {@link PipelineStepTypes}.
  */
 export interface Pipeline<T extends NameOfStep[] = NameOfStep[]> {
 	readonly steps: ReadonlyMap<NameOfStep, IStep>
 	readonly order: T
 }
+
+/**
+ * Returns the types of all steps in the given pipeline.
+ *
+ * @see Pipeline for details
+ */
+export type PipelineStepTypes<T extends Pipeline> = T extends Pipeline<infer U> ? U[0] : never
 
 /**
  * Creates a pipeline from the given steps.
--- test/functionality/pipelines/create/dependency-check-tests.ts
+++ test/functionality/pipelines/create/dependency-check-tests.ts
@@ -1,4 +1,4 @@
-import { createPipeline, Pipeline } from '../../../../src/core/steps/pipeline'
+import { createPipeline, Pipeline, PipelineStepTypes } from '../../../../src/core/steps/pipeline'
 import { IStep, NameOfStep } from '../../../../src/core/steps'
 import { expect } from 'chai'
 import { PARSE_WITH_R_SHELL_STEP } from '../../../../src/core/steps/all/00-parse'

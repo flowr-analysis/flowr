f1adaa5e25462d86343bba79ef0d49bd958d6060
EagleoutIce <florian.sihler@uni-ulm.de>
2023-12-08 14:51:50 +0100

    refactor(doc): fix typo (theis->this)

--- test/functionality/_helper/shell.ts
+++ test/functionality/_helper/shell.ts
@@ -182,7 +182,7 @@ function printIdMapping(ids: NodeId[], map: DecoratedAstMap): string {
 }
 
 /**
- * Please note, that theis executes the reconstruction step separately, as it predefines the result of the slice with the given ids.
+ * Please note, that this executes the reconstruction step separately, as it predefines the result of the slice with the given ids.
  */
 export function assertReconstructed(name: string, shell: RShell, input: string, ids: NodeId | NodeId[], expected: string, userConfig?: Partial<TestConfiguration>, getId: IdGenerator<NoInfo> = deterministicCountingIdGenerator(0)): Mocha.Test {
 	const selectedIds = Array.isArray(ids) ? ids : [ids]

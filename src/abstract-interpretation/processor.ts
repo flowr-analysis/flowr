20fe13b94447dcbe0ff4fe838b9664df288c1839
Lukas Pietzschmann <lukas.pietzschmann@uni-ulm.de>
2023-12-08 21:05:56 +0100

    refactor: Refactored the unification of domains

--- src/abstract-interpretation/processor.ts
+++ src/abstract-interpretation/processor.ts
@@ -2,7 +2,7 @@ import {DataflowInformation} from '../dataflow/internal/info'
 import {NodeId, NormalizedAst, ParentInformation, RAssignmentOp, RBinaryOp, RType} from '../r-bridge'
 import {CfgVertexType, extractCFG} from '../util/cfg/cfg'
 import {visitCfg} from '../util/cfg/visitor'
-import {guard} from '../util/assert'
+import {assertUnreachable, guard} from '../util/assert'
 import {DataflowGraphVertexInfo, EdgeType, OutgoingEdges} from '../dataflow'
 
 const constraintMap = new Map<NodeId, Constraints>()
@@ -26,11 +26,25 @@ interface IHandler<ValueType> {
 	next:  (value: ValueType) => void
 }
 
-interface Interval {
-	min:          number,
-	max:          number,
-	minInclusive: boolean,
-	maxInclusive: boolean
+interface IntervalBound {
+	value:     number,
+	inclusive: boolean
+}
+
+class Interval {
+	readonly min: IntervalBound
+	readonly max: IntervalBound
+
+	constructor(min: IntervalBound, max: IntervalBound) {
+		guard(min.value <= max.value, `The interval ${this.toString()} has a minimum that is greater than its maximum`)
+		guard(min.value !== max.value || (min.inclusive === max.inclusive), `The bound ${min.value} cannot be in- and exclusive at the same time`)
+		this.min = min
+		this.max = max
+	}
+
+	toString(): string {
+		return `${this.min.inclusive ? '[' : '('}${this.min.value}, ${this.max.value}${this.max.inclusive ? ']' : ')'}`
+	}
 }
 
 type Domain = Set<Interval>
@@ -41,41 +55,82 @@ interface Constraints {
 	debugMsg: string
 }
 
-function compareIntervals(interval1: Interval, interval2: Interval): number {
-	const diff = interval1.min - interval2.min
+const enum CompareType {
+	/** The bound that's inclusive is the smaller one */
+	Min,
+	/** The bound that's inclusive is the greater one */
+	Max
+}
+
+function compareIntervals(compareType: CompareType, interval1: IntervalBound, interval2: IntervalBound): number {
+	const diff = interval1.value - interval2.value
 	if(diff !== 0) {
 		return diff
 	}
-	return Number(!interval1.minInclusive) - Number(!interval2.minInclusive)
+	switch(compareType) {
+		case CompareType.Min: return Number(!interval1.inclusive) - Number(!interval2.inclusive)
+		case CompareType.Max: return Number(interval1.inclusive) - Number(interval2.inclusive)
+		default: assertUnreachable(compareType)
+	}
 }
 
-function unifyDomains(domains: Domain[]): Domain {
-	const allIntervals = new Array<Interval>()
-	domains.forEach(domain => allIntervals.push(...domain))
-	allIntervals.sort(compareIntervals)
-	if(!allIntervals.length) {
-		return new Set<Interval>()
+function compareIntervalsByTheirMinimum(interval1: Interval, interval2: Interval): number {
+	return compareIntervals(CompareType.Min, interval1.min, interval2.min)
+}
+
+function compareIntervalsByTheirMaximum(interval1: Interval, interval2: Interval): number {
+	return compareIntervals(CompareType.Max, interval1.max, interval2.max)
+}
+
+function doIntervalsOverlap(interval1: Interval, interval2: Interval): boolean {
+	const diff1 = compareIntervals(CompareType.Max, interval1.max, interval2.min)
+	const diff2 = compareIntervals(CompareType.Max, interval2.max, interval1.min)
+
+	// If one interval ends before the other starts, they don't overlap
+	if(diff1 < 0 || diff2 < 0) {
+		return false
+	}
+	// If their end and start are equal, they only overlap if both are inclusive
+	if(diff1 === 0) {
+		return interval1.max.inclusive && interval2.min.inclusive
+	}
+	if(diff2 === 0) {
+		return interval2.max.inclusive && interval1.min.inclusive
 	}
 
-	const resultingDomain = new Set<Interval>()
-	let currentInterval = allIntervals[0]
-	allIntervals.forEach(interval => {
-		if(interval.min < currentInterval.max) {
-			if(interval.max > currentInterval.max) {
-				currentInterval.max = interval.max
-				currentInterval.maxInclusive = interval.maxInclusive
+	return true
+}
+
+function unifyDomains(domains: Domain[]) : Domain {
+	const sortedIntervals = domains.flatMap(domain => [...domain]).sort(compareIntervalsByTheirMinimum)
+	if(sortedIntervals.length === 0) {
+		return new Set()
+	}
+
+	const unifiedDomain = new Set<Interval>()
+	let currentInterval = sortedIntervals[0]
+	sortedIntervals.forEach(nextInterval => {
+		if(doIntervalsOverlap(currentInterval, nextInterval)) {
+			const intervalWithEarlierStart = compareIntervalsByTheirMinimum(currentInterval, nextInterval) < 0 ? currentInterval : nextInterval
+			const intervalWithLaterEnd = compareIntervalsByTheirMaximum(currentInterval, nextInterval) > 0 ? currentInterval : nextInterval
+			currentInterval = {
+				min: intervalWithEarlierStart.min,
+				max: intervalWithLaterEnd.max,
 			}
 		} else {
-			resultingDomain.add(currentInterval)
-			currentInterval = interval
+			unifiedDomain.add(currentInterval)
+			currentInterval = nextInterval
 		}
 	})
-	resultingDomain.add(currentInterval)
-	return resultingDomain
+	unifiedDomain.add(currentInterval)
+	return unifiedDomain
 }
 
 function domainFromScalar(n: number): Domain {
-	return new Set([{min: n, minInclusive: true, max: n, maxInclusive: true}])
+	return new Set([{
+		min: {value: n, inclusive: true},
+		max: {value: n, inclusive: true}}
+	])
 }
 
 function getDomainOfDfgChild(node: NodeId, dfg: DataflowInformation): Domain {

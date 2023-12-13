0cd9ab26ce58cd9847068807ad24846a0fdfcbeb
Ehcsan <68707578+Ehcsan@users.noreply.github.com>
2023-12-13 17:38:17 +0100

    feat(test): added more cases to merge Last Test Case ignores if merge sorts stable or not Maybe investigate stable behavior of merge later

--- test/functionality/slicing/reconstruct/function-tests.ts
+++ test/functionality/slicing/reconstruct/function-tests.ts
@@ -69,5 +69,78 @@ describe('Functions_Reconstruct', () => {
 				positive(testCase.snipbit, testCase.expected)
 			}
 		})
+		describe('three lines merge', () => {
+			const testCases = [
+				//Case 1 (in order) 123
+				{
+					snipbit:  [[{linePart: [{part: 'Hello', loc: {line: 0, column: 0}}, {part: 'World', loc: {line: 1, column: 7}}], indent: 0}],[{linePart: [{part: '24', loc: {line: 2, column: 7}}], indent: 0}]],
+					expected: [{linePart: [{part: 'Hello', loc: {line: 0, column: 0}}], indent: 0},{linePart: [{part: 'World', loc: {line: 1, column: 7}}], indent: 0},{linePart: [{part: '24', loc: {line: 2, column: 7}}], indent: 0}]
+				},
+				//Case 2 (2nd Line out of order) 213
+				{
+					snipbit:  [[{linePart: [{part: 'World', loc: {line: 1, column: 7}}, {part: 'Hello', loc: {line: 0, column: 0}}], indent: 0}],[{linePart: [{part: '24', loc: {line: 2, column: 7}}], indent: 0}]],
+					expected: [{linePart: [{part: 'Hello', loc: {line: 0, column: 0}}], indent: 0},{linePart: [{part: 'World', loc: {line: 1, column: 7}}], indent: 0},{linePart: [{part: '24', loc: {line: 2, column: 7}}], indent: 0}]
+				},
+				//Case 3 (3rd Line out of order) 231
+				{
+					snipbit:  [[{linePart: [{part: 'World', loc: {line: 1, column: 7}}], indent: 0}],[{linePart: [{part: '24', loc: {line: 2, column: 7}}], indent: 0}], [{linePart: [{part: 'Hello', loc: {line: 0, column: 0}}], indent: 0}]],
+					expected: [{linePart: [{part: 'Hello', loc: {line: 0, column: 0}}], indent: 0},{linePart: [{part: 'World', loc: {line: 1, column: 7}}], indent: 0},{linePart: [{part: '24', loc: {line: 2, column: 7}}], indent: 0}]
+				},
+				//Case 4 (reverse order) 321
+				{
+					snipbit:  [[{linePart: [{part: '24', loc: {line: 2, column: 7}}], indent: 0}],[{linePart: [{part: 'World', loc: {line: 1, column: 7}}], indent: 0}], [{linePart: [{part: 'Hello', loc: {line: 0, column: 0}}], indent: 0}]],
+					expected: [{linePart: [{part: 'Hello', loc: {line: 0, column: 0}}], indent: 0},{linePart: [{part: 'World', loc: {line: 1, column: 7}}], indent: 0},{linePart: [{part: '24', loc: {line: 2, column: 7}}], indent: 0}]
+				},
+				//Case 5 () 312
+				{
+					snipbit:  [[{linePart: [{part: '24', loc: {line: 2, column: 7}}], indent: 0}], [{linePart: [{part: 'Hello', loc: {line: 0, column: 0}}], indent: 0}], [{linePart: [{part: 'World', loc: {line: 1, column: 7}}], indent: 0}]],
+					expected: [{linePart: [{part: 'Hello', loc: {line: 0, column: 0}}], indent: 0},{linePart: [{part: 'World', loc: {line: 1, column: 7}}], indent: 0},{linePart: [{part: '24', loc: {line: 2, column: 7}}], indent: 0}]
+				},
+				//Case 6 () 132
+				{
+					snipbit:  [[{linePart: [{part: 'Hello', loc: {line: 0, column: 0}}], indent: 0}], [{linePart: [{part: '24', loc: {line: 2, column: 7}}], indent: 0}], [{linePart: [{part: 'World', loc: {line: 1, column: 7}}], indent: 0}]],
+					expected: [{linePart: [{part: 'Hello', loc: {line: 0, column: 0}}], indent: 0},{linePart: [{part: 'World', loc: {line: 1, column: 7}}], indent: 0},{linePart: [{part: '24', loc: {line: 2, column: 7}}], indent: 0}]
+				}
+			]
+			for(const testCase of testCases) {
+				positive(testCase.snipbit, testCase.expected)
+			}
+		})
+		describe('random lines merge', () => {
+			function makeTestCase(lines: number): Code[] {
+				const testCase = [] as Code[]
+				const partPool = ['Hello', 'World', 'FlowR', 'Is', 'Incredible']
+				for(let i = 0; i < lines; i++) {
+					const line: Code = [{linePart: [{part: partPool[Math.random() % 5], loc: {line: Math.random() % lines, column: Math.random() % 15}}], indent: 0}]
+					testCase.push(line)
+				}
+				return testCase
+			}
+			function checkTestCase(code: Code): boolean {
+				let currentLoc = {line: 0, column: 0}
+				for(let line = 0; line < code.length; line++) {
+					for(let part = 0; part < code[line].linePart.length; part++) {
+						const nextLoc = code[line].linePart[part].loc
+						if(currentLoc.line <= nextLoc.line && currentLoc.column <= nextLoc.column) {
+							currentLoc = nextLoc
+						}
+						else {
+							return false
+						}
+					}
+				}
+				return true
+			}
+			function positive(lines: number) {
+				const snipbit = makeTestCase(lines)
+				it(`case with ${lines} lines`, () => {
+					const merged = merge(snipbit)
+					assert.isTrue(checkTestCase(merged), JSON.stringify(merged))
+				})
+			}
+			for(let i = 0; i < 15; i++) {
+				positive(i)
+			}
+		})
 	})
 })

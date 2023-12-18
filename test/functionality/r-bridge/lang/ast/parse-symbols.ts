0a56f8cd6983b6ebf06db1d45fa6b12ece8af639
Florian Sihler <florian.sihler@uni-ulm.de>
2023-12-18 22:32:31 +0100

    refactor: migrate parse-symbols tests

--- test/functionality/r-bridge/lang/ast/parse-symbols.ts
+++ test/functionality/r-bridge/lang/ast/parse-symbols.ts
@@ -1,32 +1,39 @@
-import { assertAst, withShell } from '../../../_helper/shell'
+import { assertAst, sameForSteps, withShell } from '../../../_helper/shell'
 import { exprList } from '../../../_helper/ast-builder'
 import { rangeFrom } from '../../../../../src/util/range'
 import { RType } from '../../../../../src/r-bridge'
+import { DESUGAR_NORMALIZE, NORMALIZE } from '../../../../../src/core/steps/all/core/10-normalize'
 
 describe('Parse symbols', withShell(shell => {
-	assertAst('Simple Symbol', shell, 'a', exprList({
-		type:      RType.Symbol,
-		location:  rangeFrom(1, 1, 1, 1),
-		namespace: undefined,
-		lexeme:    'a',
-		content:   'a',
-		info:      {}
-	}))
-	assertAst('With Namespace', shell, 'a::b', exprList({
-		type:      RType.Symbol,
-		location:  rangeFrom(1, 4, 1, 4),
-		namespace: 'a',
-		lexeme:    'b',
-		content:   'b',
-		info:      {}
-	}))
-	assertAst('With Quotes and Namespace', shell, 'a::"b"', exprList({
-		type:      RType.Symbol,
-		location:  rangeFrom(1, 4, 1, 6),
-		namespace: 'a',
-		lexeme:    '"b"',
-		content:   '"b"',
-		info:      {}
-	}))
+	assertAst('Simple Symbol', shell, 'a', sameForSteps([NORMALIZE, DESUGAR_NORMALIZE],
+		exprList({
+			type:      RType.Symbol,
+			location:  rangeFrom(1, 1, 1, 1),
+			namespace: undefined,
+			lexeme:    'a',
+			content:   'a',
+			info:      {}
+		}))
+	)
+	assertAst('With Namespace', shell, 'a::b', sameForSteps([NORMALIZE, DESUGAR_NORMALIZE],
+		exprList({
+			type:      RType.Symbol,
+			location:  rangeFrom(1, 4, 1, 4),
+			namespace: 'a',
+			lexeme:    'b',
+			content:   'b',
+			info:      {}
+		}))
+	)
+	assertAst('With Quotes and Namespace', shell, 'a::"b"', sameForSteps([NORMALIZE, DESUGAR_NORMALIZE],
+		exprList({
+			type:      RType.Symbol,
+			location:  rangeFrom(1, 4, 1, 6),
+			namespace: 'a',
+			lexeme:    '"b"',
+			content:   '"b"',
+			info:      {}
+		}))
+	)
 }))
 

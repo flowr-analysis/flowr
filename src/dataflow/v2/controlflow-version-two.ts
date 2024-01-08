ab050526f69fe5da2b0979eb3856573969cd6cf2
Andreas Barth <andreas.barth@uni-ulm.de>
2024-01-08 16:55:16 +0100

    refactor: rename file to match lint rule

---
+++ src/dataflow/v2/controlflow-version-two.ts
@@ -0,0 +1,92 @@
+import { Expression } from 'xpath-ts2'
+import { Environment } from '../v1'
+import { Literal } from 'n3'
+
+export enum ExecutionState{
+	Always,
+	Maybe,
+	Never
+}
+
+
+export interface IExecutionTuple {
+	readonly executed:               ExecutionState
+	readonly influencingExpressions: Expression[] //TODO: correct?
+}
+
+export class ExecutionTuple implements IExecutionTuple{
+	readonly executed:               ExecutionState
+	readonly influencingExpressions: Expression[]
+	constructor(executed:ExecutionState, influencingExpressions: Expression[]){
+		this.executed = executed
+		this.influencingExpressions = influencingExpressions
+	}
+}
+
+export function expressionExecution(previousExecutionState: ExecutionState, 
+																																				environmentUntilNow: Environment, 
+																																				currentExpression: Expression, 
+																																				callStack: Expression[], 
+																																				influencingExpressions: Expression[]){
+	//Set the State in which the Expression was found in
+	//Expression.entry = previousExecutionState
+	//TODO: how to save? efficient way unknown
+    
+	if(previousExecutionState == ExecutionState.Never){
+		//Expression.exit = new ExecutionTuple(previousExecutionState, influencingExpressions);
+		return
+	}
+
+	const currentExpressionType = evaluateExpressionType(currentExpression, environmentUntilNow)
+
+	switch(currentExpressionType){
+		case ExpressionType.Literal:
+		case ExpressionType.If:
+		case ExpressionType.Loop:
+		case ExpressionType.Function:
+		case ExpressionType.Then:
+		case ExpressionType.Else:
+			//TODO how to save
+			//Expr.exit = (ALWAYS, List(empty))
+			break
+		case ExpressionType.Break:
+		case ExpressionType.Next:
+		case ExpressionType.Return:
+			//TODO how to save    
+			//Expr.exit = (NEVER, List(Expr))
+			break
+	}
+}
+
+enum ExpressionType{
+	Literal,
+	If,
+	Loop,
+	Function,
+	Then,
+	Else,
+	Return,
+	Break,
+	Next
+}
+
+
+function evaluateExpressionType(toEvaluateExpression: Expression, currentEnvironment: Environment):ExpressionType {
+	//TODO: actually implement that 
+	return ExpressionType.Function //TODO: comment out
+}
+
+export function onCallStackReduction(lowerLevel: ExpressionType, upperLevel: ExpressionType):ExecutionTuple{
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+	return new ExecutionTuple(ExecutionState.Never, []) //TODO comment out
+}
+

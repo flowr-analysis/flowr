d1ea24ad56362ce1c45fc988243c95a31dad5a9e
Ellpeck <me@ellpeck.de>
2024-02-07 13:43:52 +0100

    test: reset the source provider to the default value after each describe

--- test/functionality/dataflow/dataflow.spec.ts
+++ test/functionality/dataflow/dataflow.spec.ts
@@ -1,5 +1,7 @@
 import { requireAllTestsInFolder } from '../_helper/collect-tests'
 import path from 'path'
+import {setSourceProvider} from '../../../src/dataflow/internal/process/functions/source'
+import {requestProviderFromFile} from '../../../src'
 
 describe('Dataflow', () => {
 	describe('Environments', () =>
@@ -9,6 +11,9 @@ describe('Dataflow', () => {
 	describe('Graph', () =>
 		requireAllTestsInFolder(path.join(__dirname, 'graph'))
 	)
+
+	// reset the source provider back to the default value after each "describe" section
+	after(() => setSourceProvider(requestProviderFromFile()))
 
 	require('./processing-of-elements/processing-of-elements')
 })

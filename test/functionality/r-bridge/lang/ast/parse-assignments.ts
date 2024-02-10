4f6f042ab4ce73e32ceca8c3d32e45bed4ca6f88
Florian Sihler <florian.sihler@uni-ulm.de>
2024-02-10 10:57:46 +0100

    tests: first simple assignment test with desugar

--- test/functionality/r-bridge/lang/ast/parse-assignments.ts
+++ test/functionality/r-bridge/lang/ast/parse-assignments.ts
@@ -2,40 +2,76 @@ import { assertAst, withShell } from '../../../_helper/shell'
 import { exprList, numVal } from '../../../_helper/ast-builder'
 import { RAssignmentOpPool } from '../../../_helper/provider'
 import { rangeFrom } from '../../../../../src/util/range'
-import { RType } from '../../../../../src/r-bridge'
+import { RType } from '../../../../../src'
+import { DESUGAR_NORMALIZE, NORMALIZE } from '../../../../../src/core/steps/all/core/10-normalize'
 
 describe('Parse simple assignments',
-	withShell((shell) => {
+	withShell(shell => {
 		describe('Constant Assignments', () => {
 			for(const op of RAssignmentOpPool) {
 				const opOffset = op.str.length - 1
-				assertAst(
-					'Assign to 5',
+				assertAst('Assign to 5',
 					shell,
-					`x ${op.str} 5`,
-					exprList({
-						type:     RType.BinaryOp,
-						location: rangeFrom(1, 3, 1, 3 + opOffset),
-						flavor:   'assignment',
-						lexeme:   op.str,
-						operator: op.str,
-						info:     {},
-						lhs:      {
-							type:      RType.Symbol,
-							location:  rangeFrom(1, 1, 1, 1),
-							namespace: undefined,
-							lexeme:    'x',
-							content:   'x',
-							info:      {}
-						},
-						rhs: {
-							type:     RType.Number,
-							location: rangeFrom(1, 5 + opOffset, 1, 5 + opOffset),
-							lexeme:   '5',
-							content:  numVal(5),
-							info:     {}
-						},
-					})
+					`x ${op.str} 5`,[{
+						step:   NORMALIZE,
+						wanted: exprList({
+							type:     RType.BinaryOp,
+							location: rangeFrom(1, 3, 1, 3 + opOffset),
+							flavor:   'assignment',
+							lexeme:   op.str,
+							operator: op.str,
+							info:     {},
+							lhs:      {
+								type:      RType.Symbol,
+								location:  rangeFrom(1, 1, 1, 1),
+								namespace: undefined,
+								lexeme:    'x',
+								content:   'x',
+								info:      {}
+							},
+							rhs: {
+								type:     RType.Number,
+								location: rangeFrom(1, 5 + opOffset, 1, 5 + opOffset),
+								lexeme:   '5',
+								content:  numVal(5),
+								info:     {}
+							},
+						})
+					}, {
+						step:   DESUGAR_NORMALIZE,
+						wanted: exprList({
+							type:         RType.FunctionCall,
+							location:     rangeFrom(1, 3,1, 3 + opOffset),
+							lexeme:       `x ${op.str} 5`,
+							flavor:       'named',
+							info:         {},
+							functionName: {
+								type:      RType.Symbol,
+								location:  rangeFrom(1, 3, 1, 3 + opOffset),
+								lexeme:    op.str,
+								content:   op.str,
+								namespace: undefined,
+								info:      {}
+							},
+							arguments: [
+								{
+									type:      RType.Symbol,
+									location:  rangeFrom(1, 1, 1, 1),
+									lexeme:    'x',
+									content:   'x',
+									namespace: undefined,
+									info:      {}
+								},
+								{
+									type:     RType.Number,
+									location: rangeFrom(1, 5 + opOffset, 1, 5 + opOffset),
+									lexeme:   '5',
+									content:  numVal(5),
+									info:     {}
+								}
+							]
+						})
+					}]
 				)
 			}
 		})

010db509dca2cd0c0d08a14c1b75f5d2d3d87838
Ehcsan <68707578+Ehcsan@users.noreply.github.com>
2024-02-23 12:19:21 +0100

    test(slicing): mermaid link shown again

--- test/functionality/_helper/shell.ts
+++ test/functionality/_helper/shell.ts
@@ -225,10 +225,10 @@ export function assertSliced(name: string, shell: RShell, input: string, criteri
 		try {
 			assert.strictEqual(
 				result.reconstruct.code, expected,
-				//`got: ${result.reconstruct.code}, vs. expected: ${expected}, for input ${input} (slice: ${printIdMapping(result.slice.decodedCriteria.map(({ id }) => id), result.normalize.idMap)}), url: ${graphToMermaidUrl(result.dataflow.graph, result.normalize.idMap, true, result.slice.result)}`
+				`got: ${result.reconstruct.code}, vs. expected: ${expected}, for input ${input} (slice: ${printIdMapping(result.slice.decodedCriteria.map(({ id }) => id), result.normalize.idMap)}), url: ${graphToMermaidUrl(result.dataflow.graph, result.normalize.idMap, true, result.slice.result)}`
 			)
 		} catch(e) {
-			//console.error('vis-got:\n', graphToMermaidUrl(result.dataflow.graph, result.normalize.idMap))
+			console.error('vis-got:\n', graphToMermaidUrl(result.dataflow.graph, result.normalize.idMap))
 			throw e
 		}
 	})

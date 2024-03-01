fa5dcc93bf5d5c111184489db1914007ae1e9f6d
Florian Sihler <florian.sihler@uni-ulm.de>
2024-03-01 13:43:00 +0100

    wip: benchmarking instrumentation

--- src/r-bridge/init.ts
+++ src/r-bridge/init.ts
@@ -9,11 +9,11 @@ export function initCommand(eol: string): string {
 	 */
 	return 'flowr_get_ast<-compiler::cmpfun(function(...){tryCatch({'
 		/* the actual code to parse the R code, ... allows us to keep the old 'file=path' and 'text=content' semantics. we define flowr_output using the super assignment to persist it in the env! */
-		+ 'flowr_output<<-utils::getParseData(parse(...,keep.source=TRUE),includeText=TRUE);'
+		+ 's <- Sys.time();flowr_output<<-utils::getParseData(parse(...,keep.source=TRUE),includeText=TRUE);cat((Sys.time() - s)*1000,"\n",file="parse-log.txt",append=TRUE);'
 		/* json conversion of the output, dataframe="values" allows us to receive a list of lists (which is more compact)!
 		 * so we do not depend on jsonlite and friends, we do so manually (:sparkles:)
 		 */
-		+ 'apply(flowr_output,1,function(o)cat(sprintf("[%s,%s,%s,%s,%s,%s,%s,%s,%s],",o[[1]],o[[2]],o[[3]],o[[4]],o[[5]],o[[6]],deparse(o[[7]]),if(o[[8]])"true"else"false",deparse(o[[9]]))));'
+		+ 's <- Sys.time();apply(flowr_output,1,function(o)cat(sprintf("[%s,%s,%s,%s,%s,%s,%s,%s,%s],",o[[1]],o[[2]],o[[3]],o[[4]],o[[5]],o[[6]],deparse(o[[7]]),if(o[[8]])"true"else"false",deparse(o[[9]]))));'
 		/* error handling (just produce the marker) */
-		+ `},error=function(e){cat("${ErrorMarker}")});cat(${ts2r(eol)})});`
+		+ `cat((Sys.time() - s)*1000,"\n",file="out-log.txt",append=TRUE);},error=function(e){cat("${ErrorMarker}")});cat(${ts2r(eol)})});`
 }

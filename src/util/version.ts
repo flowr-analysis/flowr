d8c07d47b8fe7fb53bfa638408166e9b44853d6a
Ellpeck <me@ellpeck.de>
2024-04-02 14:22:22 +0200

    dep: use release-it-regex-bumper to avoid packaging package.json

--- src/util/version.ts
+++ src/util/version.ts
@@ -1,5 +1,7 @@
-import { version } from '../../package.json'
 import { SemVer } from 'semver'
+
+// this is automatically replaced with the current version by release-it
+const version = '1.0.0'
 
 export function flowrVersion(): SemVer {
 	return new SemVer(version)

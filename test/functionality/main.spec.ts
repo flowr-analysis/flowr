88a3160df4c8441249b822df0c36f8460e51a188
EagleoutIce <florian.sihler@uni-ulm.de>
2024-05-03 10:10:45 +0200

    refactor(test): clean up main test spec

--- test/functionality/main.spec.ts
+++ test/functionality/main.spec.ts
@@ -14,13 +14,3 @@ chai.config.truncateThreshold = 0
 
 export const VERBOSE_TESTS = process.argv.includes('--verbose')
 before(() => setMinLevelOfAllLogs(VERBOSE_TESTS ? LogLevel.Trace : LogLevel.Error, VERBOSE_TESTS))
-
-/** controlled with the `--test-installation` parameter */
-export const RUN_INSTALLATION_TESTS = process.argv.includes('--test-installation')
-
-export function isInstallTest(test: Mocha.Context): void {
-	if(!RUN_INSTALLATION_TESTS) {
-		console.warn('skipping installation test (set RUN_INSTALLATION_TESTS to run it)')
-		test.skip()
-	}
-}

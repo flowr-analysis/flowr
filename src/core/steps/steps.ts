ba7a98d635899c7a91cb8e76d554365ae2e18e54
Florian Sihler <florian.sihler@uni-ulm.de>
2024-05-07 18:15:39 +0200

    lint-fix: handle linter errors

--- src/core/steps/steps.ts
+++ src/core/steps/steps.ts
@@ -32,8 +32,8 @@ export const STEPS_PER_SLICE = {
 } as const
 
 export const STEPS = { ...STEPS_PER_FILE, ...STEPS_PER_SLICE } as const
-export const LAST_PER_FILE_STEP = 'dataflow' as const
-export const LAST_STEP = 'reconstruct' as const
+export const LAST_PER_FILE_STEP = 'dataflow'
+export const LAST_STEP = 'reconstruct'
 
 export type StepName = keyof typeof STEPS
 export type Step<Name extends StepName> = typeof STEPS[Name]

bc31c062abd8f5b62849812fdf83cd33b66c3e48
Florian Sihler <florian.sihler@uni-ulm.de>
2024-05-10 23:15:07 +0200

    refactor: omit `built-in-environment` on env-fingerprinting

--- src/slicing/static/fingerprint.ts
+++ src/slicing/static/fingerprint.ts
@@ -1,11 +1,12 @@
 import type { REnvironmentInformation } from '../../dataflow'
+import { BuiltInEnvironment } from '../../dataflow'
 import objectHash from 'object-hash'
 import type { NodeId } from '../../r-bridge'
 
 export type Fingerprint = string
 
 export function envFingerprint(env: REnvironmentInformation): Fingerprint {
-	return objectHash(env, { excludeKeys: key => key === 'id' })
+	return objectHash(env, { excludeKeys: key => key === 'id', replacer: (v: unknown) => v === BuiltInEnvironment ? undefined : v })
 }
 
 export function fingerprint(id: NodeId, envFingerprint: Fingerprint, onlyForSideEffects: boolean): Fingerprint {

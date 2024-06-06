8892c75d834db8c4f4c456dcc617ff9fdcb0a261
Lukas Pietzschmann <lukas.pietzschmann@uni-ulm.de>
2024-06-06 17:38:00 +0200

    feat: generate domain for symbols

--- src/dataflow/internal/process/process-symbol.ts
+++ src/dataflow/internal/process/process-symbol.ts
@@ -6,11 +6,21 @@ import type { ParentInformation } from '../../../r-bridge/lang-4.x/ast/model/pro
 import { RNa, RNull } from '../../../r-bridge/lang-4.x/convert-values'
 import { DataflowGraph } from '../../graph/graph'
 import { VertexType } from '../../graph/vertex'
+import type { Domain } from '../../../abstract-interpretation/domain'
+import { unifyDomains } from '../../../abstract-interpretation/domain'
+import type { InGraphIdentifierDefinition } from '../../environments/identifier'
 
 export function processSymbol<OtherInfo>(symbol: RSymbol<OtherInfo & ParentInformation>, data: DataflowProcessorInformation<OtherInfo>): DataflowInformation {
 	if(symbol.content === RNull || symbol.content === RNa) {
 		return processValue(symbol, data)
 	}
+
+	const domains = data.environment.current.memory.get(symbol.content)
+		?.filter(id => id.kind === 'function' || id.kind === 'variable' || id.kind === 'parameter' || id.kind === 'argument')
+		.map(id => (id as InGraphIdentifierDefinition).domain)
+		.filter(domain => domain !== undefined)
+		.map(domain => domain as Domain)
+	const domain = domains && unifyDomains(domains)
 
 	return {
 		unknownReferences: [ { nodeId: symbol.info.id, name: symbol.content, controlDependencies: data.controlDependencies } ],
@@ -21,9 +31,11 @@ export function processSymbol<OtherInfo>(symbol: RSymbol<OtherInfo & ParentInfor
 			tag: VertexType.Use,
 			id:  symbol.info.id,
 			controlDependencies:
-			data.controlDependencies
+			data.controlDependencies,
+			domain: domain
 		}),
 		entryPoint: symbol.info.id,
-		exitPoints: [{ nodeId: symbol.info.id, type: ExitPointType.Default, controlDependencies: data.controlDependencies }]
+		exitPoints: [{ nodeId: symbol.info.id, type: ExitPointType.Default, controlDependencies: data.controlDependencies }],
+		domain:     domain
 	}
 }

7de6f5b9349162858a64243a352f357dd0e9a7f1
Lukas Pietzschmann <lukas.pietzschmann@uni-ulm.de>
2024-06-06 17:38:27 +0200

    feat: generate domains for numbers and logicals

--- src/dataflow/internal/process/process-value.ts
+++ src/dataflow/internal/process/process-value.ts
@@ -3,8 +3,15 @@ import type { DataflowProcessorInformation } from '../../processor'
 import type { RNodeWithParent } from '../../../r-bridge/lang-4.x/ast/model/processing/decorate'
 import { DataflowGraph } from '../../graph/graph'
 import { VertexType } from '../../graph/vertex'
+import { Domain } from '../../../abstract-interpretation/domain'
+import { RType } from '../../../r-bridge/lang-4.x/ast/model/type'
 
 export function processValue<OtherInfo>(value: RNodeWithParent, data: DataflowProcessorInformation<OtherInfo>): DataflowInformation {
+	let domain: Domain | undefined
+	switch(value.type) {
+		case RType.Number: domain = Domain.fromScalar(value.content.num); break
+		case RType.Logical: domain = value.content ? Domain.top() : Domain.bottom(); break
+	}
 	return {
 		unknownReferences: [],
 		in:                [{ nodeId: value.info.id, name: undefined, controlDependencies: data.controlDependencies }],
@@ -14,9 +21,11 @@ export function processValue<OtherInfo>(value: RNodeWithParent, data: DataflowPr
 			tag:                 VertexType.Value,
 			id:                  value.info.id,
 			value:               value.lexeme,
-			controlDependencies: data.controlDependencies
+			controlDependencies: data.controlDependencies,
+			domain:              domain
 		}),
 		exitPoints: [{ nodeId: value.info.id, type: ExitPointType.Default, controlDependencies: data.controlDependencies }],
-		entryPoint: value.info.id
+		entryPoint: value.info.id,
+		domain:     domain
 	}
 }

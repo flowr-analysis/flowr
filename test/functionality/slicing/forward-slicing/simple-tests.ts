4fe6777f201fe92fc1f58bc4333a65f778d7c6a4
Ellpeck <me@ellpeck.de>
2024-06-11 12:29:24 +0200

    test: added forward slicing tests to test with

--- test/functionality/slicing/forward-slicing/simple-tests.ts
+++ test/functionality/slicing/forward-slicing/simple-tests.ts
@@ -1,7 +1,46 @@
 import { assertForwardSliced, withShell } from '../../_helper/shell'
 import { label } from '../../_helper/label'
 
-describe('Simple', withShell(shell => {
-	assertForwardSliced(label('value of variable', []), shell,
+describe('Assignments', withShell(shell => {
+	assertForwardSliced(label('simple value', []), shell,
 		'x <- 1\nx', ['1@x'], 'x <- 1\nx')
+
+	const ifElse = `
+x <- 1
+if(y) {
+   x <- 2
+} else {
+   x <- 3
+}
+print(x)`.trimStart()
+	assertForwardSliced(label('before if/else', []), shell,
+		ifElse, ['1@x'], 'x <- 1')
+	assertForwardSliced(label('within if/else', []), shell,
+		ifElse, ['3@x'], 'if(y) {\nx <- 2\n}\nprint(x)')
+
+	assertForwardSliced(label('if', []), shell,
+		`
+x <- 1
+if(y) {
+   x <- 2
+}
+print(x)`.trimStart(), ['1@x'], 'x <- 1\nprint(x)')
+
+	const complexAssign = `
+x <- 3
+y <- x * 2
+z <- y - 4
+d <- 1
+print(z)`.trimStart()
+	assertForwardSliced(label('complex assign x', []), shell,
+		complexAssign, ['1@x'], `
+x <- 3
+y <- x * 2
+z <- y - 4
+print(z)`.trimStart())
+	assertForwardSliced(label('complex assign y', []), shell,
+		complexAssign, ['2@y'], `
+y <- x * 2
+z <- y - 4
+print(z)`.trimStart())
 }))

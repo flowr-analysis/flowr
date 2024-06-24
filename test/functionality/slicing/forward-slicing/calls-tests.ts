a2ce0f75ab60ee5df90e6ac82fef81e1b92d5db0
Ellpeck <me@ellpeck.de>
2024-06-24 15:23:34 +0200

    wip: started on calls tests

---
+++ test/functionality/slicing/forward-slicing/calls-tests.ts
@@ -0,0 +1,18 @@
+import { assertForwardSliced, withShell } from '../../_helper/shell'
+import { label } from '../../_helper/label'
+import { OperatorDatabase } from '../../../../src/r-bridge/lang-4.x/ast/model/operators'
+
+describe('Calls', withShell(shell => {
+	describe('Simple Calls', () => {
+		assertForwardSliced(label('function call', ['function-definitions', 'resolve-arguments', 'formals-named', 'name-normal', 'call-normal', ...OperatorDatabase['<-'].capabilities, 'unnamed-arguments']),
+			shell, 'i <- 4\na <- function(x) { x }\na(i)', ['1@i'], 'i <- 4\na(i)')
+	})
+
+	describe('Functions using environment', () => {
+		const code = `i <- 4
+a <- function(x) { x + i }
+a(17)`
+		assertForwardSliced(label('Must include read', ['name-normal', 'resolve-arguments', 'unnamed-arguments', 'formals-named', 'implicit-return', 'call-normal', ...OperatorDatabase['<-'].capabilities, 'newlines', 'binary-operator', 'infix-calls', ...OperatorDatabase['+'].capabilities, 'numbers']),
+			shell, code, ['1@i'], code)
+	})
+}))

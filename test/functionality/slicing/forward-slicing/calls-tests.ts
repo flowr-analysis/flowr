8529934383a771ab78694f634b0a3328177f65b7
Ellpeck <me@ellpeck.de>
2024-06-26 13:21:42 +0200

    test: redefinition tests

--- test/functionality/slicing/forward-slicing/calls-tests.ts
+++ test/functionality/slicing/forward-slicing/calls-tests.ts
@@ -15,4 +15,18 @@ a(17)`
 		assertForwardSliced(label('Must include read', ['name-normal', 'resolve-arguments', 'unnamed-arguments', 'formals-named', 'implicit-return', 'call-normal', ...OperatorDatabase['<-'].capabilities, 'newlines', 'binary-operator', 'infix-calls', ...OperatorDatabase['+'].capabilities, 'numbers']),
 			shell, code, ['1@i'], code)
 	})
+
+	describe('Redefine', () => {
+		assertForwardSliced(label('Only keep first definition', ['name-normal', 'resolve-arguments', 'unnamed-arguments', 'formals-named', 'implicit-return', 'call-normal', ...OperatorDatabase['<-'].capabilities, 'newlines', 'binary-operator', 'infix-calls', ...OperatorDatabase['+'].capabilities, 'numbers']), shell, `i <- 3
+a <- function(x) { x + i }
+i <- 4
+a(5)`, ['1@i'], 'i <- 3')
+		assertForwardSliced(label('Only keep first definition and call', ['name-normal', 'resolve-arguments', 'unnamed-arguments', 'formals-named', 'implicit-return', 'call-normal', ...OperatorDatabase['<-'].capabilities, 'newlines', 'binary-operator', 'infix-calls', ...OperatorDatabase['+'].capabilities, 'numbers']), shell, `i <- 3
+a <- function(x) { x + i }
+a(5)
+i <- 4
+a(5)`, ['1@i'], `i <- 3
+a <- function(x) { x + i }
+a(5)`)
+	})
 }))

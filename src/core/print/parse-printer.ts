4efd0f17a91b11a30f6474c7bc5e4666bf37e4f8
Ellpeck <me@ellpeck.de>
2024-07-19 14:14:49 +0200

    refactor: clean up parseToQuads

--- src/core/print/parse-printer.ts
+++ src/core/print/parse-printer.ts
@@ -3,10 +3,5 @@ import { serialize2quads } from '../../util/quads'
 import { convertPreparedParsedData, prepareParsedData } from '../../r-bridge/lang-4.x/ast/parser/json/format'
 
 export function parseToQuads(code: string, config: QuadSerializationConfiguration): string{
-	const root = convertPreparedParsedData(prepareParsedData(code))
-	// recursively filter so that if the object contains one of the keys 'a', 'b' or 'c', all other keys are ignored
-	return serialize2quads(
-		root,
-		config
-	)
+	return serialize2quads(convertPreparedParsedData(prepareParsedData(code)), config)
 }

45a3a2b3d442a682baa10a5ad0dbee162065904e
Ellpeck <me@ellpeck.de>
2024-07-19 14:12:16 +0200

    refactor: this function is pointless

--- src/core/print/parse-printer.ts
+++ src/core/print/parse-printer.ts
@@ -1,17 +1,12 @@
 import type { QuadSerializationConfiguration } from '../../util/quads'
 import { serialize2quads } from '../../util/quads'
-import type { JsonEntry } from '../../r-bridge/lang-4.x/ast/parser/json/format'
 import { convertPreparedParsedData, prepareParsedData } from '../../r-bridge/lang-4.x/ast/parser/json/format'
-
-function filterObject(obj: JsonEntry): JsonEntry {
-	return obj
-}
 
 export function parseToQuads(code: string, config: QuadSerializationConfiguration): string{
 	const root = convertPreparedParsedData(prepareParsedData(code))
 	// recursively filter so that if the object contains one of the keys 'a', 'b' or 'c', all other keys are ignored
 	return serialize2quads(
-		filterObject(root),
+		root,
 		config
 	)
 }

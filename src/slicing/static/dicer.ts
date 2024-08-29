a67b08d63c1b839d797e4227faf235bd964ff874
Ehcsan <68707578+Ehcsan@users.noreply.github.com>
2024-08-29 22:21:19 +0200

    lint-fix(dicing): linter fixes

--- src/slicing/static/dicer.ts
+++ src/slicing/static/dicer.ts
@@ -1,18 +1,18 @@
-import { DataflowGraph } from "../../dataflow/graph/graph";
-import { NormalizedAst } from "../../r-bridge/lang-4.x/ast/model/processing/decorate";
-import { SlicingCriteria } from "../criterion/parse";
-import { SliceResult } from "./slicer-types";
-import { staticSlicing } from "./static-slicer";
+import type { DataflowGraph } from '../../dataflow/graph/graph'
+import type { NormalizedAst } from '../../r-bridge/lang-4.x/ast/model/processing/decorate'
+import type { SlicingCriteria } from '../criterion/parse'
+import type { SliceResult } from './slicer-types'
+import { staticSlicing } from './static-slicer'
 
 
 
 export function staticDicing(graph: DataflowGraph, ast: NormalizedAst, endCriteria: SlicingCriteria, startCriteria: SlicingCriteria, threshold = 75):  Readonly<SliceResult> {
-    const backwardsSlice = staticSlicing(graph, ast, endCriteria, threshold)
-    const forwardSlice = forwardSlicing(graph, ast, startCriteria, threshold)
+	const backwardsSlice = staticSlicing(graph, ast, endCriteria, threshold)
+	const forwardSlice = forwardSlicing(graph, ast, startCriteria, threshold)
 
-    return {timesHitThreshold: backwardsSlice.timesHitThreshold, result: backwardsSlice.result.intersection(forwardSlice.result), decodedCriteria: backwardsSlice.decodedCriteria}
+	return { timesHitThreshold: backwardsSlice.timesHitThreshold, result: backwardsSlice.result.intersection(forwardSlice.result), decodedCriteria: backwardsSlice.decodedCriteria }
 }
 
 function forwardSlicing(graph: DataflowGraph, ast: NormalizedAst, criteria: SlicingCriteria, threshold = 75): Readonly<SliceResult> {
-
+	return staticSlicing(graph, ast, criteria, threshold)
 }
--- test/functionality/slicing/dicing/simple-tests.ts
+++ test/functionality/slicing/dicing/simple-tests.ts
@@ -1,19 +1,15 @@
-import { label } from '../../_helper/label'
-import type { SupportedFlowrCapabilityId } from '../../../../src/r-bridge/data/get'
-import { OperatorDatabase } from '../../../../src/r-bridge/lang-4.x/ast/model/operators'
-import type { NodeId } from '../../../../src/r-bridge/lang-4.x/ast/model/processing/node-id'
 import { assertSliced, withShell } from '../../_helper/shell'
-import { SlicingCriteria } from '../../../../src/slicing/criterion/parse'
+import type { SlicingCriteria } from '../../../../src/slicing/criterion/parse'
 
 describe.only('Simple', withShell(shell => {
-    describe('Base Dicing Cases', () => {
-        const testcases: [{name: string, input: string, endCriterion: SlicingCriteria, startCriterion: SlicingCriteria, expected: string}] 
-        = [
-            {name: 'Simple Example', input: 'a = 3\nb = 4\nc = a + b', endCriterion: ['3@c'], startCriterion: ['1@a'], expected: 'a = 3\nc = a + b'}
-        ]
+	describe('Base Dicing Cases', () => {
+		const testcases: [{ name: string, input: string, endCriterion: SlicingCriteria, startCriterion: SlicingCriteria, expected: string }] 
+		= [
+			{ name: 'Simple Example', input: 'a = 3\nb = 4\nc = a + b', endCriterion: ['3@c'], startCriterion: ['1@a'], expected: 'a = 3\nc = a + b' }
+		]
 
-        for (const testcase of testcases) {
-            assertSliced(testcase.name, shell, testcase.input, testcase.endCriterion, testcase.expected)
-        }
-    })
+		for(const testcase of testcases) {
+			assertSliced(testcase.name, shell, testcase.input, testcase.endCriterion, testcase.expected)
+		}
+	})
 }))

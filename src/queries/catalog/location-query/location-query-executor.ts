4ac9a3c6854e5742922817a421aca24adece1ae3
Lukas Pietzschmann <lukas.pietzschmann@uni-ulm.de>
2024-10-08 16:29:58 +0200

    feat: switched from dfg to ast in order to get location information

--- src/queries/catalog/location-query/location-query-executor.ts
+++ src/queries/catalog/location-query/location-query-executor.ts
@@ -2,13 +2,12 @@ import type { BasicQueryData } from '../../query';
 import type { LocationQuery, LocationQueryResult } from './location-query-format';
 
 
-export function executeLocationQuery({ graph }: BasicQueryData, queries: readonly LocationQuery[]): LocationQueryResult {
+export function executeLocationQuery({ ast }: BasicQueryData, queries: readonly LocationQuery[]): LocationQueryResult {
 	return {
 		'.meta': {
 			/* there is no sense in measuring a get */
 			timing: 0
 		},
-		// TODO: add qrouping of locations
-		location: Object.fromEntries(queries.map(({ nodeId }) => [nodeId, graph.idMap?.get(nodeId)?.location]))
+		location: Object.fromEntries(queries.map(({ nodeId }) => [nodeId, ast.idMap.get(nodeId)?.location]))
 	};
 }

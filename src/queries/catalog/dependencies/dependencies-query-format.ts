ba12ce30321b73d9b5d160cf5689dfd1730dfa8d
Ellpeck <me@ellpeck.de>
2024-10-11 14:36:04 +0200

    refactor: const all the arrays

--- src/queries/catalog/dependencies/dependencies-query-format.ts
+++ src/queries/catalog/dependencies/dependencies-query-format.ts
@@ -2,8 +2,8 @@ import type { BaseQueryFormat, BaseQueryResult } from '../../base-query-format';
 import type { NodeId } from '../../../r-bridge/lang-4.x/ast/model/processing/node-id';
 
 // these lists are based on https://github.com/duncantl/CodeDepends/blob/7fd96dfee16b252e5f642c77a7ababf48e9326f8/R/codeTypes.R
-export const LibraryFunctions = ['library', 'require'];
-export const SourceFunctions = ['source'];
+export const LibraryFunctions = ['library', 'require'] as const;
+export const SourceFunctions = ['source'] as const;
 export const ReadFunctions = ['read.table', 'read.csv', 'read.csv2', 'read.delim', 'read.fwf', 'file', 'url', 'load', 'gzfile', 'bzfile', 'download.file', 'pipe', 'fifo', 'unz', 'data.frame', 'matrix', 'readRDS', 'readLines'] as const;
 export const WriteFunctions = ['save', 'save.image', 'write', 'dput', 'dump', 'write.table', 'write.csv', 'saveRDS', 'print', 'cat'] as const;
 

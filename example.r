5cc13b7f1e051b9d86a62117c6361b729d42aae4
Felix Schlegel <felix.schlegel@gmx.de>
2024-10-17 17:05:07 +0200

    chore: investigation

---
+++ example.r
@@ -0,0 +1,12 @@
+numbers <- c(1, 2, 3, 4, 5)
+
+person <- list(
+  name = "John",
+  age = 25,
+  isMale = TRUE,
+  grades = c(1.0, 1.3, 2.7, 4.0)
+)
+
+maximum <- max(3, 4)
+
+number <- numbers[[1]]

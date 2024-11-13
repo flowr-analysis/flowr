9879ad5f9d9a165cb18daf0fd76b4da6fa39f91f
Ellpeck <me@ellpeck.de>
2024-11-13 16:18:33 +0100

    doc: made linting and testing generated

--- src/documentation/print-linting-and-testing-wiki.ts
+++ src/documentation/print-linting-and-testing-wiki.ts
@@ -1,4 +1,10 @@
+import { setMinLevelOfAllLogs } from '../../test/functionality/_helper/log';
+import { LogLevel } from '../util/log';
+import { RShell } from '../r-bridge/shell';
+import { codeBlock } from './doc-util/doc-code';
 
+async function getText(shell: RShell) {
+	return `
 For the latest code-coverage information, see [codecov.io](https://app.codecov.io/gh/flowr-analysis/flowr), 
 for the latest benchmark results, see the [benchmark results](https://flowr-analysis.github.io/flowr/wiki/stats/benchmark) wiki page.
 
@@ -20,7 +26,7 @@ for the latest benchmark results, see the [benchmark results](https://flowr-anal
 ## Testing Suites
 
 Currently, flowR contains two testing suites: one for [functionality](#functionality-tests) and one for [performance](#performance-tests). We explain each of them in the following.
-A more general test experience is granted by `npm run checkup` which will include the construction of the docker image, the generation of the wiki pages, and the linter.
+A more general test experience is granted by \`npm run checkup\` which will include the construction of the docker image, the generation of the wiki pages, and the linter.
 
 ### Functionality Tests
 
@@ -28,42 +34,26 @@ The functionality tests represent conventional unit (and depending on your termi
 We use [vitest](https://vitest.dev/) as our testing framework.
 You can run the tests by issuing:
 
-
-```shell
-npm run test
-```
-
+${codeBlock('shell', 'npm run test')}
 
 Within the commandline,
 this should automatically drop you into a watch mode which will automatically re-run the tests if you change the code.
-If, at any time there are too many errors, you can use `--bail=<value>` to stop the tests after a certain number of errors.
+If, at any time there are too many errors, you can use \`--bail=<value>\` to stop the tests after a certain number of errors.
 For example:
 
-
-```shell
-npm run test -- --bail=1
-```
-
+${codeBlock('shell', 'npm run test -- --bail=1')}
 
 If you want to run the tests without the watch mode, you can use:
 
-
-```shell
-npm run test -- --no-watch
-```
-
+${codeBlock('shell', 'npm run test -- --no-watch')}
 
 To run all tests, including a coverage report and label summary, run: 
 
-
-```shell
-npm run test-full
-```
-
+${codeBlock('shell', 'npm run test-full')}
 
 However, depending on your local R version, your network connection and potentially other factors, some tests may be skipped automatically as they don’t apply to your current system setup 
 (or can't be tested with the current prerequisites). 
-Each test can specify such requirements as part of the `TestConfiguration`, which is then used in the `test.skipIf` function of _vitest_.
+Each test can specify such requirements as part of the \`TestConfiguration\`, which is then used in the \`test.skipIf\` function of _vitest_.
 It is up to the [ci](#ci-pipeline) to run the tests on different systems to ensure that those tests are ensured to run.
 
 #### Test Structure
@@ -72,12 +62,12 @@ All functionality tests are to be located under [test/functionality](https://git
 
 This folder contains three special and important elements:
 
-- `test-setup` which is the entry point if *all* tests are run. It should automatically disable logging statements and configure global variables (e.g., if installation tests should run).
-- `_helper` which contains helper functions to be used by other tests.
-- `test-summary` which may produce a summary of the covered capabilities.
+- \`test-setup\` which is the entry point if *all* tests are run. It should automatically disable logging statements and configure global variables (e.g., if installation tests should run).
+- \`_helper\` which contains helper functions to be used by other tests.
+- \`test-summary\` which may produce a summary of the covered capabilities.
 
-We name all tests using the `.test.ts` suffix and try to run them in parallel. 
-Whenever this is not possible (e.g., when using `withShell`), please use `describe.sequential` to disable parallel execution for the respective test.
+We name all tests using the \`.test.ts\` suffix and try to run them in parallel. 
+Whenever this is not possible (e.g., when using \`withShell\`), please use \`describe.sequential\` to disable parallel execution for the respective test.
 
 #### Writing a Test
 
@@ -87,7 +77,7 @@ and it is probably best to have a look at existing tests in that area to get an 
 #### Running Only Some Tests
 
 To run only some tests, vitest allows you to [filter](https://vitest.dev/guide/filtering.html) tests. 
-Besides, you can use the watch mode (with `npm run test`) to only run tests that are affected by your changes.
+Besides, you can use the watch mode (with \`npm run test\`) to only run tests that are affected by your changes.
 
 ### Performance Tests
 
@@ -96,11 +86,7 @@ Although we measure wall time in the CI (which is subject to rather large variat
 Furthermore, the respective scripts can be used locally as well.
 To run them, issue:
 
-
-```shell
-npm run performance-test
-```
-
+${codeBlock('shell', 'npm run performance-test')}
 
 See [test/performance](https://github.com/flowr-analysis/flowr/tree/main/test/performance) for more information on the suites, how to run them, and their results. If you are interested in the results of the benchmarks, see [here](https://flowr-analysis.github.io/flowr/wiki/stats/benchmark).
 
@@ -146,19 +132,11 @@ We explain the most important workflows in the following:
 There are two linting scripts.
 The main one:
 
-
-```shell
-npm run lint
-```
-
+${codeBlock('shell', 'npm run lint')}
 
 And a weaker version of the first (allowing for *todo* comments) which is run automatically in the [pre-push githook](../.githooks/pre-push) as explained in the [CONTRIBUTING.md](../.github/CONTRIBUTING.md):
 
-
-```shell
-npm run lint-local
-```
-
+${codeBlock('shell', 'npm run lint-local')}
 
 Besides checking coding style (as defined in the [package.json](../package.json)), the *full* linter runs the [license checker](#license-checker).
 
@@ -166,11 +144,7 @@ In case you are unaware,
 eslint can automatically fix several linting problems[](https://eslint.org/docs/latest/use/command-line-interface#fix-problems).
 So you may be fine by just running:
 
-
-```shell
-npm run lint-local -- --fix
-```
-
+${codeBlock('shell', 'npm run lint-local -- --fix')}
 
 ### Oh no, the linter fails
 
@@ -182,4 +156,17 @@ However, in case you think that the linter is wrong, please do not hesitate to o
 
 ### License Checker
 
-*flowR* is licensed under the [GPLv3 License](https://github.com/flowr-analysis/flowr/blob/main/LICENSE) requiring us to only rely on [compatible licenses](https://www.gnu.org/licenses/license-list.en.html). For now, this list is hardcoded as part of the npm [`license-compat`](../package.json) script so it can very well be that a new dependency you add causes the checker to fail &mdash; *even though it is compatible*. In that case, please either open a [new issue](https://github.com/flowr-analysis/flowr/issues/new/choose) or directly add the license to the list (including a reference to why it is compatible).
+*flowR* is licensed under the [GPLv3 License](https://github.com/flowr-analysis/flowr/blob/main/LICENSE) requiring us to only rely on [compatible licenses](https://www.gnu.org/licenses/license-list.en.html). For now, this list is hardcoded as part of the npm [\`license-compat\`](../package.json) script so it can very well be that a new dependency you add causes the checker to fail &mdash; *even though it is compatible*. In that case, please either open a [new issue](https://github.com/flowr-analysis/flowr/issues/new/choose) or directly add the license to the list (including a reference to why it is compatible).
+`;
+}
+
+if(require.main === module) {
+	setMinLevelOfAllLogs(LogLevel.Fatal);
+
+	const shell = new RShell();
+	void getText(shell).then(str => {
+		console.log(str);
+	}).finally(() => {
+		shell.close();
+	});
+}

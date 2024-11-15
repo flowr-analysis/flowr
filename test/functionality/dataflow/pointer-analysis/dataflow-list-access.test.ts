07fb1597952406ddd6cb6ee75ad1114cc2298e3f
Felix Schlegel <felix.schlegel@gmx.de>
2024-11-15 15:41:10 +0100

    test(list-access): added dataflow tests

---
+++ test/functionality/dataflow/pointer-analysis/dataflow-list-access.test.ts
@@ -0,0 +1,19 @@
+import { describe } from 'vitest';
+import { assertDataflow, withShell } from '../../_helper/shell';
+import { emptyGraph } from '../../../../src/dataflow/graph/dataflowgraph-builder';
+import { label } from '../../_helper/label';
+
+describe.sequential('List Access', withShell(shell => {
+	describe('Access named argument', () => {
+		assertDataflow(label('Assert reads edge to named argument', ['name-normal', 'function-calls', 'named-arguments', 'dollar-access', 'subsetting']), shell, `person <- list(age = 24, name = "John")
+person$name`,
+		emptyGraph()
+			.defineVariable('1@person')
+			.reads('2@person', '1@person')
+			.reads('2@$', '1@name'),
+		{
+			expectIsSubgraph:      true,
+			resolveIdsAsCriterion: true,
+		});
+	});
+}));

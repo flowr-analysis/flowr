8c1fa7e8ee622b9751f99fd590ecbfb51b20fb36
snh21 <lars.pfrenger@uni-ulm.de>
2024-11-19 11:06:46 +0100

    feat(absint): make visitor functions optional (#1153)

--- src/abstract-interpretation/normalized-ast-visitor.ts
+++ src/abstract-interpretation/normalized-ast-visitor.ts
@@ -25,31 +25,31 @@ import { RType } from '../r-bridge/lang-4.x/ast/model/type';
 import { assertUnreachable } from '../util/assert';
 
 export interface Visitor<Info = NoInfo> {
-    visitNumber(num: RNumber<Info>): void;
-	visitString(str: RString<Info>): void;
-	visitLogical(logical: RLogical<Info>): void;
-	visitSymbol(symbol: RSymbol<Info>): void;
-	visitAccess(node: RAccess<Info>): void;
-	visitBinaryOp(op: RBinaryOp<Info>): void;
-	visitPipe(op: RPipe<Info>): void;
-	visitUnaryOp(op: RUnaryOp<Info>): void;
-    visitFor(loop: RForLoop<Info>): void;
-	visitWhile(loop: RWhileLoop<Info>): void;
-	visitRepeat(loop: RRepeatLoop<Info>): void;
-	visitNext(next: RNext<Info>): void;
-	visitBreak(next: RBreak<Info>): void;
-	visitComment(comment: RComment<Info>): void;
-	visitLineDirective(comment: RLineDirective<Info>): void;
-	visitIfThenElse(ifThenExpr: RIfThenElse<Info>): void;
-	visitExprList(exprList: RExpressionList<Info>): void;
-	visitFunctionDefinition(definition: RFunctionDefinition<Info>): void;
-	visitFunctionCall(call: RFunctionCall<Info>): void;
-	visitArgument(argument: RArgument<Info>): void;
-	visitParameter(parameter: RParameter<Info>): void;
+    visitNumber?(num: RNumber<Info>): void;
+	visitString?(str: RString<Info>): void;
+	visitLogical?(logical: RLogical<Info>): void;
+	visitSymbol?(symbol: RSymbol<Info>): void;
+	visitAccess?(node: RAccess<Info>): void;
+	visitBinaryOp?(op: RBinaryOp<Info>): void;
+	visitPipe?(op: RPipe<Info>): void;
+	visitUnaryOp?(op: RUnaryOp<Info>): void;
+    visitFor?(loop: RForLoop<Info>): void;
+	visitWhile?(loop: RWhileLoop<Info>): void;
+	visitRepeat?(loop: RRepeatLoop<Info>): void;
+	visitNext?(next: RNext<Info>): void;
+	visitBreak?(next: RBreak<Info>): void;
+	visitComment?(comment: RComment<Info>): void;
+	visitLineDirective?(comment: RLineDirective<Info>): void;
+	visitIfThenElse?(ifThenExpr: RIfThenElse<Info>): void;
+	visitExprList?(exprList: RExpressionList<Info>): void;
+	visitFunctionDefinition?(definition: RFunctionDefinition<Info>): void;
+	visitFunctionCall?(call: RFunctionCall<Info>): void;
+	visitArgument?(argument: RArgument<Info>): void;
+	visitParameter?(parameter: RParameter<Info>): void;
 }
 
-// eslint-disable-next-line unused-imports/no-unused-vars
-class NormalizedAstVisitor<Info = NoInfo> {
+
+export class NormalizedAstVisitor<Info = NoInfo> {
 	private readonly root: RNode<Info>;
 
 	constructor(root: RNode<Info>) {
@@ -78,95 +78,95 @@ class NormalizedAstVisitor<Info = NoInfo> {
 		const type = node.type;
 		switch(type) {
 			case RType.FunctionCall:
-				v.visitFunctionCall(node);
+				v.visitFunctionCall?.(node);
 				this.visitSingle(node.named ? node.functionName : node.calledFunction, v, v);
 				this.visit(node.arguments, v);
 				break;
 			case RType.FunctionDefinition:
-				v.visitFunctionDefinition(node);
+				v.visitFunctionDefinition?.(node);
 				this.visit(node.parameters, v);
 				this.visitSingle(node.body, v);
 				break;
 			case RType.ExpressionList:
-				v.visitExprList(node);
+				v.visitExprList?.(node);
 				this.visit(node.grouping, v);
 				this.visit(node.children, v);
 				break;
 			case RType.ForLoop:
-				v.visitFor(node);
+				v.visitFor?.(node);
 				this.visitSingle(node.variable, v);
 				this.visitSingle(node.vector, v);
 				this.visitSingle(node.body, v);
 				break;
 			case RType.WhileLoop:
-				v.visitWhile(node);
+				v.visitWhile?.(node);
 				this.visitSingle(node.condition, v);
 				this.visitSingle(node.body, v);
 				break;
 			case RType.RepeatLoop:
-				v.visitRepeat(node);
+				v.visitRepeat?.(node);
 				this.visitSingle(node.body, v);
 				break;
 			case RType.IfThenElse:
-				v.visitIfThenElse(node);
+				v.visitIfThenElse?.(node);
 				this.visitSingle(node.condition, v);
 				this.visitSingle(node.then, v);
 				this.visit(node.otherwise, v);
 				break;
 			case RType.BinaryOp:
-				v.visitBinaryOp(node);
+				v.visitBinaryOp?.(node);
 				this.visitSingle(node.lhs, v);
 				this.visitSingle(node.rhs, v);
 				break;
 			case RType.Pipe:
-				v.visitPipe(node);
+				v.visitPipe?.(node);
 				this.visitSingle(node.lhs, v);
 				this.visitSingle(node.rhs, v);
 				break;
 			case RType.UnaryOp:
-				v.visitUnaryOp(node);
+				v.visitUnaryOp?.(node);
 				this.visitSingle(node.operand, v);
 				break;
 			case RType.Parameter:
-				v.visitParameter(node);
+				v.visitParameter?.(node);
 				this.visitSingle(node.name, v);
 				this.visit(node.defaultValue, v);
 				break;
 			case RType.Argument:
-				v.visitArgument(node);
+				v.visitArgument?.(node);
 				this.visit(node.name, v);
 				this.visit(node.value, v);
 				break;
 			case RType.Access:
-				v.visitAccess(node);
+				v.visitAccess?.(node);
 				this.visitSingle(node.accessed, v);
 				if(node.operator === '[' || node.operator === '[[') {
 					this.visit(node.access, v);
 				}
 				break;
 			case RType.Symbol:
-				v.visitSymbol(node);
+				v.visitSymbol?.(node);
 				break;
 			case RType.Logical:
-				v.visitLogical(node);
+				v.visitLogical?.(node);
 				break;
 			case RType.Number:
-				v.visitNumber(node);
+				v.visitNumber?.(node);
 				break;
 			case RType.String:
-				v.visitString(node);
+				v.visitString?.(node);
 				break;
 			case RType.Comment:
-				v.visitComment(node);
+				v.visitComment?.(node);
 				break;
 			case RType.Break:
-				v.visitBreak(node);
+				v.visitBreak?.(node);
 				break;
 			case RType.Next:
-				v.visitNext(node);
+				v.visitNext?.(node);
 				break;
 			case RType.LineDirective:
-				v.visitLineDirective(node);
+				v.visitLineDirective?.(node);
 				break;
 			default:
 				assertUnreachable(type);

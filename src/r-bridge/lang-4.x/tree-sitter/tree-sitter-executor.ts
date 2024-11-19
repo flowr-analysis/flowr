87404ff34bde5c05a334bf734790fe1bd93c5197
Ellpeck <me@ellpeck.de>
2024-11-19 16:50:43 +0100

    wip: stuff we talked about

--- src/r-bridge/lang-4.x/tree-sitter/tree-sitter-executor.ts
+++ src/r-bridge/lang-4.x/tree-sitter/tree-sitter-executor.ts
@@ -1,35 +1,32 @@
 import type { Language, Tree } from 'web-tree-sitter';
 import Parser from 'web-tree-sitter';
 import type { RParseRequest } from '../../retriever';
-import { guard } from '../../../util/assert';
 import fs from 'fs';
 
 export class TreeSitterExecutor {
 
 	private static language: Language;
-	private parser:          Parser | undefined;
+	private readonly parser: Parser;
 
-	public async init(): Promise<void> {
-		if(!TreeSitterExecutor.language) {
-			await Parser.init();
-			TreeSitterExecutor.language = await Parser.Language.load(`${__dirname}/tree-sitter-r.wasm`);
-		}
+	public static async initTreeSitter(): Promise<void> {
+		await Parser.init();
+		// TODO pass config variable to specify where the wasm is
+		TreeSitterExecutor.language = await Parser.Language.load(`${__dirname}/tree-sitter-r.wasm`);
+	}
+
+	constructor() {
 		this.parser = new Parser();
 		this.parser.setLanguage(TreeSitterExecutor.language);
 	}
 
 	public parse(request: RParseRequest): Tree {
-		guard(this.parser !== undefined, 'Parser not initialized');
-
 		let sourceCode: string;
 		if(request.request === 'file' ){
-			// TODO should we be using fs here?
 			// TODO what base path should this be based on (pass one like in RShell?)
 			sourceCode = fs.readFileSync(request.content, 'utf8');
 		} else {
 			sourceCode = request.content;
 		}
-
 		return this.parser.parse(sourceCode);
 	}
 }

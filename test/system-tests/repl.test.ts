087ea8f7d31e86cd91024f5e8d4e282c8b4b2ee6
snh21 <lars.pfrenger@uni-ulm.de>
2024-11-28 20:45:36 +0100

    ci(systets): add systests to ci (#1164)

--- test/system-tests/repl.test.ts
+++ test/system-tests/repl.test.ts
@@ -7,6 +7,16 @@ describe('repl', () => {
 		assert.include(output, 'flowchart');
 	});
 
+	test(':df x <- 3', async() => {
+		const output = await flowrRepl([':df x <- 3 ', ':quit']);
+		assert.include(output, 'flowchart');
+	});
+
+	test(':df "x <- 3\nprint(x)"', async() => {
+		const output = await flowrRepl([':df "x <- 3\\nprint(x)"', ':quit']);
+		assert.include(output, 'flowchart');
+	});
+
 	test(':slicer', async() => {
 		const output = await flowrRepl([':slicer -c "3@a" -r "a <- 3\\nb <- 4\\nprint(a)"', ':quit']);
 		assert.include(output, 'a <- 3\na');

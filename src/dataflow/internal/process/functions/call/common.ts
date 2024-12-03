1bf8e14bf7062c4adf5731f6a14b9d680224f428
Felix Schlegel <felix.schlegel@gmx.de>
2024-12-03 12:54:12 +0100

    feat(list-access): add whole list reference only to list call
    Before, an access to a list object would reference the whole object, now that's only
    when the list function is called. Single assignments are referenced directly by their
    access operator. This allows us to skip not relevant list accesses.

--- src/dataflow/internal/process/functions/call/common.ts
+++ src/dataflow/internal/process/functions/call/common.ts
@@ -9,7 +9,7 @@ import { EmptyArgument } from '../../../../../r-bridge/lang-4.x/ast/model/nodes/
 import type { DataflowGraph, FunctionArgument } from '../../../../graph/graph';
 import type { NodeId } from '../../../../../r-bridge/lang-4.x/ast/model/processing/node-id';
 import type { REnvironmentInformation } from '../../../../environments/environment';
-import type { IdentifierReference } from '../../../../environments/identifier';
+import type { IdentifierReference, InGraphIdentifierDefinition } from '../../../../environments/identifier';
 import { ReferenceType } from '../../../../environments/identifier';
 import { overwriteEnvironment } from '../../../../environments/overwrite';
 import { resolveByName } from '../../../../environments/resolve-by-name';
@@ -78,7 +78,6 @@ function forceVertexArgumentValueReferences(rootId: NodeId, value: DataflowInfor
 
 export function processAllArguments<OtherInfo>(
 	{ functionName, args, data, finalGraph, functionRootId, forceArgs = [], patchData = d => d }: ProcessAllArgumentInput<OtherInfo>,
-	isSingleIndex: boolean = false,
 ): ProcessAllArgumentResult {
 	let finalEnv = functionName.environment;
 	// arg env contains the environments with other args defined
@@ -117,6 +116,8 @@ export function processAllArguments<OtherInfo>(
 						assumeItMayHaveAHigherTarget = false;
 					}
 					// When only a single index is referenced, we don't need to reference the whole object
+					const resolvedInGraphDef = resolved as InGraphIdentifierDefinition;
+					const isSingleIndex = resolvedInGraphDef?.indicesCollection?.every((indices) => indices.isSingleIndex);
 					if(!isSingleIndex) {
 						finalGraph.addEdge(ingoing.nodeId, resolved.nodeId, EdgeType.Reads);
 					}

e38748204315c4c5708f04010b3fae3792913a2d
snh21 <lars.pfrenger@uni-ulm.de>
2024-12-23 00:08:08 +0100

    test(alias): add 2 new test cases (#1106)

--- test/functionality/slicing/static-program-slices/alias-tracking.test.ts
+++ test/functionality/slicing/static-program-slices/alias-tracking.test.ts
@@ -16,18 +16,15 @@ async function runPipeline(code: string, shell: RShell) {
 }
 
 describe.sequential('Alias Tracking', withShell(shell => {
-
 	test.each([
 		['x <- TRUE; print(x);', 'x', [true]],
 		['x <- TRUE; y <- x; print(y);', 'y', [true]],
 		['x <- 42; y <- x; print(y);', 'y', [numVal(42)]],
 		['x <- TRUE; y <- FALSE; z <- x; z <- y; print(z);', 'z', [false]],
 		['x <- TRUE; y <- FALSE; if(x) { y <- TRUE; }; print(y);', 'y', [true]],
-		[`x <- TRUE;
-while(x) {
-  if(runif(1)) 
-     x <- FALSE
-}`, 'x', [true, false]]
+		['x <- TRUE; while(x) { if(runif(1)) { x <- FALSE } }', 'x', [true, false]],
+		['k <- 4; if(u) { x <- 2; } else { x <- 3; }; y <- x; print(y);', 'y', [numVal(2), numVal(3)]],
+		['f <- function(a = u) { if(k) { u <- 1; } else { u <- 2; }; print(a); }; f();', 'a', undefined] // Note: This should result in a in [1,2] in the future
 	])('%s should resolve %s to %o', async(code, identifier, expectedValues) => {
 		const result = await runPipeline(code, shell);
 		const values = resolveToValues(identifier as Identifier, result.dataflow.environment, result.dataflow.graph);

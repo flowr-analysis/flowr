69f0ad22e2a69adc9f74f9123f193d0eda976867
Florian Sihler <florian.sihler@uni-ulm.de>
2025-01-02 22:59:29 +0100

    test(pointer-analysis): some new conditional tests

--- test/functionality/slicing/pointer-analysis/list-access.test.ts
+++ test/functionality/slicing/pointer-analysis/list-access.test.ts
@@ -3,59 +3,52 @@ import { assertSliced, withShell } from '../../_helper/shell';
 import { label } from '../../_helper/label';
 
 describe.sequential('List access', withShell(shell => {
+	const basicCapabilities = ['name-normal', 'function-calls', 'named-arguments', 'dollar-access', 'subsetting'] as const;
 	describe('Simple access', () => {
-		assertSliced(
-			label('List with single element', ['name-normal', 'function-calls', 'named-arguments', 'dollar-access', 'subsetting']), shell,
-			`person <- list(name = "John")
-result <- person$name`,
-			['2@result'],
-			`person <- list(name = "John")
-result <- person$name`,
+		assertSliced(label('List with single element', basicCapabilities), shell,
+			'person <- list(name = "John")\nprint(person$name)',
+			['2@print'],
+			'person <- list(name = "John")\nprint(person$name)',
 		);
 
-		assertSliced(
-			label('List with several elements', ['name-normal', 'function-calls', 'named-arguments', 'dollar-access', 'subsetting']), shell,
-			`person <- list(name = "John", age = 24, is_male = TRUE)
-result <- person$name`,
-			['2@result'],
-			`person <- list(name = "John", age = 24, is_male = TRUE)
-result <- person$name`,
+		/* we reconstruct everything as every other modification could mess with the correctness of the result */
+		assertSliced(label('List with several elements', basicCapabilities), shell,
+			'person <- list(name = "John", age = 24, is_male = TRUE)\nprint(person$name)',
+			['2@print'],
+			'person <- list(name = "John", age = 24, is_male = TRUE)\nprint(person$name)'
 		);
 	});
 
 	describe('Whole list access', () => {
-		assertSliced(
-			label('When each argument of a list is redefined, then original list is still in slice', ['name-normal', 'function-calls', 'named-arguments', 'dollar-access', 'subsetting']), shell,
+		assertSliced(label('When each argument of a list is redefined, then original list is still in slice', basicCapabilities), shell,
 			`person <- list(age = 24, name = "John", is_male = TRUE)
 person$is_male <- FALSE
 person$name <- "Alice"
 person$age <- 33
-result <- person`,
-			['5@result'],
+print(person)`,
+			['5@print'],
 			`person <- list(age = 24, name = "John", is_male = TRUE)
 person$is_male <- FALSE
 person$name <- "Alice"
 person$age <- 33
-result <- person`
+print(person)`
 		);
 
-		assertSliced(
-			label('When arguments are added to an empty list, then original list is in slice', ['name-normal', 'function-calls', 'named-arguments', 'dollar-access', 'subsetting']), shell,
+		assertSliced(label('When arguments are added to an empty list, then original list is in slice', basicCapabilities), shell,
 			`person <- list()
 person$is_male <- FALSE
 person$name <- "Alice"
 person$age <- 33
-result <- person`,
-			['5@result'],
+print(person)`,
+			['5@print'],
 			`person <- list()
 person$is_male <- FALSE
 person$name <- "Alice"
 person$age <- 33
-result <- person`
+print(person)`
 		);
 
-		assertSliced(
-			label('When whole list is redefined, then every list assignment before is not in slice', ['name-normal', 'function-calls', 'named-arguments', 'dollar-access', 'subsetting']), shell,
+		assertSliced(label('When whole list is redefined, then every list assignment before is not in slice', basicCapabilities), shell,
 			`person <- list(age = 24, name = "John")
 person$age <- 33
 person$name <- "Jane"
@@ -72,60 +65,55 @@ result <- person`
 	});
 
 	describe('Access with other accesses', () => {
-		assertSliced(
-			label('With other list', ['name-normal', 'function-calls', 'named-arguments', 'dollar-access', 'subsetting']), shell,
+		assertSliced(label('With other list', basicCapabilities), shell,
 			`person <- list(age = 24, name = "John")
 other_person <- list(age = 24, name = "John")
 a <- other_person$name
-result <- person$name`,
-			['4@result'],
+print(person$name)`,
+			['4@print'],
 			`person <- list(age = 24, name = "John")
-result <- person$name`,
+print(person$name)`,
 		);
 
-		assertSliced(
-			label('With other accesses', ['name-normal', 'function-calls', 'named-arguments', 'dollar-access', 'subsetting']), shell,
+		assertSliced(label('With other accesses', basicCapabilities), shell,
 			`person <- list(age = 24, name = "John", is_male = TRUE)
 a <- person$age
 b <- person$is_male
-result <- person$age`,
-			['4@result'],
+print(person$age)`,
+			['4@print'],
 			`person <- list(age = 24, name = "John", is_male = TRUE)
-result <- person$age`,
+print(person$age)`,
 		);
 	});
 
 	describe('Access with assignment', () => {
-		assertSliced(
-			label('When there is more than one assignment to the same index, then the last assignment is in the slice', ['name-normal', 'function-calls', 'named-arguments', 'dollar-access', 'subsetting']),
+		assertSliced(label('When there is more than one assignment to the same index, then the last assignment is in the slice', basicCapabilities),
 			shell,
 			`person <- list(age = 24, name = "John")
 person$name <- "Bob"
 person$name <- "Alice"
 person$name <- "Malory"
-result <- person$name`,
-			['5@result'],
+print(person$name)`,
+			['5@print'],
 			`person <- list(age = 24, name = "John")
 person$name <- "Malory"
-result <- person$name`,
+print(person$name)`,
 		);
 
-		assertSliced(
-			label('When there are assignments to the other indices, then they are not in the slice', ['name-normal', 'function-calls', 'named-arguments', 'dollar-access', 'subsetting']),
+		assertSliced(label('When there are assignments to the other indices, then they are not in the slice', basicCapabilities),
 			shell,
 			`person <- list(age = 24, name = "John", is_male = TRUE)
 person$is_male <- FALSE
 person$name <- "Alice"
 person$age <- 33
-result <- person$name`,
-			['5@result'],
+print(person$name)`,
+			['5@print'],
 			`person <- list(age = 24, name = "John", is_male = TRUE)
 person$name <- "Alice"
-result <- person$name`,
+print(person$name)`,
 		);
 
-		assertSliced(
-			label('When there are assignments to only other indices, then only list is in the slice', ['name-normal', 'function-calls', 'named-arguments', 'dollar-access', 'subsetting']),
+		assertSliced(label('When there are assignments to only other indices, then only list is in the slice', basicCapabilities),
 			shell,
 			`person <- list(age = 24, name = "John", is_male = TRUE)
 person$is_male <- FALSE
@@ -135,11 +123,61 @@ result <- person$age`,
 			`person <- list(age = 24, name = "John", is_male = TRUE)
 result <- person$age`,
 		);
+		
+		describe('Access within conditionals', () => {
+			assertSliced(label('Only a potential overwrite', basicCapabilities),
+				shell,
+				`person <- list(age = 24)
+if(u) 
+			person$age <- 33
+print(person$age)`,
+				['4@print'],
+				`person <- list(age = 24)
+if(u) person$age <- 33
+print(person$age)`)
+	assertSliced(label('Potential wipe', basicCapabilities),
+		shell,
+`person <- list(age = 24)
+if(u) {
+	person$age <- 33
+} else {
+	person$age <- 42
+	person <- list()
+}
+print(person$age)`,
+['8@print'],
+`person <- list(age = 24)
+if(u) { person$age <- 33 } else
+{ person <- list() }
+print(person$age)`)
+assertSliced(label('Potential addition in nesting', basicCapabilities),
+shell,
+`person <- list(age = 24)
+if(u) person$name <- "peter"
+wrapper <- list(person = person)
+print(wrapper$person$name)`,
+['4@print'],
+`person <- list(age = 24)
+if(u) person$name <- "peter"
+wrapper <- list(person = person)
+print(wrapper$person$name)`)
+it.fails('Currently we can not handle the indirect passing minimally and include the name line', () => {
+	assertSliced(label('Potential addition in nesting (not needed)', basicCapabilities),
+	shell,
+	`person <- list(age = 24)
+	if(u) person$name <- "peter"
+	wrapper <- list(person = person)
+	print(wrapper$person$age)`,
+	['4@print'],
+	`person <- list(age = 24)
+	wrapper <- list(person = person)
+	print(wrapper$person$age)`)
+})
+			})
 	});
-
+	
 	describe('Nested lists', () => {
-		assertSliced(
-			label('When index of nested list is overwritten, then overwrite is also in slice', ['name-normal', 'function-calls', 'named-arguments', 'dollar-access', 'subsetting']),
+		assertSliced(label('When index of nested list is overwritten, then overwrite is also in slice', basicCapabilities),
 			shell,
 			`grades <- list(algebra = 1.3, german = 2.0, english = 2.3, sports = 1.7)
 grades$algebra <- 1.0
@@ -155,8 +193,7 @@ person <- list(age = 24, name = "John", height = 164, is_male = FALSE, grades = 
 result <- person$grades$algebra`,
 		);
 
-		assertSliced(
-			label('When index of nested list is overwritten after nesting, then overwrite is also in slice', ['name-normal', 'function-calls', 'named-arguments', 'dollar-access', 'subsetting']),
+		assertSliced(label('When index of nested list is overwritten after nesting, then overwrite is also in slice', basicCapabilities),
 			shell,
 			`grades <- list(algebra = 1.3, german = 2.0, english = 2.3, sports = 1.7)
 grades$algebra <- 1.0
@@ -174,8 +211,7 @@ person$grades$algebra <- 4.0
 result <- person$grades$algebra`,
 		);
 
-		assertSliced(
-			label('When nested list is overwritten, then only overwrite list is in slice', ['name-normal', 'function-calls', 'named-arguments', 'dollar-access', 'subsetting']),
+		assertSliced(label('When nested list is overwritten, then only overwrite list is in slice', basicCapabilities),
 			shell,
 			`grades <- list(algebra = 1.3, german = 2.0, english = 2.3, sports = 1.7)
 grades$algebra <- 1.0
@@ -193,8 +229,7 @@ person <- list(age = 24, name = "John", height = 164, is_male = FALSE, grades = 
 result <- person$grades$music`,
 		);
 
-		assertSliced(
-			label('When nested list is overwritten after nesting, then only overwrite list is in slice', ['name-normal', 'function-calls', 'named-arguments', 'dollar-access', 'subsetting']),
+		assertSliced(label('When nested list is overwritten after nesting, then only overwrite list is in slice', basicCapabilities),
 			shell,
 			`grades <- list(algebra = 1.3, german = 2.0, english = 2.3, sports = 1.7)
 grades$algebra <- 1.0
@@ -211,8 +246,7 @@ person$grades <- list(arts = 4.0, music = 3.0)
 result <- person$grades$music`,
 		);
 
-		assertSliced(
-			label('When nested list is accessed, then accesses to nested list are in slice', ['name-normal', 'function-calls', 'named-arguments', 'dollar-access', 'subsetting']),
+		assertSliced(label('When nested list is accessed, then accesses to nested list are in slice', basicCapabilities),
 			shell,
 			`grades <- list(algebra = 1.3, sports = 1.7)
 grades$algebra <- 1.0
@@ -229,8 +263,7 @@ person <- list(name = "John", grades = grades)
 result <- person$grades`,
 		);
 
-		assertSliced(
-			label('When nested list is accessed, then accesses to nested lists are in slice', ['name-normal', 'function-calls', 'named-arguments', 'dollar-access', 'subsetting']),
+		assertSliced(label('When nested list is accessed, then accesses to nested lists are in slice', basicCapabilities),
 			shell,
 			`algebra_grades <- list(test = 1.0, exam = 3.0)
 algebra_grades$test <- 4.0
@@ -248,8 +281,7 @@ person <- list(name = "John", grades = grades)
 result <- person$grades`,
 		);
 
-		assertSliced(
-			label('When double nested list is accessed, then accesses to nested lists are in slice', ['name-normal', 'function-calls', 'named-arguments', 'dollar-access', 'subsetting']),
+		assertSliced(label('When double nested list is accessed, then accesses to nested lists are in slice', basicCapabilities),
 			shell,
 			`algebra_grades <- list(test = 1.0, exam = 3.0)
 algebra_grades$test <- 4.0
@@ -267,8 +299,7 @@ result <- person$grades$algebra`,
 		);
 
 		it.fails('When list is assigned, then accesses to list and nested lists are in slice', () => {
-			assertSliced(
-				label('When list is assigned, then accesses to list and nested lists are in slice', ['name-normal', 'function-calls', 'named-arguments', 'dollar-access', 'subsetting']),
+			assertSliced(label('When list is assigned, then accesses to list and nested lists are in slice', basicCapabilities),
 				shell,
 				`algebra_grades <- list(test = 1.0, exam = 3.0)
 algebra_grades$test <- 4.0
@@ -289,8 +320,7 @@ result <- person`,
 		});
 
 		it.fails('When nested list is redefined twice, then only second redefinition is in slice', () => {
-			assertSliced(
-				label('When nested list is redefined twice, then only second redefinition is in slice', ['name-normal', 'function-calls', 'named-arguments', 'dollar-access', 'subsetting']),
+			assertSliced(label('When nested list is redefined twice, then only second redefinition is in slice', basicCapabilities),
 				shell,
 				`grades <- list(algebra = 1.3, german = 2.0, english = 2.3, sports = 1.7)
 grades$algebra <- 1.0
@@ -312,8 +342,7 @@ result <- person$grades$music`,
 		});
 
 		it.fails('When nested list is redefined with static value, then only static value assignment is in slice', () => {
-			assertSliced(
-				label('When nested list is redefined with static value, then only static value assignment is in slice', ['name-normal', 'function-calls', 'named-arguments', 'dollar-access', 'subsetting']),
+			assertSliced(label('When nested list is redefined with static value, then only static value assignment is in slice', basicCapabilities),
 				shell,
 				`grades <- list(algebra = 1.3, german = 2.0, english = 2.3, sports = 1.7)
 grades$algebra <- 1.0
@@ -333,8 +362,7 @@ result <- person$grades`,
 		});
 
 		it.fails('When static list value is redefined with list, then only list value assignment is in slice', () => {
-			assertSliced(
-				label('When static list value is redefined with list, then only list value assignment is in slice', ['name-normal', 'function-calls', 'named-arguments', 'dollar-access', 'subsetting']),
+			assertSliced(label('When static list value is redefined with list, then only list value assignment is in slice', basicCapabilities),
 				shell,
 				`grades <- list(algebra = 1.3, german = 2.0, english = 2.3, sports = 1.7)
 grades$algebra <- 1.0

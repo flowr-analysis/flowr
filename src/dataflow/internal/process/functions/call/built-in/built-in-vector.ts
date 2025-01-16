0a15e96098e51234cef04aa2c1c1ff88861946b3
Felix Schlegel <felix.schlegel@gmx.de>
2025-01-16 14:41:49 +0100

    feat(vector-def): add flattened indices for nested vectors
    For an expression `c(1, 2, c(3, 4))` the nested vector would be flattened so that it equals `c(1, 2, 3, 4)`.
    For this to work, we need to fetch the indices for the nested vector and add them to the existing indices.
    In a case where there are values after a nested index we need to rewrite the indices so that they are in correct order.
    E.g. for `c(1, c(2, 3), 4)` the argument with value '4' has initially the index 3, after it has been flattened,
    it has to be 4. This is done by merging both lists with new indices similar to the merging part of MergeSort.

--- src/dataflow/internal/process/functions/call/built-in/built-in-vector.ts
+++ src/dataflow/internal/process/functions/call/built-in/built-in-vector.ts
@@ -7,8 +7,14 @@ import { RType } from '../../../../../../r-bridge/lang-4.x/ast/model/type';
 import type { ContainerIndices, ContainerIndex } from '../../../../../graph/vertex';
 import type { DataflowInformation } from '../../../../../info';
 import type { DataflowProcessorInformation } from '../../../../../processor';
+import type { ProcessKnownFunctionCallResult } from '../known-call-handling';
 import { processKnownFunctionCall } from '../known-call-handling';
 import { getConfig } from '../../../../../../config';
+
+interface UnresolvedArg {
+	id:    NodeId,
+	index: number,
+}
 
 /**
  * Process a vector call.
@@ -28,13 +34,22 @@ export function processVector<OtherInfo>(
 		return processKnownFunctionCall({ name, args, rootId, data }).information;
 	}
 
-	const vectorArguments: ContainerIndex[] = [];
-	for(let i = 0; i < args.length; i++) {
-		const arg = args[i];
+	const vectorArgs: ContainerIndex[] = [];
+	const unresolvedArgs: UnresolvedArg[] = [];
+	for(const arg of args) {
+		// Skip invalid argument types
+		if(arg === EmptyArgument || arg.type !== RType.Argument) {
+			continue;
+		}
 		
-		// Skip invalid arguments
-		if(arg === EmptyArgument || arg.type !== RType.Argument
-			|| arg.name !== undefined || arg.value?.type !== RType.Number) {
+		// Skip but store non number arguments
+		if(arg.value?.type !== RType.Number) {
+			if(arg.value) {
+				unresolvedArgs.push({
+					index: arg.info.index,
+					id:    arg.value.info.id
+				});
+			}
 			continue;
 		}
 
@@ -43,12 +58,66 @@ export function processVector<OtherInfo>(
 			nodeId:     arg.value.info.id,
 		};
 
-		vectorArguments.push(newIndex);
+		vectorArgs.push(newIndex);
 	}
 
 	const indices: ContainerIndices = {
-		indices:     vectorArguments,
+		indices:     vectorArgs,
 		isContainer: true,
 	};
-	return processKnownFunctionCall({ name, args, rootId, data }, [indices]).information;
+
+	const fnCall = processKnownFunctionCall({ name, args, rootId, data }, [indices]);
+
+	const vertex = fnCall.information.graph.getVertex(rootId);
+
+	if(!vertex || unresolvedArgs.length == 0) {
+		return fnCall.information;
+	}
+
+	let vectorArgsIndex = 0;
+	let unresolvedArgsIndex = 0;
+	const newIndices: ContainerIndex[] = [];
+	let newIndex = 1;
+	function addToIndices(index: ContainerIndex) {
+		newIndices.push({
+			...index,
+			identifier: {
+				index: newIndex++,
+			}
+		});
+	}
+
+	while(vectorArgsIndex < vectorArgs.length && unresolvedArgsIndex < unresolvedArgs.length) {
+		const vectorArg = vectorArgs[vectorArgsIndex];
+		const unresolvedArg = unresolvedArgs[unresolvedArgsIndex];
+
+		if(unresolvedArg.index < (vectorArg.identifier.index ?? Number.MAX_VALUE)) {
+			getVertexIndices(fnCall, unresolvedArg).map(addToIndices);
+			unresolvedArgsIndex++;
+		} else {
+			addToIndices(vectorArg);
+			vectorArgsIndex++;
+		}
+	}
+
+	// Add rests lists (one of both is empty)
+	for(let i = vectorArgsIndex; i < vectorArgs.length; i++) {
+		addToIndices(vectorArgs[i]);
+	}
+	for(let i = unresolvedArgsIndex; i < unresolvedArgs.length; i++) {
+		getVertexIndices(fnCall, unresolvedArgs[i]).map(addToIndices);
+	}
+
+	const resolvedIndices: ContainerIndices = {
+		indices:     newIndices,
+		isContainer: true,
+	};
+	vertex.indicesCollection = [ resolvedIndices ];
+
+	return fnCall.information;
+}
+
+function getVertexIndices(fnCall: ProcessKnownFunctionCallResult, arg: UnresolvedArg): ContainerIndex[] {
+	const argVertex = fnCall.information.graph.getVertex(arg.id);
+	return argVertex?.indicesCollection?.flatMap(collection => collection.indices) ?? [];
 }

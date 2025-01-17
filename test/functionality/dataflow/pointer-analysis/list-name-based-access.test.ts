7964fe989da1204d76206b83f08274cdf6326ebe
Felix Schlegel <felix.schlegel@gmx.de>
2025-01-17 16:46:58 +0100

    refactor: rename pointer analysis test files
    - removed 'dataflow' prefix, because the tests are in the `test/functionality/dataflow` directory, which makes the prefix obsolete
    - added the type of access to the test files: list-access -> list-name-based-access to differentiate other types of access

--- test/functionality/dataflow/pointer-analysis/list-name-based-access.test.ts
+++ test/functionality/dataflow/pointer-analysis/list-name-based-access.test.ts
@@ -5,15 +5,19 @@ import { label } from '../../_helper/label';
 import { amendConfig, defaultConfigOptions } from '../../../../src/config';
 
 describe.sequential('List Access', withShell(shell => {
+	const basicCapabilities = ['name-normal', 'function-calls', 'named-arguments', 'dollar-access', 'subsetting-multiple'] as const;
+
 	beforeAll(() => {
 		amendConfig({ solver: { ...defaultConfigOptions.solver, pointerTracking: true } });
 	});
+
 	afterAll(() => {
 		amendConfig({ solver: { ...defaultConfigOptions.solver, pointerTracking: false } });
 	});
+
 	describe('Access named argument', () => {
 		assertDataflow(
-			label('Assert reads edge to named argument', ['name-normal', 'function-calls', 'named-arguments', 'dollar-access', 'subsetting-multiple']),
+			label('Assert reads edge to named argument', basicCapabilities),
 			shell,
 			`person <- list(age = 24, name = "John")
 person$name`,

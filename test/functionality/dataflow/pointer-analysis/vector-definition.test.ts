9963f74cf5679501286d5830053e2bf380b73b77
Felix Schlegel <felix.schlegel@gmx.de>
2025-01-20 15:10:28 +0100

    test(container-def): add tests for passing nested container as variable

--- test/functionality/dataflow/pointer-analysis/vector-definition.test.ts
+++ test/functionality/dataflow/pointer-analysis/vector-definition.test.ts
@@ -112,6 +112,18 @@ describe.sequential('Vector Definition', withShell(shell => {
 				{ identifier: { index: 4 }, nodeId: 10, },
 			]
 		);
+
+		assertContainerIndicesDefinition(
+			label('When vector has nested vector passed as variable, then index has sub-indices', basicCapabilities),
+			shell,
+			`a <- c(1, 2)
+			c(a)`,
+			Q.criterion('2@c'),
+			[
+				{ identifier: { index: 1 }, nodeId: 2, },
+				{ identifier: { index: 2 }, nodeId: 4, },
+			]
+		);
 	});
 
 	describe('Nested lists with named arguments', () => {
@@ -131,7 +143,7 @@ describe.sequential('Vector Definition', withShell(shell => {
 		);
 	
 		assertContainerIndicesDefinition(
-			label('When vector has nested vector in the middle, then indices are flattened and correctly defined', capabilities),
+			label('When vector has nested list in the middle, then indices are flattened and correctly defined', capabilities),
 			shell,
 			'c(1, list(b = 2, c = 3), 4)',
 			Q.criterion('1@c'),
@@ -144,7 +156,7 @@ describe.sequential('Vector Definition', withShell(shell => {
 		);
 	
 		assertContainerIndicesDefinition(
-			label('When vector ends with nested vector, then indices are flattened and correctly defined', capabilities),
+			label('When vector ends with nested list, then indices are flattened and correctly defined', capabilities),
 			shell,
 			'c(1, 2, list(c = 3, d = 4))',
 			Q.criterion('1@c'),
@@ -157,7 +169,7 @@ describe.sequential('Vector Definition', withShell(shell => {
 		);
 
 		assertContainerIndicesDefinition(
-			label('When vector consists of nested vectors, then indices are flattened and correctly defined', capabilities),
+			label('When vector consists of nested lists, then indices are flattened and correctly defined', capabilities),
 			shell,
 			'c(list(a = 1, b = 2), list(c = 3, d = 4))',
 			Q.criterion('1@c'),
@@ -166,6 +178,18 @@ describe.sequential('Vector Definition', withShell(shell => {
 				{ identifier: { index: 2 }, nodeId: 7, },
 				{ identifier: { index: 3 }, nodeId: 13, },
 				{ identifier: { index: 4 }, nodeId: 16, },
+			]
+		);
+
+		assertContainerIndicesDefinition(
+			label('When vector has nested list passed as variable, then index has sub-indices', capabilities),
+			shell,
+			`a <- list(a = 1, b = 2)
+			c(a)`,
+			Q.criterion('2@c'),
+			[
+				{ identifier: { index: 1 }, nodeId: 4, },
+				{ identifier: { index: 2 }, nodeId: 7, },
 			]
 		);
 	});
@@ -185,7 +209,7 @@ describe.sequential('Vector Definition', withShell(shell => {
 		);
 	
 		assertContainerIndicesDefinition(
-			label('When vector has nested vector in the middle, then indices are flattened and correctly defined', basicCapabilities),
+			label('When vector has nested list in the middle, then indices are flattened and correctly defined', basicCapabilities),
 			shell,
 			'c(1, list(2, 3), 4)',
 			Q.criterion('1@c'),
@@ -198,7 +222,7 @@ describe.sequential('Vector Definition', withShell(shell => {
 		);
 	
 		assertContainerIndicesDefinition(
-			label('When vector ends with nested vector, then indices are flattened and correctly defined', basicCapabilities),
+			label('When vector ends with nested list, then indices are flattened and correctly defined', basicCapabilities),
 			shell,
 			'c(1, 2, list(3, 4))',
 			Q.criterion('1@c'),
@@ -211,7 +235,7 @@ describe.sequential('Vector Definition', withShell(shell => {
 		);
 
 		assertContainerIndicesDefinition(
-			label('When vector consists of nested vectors, then indices are flattened and correctly defined', basicCapabilities),
+			label('When vector consists of nested lists, then indices are flattened and correctly defined', basicCapabilities),
 			shell,
 			'c(list(1, 2), list(3, 4))',
 			Q.criterion('1@c'),
@@ -220,6 +244,18 @@ describe.sequential('Vector Definition', withShell(shell => {
 				{ identifier: { index: 2 }, nodeId: 4, },
 				{ identifier: { index: 3 }, nodeId: 9, },
 				{ identifier: { index: 4 }, nodeId: 11, },
+			]
+		);
+
+		assertContainerIndicesDefinition(
+			label('When vector has nested list passed as variable, then index has sub-indices', basicCapabilities),
+			shell,
+			`a <- list(1, 2)
+			c(a)`,
+			Q.criterion('2@c'),
+			[
+				{ identifier: { index: 1 }, nodeId: 2, },
+				{ identifier: { index: 2 }, nodeId: 4, },
 			]
 		);
 	});

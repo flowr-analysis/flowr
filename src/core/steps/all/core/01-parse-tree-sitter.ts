73f9f16598a8695ba4cf20b7768c601792b58738
Florian Sihler <florian.sihler@uni-ulm.de>
2025-02-05 08:08:01 +0100

    refactor: provide meta information within tree-sitter (#1289)

--- src/core/steps/all/core/01-parse-tree-sitter.ts
+++ src/core/steps/all/core/01-parse-tree-sitter.ts
@@ -1,12 +1,21 @@
 import { type IPipelineStep, PipelineStepStage } from '../../pipeline-step';
 import { internalPrinter, StepOutputFormat } from '../../../print/print';
 import type { DeepReadonly } from 'ts-essentials';
-import type { Tree } from 'web-tree-sitter';
+import type { SyntaxNode, Tree } from 'web-tree-sitter';
 import type { ParseRequiredInput } from '../../../../r-bridge/parser';
 import { parseRequests } from '../../../../r-bridge/parser';
+import { TreeSitterType } from '../../../../r-bridge/lang-4.x/tree-sitter/tree-sitter-types';
 
 export interface ParseStepOutputTS {
     readonly parsed: Tree
+}
+
+export interface TreeSitterParseJson {
+	readonly '.meta': {
+		readonly tokenCount:           number,
+		readonly tokenCountNoComments: number
+	},
+	readonly str: string
 }
 
 export const PARSE_WITH_TREE_SITTER_STEP = {
@@ -17,7 +26,23 @@ export const PARSE_WITH_TREE_SITTER_STEP = {
 	executed:          PipelineStepStage.OncePerFile,
 	printer:           {
 		[StepOutputFormat.Internal]: internalPrinter,
-		[StepOutputFormat.Json]:     JSON.stringify
+		[StepOutputFormat.Json]:     ({ parsed }) => {
+			const countChildren = function(node: SyntaxNode, ignoreComments: boolean): number {
+				let ret = node.type === TreeSitterType.Comment && ignoreComments ? 0 : 1;
+				for(const child of node.children) {
+					ret += countChildren(child, ignoreComments);
+				}
+				return ret;
+			};
+			const out: TreeSitterParseJson = {
+				'.meta': {
+					tokenCount:           countChildren(parsed.rootNode, false),
+					tokenCountNoComments: countChildren(parsed.rootNode, true)
+				},
+				str: parsed.rootNode.toString()
+			};
+			return JSON.stringify(out);
+		}
 	},
 	dependencies:  [],
 	requiredInput: undefined as unknown as ParseRequiredInput<Tree>

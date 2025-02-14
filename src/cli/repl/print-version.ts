e0a9e05690277759514d68149180dc9fc3e3fb2b
Florian Sihler <florian.sihler@uni-ulm.de>
2025-02-14 19:12:39 +0100

    refactor: provide tree-sitter lang version

--- src/cli/repl/print-version.ts
+++ src/cli/repl/print-version.ts
@@ -4,7 +4,8 @@ import type { KnownParser } from '../../r-bridge/parser';
 export async function versionReplString(parser: KnownParser): Promise<string> {
 	const version = await retrieveVersionInformation(parser);
 	const rVersion = version.r === 'none' ? '' : version.r === 'unknown' ? ', R version unknown' : `, R v${version.r}`;
-	return `flowR repl using flowR v${version.flowr}${rVersion} (${version.engine} engine)`;
+	const treeSitterVer = parser.name === 'tree-sitter' ? `, R grammar v${parser.treeSitterVersion()}` : '';
+	return `flowR repl using flowR v${version.flowr}${rVersion}${treeSitterVer} (${version.engine} engine)`;
 }
 export async function printVersionRepl(parser: KnownParser): Promise<void> {
 	console.log(await versionReplString(parser));

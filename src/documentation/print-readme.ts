70438f38299d498c9b1e6fc5944c7c2d6f0d76cb
Florian Sihler <florian.sihler@uni-ulm.de>
2025-02-16 18:28:14 +0100

    feat: us `contrib.rocks` (closes #1250)

--- src/documentation/print-readme.ts
+++ src/documentation/print-readme.ts
@@ -23,7 +23,6 @@ async function getText(shell: RShell) {
 [![Docker Image Version (latest semver)](https://img.shields.io/docker/v/eagleoutice/flowr?logo=docker&logoColor=white&label=dockerhub)](${FlowrDockerRef})
 [![latest tag](https://badgen.net/github/tag/flowr-analysis/flowr?label=latest&color=purple)](${FlowrGithubBaseRef}/flowr/releases/latest)
 [![Marketplace](https://badgen.net/vs-marketplace/v/code-inspect.vscode-flowr)](https://marketplace.visualstudio.com/items?itemName=code-inspect.vscode-flowr)
-[![All Contributors](https://img.shields.io/github/all-contributors/flowr-analysis/flowr)](#contributors)
 [![DOI](https://zenodo.org/badge/624819038.svg)](https://zenodo.org/doi/10.5281/zenodo.13319290)
 
 _flowR_ is a static [dataflow analyzer](https://en.wikipedia.org/wiki/Data-flow_analysis) for the [_R_](https://www.r-project.org/) programming language (tested for versions \`4.x\` and \`3.6.x\`)
@@ -63,39 +62,9 @@ We welcome every contribution! Please check out the [contributing guidelines](${
 
 ### Contributors
 
-<!-- ALL-CONTRIBUTORS-LIST:START - Do not remove or modify this section -->
-<!-- prettier-ignore-start -->
-<!-- markdownlint-disable -->
-<table>
-  <tbody>
-    <tr>
-      <td align="center" valign="top" width="14.28%"><a href="https://github.com/EagleoutIce"><img src="https://avatars.githubusercontent.com/u/9303573?v=4?s=100" width="100px;" alt="Florian Sihler"/><br /><sub><b>Florian Sihler</b></sub></a><br /><a href="${FlowrGithubBaseRef}/flowr/commits?author=EagleoutIce" title="Code">💻</a> <a href="#ideas-EagleoutIce" title="Ideas, Planning, & Feedback">🤔</a> <a href="#maintenance-EagleoutIce" title="Maintenance">🚧</a> <a href="#projectManagement-EagleoutIce" title="Project Management">📆</a> <a href="#research-EagleoutIce" title="Research">🔬</a> <a href="${FlowrGithubBaseRef}/flowr/commits?author=EagleoutIce" title="Tests">⚠️</a> <a href="#talk-EagleoutIce" title="Talks">📢</a></td>
-      <td align="center" valign="top" width="14.28%"><a href="https://ellpeck.de/"><img src="https://avatars.githubusercontent.com/u/5741138?v=4?s=100" width="100px;" alt="Ell"/><br /><sub><b>Ell</b></sub></a><br /><a href="${FlowrGithubBaseRef}/flowr/commits?author=Ellpeck" title="Code">💻</a> <a href="#maintenance-Ellpeck" title="Maintenance">🚧</a> <a href="${FlowrGithubBaseRef}/flowr/commits?author=Ellpeck" title="Tests">⚠️</a> <a href="#plugin-Ellpeck" title="Plugin/utility libraries">🔌</a></td>
-      <td align="center" valign="top" width="14.28%"><a href="https://github.com/gigalasr"><img src="https://avatars.githubusercontent.com/u/25102989?v=4?s=100" width="100px;" alt="Lars"/><br /><sub><b>Lars</b></sub></a><br /><a href="${FlowrGithubBaseRef}/flowr/commits?author=gigalasr" title="Code">💻</a> <a href="${FlowrGithubBaseRef}/flowr/commits?author=gigalasr" title="Tests">⚠️</a></td>
-      <td align="center" valign="top" width="14.28%"><a href="https://lukas.pietzschmann.org/"><img src="https://avatars.githubusercontent.com/u/49213919?v=4?s=100" width="100px;" alt="Lukas Pietzschmann"/><br /><sub><b>Lukas Pietzschmann</b></sub></a><br /><a href="${FlowrGithubBaseRef}/flowr/commits?author=LukasPietzschmann" title="Code">💻</a> <a href="${FlowrGithubBaseRef}/flowr/commits?author=LukasPietzschmann" title="Tests">⚠️</a></td>
-      <td align="center" valign="top" width="14.28%"><a href="https://github.com/bjthehun"><img src="https://avatars.githubusercontent.com/u/38729215?v=4?s=100" width="100px;" alt="Benedikt Jutz"/><br /><sub><b>Benedikt Jutz</b></sub></a><br /><a href="${FlowrGithubBaseRef}/flowr/commits?author=bjthehun" title="Code">💻</a> <a href="${FlowrGithubBaseRef}/flowr/commits?author=bjthehun" title="Tests">⚠️</a></td>
-      <td align="center" valign="top" width="14.28%"><a href="https://github.com/Core5563"><img src="https://avatars.githubusercontent.com/u/140061253?v=4?s=100" width="100px;" alt="Core5563"/><br /><sub><b>Core5563</b></sub></a><br /><a href="${FlowrGithubBaseRef}/flowr/commits?author=Core5563" title="Code">💻</a> <a href="${FlowrGithubBaseRef}/flowr/commits?author=Core5563" title="Tests">⚠️</a></td>
-      <td align="center" valign="top" width="14.28%"><a href="https://github.com/Ehcsan"><img src="https://avatars.githubusercontent.com/u/68707578?v=4?s=100" width="100px;" alt="Ehcsan"/><br /><sub><b>Ehcsan</b></sub></a><br /><a href="${FlowrGithubBaseRef}/flowr/commits?author=Ehcsan" title="Code">💻</a> <a href="${FlowrGithubBaseRef}/flowr/commits?author=Ehcsan" title="Tests">⚠️</a></td>
-    </tr>
-    <tr>
-      <td align="center" valign="top" width="14.28%"><a href="https://github.com/Slartibartfass2"><img src="https://avatars.githubusercontent.com/u/40503329?v=4?s=100" width="100px;" alt="Felix Schlegel"/><br /><sub><b>Felix Schlegel</b></sub></a><br /><a href="${FlowrGithubBaseRef}/flowr/commits?author=Slartibartfass2" title="Code">💻</a> <a href="${FlowrGithubBaseRef}/flowr/commits?author=Slartibartfass2" title="Tests">⚠️</a></td>
-    </tr>
-  </tbody>
-  <tfoot>
-    <tr>
-      <td align="center" size="13px" colspan="7">
-        <img src="https://raw.githubusercontent.com/all-contributors/all-contributors-cli/1b8533af435da9854653492b1327a23a4dbd0a10/assets/logo-small.svg">
-          <a href="https://all-contributors.js.org/docs/en/bot/usage">Add your contributions</a>
-        </img>
-      </td>
-    </tr>
-  </tfoot>
-</table>
-
-<!-- markdownlint-restore -->
-<!-- prettier-ignore-end -->
-
-<!-- ALL-CONTRIBUTORS-LIST:END -->
+<a href="https://github.com/flowr-analysis/flowr/graphs/contributors">
+  <img src="https://contrib.rocks/image?repo=flowr-analysis/flowr"  alt="flowR Contributors"/>
+</a>
 
 ----
 

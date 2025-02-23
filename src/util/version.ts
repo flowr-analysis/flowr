134165d65eec93627ee529fedce05d459c92c275
GitHub Action <action@github.com>
2025-02-23 08:03:20 +0000

    [skip ci] Release v2.2.10

--- src/util/version.ts
+++ src/util/version.ts
@@ -1,7 +1,7 @@
 import { SemVer } from 'semver';
 
 // this is automatically replaced with the current version by release-it
-const version = '2.2.9';
+const version = '2.2.10';
 
 export function flowrVersion(): SemVer {
 	return new SemVer(version);

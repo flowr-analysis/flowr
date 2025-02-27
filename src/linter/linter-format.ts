bc8d7a79ec5c5d4e4cfc978087d17b9c49793e6d
Ellpeck <me@ellpeck.de>
2025-02-27 14:30:03 +0100

    refactor: type safety for createSearch and processSearchResult

--- src/linter/linter-format.ts
+++ src/linter/linter-format.ts
@@ -1,15 +1,16 @@
 import type { FlowrSearchLike } from '../search/flowr-search-builder';
-import type { FlowrSearchElements } from '../search/flowr-search';
+import type { FlowrSearchElement, FlowrSearchElements } from '../search/flowr-search';
 import type { NoInfo } from '../r-bridge/lang-4.x/ast/model/model';
 import type { MergeableRecord } from '../util/objects';
 import type { R1_DEPRECATED_FUNCTIONS } from './rules/1-deprecated-functions';
+import type { GeneratorNames } from '../search/search-executor/search-generators';
+import type { TransformerNames } from '../search/search-executor/search-transformer';
 
-export interface LintingRule<Result extends LintingResult, Config extends MergeableRecord = never, Info = NoInfo> {
+export interface LintingRule<Result extends LintingResult, Config extends MergeableRecord = never, Info = NoInfo, Elements extends FlowrSearchElement<Info>[] = FlowrSearchElement<Info>[]> {
 	readonly name:                string
-	readonly createSearch:        (config: Config) => FlowrSearchLike<Info>
-	// between these two, there's a chance for the search for multiple rules to be combined or optimized or smth maybe? who knows
-	// TODO i think the cool type safety stuff is lost between these two, what do we do about that
-	readonly processSearchResult: (elements: FlowrSearchElements<Info>, config: Config) => Result[]
+	readonly createSearch:        (config: Config) => FlowrSearchLike<Info, GeneratorNames, TransformerNames[], FlowrSearchElements<Info, Elements>>
+	// between these two, there's a chance for the search for multiple rules to be combined or optimized maybe
+	readonly processSearchResult: (elements: FlowrSearchElements<Info, Elements>, config: Config) => Result[]
 	readonly printers:            { [O in LintingPrintStyle]?: (result: Result, config: Config) => string }
 }
 

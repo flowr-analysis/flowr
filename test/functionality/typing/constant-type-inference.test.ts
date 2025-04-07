8072fe6d1de0ae207508ec9eaf3a0f37ccf83be2
Lukas Rohde <lukas.rohde@silupe.de>
2025-04-07 17:52:27 +0200

    test(typing): add tests for unsupported expression types

--- test/functionality/typing/constant-type-inference.test.ts
+++ test/functionality/typing/constant-type-inference.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, test } from 'vitest';
 import { retrieveNormalizedAst, withShell } from '../_helper/shell';
 import { RDataType, TypeInferencer } from '../../../src/typing/type-inferencer';
 
-describe.sequential('Infer types for R expressions', withShell(shell => {
+describe.sequential('Infer types for currently supported R expressions', withShell(shell => {
 	const inferencer = new TypeInferencer();
 	
 	describe.each([
@@ -14,6 +14,20 @@ describe.sequential('Infer types for R expressions', withShell(shell => {
 			const ast = await retrieveNormalizedAst(shell, input).then(promise => promise.ast);
 			const inferredType = inferencer.fold(ast);
 			expect(inferredType).toBe(expectedType);
+		});
+	});
+
+	describe('Infer no type information for currently unsupported R expressions', () => {
+		test('Infer no type information for binary operations', async() => {
+			const ast = await retrieveNormalizedAst(shell, '1 + 2').then(promise => promise.ast);
+			const inferredType = inferencer.fold(ast);
+			expect(inferredType).toBe(undefined);
+		});
+
+		test('Infer no type information for function calls', async() => {
+			const ast = await retrieveNormalizedAst(shell, 'print("foo")').then(promise => promise.ast);
+			const inferredType = inferencer.fold(ast);
+			expect(inferredType).toBe(undefined);
 		});
 	});
 }));

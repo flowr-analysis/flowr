e53e00327467e6e428c736ce768c3e98c868070f
Lukas Rohde <lukas.rohde@silupe.de>
2025-04-07 17:42:34 +0200

    feat(typing): add preliminary support for expression lists and `NULL`

--- src/typing/type-inferencer.ts
+++ src/typing/type-inferencer.ts
@@ -1,4 +1,5 @@
 import { DefaultNormalizedAstFold } from '../abstract-interpretation/normalized-ast-fold';
+import type { RExpressionList } from '../r-bridge/lang-4.x/ast/model/nodes/r-expression-list';
 import type { RLogical } from '../r-bridge/lang-4.x/ast/model/nodes/r-logical';
 import type { RNumber } from '../r-bridge/lang-4.x/ast/model/nodes/r-number';
 import type { RString } from '../r-bridge/lang-4.x/ast/model/nodes/r-string';
@@ -6,19 +7,21 @@ import type { RString } from '../r-bridge/lang-4.x/ast/model/nodes/r-string';
 export enum RDataType {
     Logical = 'LogicalType',
     Numeric = 'NumericType',
-    String = 'StringType',
+    String  = 'StringType',
+	Null    = 'NullType',
 }
 
-export type TypingInfo = RDataType | undefined;
+export type TypingInfo = RDataType
+					   | undefined; // Represents missing type information
 
 export class TypeInferencer<Info> extends DefaultNormalizedAstFold<TypingInfo, Info> {
 	constructor() {
 		super(undefined);
 	}
 
-	protected override concat(a: TypingInfo, b: TypingInfo): TypingInfo {
+	protected override concat(_a: TypingInfo, _b: TypingInfo): TypingInfo {
 		// Do not combine typing information for now
-		return b;
+		return undefined;
 	}
 
 	override foldRLogical(_node: RLogical<Info>): TypingInfo {
@@ -31,5 +34,14 @@ export class TypeInferencer<Info> extends DefaultNormalizedAstFold<TypingInfo, I
 
 	override foldRString(_node: RString<Info>): TypingInfo {
 		return RDataType.String;
+	}
+
+	override foldRExpressionList(exprList: RExpressionList<Info>): TypingInfo {
+		if(exprList.children.length === 0) {
+			return RDataType.Null;
+		} else {
+			// Do not handle early returns for now
+			return this.fold(exprList.children[exprList.children.length - 1]);
+		}
 	}
 }

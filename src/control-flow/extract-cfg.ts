27d0322fa9afa9204d66f4a57fe9894c43741c6e
Florian Sihler <florian.sihler@uni-ulm.de>
2025-04-12 20:14:45 +0200

    feat(cfg): condition marker for `if`

--- src/control-flow/extract-cfg.ts
+++ src/control-flow/extract-cfg.ts
@@ -109,6 +109,7 @@ function identifyMayStatementType(node: RNodeWithParent) {
 function cfgIfThenElse(ifNode: RNodeWithParent, condition: ControlFlowInformation, then: ControlFlowInformation, otherwise: ControlFlowInformation | undefined): ControlFlowInformation {
 	const graph = new ControlFlowGraph();
 	graph.addVertex({ id: ifNode.info.id, type: identifyMayStatementType(ifNode) });
+	graph.addVertex({ id: ifNode.info.id + '-condition', kind: 'condition', type: CfgVertexType.MidMarker, root: ifNode.info.id });
 	graph.addVertex({ id: ifNode.info.id + '-exit', type: CfgVertexType.EndMarker, root: ifNode.info.id });
 	graph.merge(condition.graph);
 	graph.merge(then.graph);
@@ -117,13 +118,16 @@ function cfgIfThenElse(ifNode: RNodeWithParent, condition: ControlFlowInformatio
 	}
 
 	for(const exitPoint of condition.exitPoints) {
-		for(const entryPoint of then.entryPoints) {
-			graph.addEdge(entryPoint, exitPoint, { label: CfgEdgeType.Cd, when: RTrue, caused: ifNode.info.id });
-		}
-		for(const entryPoint of otherwise?.entryPoints ?? []) {
-			graph.addEdge(entryPoint, exitPoint, { label: CfgEdgeType.Cd, when: RFalse, caused: ifNode.info.id });
-		}
+		graph.addEdge(ifNode.info.id + '-condition', exitPoint, { label: CfgEdgeType.Fd });
 	}
+
+	for(const entryPoint of then.entryPoints) {
+		graph.addEdge(entryPoint, ifNode.info.id + '-condition', { label: CfgEdgeType.Cd, when: RTrue, caused: ifNode.info.id });
+	}
+	for(const entryPoint of otherwise?.entryPoints ?? []) {
+		graph.addEdge(entryPoint, ifNode.info.id + '-condition', { label: CfgEdgeType.Cd, when: RFalse, caused: ifNode.info.id });
+	}
+
 	for(const entryPoint of condition.entryPoints) {
 		graph.addEdge(entryPoint, ifNode.info.id, { label: CfgEdgeType.Fd });
 	}
@@ -132,9 +136,7 @@ function cfgIfThenElse(ifNode: RNodeWithParent, condition: ControlFlowInformatio
 		graph.addEdge(ifNode.info.id + '-exit', exit, { label: CfgEdgeType.Fd });
 	}
 	if(!otherwise) {
-		for(const exitPoint of condition.exitPoints) {
-			graph.addEdge(ifNode.info.id + '-exit', exitPoint, { label: CfgEdgeType.Cd, when: RFalse, caused: ifNode.info.id });
-		}
+		graph.addEdge(ifNode.info.id + '-exit', ifNode.info.id + '-condition', { label: CfgEdgeType.Cd, when: RFalse, caused: ifNode.info.id });
 	}
 
 	return {

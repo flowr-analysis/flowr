bef4fd95305b6249ef449b94e97a1bf7979e13e6
Ellpeck <me@ellpeck.de>
2025-05-01 12:25:25 +0200

    feat: added setup:dev script

--- src/documentation/print-onboarding-wiki.ts
+++ src/documentation/print-onboarding-wiki.ts
@@ -12,19 +12,22 @@ To get started developing on *flowR*, we recommend carefully reading the followi
 - 💖 [Contributing guidelines](${FlowrGithubBaseRef}/flowr/tree/main/.github/CONTRIBUTING.md). This page also includes information about how to set up **git-lfs** and several **git hooks**.
 
 ## ⌛ TL;DR
-The most important steps to get the *flowR* development environment set up (after installing **R** and **Node.js**) are:
+
+The most important steps to get the *flowR* development environment set up (after installing **R** and **Node.js**) can be seen below. For convenience, they can be executed all at once using the following command:
+${codeBlock('shell', 'npm run setup:dev')}
+
 ${codeBlock('shell', `
-# Installing git-lfs (if you haven't already)
+# Installing git-lfs for your current user (if you haven't already)
 git lfs install
 # Cloning the repository
 git clone https://github.com/flowr-analysis/flowr.git
+# Installing dependencies
+npm ci
 # Configuring git hooks
 git config --local core.hooksPath .githooks/
 # Test if the git hooks are working correctly
 # Running this command should lint the code
 git push --dry-run
-# Installing dependencies
-npm ci
 `)}
     `.trim();
 }

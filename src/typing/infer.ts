2427695a856fdd968db46dc3056f341660e5325a
Lukas Rohde <lukas.rohde@silupe.de>
2025-05-15 12:09:37 +0200

    feat(typing): add support for variables

--- src/typing/infer.ts
+++ src/typing/infer.ts
@@ -1,6 +1,6 @@
 import { extractCFG } from '../control-flow/extract-cfg';
 import { SemanticCfgGuidedVisitor } from '../control-flow/semantic-cfg-guided-visitor';
-import type { DataflowGraphVertexFunctionCall, DataflowGraphVertexValue } from '../dataflow/graph/vertex';
+import type { DataflowGraphVertexFunctionCall, DataflowGraphVertexUse, DataflowGraphVertexValue } from '../dataflow/graph/vertex';
 import type { DataflowInformation } from '../dataflow/info';
 import { EmptyArgument } from '../r-bridge/lang-4.x/ast/model/nodes/r-function-call';
 import type { RLogical } from '../r-bridge/lang-4.x/ast/model/nodes/r-logical';
@@ -12,6 +12,9 @@ import type { RDataType } from './types';
 import { RTypeVariable , RComplexType, RDoubleType, RIntegerType, RLogicalType, RStringType, resolveType, RNullType } from './types';
 import type { RExpressionList } from '../r-bridge/lang-4.x/ast/model/nodes/r-expression-list';
 import { RType } from '../r-bridge/lang-4.x/ast/model/type';
+import { guard } from '../util/assert';
+import { OriginType } from '../dataflow/origin/dfg-get-origin';
+import type { NodeId } from '../r-bridge/lang-4.x/ast/model/processing/node-id';
 
 export function inferDataTypes<Info extends { typeVariable?: undefined }>(ast: NormalizedAst<Info>, dataFlowInfo: DataflowInformation): NormalizedAst<Info & DataTypeInfo> {
 	const astWithTypeVars = decorateTypeVariables(ast);
@@ -64,6 +67,34 @@ class TypeInferingCfgGuidedVisitor extends SemanticCfgGuidedVisitor<UnresolvedTy
 
 	override onStringConstant(_vertex: DataflowGraphVertexValue, node: RString<UnresolvedTypeInfo>): void {
 		node.info.typeVariable.unify(new RStringType());
+	}
+
+	override onVariableUse(vertex: DataflowGraphVertexUse): void {
+		const node = this.getNormalizedAst(vertex.id);
+		guard(node !== undefined && node.type === RType.Symbol, 'Expected Symbol node');
+		const origins = this.getOrigins(vertex.id) ?? [];
+		const readOrigins = origins.filter((origin) => origin.type === OriginType.ReadVariableOrigin);
+		if(readOrigins.length === 0) {
+			node.info.typeVariable.unify(new RNullType());
+			return;
+		}
+		for(const readOrigin of readOrigins) {
+			const readNode = this.getNormalizedAst(readOrigin.id);
+			guard(readNode !== undefined, 'Expected read node');
+			node.info.typeVariable.unify(readNode.info.typeVariable);
+		}
+	}
+
+	override onAssignmentCall(data: { call: DataflowGraphVertexFunctionCall; target?: NodeId; source?: NodeId; }): void {
+		if(data.target === undefined || data.source === undefined) {
+			return;
+		}
+		const variableNode = this.getNormalizedAst(data.target);
+		const valueNode = this.getNormalizedAst(data.source);
+		const assignmentNode = this.getNormalizedAst(data.call.id);
+		guard(variableNode !== undefined && valueNode !== undefined && assignmentNode !== undefined);
+		variableNode.info.typeVariable.unify(valueNode.info.typeVariable);
+		assignmentNode.info.typeVariable.unify(variableNode.info.typeVariable);
 	}
 
 	override onProgram(node: RExpressionList<UnresolvedTypeInfo>) {

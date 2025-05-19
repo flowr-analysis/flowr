85ffe8b20f1c98cb0cb8e1a60f1b06c4fe79dc55
Ellpeck <me@ellpeck.de>
2025-05-19 14:24:32 +0200

    feat-fix: fix up linter and origin queries

--- src/queries/catalog/linter-query/linter-query-format.ts
+++ src/queries/catalog/linter-query/linter-query-format.ts
@@ -1,7 +1,7 @@
 import type { BaseQueryFormat, BaseQueryResult } from '../../base-query-format';
 import type { QueryResults, SupportedQuery } from '../../query';
-import { bold } from '../../../util/ansi';
-import { printAsMs } from '../../../util/time';
+import { bold } from '../../../util/text/ansi';
+import { printAsMs } from '../../../util/text/time';
 import Joi from 'joi';
 import { executeLinterQuery } from './linter-query-executor';
 import type { LintingRuleNames, LintingRuleResult } from '../../../linter/linter-rules';

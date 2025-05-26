24a4e33e3ff0f1b1c512d9022de0c642b0ab24d9
Lukas Rohde <lukas.rohde@silupe.de>
2025-05-26 15:34:31 +0200

    test(typing): add tests for currently inferrable builtin functions

---
+++ test/functionality/typing/builtin-function-type-inference.test.ts
@@ -0,0 +1,30 @@
+import { describe } from 'vitest';
+import { RDataTypeTag } from '../../../src/typing/types';
+import { assertInferredType, assertInferredTypes } from '../_helper/typing/assert-inferred-type';
+import { Q } from '../../../src/search/flowr-search-builder';
+
+describe('Infer types for builtin functions', () => {
+	assertInferredType('rm(x)', { tag: RDataTypeTag.Null as const });
+
+	assertInferredTypes(
+		'x <- 42\nget("x")',
+		{ query: Q.criterion('1@x').build(), expectedType: { tag: RDataTypeTag.Integer as const } },
+		{ query: Q.criterion('2@get').build(), expectedType: { tag: RDataTypeTag.Integer as const } },
+		{ query: Q.criterion('2@"x"').build(), expectedType: { tag: RDataTypeTag.String as const } }
+	);
+
+	assertInferredTypes(
+		'eval(quote(TRUE))',
+		{ query: Q.criterion('1@eval').build(), expectedType: { tag: RDataTypeTag.Any as const } },
+		{ query: Q.criterion('1@quote').build(), expectedType: { tag: RDataTypeTag.Language as const } },
+		{ query: Q.criterion('1@TRUE').build(), expectedType: { tag: RDataTypeTag.Logical as const } }
+	);
+	
+	assertInferredType('list(1, 2, 3)', { tag: RDataTypeTag.List as const });
+
+	assertInferredTypes(
+		'c("Hello", "Flo", "!")',
+		{ query: Q.criterion('1@c').build(), expectedType: { tag: RDataTypeTag.String as const } },
+		{ query: Q.criterion('1@"Hello"').build(), expectedType: { tag: RDataTypeTag.String as const } }
+	);
+});

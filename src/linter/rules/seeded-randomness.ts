021021a9c20681e3793b4fd79b03b17d22501943
Ellpeck <me@ellpeck.de>
2025-06-13 14:46:42 +0200

    feat: seeded-randomness rule now detects manual writes to .Random.seed

--- src/linter/rules/seeded-randomness.ts
+++ src/linter/rules/seeded-randomness.ts
@@ -7,6 +7,11 @@ import { formatRange } from '../../util/mermaid/dfg';
 import { Enrichment, enrichmentContent } from '../../search/search-executor/search-enrichers';
 import type { Identifier } from '../../dataflow/environments/identifier';
 import { FlowrFilter } from '../../search/flowr-search-filters';
+import { DefaultBuiltinConfig } from '../../dataflow/environments/default-builtin-config';
+import type { DataflowGraphVertexFunctionCall } from '../../dataflow/graph/vertex';
+import { getReferenceOfArgument } from '../../dataflow/graph/graph';
+import { isNotUndefined } from '../../util/assert';
+import { CascadeAction } from '../../queries/catalog/call-context-query/cascade-action';
 
 export interface SeededRandomnessResult extends LintingResult {
 	function: string
@@ -14,15 +19,17 @@ export interface SeededRandomnessResult extends LintingResult {
 }
 
 export interface SeededRandomnessConfig extends MergeableRecord {
-    randomnessProducers: string[]
+    randomnessProducers: {type: 'function' | 'variable', name: string }[]
     randomnessConsumers: string[]
 }
 
 export interface SeededRandomnessMeta extends MergeableRecord {
-	consumerCalls:      number
-	callsWithProducers: number
+	consumerCalls:                number
+	callsWithFunctionProducers:   number
+	callsWithAssignmentProducers: number
 }
 
+const assignments = new Set<Identifier>(DefaultBuiltinConfig.filter(b => b.type === 'function' && b.processor == 'builtin:assignment').flatMap(b => b.names));
 export const SEEDED_RANDOMNESS = {
 	createSearch: (config) => Q.all()
 		.with(Enrichment.CallTargets, { onlyBuiltin: true })
@@ -33,11 +40,15 @@ export const SEEDED_RANDOMNESS = {
 				test:       new RegExp(`"(${config.randomnessConsumers.join('|')})"`)
 			}
 		})
-		.with(Enrichment.LastCall, config.randomnessProducers.map(f => ({ callName: f }))),
-	processSearchResult: (elements) => {
+		.with(Enrichment.LastCall,[
+			...config.randomnessProducers.filter(p => p.type === 'function').map(p => ({ callName: p.name })),
+			...[...assignments].map(a => ({ callName: a, cascadeIf: () => CascadeAction.Continue }))
+		]),
+	processSearchResult: (elements, config, { dataflow }) => {
 		const metadata: SeededRandomnessMeta = {
-			consumerCalls:      0,
-			callsWithProducers: 0
+			consumerCalls:                0,
+			callsWithFunctionProducers:   0,
+			callsWithAssignmentProducers: 0
 		};
 		return {
 			results: elements.getElements()
@@ -52,11 +63,25 @@ export const SEEDED_RANDOMNESS = {
 				}))
 				// filter by calls that aren't preceded by a randomness producer
 				.filter(element => {
-					const producers = enrichmentContent(element.searchElement, Enrichment.LastCall).linkedIds;
-					if(producers.length > 0/* && producers.some(p => (p.node as RFunctionCall).arguments.all(a => resolveToConstants()))*/){
-						metadata.callsWithProducers++;
+					const producers = enrichmentContent(element.searchElement, Enrichment.LastCall).linkedIds
+						.map(e => dataflow.graph.getVertex(e.node.info.id) as DataflowGraphVertexFunctionCall);
+					const { assignment, func } = Object.groupBy(producers, f => assignments.has(f.name) ? 'assignment' : 'func');
+
+					// function calls are already taken care of through the LastCall enrichment itself
+					if(func?.length) {
+						metadata.callsWithFunctionProducers++;
 						return false;
 					}
+
+					// assignments have to be queried for their variable
+					const assignmentDestinations = assignment
+						?.map(a => getReferenceOfArgument(a.args[0])).filter(isNotUndefined)
+						.map(i => dataflow.graph.idMap?.get(i)).filter(isNotUndefined);
+					if(assignmentDestinations?.some(d => config.randomnessProducers.some(p => p.type === 'variable' && p.name === d.lexeme))) {
+						metadata.callsWithAssignmentProducers++;
+						return false;
+					}
+
 					return true;
 				})
 
@@ -69,7 +94,7 @@ export const SEEDED_RANDOMNESS = {
 		};
 	},
 	defaultConfig: {
-		randomnessProducers: ['set.seed'],
+		randomnessProducers: [{ type: 'function', name: 'set.seed' }, { type: 'variable', name: '.Random.seed' }],
 		randomnessConsumers: ['jitter', 'sample', 'sample.int', 'arima.sim', 'kmeans', 'princomp', 'rcauchy', 'rchisq', 'rexp', 'rgamma', 'rgeom', 'rlnorm', 'rlogis', 'rmultinom', 'rnbinom', 'rnorm', 'rpois', 'runif', 'pointLabel', 'some', 'rbernoulli',
 		],
 	},
--- test/functionality/linter/lint-seeded-randomness.test.ts
+++ test/functionality/linter/lint-seeded-randomness.test.ts
@@ -7,35 +7,46 @@ import { LintingCertainty } from '../../../src/linter/linter-format';
 
 describe('flowR linter', withTreeSitter(parser => {
 	describe('R3 seeded randomness', () => {
-		assertLinter('none', parser, 'cat("hello")', 'seeded-randomness', [], { consumerCalls: 0, callsWithProducers: 0 });
+		assertLinter('none', parser, 'cat("hello")', 'seeded-randomness', [], { consumerCalls: 0, callsWithFunctionProducers: 0, callsWithAssignmentProducers: 0 });
 		assertLinter('simple no producer', parser, 'runif(1)', 'seeded-randomness',
-			[{ range: [1,1,1,8], function: 'runif', certainty: LintingCertainty.Definitely }], { consumerCalls: 1, callsWithProducers: 0 });
-		assertLinter('simple no consumer', parser, 'set.seed(17)', 'seeded-randomness', [], { consumerCalls: 0, callsWithProducers: 0 });
-		assertLinter('simple both', parser, 'set.seed(17)\nrunif(1)', 'seeded-randomness', [], { consumerCalls: 1, callsWithProducers: 1 });
+			[{ range: [1,1,1,8], function: 'runif', certainty: LintingCertainty.Definitely }], { consumerCalls: 1, callsWithFunctionProducers: 0, callsWithAssignmentProducers: 0 });
+		assertLinter('simple no consumer', parser, 'set.seed(17)', 'seeded-randomness', [], { consumerCalls: 0, callsWithFunctionProducers: 0, callsWithAssignmentProducers: 0 });
+		assertLinter('simple both', parser, 'set.seed(17)\nrunif(1)', 'seeded-randomness', [], { consumerCalls: 1, callsWithFunctionProducers: 1, callsWithAssignmentProducers: 0 });
 		assertLinter('simple after', parser, 'runif(1)\nset.seed(17)', 'seeded-randomness',
-			[{ range: [1,1,1,8], function: 'runif', certainty: LintingCertainty.Definitely }], { consumerCalls: 1, callsWithProducers: 0 });
+			[{ range: [1,1,1,8], function: 'runif', certainty: LintingCertainty.Definitely }], { consumerCalls: 1, callsWithFunctionProducers: 0, callsWithAssignmentProducers: 0 });
 
-		assertLinter('loop valid', parser, 'for(i in 1:10) { set.seed(17); runif(1); }', 'seeded-randomness', [], { consumerCalls: 1, callsWithProducers: 1 });
+		assertLinter('loop valid', parser, 'for(i in 1:10) { set.seed(17); runif(1); }', 'seeded-randomness', [], { consumerCalls: 1, callsWithFunctionProducers: 1, callsWithAssignmentProducers: 0 });
 		// TODO why does this one pass?
-		assertLinter('loop invalid', parser, 'for(i in 1:10) { runif(1); set.seed(17); }', 'seeded-randomness', [], { consumerCalls: 1, callsWithProducers: 1 });
+		assertLinter('loop invalid', parser, 'for(i in 1:10) { runif(1); set.seed(17); }', 'seeded-randomness', [], { consumerCalls: 1, callsWithFunctionProducers: 1, callsWithAssignmentProducers: 0 });
 
 		assertLinter('condition', parser, 'if(FALSE) { set.seed(17); }\nrunif(1);', 'seeded-randomness',
-			[{ range: [2,1,2,8], function: 'runif', certainty: LintingCertainty.Definitely }], { consumerCalls: 1, callsWithProducers: 0 });
-		assertLinter('condition true', parser, 'if(TRUE) { set.seed(17); }\nrunif(1);', 'seeded-randomness', [], { consumerCalls: 1, callsWithProducers: 1 });
+			[{ range: [2,1,2,8], function: 'runif', certainty: LintingCertainty.Definitely }], { consumerCalls: 1, callsWithFunctionProducers: 0, callsWithAssignmentProducers: 0 });
+		assertLinter('condition true', parser, 'if(TRUE) { set.seed(17); }\nrunif(1);', 'seeded-randomness', [], { consumerCalls: 1, callsWithFunctionProducers: 1, callsWithAssignmentProducers: 0 });
 
-		assertLinter('non-constant seed', parser, 'num<-1 + 7;\nset.seed(num);\nrunif(1);', 'seeded-randomness', [], { consumerCalls: 1, callsWithProducers: 1 });
+		assertLinter('non-constant seed', parser, 'num<-1 + 7;\nset.seed(num);\nrunif(1);', 'seeded-randomness', [], { consumerCalls: 1, callsWithFunctionProducers: 1 , callsWithAssignmentProducers: 0 });
 		// TODO why does this one pass? -> check if seed arguments are constant using resolve value!
-		assertLinter('random seed', parser, 'set.seed(runif(1));\nrunif(1);', 'seeded-randomness', [], { consumerCalls: 2, callsWithProducers: 2 });
+		assertLinter('random seed', parser, 'set.seed(runif(1));\nrunif(1);', 'seeded-randomness', [], { consumerCalls: 2, callsWithFunctionProducers: 2, callsWithAssignmentProducers: 0 });
 
-		assertLinter('multiple seeds', parser, 'set.seed(1);\nset.seed(2);\nrunif(1);', 'seeded-randomness', [], { consumerCalls: 1, callsWithProducers: 1 });
-		assertLinter('multiple consumers', parser, 'set.seed(1);\nset.seed(2);\nrunif(1);\nrunif(1);', 'seeded-randomness', [], { consumerCalls: 2, callsWithProducers: 2 });
+		assertLinter('multiple seeds', parser, 'set.seed(1);\nset.seed(2);\nrunif(1);', 'seeded-randomness', [], { consumerCalls: 1, callsWithFunctionProducers: 1, callsWithAssignmentProducers: 0 });
+		assertLinter('multiple consumers', parser, 'set.seed(1);\nset.seed(2);\nrunif(1);\nrunif(1);', 'seeded-randomness', [], { consumerCalls: 2, callsWithFunctionProducers: 2, callsWithAssignmentProducers: 0 });
 
 		assertLinter('custom set.seed', parser, 'set.seed <- function(x) {}\nset.seed(17)\nrunif(1)', 'seeded-randomness',
-			[{ range: [3,1,3,8], function: 'runif', certainty: LintingCertainty.Definitely }], { consumerCalls: 1, callsWithProducers: 0 });
+			[{ range: [3,1,3,8], function: 'runif', certainty: LintingCertainty.Definitely }], { consumerCalls: 1, callsWithFunctionProducers: 0, callsWithAssignmentProducers: 0 });
+		// TODO this quote one shouldn't fail!
+		//assertLinter('set.seed in quote', parser, 'quote(runif(1))', 'seeded-randomness', []);
 
-		// 7. set.seed in einer quote Funktion
-		// 8. Manually assigning .Random.seed
-		// 9. verwenden der seed funktionen nested in den argumenten von anderen
+		assertLinter('set .Random.seed', parser, '.Random.seed <- 17\nrunif(1)', 'seeded-randomness', [],
+			{ consumerCalls: 1, callsWithAssignmentProducers: 1, callsWithFunctionProducers: 0 });
+		assertLinter('set .Random.seed override <-', parser, '`<-`<-function(){}\n.Random.seed <- 17\nrunif(1)', 'seeded-randomness',
+			[{ range: [3,1,3,8], function: 'runif', certainty: LintingCertainty.Definitely }],
+			{ consumerCalls: 1, callsWithAssignmentProducers: 0, callsWithFunctionProducers: 0 });
+
+		assertLinter('set in function call', parser, 'runif(set.seed(17))', 'seeded-randomness',
+			[{ range: [1,1,1,19], function: 'runif', certainty: LintingCertainty.Definitely }]);
+		assertLinter('get in function call', parser, 'runif(runif(1))', 'seeded-randomness', [
+			{ range: [1,7,1,14], function: 'runif', certainty: LintingCertainty.Definitely },
+			{ range: [1,1,1,15], function: 'runif', certainty: LintingCertainty.Definitely }]);
+
 		// 10. Using RNGversion to set versions
 		// 11. zumindest dokumentieren wo das nicht geht: parallel, scoping, withr::with_seed, future with seed
 		// 12. Maybe also warn if set.seed is used without a kind? (das wäre ne separate regel, aber ich hab gerade rasugefudnen, dass die den default manchmal ändern)

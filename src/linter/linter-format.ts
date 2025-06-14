a413f9da75b99049b051977dd734d12973873286
Florian Sihler <florian.sihler@uni-ulm.de>
2025-06-14 22:20:23 +0200

    Revert "wip: flo has to go"
    This reverts commit 595615ee929e7217d7b63adc79a3dc17a43709b4.

--- src/linter/linter-format.ts
+++ src/linter/linter-format.ts
@@ -6,98 +6,43 @@ import type { TransformerNames } from '../search/search-executor/search-transfor
 import type { NormalizedAst, ParentInformation } from '../r-bridge/lang-4.x/ast/model/processing/decorate';
 import type { LintingRuleConfig, LintingRuleMetadata, LintingRuleNames, LintingRuleResult } from './linter-rules';
 import type { DataflowInformation } from '../dataflow/info';
-import type { DeepPartial, DeepReadonly } from 'ts-essentials';
-import type { UniqueArray } from '../util/collections/arrays';
-import type { LintingRuleTag } from './linter-tags';
-
-
-export class LintingRuleInformation<Config extends MergeableRecord = never> {
-	private readonly defaultConfig: NoInfer<DeepReadonly<Config>>;
-	private readonly tags:          UniqueArray<LintingRuleTag>;
-	private readonly description:   string;
-
-	constructor(
-		config: NoInfer<DeepReadonly<Config>>,
-		tags: UniqueArray<LintingRuleTag>,
-		description: string
-	) {
-		this.defaultConfig = config;
-		this.tags = tags;
-		this.description = description;
-	}
-
-	/**
-	 * The default config for this linting rule.
-	 * This config is combined with the user config when executing the rule.
-	 */
-	public getDefaultConfig(): NoInfer<DeepReadonly<Config>> {
-		return this.defaultConfig;
-	}
-
-	/**
-	 * A short list of tags that describe and categorize the linting rule.
-	 */
-	public getTags(): UniqueArray<LintingRuleTag> {
-		return this.tags;
-	}
-
-	/**
-	 * A short description of the linting rule.
-	 * This is used to display the rule in the UI and to provide a brief overview of what the rule does.
-	 */
-	public describe(): string {
-		return this.description;
-	}
-}
+import type { DeepPartial } from 'ts-essentials';
 
 /**
- * The base class for a linting rule, which contains all of its relevant settings.
+ * The base interface for a linting rule, which contains all of its relevant settings.
  * The registry of valid linting rules is stored in {@link LintingRules}.
  */
-export abstract class LintingRule<
-	Result extends LintingResult,
-	Metadata extends MergeableRecord,
-	Config extends MergeableRecord = never,
-	Info = ParentInformation, Elements extends FlowrSearchElement<Info>[] = FlowrSearchElement<Info>[]
-> {
-	/**
-	 * The information attached to this linting rule.
-	 */
-	public readonly info: LintingRuleInformation<Config>;
-
-	protected constructor(info: LintingRuleInformation<Config>) {
-		this.info = info;
-	}
-
+export interface LintingRule<Result extends LintingResult, Metadata extends MergeableRecord, Config extends MergeableRecord = never, Info = ParentInformation, Elements extends FlowrSearchElement<Info>[] = FlowrSearchElement<Info>[]> {
 	/**
 	 * Creates a flowR search that will then be executed and whose results will be passed to {@link processSearchResult}.
 	 * In the future, additional optimizations and transformations may be applied to the search between this function and {@link processSearchResult}.
 	 */
-	public abstract createSearch(config: Config, data: { normalize: NormalizedAst, dataflow: DataflowInformation }): FlowrSearchLike<Info, GeneratorNames, TransformerNames[], FlowrSearchElements<Info, Elements>>;
+	readonly createSearch:        (config: Config, data: { normalize: NormalizedAst, dataflow: DataflowInformation }) => FlowrSearchLike<Info, GeneratorNames, TransformerNames[], FlowrSearchElements<Info, Elements>>
 	/**
 	 * Processes the search results of the search created through {@link createSearch}.
 	 * This function is expected to return the linting results from this rule for the given search, ie usually the given script file.
 	 */
-	public abstract processSearchResult(elements: FlowrSearchElements<Info, Elements>, config: Config, data: { normalize: NormalizedAst, dataflow: DataflowInformation }): {
+	readonly processSearchResult: (elements: FlowrSearchElements<Info, Elements>, config: Config, data: { normalize: NormalizedAst, dataflow: DataflowInformation }) => {
 		results: Result[],
 		'.meta': Metadata
-	};
+	}
+	/**
+	 * A function used to pretty-print the given linting result.
+	 * By default, the {@link LintingResult#certainty} is automatically printed alongside this information.
+	 */
+	readonly prettyPrint:   (result: Result, metadata: Metadata) => string
+	/**
+	 * The default config for this linting rule.
+	 * The default config is combined with the user config when executing the rule.
+	 */
+	readonly defaultConfig: NoInfer<Config>
 }
 
 /**
  * A linting result for a single linting rule match.
  */
-export abstract class LintingResult {
-	public readonly certainty: LintingCertainty;
-	/**
-	 * A function used to pretty-print the given linting result.
-	 * By default, the {@link LintingResult#certainty} is automatically printed alongside this information.
-	 */
-	public abstract prettyPrint(): string;
-
-	constructor(certainty: LintingCertainty) {
-		this.certainty = certainty;
-	}
+export interface LintingResult {
+	readonly certainty: LintingCertainty
 }
 
 export interface ConfiguredLintingRule<Name extends LintingRuleNames = LintingRuleNames> {

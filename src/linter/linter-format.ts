1742fbf965c1f3da9b92330481fca7f55aac978e
Florian Sihler <florian.sihler@uni-ulm.de>
2025-06-14 22:20:23 +0200

    Revert "refactor: flo dislikes"
    This reverts commit 96ed0e469888fbf5dacae5bf4f1e6e3da4a65f3e.

--- src/linter/linter-format.ts
+++ src/linter/linter-format.ts
@@ -1,54 +1,36 @@
 import type { FlowrSearchLike } from '../search/flowr-search-builder';
-import type { FlowrSearchElement } from '../search/flowr-search';
-import { FlowrSearchElements } from '../search/flowr-search';
+import type { FlowrSearchElement, FlowrSearchElements } from '../search/flowr-search';
 import type { MergeableRecord } from '../util/objects';
-import { deepMergeObject } from '../util/objects';
 import type { GeneratorNames } from '../search/search-executor/search-generators';
 import type { TransformerNames } from '../search/search-executor/search-transformer';
 import type { NormalizedAst, ParentInformation } from '../r-bridge/lang-4.x/ast/model/processing/decorate';
-import type { LintingRuleConfig, LintingRuleNames } from './linter-rules';
+import type { LintingRuleConfig, LintingRuleMetadata, LintingRuleNames, LintingRuleResult } from './linter-rules';
 import type { DataflowInformation } from '../dataflow/info';
 import type { DeepPartial, DeepReadonly } from 'ts-essentials';
 import type { UniqueArray } from '../util/collections/arrays';
 import type { LintingRuleTag } from './linter-tags';
-import { guard } from '../util/assert';
-import { runSearch } from '../search/flowr-search-executor';
 
 
-export class LintingRuleInformation<Name extends string = string, Config extends MergeableRecord = never> {
-	private readonly name:          Name;
+export class LintingRuleInformation<Config extends MergeableRecord = never> {
 	private readonly defaultConfig: NoInfer<DeepReadonly<Config>>;
 	private readonly tags:          UniqueArray<LintingRuleTag>;
 	private readonly description:   string;
 
 	constructor(
-		name: Name,
 		config: NoInfer<DeepReadonly<Config>>,
 		tags: UniqueArray<LintingRuleTag>,
 		description: string
 	) {
-		guard(name.trim().length > 0, 'Linting rule name must not be empty');
-		guard(config !== undefined, 'Linting rule config must not be undefined');
-		this.name = name;
 		this.defaultConfig = config;
 		this.tags = tags;
 		this.description = description;
 	}
 
 	/**
-	 * The name of the linting rule.
-	 * This is used to identify the rule in the UI and in the configuration.
-	 * Please try to make it unique and descriptive to avoid confusion with other rules.
-	 */
-	public getName(): Name {
-		return this.name;
-	}
-
-	/**
 	 * The default config for this linting rule.
 	 * This config is combined with the user config when executing the rule.
 	 */
-	public getDefaultConfig(): DeepReadonly<Config> {
+	public getDefaultConfig(): NoInfer<DeepReadonly<Config>> {
 		return this.defaultConfig;
 	}
 
@@ -73,7 +55,6 @@ export class LintingRuleInformation<Name extends string = string, Config extends
  * The registry of valid linting rules is stored in {@link LintingRules}.
  */
 export abstract class LintingRule<
-	Name extends string,
 	Result extends LintingResult,
 	Metadata extends MergeableRecord,
 	Config extends MergeableRecord = never,
@@ -82,52 +63,22 @@ export abstract class LintingRule<
 	/**
 	 * The information attached to this linting rule.
 	 */
-	public readonly info: LintingRuleInformation<Name, Config>;
+	public readonly info: LintingRuleInformation<Config>;
 
-	protected constructor(info: LintingRuleInformation<Name, Config>) {
+	protected constructor(info: LintingRuleInformation<Config>) {
 		this.info = info;
-	}
-
-	/**
-	 * Executes the linting rule on the given input data.
-	 * @param config  - The configuration for this linting rule, which is merged with the default config of the rule.
-	 * @param input   - The data to be used for the search, which contains the normalized AST and the dataflow information.
-	 */
-	public search(config: DeepPartial<Config>, input: { normalize: NormalizedAst, dataflow: DataflowInformation }): LintingResults<Result, Metadata> {
-		const fullConfig = deepMergeObject(this.info.getDefaultConfig(), config);
-
-		const ruleSearch = this.createSearch(fullConfig, input);
-
-		const searchStart = Date.now();
-		const searchResult =  runSearch(ruleSearch, input);
-		const searchTime = Date.now() - searchStart;
-
-		const processStart = Date.now();
-		const result = this.processSearchResult(new FlowrSearchElements(searchResult), fullConfig, input);
-		const processTime = Date.now() - processStart;
-
-		return {
-			...result,
-			'.meta': {
-				...result['.meta'],
-				searchTimeMs:  searchTime,
-				processTimeMs: processTime
-			}
-		};
 	}
 
 	/**
 	 * Creates a flowR search that will then be executed and whose results will be passed to {@link processSearchResult}.
 	 * In the future, additional optimizations and transformations may be applied to the search between this function and {@link processSearchResult}.
-	 *
-	 * @see LintingRule#search
 	 */
-	protected abstract createSearch(config: Config, data: { normalize: NormalizedAst, dataflow: DataflowInformation }): FlowrSearchLike<Info, GeneratorNames, TransformerNames[], FlowrSearchElements<Info, Elements>>;
+	public abstract createSearch(config: Config, data: { normalize: NormalizedAst, dataflow: DataflowInformation }): FlowrSearchLike<Info, GeneratorNames, TransformerNames[], FlowrSearchElements<Info, Elements>>;
 	/**
 	 * Processes the search results of the search created through {@link createSearch}.
 	 * This function is expected to return the linting results from this rule for the given search, ie usually the given script file.
 	 */
-	protected abstract processSearchResult(elements: FlowrSearchElements<Info, Elements>, config: Config, data: { normalize: NormalizedAst, dataflow: DataflowInformation }): {
+	public abstract processSearchResult(elements: FlowrSearchElements<Info, Elements>, config: Config, data: { normalize: NormalizedAst, dataflow: DataflowInformation }): {
 		results: Result[],
 		'.meta': Metadata
 	};
@@ -140,10 +91,7 @@ export abstract class LintingResult {
 	public readonly certainty: LintingCertainty;
 	/**
 	 * A function used to pretty-print the given linting result.
-	 */
-	public abstract printShort(): string;
-	/**
-	 * Produces a more verbose but human-readable representation of the linting result.
+	 * By default, the {@link LintingResult#certainty} is automatically printed alongside this information.
 	 */
 	public abstract prettyPrint(): string;
 
@@ -157,9 +105,9 @@ export interface ConfiguredLintingRule<Name extends LintingRuleNames = LintingRu
 	readonly config: DeepPartial<LintingRuleConfig<Name>>
 }
 
-export interface LintingResults<Result extends LintingResult, Metadata extends MergeableRecord = never> {
-	results: Result[];
-	'.meta': Metadata & { readonly searchTimeMs: number; readonly processTimeMs: number; };
+export interface LintingResults<Name extends LintingRuleNames> {
+	results: LintingRuleResult<Name>[];
+	'.meta': LintingRuleMetadata<Name> & { readonly searchTimeMs: number; readonly processTimeMs: number; };
 }
 
 export enum LintingCertainty {
--- src/linter/rules/deprecated-functions.ts
+++ src/linter/rules/deprecated-functions.ts
@@ -1,30 +1,15 @@
-import { LintingRule, LintingResult, LintingCertainty, LintingRuleInformation } from '../linter-format';
+import type { LintingResult, LintingRule } from '../linter-format';
+import { LintingCertainty } from '../linter-format';
 import { Q } from '../../search/flowr-search-builder';
 import type { MergeableRecord } from '../../util/objects';
 import { formatRange } from '../../util/mermaid/dfg';
 import { Enrichment, enrichmentContent } from '../../search/search-executor/search-enrichers';
 import type { SourceRange } from '../../util/range';
 import type { Identifier } from '../../dataflow/environments/identifier';
-import type { FlowrSearchElements } from '../../search/flowr-search';
-import type { ParentInformation } from '../../r-bridge/lang-4.x/ast/model/processing/decorate';
 
-export class DeprecatedFunctionsResult extends LintingResult {
-	public readonly function: string;
-	public readonly range:    SourceRange;
-
-	constructor(certainty: LintingCertainty, functionName: string, range: SourceRange) {
-		super(certainty);
-		this.function = functionName;
-		this.range = range;
-	}
-
-	public printShort(): string {
-		return `Function \`${this.function}\` at ${formatRange(this.range)}`;
-	}
-
-	public prettyPrint(): string {
-		return `Function \`${this.function}\` called at ${formatRange(this.range)} is deprecated`;
-	}
+export interface DeprecatedFunctionsResult extends LintingResult {
+	function: string
+	range:    SourceRange
 }
 
 export interface DeprecatedFunctionsConfig extends MergeableRecord {
@@ -39,18 +24,9 @@ export interface DeprecatedFunctionsMetadata extends MergeableRecord {
 	totalNotDeprecated: number
 }
 
-export class LinterRuleDeprecatedFunctions extends LintingRule<'deprecated-functions', DeprecatedFunctionsResult, DeprecatedFunctionsMetadata, DeprecatedFunctionsConfig> {
-	public static DeprecatedLinterInfo = new LintingRuleInformation(
-		'deprecated-functions',
-
-	);
-
-
-	protected createSearch() {
-		return Q.all().with(Enrichment.CallTargets);
-	}
-
-	protected processSearchResult(elements: FlowrSearchElements<ParentInformation>, config: DeprecatedFunctionsConfig) {
+export const DEPRECATED_FUNCTIONS = {
+	createSearch:        (_config) => Q.all().with(Enrichment.CallTargets),
+	processSearchResult: (elements, config) => {
 		const metadata: DeprecatedFunctionsMetadata = {
 			totalRelevant:      0,
 			totalNotDeprecated: 0
@@ -80,15 +56,16 @@ export class LinterRuleDeprecatedFunctions extends LintingRule<'deprecated-funct
 						return false;
 					}
 				})
-				.map(element => (new DeprecatedFunctionsResult(
-					LintingCertainty.Definitely,
-					element.target,
-					element.range
-				))),
+				.map(element => ({
+					certainty: LintingCertainty.Definitely,
+					function:  element.target,
+					range:     element.range
+				})),
 			'.meta': metadata
 		};
-	}
+	},
+	prettyPrint:   result => `Function \`${result.function}\` at ${formatRange(result.range)}`,
 	defaultConfig: {
 		deprecatedFunctions: ['all_equal', 'arrange_all', 'distinct_all', 'filter_all', 'group_by_all', 'summarise_all', 'mutate_all', 'select_all', 'vars', 'all_vars', 'id', 'failwith', 'select_vars', 'rename_vars', 'select_var', 'current_vars', 'bench_tbls', 'compare_tbls', 'compare_tbls2', 'eval_tbls', 'eval_tbls2', 'location', 'changes', 'combine', 'do', 'funs', 'add_count_', 'add_tally_', 'arrange_', 'count_', 'distinct_', 'do_', 'filter_', 'funs_', 'group_by_', 'group_indices_', 'mutate_', 'tally_', 'transmute_', 'rename_', 'rename_vars_', 'select_', 'select_vars_', 'slice_', 'summarise_', 'summarize_', 'summarise_each', 'src_local', 'tbl_df', 'add_rownames', 'group_nest', 'group_split', 'with_groups', 'nest_by', 'progress_estimated', 'recode', 'sample_n', 'top_n', 'transmute', 'fct_explicit_na', 'aes_', 'aes_auto', 'annotation_logticks', 'is.Coord', 'coord_flip', 'coord_map', 'is.facet', 'fortify', 'is.ggproto', 'guide_train', 'is.ggplot', 'qplot', 'is.theme', 'gg_dep', 'liply', 'isplit2', 'list_along', 'cross', 'invoke', 'at_depth', 'prepend', 'rerun', 'splice', '`%@%`', 'rbernoulli', 'rdunif', 'when', 'update_list', 'map_raw', 'accumulate', 'reduce_right', 'flatten', 'map_dfr', 'as_vector', 'transpose', 'melt_delim', 'melt_fwf', 'melt_table', 'read_table2', 'str_interp', 'as_tibble', 'data_frame', 'tibble_', 'data_frame_', 'lst_', 'as_data_frame', 'as.tibble', 'frame_data', 'trunc_mat', 'is.tibble', 'tidy_names', 'set_tidy_names', 'repair_names', 'extract_numeric', 'complete_', 'drop_na_', 'expand_', 'crossing_', 'nesting_', 'extract_', 'fill_', 'gather_', 'nest_', 'separate_rows_', 'separate_', 'spread_', 'unite_', 'unnest_', 'extract', 'gather', 'nest_legacy', 'separate_rows', 'separate', 'spread',]
-	};
-}
+	}
+} as const satisfies LintingRule<DeprecatedFunctionsResult, DeprecatedFunctionsMetadata, DeprecatedFunctionsConfig>;

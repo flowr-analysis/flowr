66fd3a4da3b9cca4990847d922024483057dabe6
snh21 <lars.pfrenger@uni-ulm.de>
2025-06-14 22:36:50 +0200

    feat(lintr): write rule to detect casing convention - #1732

---
+++ test/functionality/linter/lint-naming-convention.test.ts
@@ -0,0 +1,22 @@
+import { describe } from 'vitest';
+import { withTreeSitter } from '../_helper/shell';
+import { assertLinter } from '../_helper/linter';
+import { LintingCertainty } from '../../../src/linter/linter-format';
+import { CasingConvention } from '../../../src/linter/rules/naming-convention';
+
+describe('flowR linter', withTreeSitter(parser => {
+	describe('simple test', () => {
+		assertLinter('simple', parser, 'testVar <- 5', 'naming-convention', [{
+			suggestion:     'testVar',
+			certainty:      LintingCertainty.Definitely,
+			detectedCasing: CasingConvention.CamelCase,
+			name:           'testVar',
+			range:          [1, 1, 1, 7]
+		}]);
+	});
+
+
+    describe.each([
+        { name: }
+    ])
+}));

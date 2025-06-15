912babc52cd80a7f6d9b1fe13b7da1c8494bca08
Florian Sihler <florian.sihler@uni-ulm.de>
2025-06-15 13:51:25 +0200

    feat(lint): abspath linter (no quickfix)

--- src/linter/rules/absolute-path.ts
+++ src/linter/rules/absolute-path.ts
@@ -9,17 +9,21 @@ import { LintingRuleTag } from '../linter-tags';
 import { RType } from '../../r-bridge/lang-4.x/ast/model/type';
 import { isAbsolutePath } from '../../util/text/strings';
 import { isRString } from '../../r-bridge/lang-4.x/ast/model/nodes/r-string';
-import { isNotUndefined } from '../../util/assert';
+import { isNotUndefined, isUndefined } from '../../util/assert';
 import { ReadFunctions } from '../../queries/catalog/dependencies-query/function-info/read-functions';
 import { WriteFunctions } from '../../queries/catalog/dependencies-query/function-info/write-functions';
 import type { FunctionInfo } from '../../queries/catalog/dependencies-query/function-info/function-info';
 import { Enrichment } from '../../search/search-executor/search-enrichers';
 import { SourceFunctions } from '../../queries/catalog/dependencies-query/function-info/source-functions';
-import { VertexType } from '../../dataflow/graph/vertex';
+import type { DataflowGraphVertexFunctionCall } from '../../dataflow/graph/vertex';
+import { isFunctionCallVertex , VertexType } from '../../dataflow/graph/vertex';
 import type { ParentInformation } from '../../r-bridge/lang-4.x/ast/model/processing/decorate';
 import type { FlowrSearchElementMaybeFromQuery } from '../../search/flowr-search';
 import type { QueryResults } from '../../queries/query';
 import { Unknown } from '../../queries/catalog/dependencies-query/dependencies-query-format';
+import type { DataflowGraph } from '../../dataflow/graph/graph';
+import { getArgumentStringValue } from '../../dataflow/eval/resolve/resolve-argument';
+import path from 'path';
 
 export interface AbsoluteFilePathResult extends LintingResult {
 	filePath: string,
@@ -31,7 +35,7 @@ export interface AbsoluteFilePathConfig extends MergeableRecord {
 	include: {
 		/** Whether to include paths that are constructed by functions */
 		constructed: boolean,
-		/** Include every string, even if it is not used as a file path */
+		/** Include every string, even if it is not used as a file path, to count, strings must have a length of at least 3 chars */
 		allStrings:  boolean
 	},
 	/** Extend the built-in absolute path recognition with additional regexes */
@@ -43,7 +47,7 @@ export interface AbsoluteFilePathConfig extends MergeableRecord {
 	additionalPathFunctions: FunctionInfo[]
 	/**
 	 * Which path should be considered to be the origin for relative paths.
-	 * `@cwd` is the current working directory.
+	 * `@cwd` is the current working directory. This is only relevant with quickfixes.
 	 */
 	useAsWd:                 '@script' | '@project' | '@file' | '@cwd' | '@home' | string
 	// TODO: wd and constructed paths
@@ -55,19 +59,46 @@ export interface AbsoluteFilePathMetadata extends MergeableRecord {
 	totalUnknown:    number
 }
 
+/** return all strings constructable by these functions */
+const PathFunctions: Record<string, (df: DataflowGraph, vtx: DataflowGraphVertexFunctionCall) => string[] | undefined> = {
+	'file.path': (df: DataflowGraph, vtx: DataflowGraphVertexFunctionCall): string[] | undefined => {
+		const fsep = getArgumentStringValue(
+			df, vtx, undefined, 'fsep', true
+		);
+		// in the future we can access `.Platform$file.sep` here
+		const sepValues: string[] = new Array(...fsep?.values()?.flatMap(s => [...s].filter(isNotUndefined)) ?? [path.sep]);
+		if(sepValues.some(s => s === Unknown || isUndefined(s))) {
+			// if we have no fsep, we cannot construct a path
+			return undefined;
+		}
+		const args = getArgumentStringValue(df, vtx, 'unnamed', undefined, true);
+		const argValues = args ? Array.from(args.values()).flatMap(v => [...v]) : [];
+		if(!argValues || argValues.length === 0 || argValues.some(v => v === Unknown || isUndefined(v))) {
+			// if we have no arguments, we cannot construct a path
+			return undefined;
+		}
+		const results: string[] = [];
+		for(const val of sepValues) {
+			results.push(argValues.join(val));
+		}
+		return results;
+	}
+};
 
 export const ABSOLUTE_PATH = {
 	/* this can be done better once we have types */
 	createSearch: (config) => {
+		let q;
 		if(config.include.allStrings) {
-			return Q.all().filter(RType.String);
+			q = Q.all().filter(RType.String);
+		} else {
+			q = Q.fromQuery({
+				type:                   'dependencies',
+				// we use the dependencies query to give us all functions that take a file path as input
+				ignoreDefaultFunctions: true,
+				readFunctions:          ReadFunctions.concat(WriteFunctions, SourceFunctions, config.additionalPathFunctions),
+			});
 		}
-		let q = Q.fromQuery({
-			type:                   'dependencies',
-			// we use the dependencies query to give us all functions that take a file path as input
-			ignoreDefaultFunctions: true,
-			readFunctions:          ReadFunctions.concat(WriteFunctions, SourceFunctions, config.additionalPathFunctions),
-		});
 		if(config.include.constructed) {
 			q = q.merge(Q.all().filter(VertexType.FunctionCall).with(Enrichment.CallTargets));
 			/* in the future we want to directly check whether this is one of the supported functions */
@@ -85,7 +116,7 @@ export const ABSOLUTE_PATH = {
 				metadata.totalConsidered++;
 				const node = element.node;
 				if(isRString(node)) {
-					if(isAbsolutePath(node.content.str, regex)) {
+					if(node.content.str.length >= 3 && isAbsolutePath(node.content.str, regex)) {
 						return [{
 							certainty: LintingCertainty.Maybe,
 							filePath:  node.content.str,
@@ -110,10 +141,23 @@ export const ABSOLUTE_PATH = {
 						// if we have no absolute paths, but all paths are known, we can return an empty array
 						return [];
 					}
+				} else {
+					const dfNode = data.dataflow.graph.getVertex(node.info.id);
+					if(isFunctionCallVertex(dfNode)) {
+						const handler = PathFunctions[dfNode.name ?? ''];
+						const strings = handler ? handler(data.dataflow.graph, dfNode) : [];
+						if(strings) {
+							return strings.filter(s => isAbsolutePath(s, regex)).map(str => ({
+								certainty: LintingCertainty.Maybe,
+								filePath:  str,
+								range:     node.info.fullRange ?? node.location ?? rangeFrom(-1, -1, -1, -1)
+							}));
+						}
+					}
+					// check whether the df node is a function call that returns a file path
 				}
 
 				metadata.totalUnknown++;
-				// TODO: check for paths
 				return undefined;
 			}).filter(isNotUndefined),
 			'.meta': metadata
--- test/functionality/linter/lint-absolute-path.test.ts
+++ test/functionality/linter/lint-absolute-path.test.ts
@@ -5,6 +5,8 @@ import { assertLinter } from '../_helper/linter';
 import { withTreeSitter } from '../_helper/shell';
 import { isAbsolutePath } from '../../../src/util/text/strings';
 import { LintingCertainty } from '../../../src/linter/linter-format';
+import { Unknown } from '../../../src/queries/catalog/dependencies-query/dependencies-query-format';
+import path from 'path';
 
 describe('flowR linter', withTreeSitter(parser => {
 	describe('absolute path linter', () => {
@@ -27,12 +29,62 @@ describe('flowR linter', withTreeSitter(parser => {
 			}
 		});
 
+		describe('all strings', () => {
+			describe('relative paths', () => {
+				assertLinter('none with all strings', parser, 'cat("hello")', 'absolute-file-paths', [], { totalConsidered: 1, totalUnknown: 0 }, {
+					include: {
+						allStrings: true
+					}
+				});
+
+				assertLinter('too short', parser, '"/x"', 'absolute-file-paths', [], { totalConsidered: 1, totalUnknown: 0 }, {
+					include: {
+						allStrings: true
+					}
+				});
+
+
+				for(const relPath of ['./file.csv', '../file.csv', 'file.csv', 'a\\b\\c.csv']) {
+					assertLinter(`"${relPath}"`, parser, `x <- "${relPath}"`, 'absolute-file-paths', [], { totalConsidered: 1, totalUnknown: 0 }, {
+						include: {
+							allStrings: true
+						}
+					});
+				}
+			});
+			describe('absolute paths', () => {
+				for(const absPath of ['/absolute/path/file.csv', 'C:\\absolute\\path\\file.csv', 'G:\\absolute\\path\\file.txt']) {
+					assertLinter(`"${absPath}"`, parser, `x <- "${absPath}"`, 'absolute-file-paths', [
+						{
+							certainty: LintingCertainty.Maybe,
+							filePath:  absPath,
+							range:     [1, 6, 1, absPath.length + 2 + 3 + 2] // +2 for the quotes and the assignment
+						}
+					], { totalConsidered: 1, totalUnknown: 0 }, {
+						include: {
+							allStrings: true
+						}
+					});
+				}
+			});
+		});
+
 		describe('path functions', () => {
 			describe.each(['read.csv', 'source', 'png'])('%s', fn => {
 				describe('relative paths', () => {
 					for(const relPath of ['./file.csv', '../file.csv', 'file.csv', 'a\\b\\c.csv']) {
 						assertLinter(`"${relPath}"`, parser, `${fn}("${relPath}")`, 'absolute-file-paths', [], { totalConsidered: 1, totalUnknown: 0 });
 					}
+				});
+				describe('raw strings', () => {
+					assertLinter('R()', parser, `${fn}(R"(./x)")`, 'absolute-file-paths', [], {
+						totalConsidered: 1,
+						totalUnknown:    0
+					});
+					assertLinter('--[]--', parser, `${fn}(R"--[./x]--")`, 'absolute-file-paths', [], {
+						totalConsidered: 1,
+						totalUnknown:    0
+					});
 				});
 				describe('unknown paths', () => {
 					for(const relPath of ['x', 'paste0("a", u)', 'runif(42)']) {
@@ -49,6 +101,70 @@ describe('flowR linter', withTreeSitter(parser => {
 							}
 						], { totalConsidered: 1, totalUnknown: 0 });
 					}
+					describe('raw strings', () => {
+						assertLinter('R()', parser, `${fn}(R"(/x/y)")`, 'absolute-file-paths', [{
+							certainty: LintingCertainty.Definitely,
+							filePath:  '/x/y',
+							range:     [1, 1, 1, 11 + fn.length] // length of the string + function name + parentheses
+						}], {
+							totalConsidered: 1,
+							totalUnknown:    0
+						});
+						assertLinter('--[]--', parser, `${fn}(R"--[C:\\hello.txt]--")`, 'absolute-file-paths', [{
+							certainty: LintingCertainty.Definitely,
+							filePath:  'C:\\hello.txt',
+							range:     [1, 1, 1, fn.length + 23] // length of the string + function name + parentheses
+						}], {
+							totalConsidered: 1,
+							totalUnknown:    0
+						});
+					});
+				});
+			});
+		});
+
+		describe('construction functions', () => {
+			describe('file.path', () => {
+				describe('relative paths', () => {
+					for(const components of [['a', 'b', 'c'], ['a/b/c', 'd/e/f'], ['a\\b\\c', 'd\\e\\f']] as const) {
+						const command = `file.path(${components.map(c => `"${c}"`).join(', ')})`;
+						assertLinter(command, parser, command, 'absolute-file-paths', [], { totalConsidered: 1, totalUnknown: 0 });
+					}
+					assertLinter('change fsep', parser, 'file.path("a", "b", fsep="\\\\")', 'absolute-file-paths', [], { totalConsidered: 1, totalUnknown: 0 });
+					assertLinter('skrewed fsep', parser, 'file.path("a", "b", fsep="")', 'absolute-file-paths', [], { totalConsidered: 1, totalUnknown: 0 });
+				});
+				describe('unknown paths', () => {
+					for(const components of [['a', Unknown, 'c'], ['a/b/c', 'd/e/f', Unknown], [Unknown, 'a\\b\\c', 'd\\e\\f']] as const) {
+						const command = `file.path(${components.map(c => c === Unknown ? 'u' : `"${c}"`).join(', ')})`;
+						assertLinter(command, parser, command, 'absolute-file-paths', [], { totalConsidered: 1, totalUnknown: 1 });
+					}
+					assertLinter('skrewed fsep', parser, 'file.path("a", "b", fsep=u)', 'absolute-file-paths', [], { totalConsidered: 1, totalUnknown: 1 });
+				});
+				describe('absolute paths', () => {
+					for(const components of [['/absolute/path', 'file.csv'], ['G:', 'a', 'b.txt'], ['', 'a.txt'], ['C:\\absolute\\path', 'file.csv'], ['G:\\absolute\\path', 'file.txt']] as const) {
+						const command = `file.path(${components.map(c => `"${c}"`).join(', ')})`;
+						assertLinter(command, parser, command, 'absolute-file-paths', [
+							{
+								certainty: LintingCertainty.Maybe,
+								filePath:  components.join(path.sep),
+								range:     [1, 1, 1, command.length]
+							}
+						], { totalConsidered: 1, totalUnknown: 0 });
+					}
+					assertLinter('change fsep', parser, 'file.path("C:", "b", fsep="\\\\")', 'absolute-file-paths', [
+						{
+							certainty: LintingCertainty.Maybe,
+							filePath:  'C:\\\\b',
+							range:     [1, 1, 1, 31]
+						}
+					], { totalConsidered: 1, totalUnknown: 0 });
+					assertLinter('skrewed fsep', parser, 'file.path("C", "b", fsep=":/")', 'absolute-file-paths', [
+						{
+							certainty: LintingCertainty.Maybe,
+							filePath:  'C:/b',
+							range:     [1, 1, 1, 30]
+						}
+					], { totalConsidered: 1, totalUnknown: 0 });
 				});
 			});
 		});

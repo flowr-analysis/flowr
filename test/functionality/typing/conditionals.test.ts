b58071cad1c1fef55fd73549fc0c940fa5e553a0
Lukas Rohde <lukas.rohde@silupe.de>
2025-06-19 21:57:51 +0200

    test-fix(typing): fix expected type due to dead code elimination updates

--- test/functionality/typing/conditionals.test.ts
+++ test/functionality/typing/conditionals.test.ts
@@ -1,5 +1,5 @@
 import { describe } from 'vitest';
-import { RDoubleType, RLogicalType, RNullType, RStringType } from '../../../src/typing/types';
+import { RDoubleType, RLogicalType, RNullType, RStringType, RUnknownType } from '../../../src/typing/types';
 import { assertInferredType, assertInferredTypes } from '../_helper/typing/assert-inferred-type';
 import { Q } from '../../../src/search/flowr-search-builder';
 import { RType } from '../../../src/r-bridge/lang-4.x/ast/model/type';
@@ -8,7 +8,7 @@ describe('Infer types for if statements', () => {
 	describe.each([
 		{ input: 'if(FALSE) { TRUE }',                     expectedType: new RNullType() },
 		{ input: 'if(TRUE) { 1 } else { NULL }',           expectedType: new RDoubleType() },
-		{ input: 'if(stop("stop here")) { 1 } else { 2 }', expectedType: new RDoubleType() },
+		{ input: 'if(stop("stop here")) { 1 } else { 2 }', expectedType: new RUnknownType() },
 	])('Infer $expectedType for $input', ({ input, expectedType }) => assertInferredType(input, expectedType));
 
 	assertInferredTypes(

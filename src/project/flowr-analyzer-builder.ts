fbafaab3a8f1b7882930cd74d595f8d810dcae95
MaxAtoms <7847075+MaxAtoms@users.noreply.github.com>
2025-06-24 14:24:58 +0200

    feat(builder): add engine and query support

--- src/project/flowr-analyzer-builder.ts
+++ src/project/flowr-analyzer-builder.ts
@@ -3,13 +3,20 @@ import { amendConfig, cloneConfig, defaultConfigOptions } from '../config';
 import type { DeepWritable } from 'ts-essentials';
 import type { RParseRequests } from '../r-bridge/retriever';
 import { FlowrAnalyzer } from './flowr-analyzer';
+import { retrieveEngineInstances } from '../engines';
+import type { KnownParser } from '../r-bridge/parser';
 
 export class FlowrAnalyzerBuilder {
-	private flowrConfig: FlowrConfigOptions = cloneConfig(defaultConfigOptions);
-	private request:     RParseRequests;
+	private flowrConfig:      DeepWritable<FlowrConfigOptions> = cloneConfig(defaultConfigOptions);
+	private readonly request: RParseRequests;
 
 	public amendConfig(func: (config: DeepWritable<FlowrConfigOptions>) => FlowrConfigOptions) : FlowrAnalyzerBuilder {
-		amendConfig(this.flowrConfig, func);
+		this.flowrConfig = amendConfig(this.flowrConfig, func);
+		return this;
+	}
+
+	public setEngine(engine : 'tree-sitter' | 'r-shell') {
+		this.flowrConfig.defaultEngine = engine;
 		return this;
 	}
 
@@ -17,9 +24,13 @@ export class FlowrAnalyzerBuilder {
 		this.request = request;
 	}
 
-	public build(): FlowrAnalyzer {
+	public async build(): Promise<FlowrAnalyzer> {
+		const engines = await retrieveEngineInstances(this.flowrConfig);
+		const parser = engines.engines[engines.default] as KnownParser;
+
 		return new FlowrAnalyzer(
 			this.flowrConfig,
+			parser,
 			this.request
 		);
 	}
--- src/project/flowr-analyzer.ts
+++ src/project/flowr-analyzer.ts
@@ -2,39 +2,78 @@ import type { FlowrConfigOptions } from '../config';
 import { cloneConfig, defaultConfigOptions } from '../config';
 import type { RParseRequests } from '../r-bridge/retriever';
 import { requestFromInput } from '../r-bridge/retriever';
-import { PipelineExecutor } from '../core/pipeline-executor';
-import { DEFAULT_DATAFLOW_PIPELINE } from '../core/steps/pipeline/default-pipelines';
-import { RShell } from '../r-bridge/shell';
+import type { DEFAULT_DATAFLOW_PIPELINE } from '../core/steps/pipeline/default-pipelines';
+import { createDataflowPipeline, createSlicePipeline } from '../core/steps/pipeline/default-pipelines';
 import type { PipelineOutput } from '../core/steps/pipeline/pipeline';
 import { FlowrAnalyzerBuilder } from './flowr-analyzer-builder';
 import { graphToMermaidUrl } from '../util/mermaid/dfg';
+import type { KnownParser } from '../r-bridge/parser';
+import type { SlicingCriteria } from '../slicing/criterion/parse';
+import type { Queries, SupportedQueryTypes } from '../queries/query';
+import { executeQueries } from '../queries/query';
+import type { DataflowInformation } from '../dataflow/info';
+import type { NormalizedAst } from '../r-bridge/lang-4.x/ast/model/processing/decorate';
 
 export class FlowrAnalyzer {
-	private flowrConfig: FlowrConfigOptions = cloneConfig(defaultConfigOptions);
-	private request:     RParseRequests;
+	private readonly flowrConfig: FlowrConfigOptions = cloneConfig(defaultConfigOptions);
+	private readonly request:     RParseRequests;
+	private readonly parser:      KnownParser;
+	private ast = undefined as unknown as NormalizedAst;
+	private dataflowInfo = undefined as unknown as DataflowInformation;
 
-	constructor(config: FlowrConfigOptions, request: RParseRequests) {
+	constructor(config: FlowrConfigOptions, parser: KnownParser, request: RParseRequests) {
 		this.flowrConfig = config;
 		this.request = request;
+		this.parser = parser;
 	}
 
-	public dataflow() : Promise<PipelineOutput<typeof DEFAULT_DATAFLOW_PIPELINE>> {
-		return new PipelineExecutor(DEFAULT_DATAFLOW_PIPELINE, {
-			parser:  new RShell(),
-			request: this.request
-		}, this.flowrConfig).allRemainingSteps();
+	public async dataflow() : Promise<PipelineOutput<typeof DEFAULT_DATAFLOW_PIPELINE>> {
+		const result = await createDataflowPipeline(
+			this.parser,
+			{ request: this.request },
+			this.flowrConfig).allRemainingSteps();
+		this.dataflowInfo = result.dataflow;
+		this.ast = result.normalize;
+		return result;
+	}
+
+	public async slice(criterion: SlicingCriteria) {
+		return createSlicePipeline(
+			this.parser,
+			{
+				request:   this.request,
+				criterion: criterion
+			},
+			this.flowrConfig).allRemainingSteps();
+	}
+
+	public async query(query: Queries<SupportedQueryTypes>) {
+		if(!this.dataflowInfo) {
+			await this.dataflow();
+		}
+		return executeQueries({ ast: this.ast, dataflow: this.dataflowInfo, config: this.flowrConfig }, query);
 	}
 }
 
 async function main() {
-	const result = await new FlowrAnalyzerBuilder(requestFromInput('x <- 1'))
+	const analyzer = await new FlowrAnalyzerBuilder(requestFromInput('x <- 1\nfoo <- function(){}\nfoo()'))
+		.setEngine('r-shell')
 		.amendConfig(c => {
 			c.solver.pointerTracking = true;
 			return c;
 		})
-		.build()
-		.dataflow();
-	console.error(graphToMermaidUrl(result.dataflow.graph));
+		.build();
+	const result = await analyzer.dataflow();
+	const query = await analyzer.query([{
+		type:     'call-context',
+		kind:     'test-kind',
+		subkind:  'test-subkind',
+		callName: /foo/
+	}]);
+	const slice = await analyzer.slice(['3@foo']);
+	console.log(graphToMermaidUrl(result.dataflow.graph));
+	console.log(slice.reconstruct);
+	console.log(query);
 }
 
 void main();

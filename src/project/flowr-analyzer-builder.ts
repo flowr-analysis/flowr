16762a90927c21a7dd8dd5f3b60c9578d9c4d1ff
stimjannik <tim.j.schmidt@web.de>
2025-07-02 21:05:25 +0200

    feat(plugin): add (un)registering plugins in FlowrAnalyzerBuilder

--- src/project/flowr-analyzer-builder.ts
+++ src/project/flowr-analyzer-builder.ts
@@ -5,10 +5,12 @@ import type { RParseRequests } from '../r-bridge/retriever';
 import { FlowrAnalyzer } from './flowr-analyzer';
 import { retrieveEngineInstances } from '../engines';
 import type { KnownParser } from '../r-bridge/parser';
+import type { AnyFlowrAnalyzerPlugin } from './plugins/flowr-analyzer-plugin';
 
 export class FlowrAnalyzerBuilder {
 	private flowrConfig:      DeepWritable<FlowrConfigOptions> = cloneConfig(defaultConfigOptions);
 	private readonly request: RParseRequests;
+	private plugins:          AnyFlowrAnalyzerPlugin[];
 
 	public amendConfig(func: (config: DeepWritable<FlowrConfigOptions>) => FlowrConfigOptions) : this {
 		this.flowrConfig = amendConfig(this.flowrConfig, func);
@@ -20,8 +22,25 @@ export class FlowrAnalyzerBuilder {
 		return this;
 	}
 
-	constructor(request: RParseRequests) {
+	constructor(request: RParseRequests, plugins?: AnyFlowrAnalyzerPlugin[]) {
 		this.request = request;
+		this.plugins = plugins ? plugins : [];
+	}
+
+	public registerPlugin(plugin: AnyFlowrAnalyzerPlugin | AnyFlowrAnalyzerPlugin[]): void {
+		if(Array.isArray(plugin)){
+			this.plugins.push(...plugin);
+		} else {
+			this.plugins.push(plugin);
+		}
+	}
+
+	public unregisterPlugin(plugin: AnyFlowrAnalyzerPlugin | AnyFlowrAnalyzerPlugin[]): void {
+		if(Array.isArray(plugin)){
+			this.plugins = this.plugins.filter(p => !plugin.includes(p));
+		} else {
+			this.plugins = this.plugins.filter(p => p !== plugin);
+		}
 	}
 
 	public async build(): Promise<FlowrAnalyzer> {

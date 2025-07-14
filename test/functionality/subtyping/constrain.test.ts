c5a3acdf729aa0423b3cdd95e04286ea8ccffce4
Lukas Rohde <lukas.rohde@silupe.de>
2025-07-14 11:46:13 +0200

    test(typing): add test for function overloading (it works! :D)

--- test/functionality/subtyping/constrain.test.ts
+++ test/functionality/subtyping/constrain.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from 'vitest';
-import { constrainWithLowerBound, constrainWithUpperBound, RComplexType, RDoubleType, resolveType, RIntegerType, RListType, RStringType, RTypeIntersection, RTypeVariable, UnresolvedRAtomicVectorType, UnresolvedRListType, UnresolvedRTypeUnion, UnresolvedRTypeVariable } from '../../../src/subtyping/types';
+import { constrainWithLowerBound, constrainWithUpperBound, RComplexType, RDoubleType, resolveType, RIntegerType, RListType, RStringType, RTypeIntersection, RTypeVariable, UnresolvedRAtomicVectorType, UnresolvedRFunctionType, UnresolvedRListType, UnresolvedRTypeIntersection, UnresolvedRTypeUnion, UnresolvedRTypeVariable } from '../../../src/subtyping/types';
 
 describe('Constrain types with lower and upper bounds', () => {
 	test('Constrain with numeric bounds', () => {
@@ -57,5 +57,46 @@ describe('Constrain types with lower and upper bounds', () => {
 		
 		expect(resolveType(typeVar)).toEqual(new RListType(new RTypeVariable(new RIntegerType(), new RTypeIntersection())));
 		expect(resolveType(elementType)).toEqual(new RTypeVariable(new RIntegerType(), new RTypeIntersection()));
+	});
+
+	test('Function overloading', () => {
+		// Predetermined types in context
+		const funcType1 = new UnresolvedRFunctionType();
+		const paramType1 = new UnresolvedRTypeVariable();
+		funcType1.parameterTypes.set(0, paramType1);
+		paramType1.constrainFromBothSides(new RIntegerType());
+		funcType1.returnType.constrainFromBothSides(new RIntegerType());
+
+		const funcType2 = new UnresolvedRFunctionType();
+		const paramType2 = new UnresolvedRTypeVariable();
+		funcType2.parameterTypes.set(0, paramType2);
+		paramType2.constrainFromBothSides(new RDoubleType());
+		funcType2.returnType.constrainFromBothSides(new RDoubleType());
+
+		const overloadedFuncType1 = new UnresolvedRTypeIntersection(funcType1, funcType2);
+		const overloadedFuncType2 = new UnresolvedRTypeIntersection(funcType1, funcType2);
+
+		// Typing of called function
+		const calledFuncType1 = new UnresolvedRTypeVariable();
+		const calledFuncType2 = new UnresolvedRTypeVariable();
+		calledFuncType1.constrainWithLowerBound(overloadedFuncType1);
+		calledFuncType2.constrainWithLowerBound(overloadedFuncType2);
+
+		// Typing of function call
+		const argType1 = new UnresolvedRTypeVariable();
+		const argType2 = new UnresolvedRTypeVariable();
+		argType1.constrainFromBothSides(new RIntegerType());
+		argType2.constrainFromBothSides(new RDoubleType());
+		const templateFuncType1 = new UnresolvedRFunctionType();
+		templateFuncType1.parameterTypes.set(0, argType1);
+		const returnType1 = templateFuncType1.returnType;
+		const templateFuncType2 = new UnresolvedRFunctionType();
+		templateFuncType2.parameterTypes.set(0, argType2);
+		const returnType2 = templateFuncType2.returnType;
+		calledFuncType1.constrainWithUpperBound(templateFuncType1);
+		calledFuncType2.constrainWithUpperBound(templateFuncType2);
+
+		expect(resolveType(returnType1)).toEqual(new RTypeVariable(new RIntegerType(), new RTypeIntersection()));
+		expect(resolveType(returnType2)).toEqual(new RTypeVariable(new RDoubleType(), new RTypeIntersection()));
 	});
 });

ddeaf326cc8ea86391e0bda93026f6231ddd0739
Lukas Rohde <lukas.rohde@silupe.de>
2025-07-16 15:29:44 +0200

    feat(typing): add support for using subtyping in datatype queries

--- src/queries/catalog/datatype-query/datatype-query-executor.ts
+++ src/queries/catalog/datatype-query/datatype-query-executor.ts
@@ -2,25 +2,42 @@ import type { DatatypeQuery, DatatypeQueryResult } from './datatype-query-format
 import { log } from '../../../util/log';
 import type { BasicQueryData } from '../../base-query-format';
 import type { NormalizedAst, ParentInformation } from '../../../r-bridge/lang-4.x/ast/model/processing/decorate';
+import type { SingleSlicingCriterion } from '../../../slicing/criterion/parse';
 import { slicingCriterionToId } from '../../../slicing/criterion/parse';
 import { inferDataTypes } from '../../../typing/unification/infer';
+import { inferDataTypes as inferDataTypesUsingSubtyping } from '../../../typing/subtyping/infer';
+import type { DataTypeInfo } from '../../../typing/types';
 
 export function executeDatatypeQuery({ dataflow, ast }: BasicQueryData, queries: readonly DatatypeQuery[]): DatatypeQueryResult {
-	const start = Date.now();
 	const result: DatatypeQueryResult['inferredTypes'] = {};
-	const typedAst = inferDataTypes(ast as NormalizedAst<ParentInformation & { typeVariable?: undefined }>, dataflow);
-	
-	for(const { criterion } of queries) {
-		if(result[criterion ?? '1:1'] !== undefined) {
-			log.warn('Duplicate criterion in datatype query:', criterion);
-			continue;
+	const extractInferredTypeFromAst = (typedAst: NormalizedAst<DataTypeInfo>, criteria: SingleSlicingCriterion[]): void => {
+		for(const criterion of criteria) {
+			if(result[criterion] !== undefined) {
+				log.warn('Duplicate criterion in datatype query:', criterion);
+				continue;
+			}
+			
+			const node = criterion !== undefined ? typedAst.idMap.get(slicingCriterionToId(criterion, typedAst.idMap)) : typedAst.ast;
+			if(node === undefined) {
+				log.warn('Criterion not found in normalized AST:', criterion);
+				continue;
+			}
+			
+			result[criterion] = node.info.inferredType;
 		}
-		const node = criterion !== undefined ? typedAst.idMap.get(slicingCriterionToId(criterion, typedAst.idMap)) : typedAst.ast;
-		if(node === undefined) {
-			log.warn('Criterion not found in normalized AST:', criterion);
-			continue;
-		}
-		result[criterion ?? '1:1'] = node.info.inferredType;
+	};
+	const [queriesWithoutSubtyping, queriesUsingSubtyping] = queries.reduce(([unificationGroup, subtypingGroup], query) => query.useSubtyping ? [unificationGroup, [...subtypingGroup, query]] : [[...unificationGroup, query], subtypingGroup], [[], []] as [DatatypeQuery[], DatatypeQuery[]]);
+
+	const start = Date.now();
+
+	if(queriesWithoutSubtyping.length > 0) {
+		const typedAst = inferDataTypes(ast as NormalizedAst<ParentInformation & { typeVariable?: undefined }>, dataflow);
+		extractInferredTypeFromAst(typedAst, queriesWithoutSubtyping.map(query => query.criterion ?? '1:1'));
+	}
+
+	if(queriesUsingSubtyping.length > 0) {
+		const typedAst = inferDataTypesUsingSubtyping(ast as NormalizedAst<ParentInformation & { typeVariable?: undefined }>, dataflow);
+		extractInferredTypeFromAst(typedAst, queriesUsingSubtyping.map(query => query.criterion ?? '1:1'));
 	}
 
 	return {
--- src/queries/catalog/datatype-query/datatype-query-format.ts
+++ src/queries/catalog/datatype-query/datatype-query-format.ts
@@ -5,7 +5,6 @@ import { bold } from '../../../util/text/ansi';
 import { printAsMs } from '../../../util/text/time';
 import Joi from 'joi';
 import { executeDatatypeQuery } from './datatype-query-executor';
-
 import { asciiDataType } from '../../query-print';
 import type { DataType } from '../../../typing/types';
 
@@ -13,8 +12,9 @@ import type { DataType } from '../../../typing/types';
  * Calculates the inferred data type for the given criterion.
  */
 export interface DatatypeQuery extends BaseQueryFormat {
-	readonly type:       'datatype';
-	readonly criterion?: SingleSlicingCriterion;
+	readonly type:          'datatype';
+	readonly criterion?:    SingleSlicingCriterion;
+	readonly useSubtyping?: boolean;
 }
 
 export interface DatatypeQueryResult extends BaseQueryResult {
@@ -36,7 +36,8 @@ export const DatatypeQueryDefinition = {
 		return [];
 	},
 	schema: Joi.object({
-		type:      Joi.string().valid('datatype').required().description('The type of the query.'),
-		criterion: Joi.string().optional().description('The slicing criterion of the node to get the inferred data type for.')
+		type:         Joi.string().valid('datatype').required().description('The type of the query.'),
+		criterion:    Joi.string().optional().description('The slicing criterion of the node to get the inferred data type for.'),
+		useSubtyping: Joi.boolean().optional().default(false).description('Whether to use subtyping to infer the data type.'),
 	}).description('Datatype query used to extract the inferred data type for a node in the normalized AST')
 } as const satisfies SupportedQuery<'datatype'>;

f050dfb9d4671074f20d5e6e931f2d9f3eb6979d
Ellpeck <me@ellpeck.de>
2025-07-17 16:02:26 +0200

    feat: very basic initial dead code rule impl

--- src/linter/rules/dead-code.ts
+++ src/linter/rules/dead-code.ts
@@ -1,29 +1,55 @@
-import { type LintingResult, LintingCertainty, type LintingRule, LintingPrettyPrintContext } from '../linter-format';
+import { type LintingResult, type LintingRule, LintingPrettyPrintContext, LintingCertainty } from '../linter-format';
 import type { SourceRange } from '../../util/range';
 import type { MergeableRecord } from '../../util/objects';
 import { Q } from '../../search/flowr-search-builder';
 import { formatRange } from '../../util/mermaid/dfg';
 import { LintingRuleTag } from '../linter-tags';
+import type { NodeId } from '../../r-bridge/lang-4.x/ast/model/processing/node-id';
+import { visitCfgInOrder } from '../../control-flow/simple-visitor';
+import { extractSimpleCfg } from '../../control-flow/extract-cfg';
+import { cfgAnalyzeDeadCode } from '../../control-flow/cfg-dead-code';
 
 export interface DeadCodeResult extends LintingResult {
 	range: SourceRange
 }
 
 export interface DeadCodeConfig extends MergeableRecord {
+	analyzeDeadCode: boolean
 }
 
 export interface DeadCodeMetadata extends MergeableRecord {
+	totalReachable: number
 }
 
 export const DEAD_CODE = {
 	createSearch:        (_config) => Q.all(),
-	processSearchResult: (elements, _config, _data) => {
-		const metadata: DeadCodeMetadata = { yes: true };
+	processSearchResult: (elements, config, data) => {
+		let cfg = extractSimpleCfg(data.normalize);
+		if(config.analyzeDeadCode) {
+			cfg = cfgAnalyzeDeadCode(cfg, { ast: data.normalize, dfg: data.dataflow.graph, config: data.config });
+		}
+
+		// see cfgRemoveDeadCode
+		const reachable = new Set<NodeId>();
+		visitCfgInOrder(cfg.graph, cfg.entryPoints, node => {
+			reachable.add(node);
+		});
+
+		const metadata: DeadCodeMetadata = {
+			totalReachable: reachable.size
+		};
 		return {
-			results: elements.getElements().flatMap(element => ({
-				certainty: LintingCertainty.Definitely,
-				range:     element.node.info.fullRange as SourceRange
-			})),
+			results: combineRanges(
+				elements.getElements()
+					.filter(element => {
+						const id = element.node.info.id;
+						return !reachable.has(id);
+					})
+					.map(element => element.node.info.fullRange as SourceRange))
+				.map(range => ({
+					certainty: LintingCertainty.Definitely,
+					range
+				})),
 			'.meta': metadata
 		};
 	},
@@ -35,6 +61,12 @@ export const DEAD_CODE = {
 		name:          'Dead Code',
 		tags:          [LintingRuleTag.Smell, LintingRuleTag.Usability, LintingRuleTag.Reproducibility],
 		description:   'Marks areas of code that are never reached or whose invocation has no effect.',
-		defaultConfig: {}
+		defaultConfig: {
+			analyzeDeadCode: true
+		}
 	}
 } as const satisfies LintingRule<DeadCodeResult, DeadCodeMetadata, DeadCodeConfig>;
+
+function combineRanges(ranges: SourceRange[]): SourceRange[] {
+	return [...new Set<SourceRange>(ranges)];
+}
--- test/functionality/linter/lint-dead-code.test.ts
+++ test/functionality/linter/lint-dead-code.test.ts
@@ -1,8 +1,22 @@
 import { describe } from 'vitest';
 import { withTreeSitter } from '../_helper/shell';
+import { assertLinter } from '../_helper/linter';
+import { LintingCertainty } from '../../../src/linter/linter-format';
 
 describe('flowR linter', withTreeSitter(parser => {
 	describe('dead code', () => {
+		assertLinter('simple always', parser, 'if(TRUE) 1 else 2', 'dead-code', [
+			{ certainty: LintingCertainty.Definitely, range: [1, 17, 1, 17] }
+		], { totalReachable: 9 });
+		assertLinter('simple never', parser, 'if(FALSE) 1 else 2', 'dead-code', [
+			{ certainty: LintingCertainty.Definitely, range: [1, 11, 1, 11] }
+		], { totalReachable: 9 });
 
+		assertLinter('non-constant always', parser, 'x <- TRUE; if(x) 1 else 2', 'dead-code', [
+			{ certainty: LintingCertainty.Definitely, range: [1, 25, 1, 25] }
+		], { totalReachable: 13 });
+		assertLinter('non-constant never', parser, 'x <- FALSE; if(x) 1 else 2', 'dead-code', [
+			{ certainty: LintingCertainty.Definitely, range: [1, 19, 1, 19] }
+		], { totalReachable: 13 });
 	});
 }));

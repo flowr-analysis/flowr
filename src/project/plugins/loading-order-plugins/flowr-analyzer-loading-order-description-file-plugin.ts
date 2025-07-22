62e50ae960bc21916461479877fcc272b6c443b9
stimjannik <tim.j.schmidt@web.de>
2025-07-22 15:45:20 +0200

    feat(plugin): add loading order from DESCRIPTION retriever

---
+++ src/project/plugins/loading-order-plugins/flowr-analyzer-loading-order-description-file-plugin.ts
@@ -0,0 +1,27 @@
+import { FlowrAnalyzerDescriptionFilePlugin } from '../file-plugins/flowr-analyzer-description-file-plugin';
+import type { FlowrAnalyzerPlugin } from '../flowr-analyzer-plugin';
+import { SemVer } from 'semver';
+import type { FlowrAnalyzer } from '../../flowr-analyzer';
+import type { FlowrConfigOptions } from '../../../config';
+import { FlowrAnalyzerLoadingOrderPlugin } from './flowr-analyzer-loading-order-plugin';
+
+export class FlowrAnalyzerLoadingOrderDescriptionFilePlugin extends FlowrAnalyzerLoadingOrderPlugin {
+	public readonly name = 'flowr-analyzer-package-version-description-file-plugin';
+	public readonly description = 'This plugin does...';
+	public readonly version = new SemVer('0.1.0');
+
+	dependencies:    FlowrAnalyzerPlugin[] = [new FlowrAnalyzerDescriptionFilePlugin()];
+	descriptionFile: Map<string, string[]> = new Map<string, string[]>();
+
+	async processor(analyzer: FlowrAnalyzer, pluginConfig: FlowrConfigOptions): Promise<void> {
+		const plugin = this.dependencies[0] as FlowrAnalyzerDescriptionFilePlugin;
+		await plugin.processor(analyzer, pluginConfig);
+		this.descriptionFile = plugin.information;
+
+		this.loadingOrder = this.descriptionFile?.get('Collate')?.slice() || [];
+
+		console.log(analyzer);
+		console.log(pluginConfig);
+		return Promise.resolve(undefined);
+	}
+}
--- src/project/plugins/loading-order-plugins/flowr-analyzer-loading-order-plugin.ts
+++ src/project/plugins/loading-order-plugins/flowr-analyzer-loading-order-plugin.ts
@@ -1,7 +1,7 @@
-import type { FlowrAnalyzerPlugin } from '../flowr-analyzer-plugin';
+import  { FlowrAnalyzerPlugin } from '../flowr-analyzer-plugin';
 import type { PathLike } from 'fs';
 
-export interface FlowrAnalyzerLoadingOrderPlugin extends FlowrAnalyzerPlugin {
-    type:         'loading-order';
-    loadingOrder: PathLike[];
+export abstract class FlowrAnalyzerLoadingOrderPlugin extends FlowrAnalyzerPlugin {
+	readonly type = 'loading-order';
+	public loadingOrder: PathLike[] = [];
 }

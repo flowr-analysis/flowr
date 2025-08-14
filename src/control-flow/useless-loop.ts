3717d1a21417f84626d3700a354f6f7b8d94ae18
snh21 <lars.pfrenger@uni-ulm.de>
2025-08-14 12:48:28 +0200

    feat(cfg): write visitor to check if only loop once - #1873

--- src/control-flow/useless-loop.ts
+++ src/control-flow/useless-loop.ts
@@ -1,14 +1,18 @@
-import type { VariableResolve } from '../config';
+import type { FlowrConfigOptions } from '../config';
 import type { BuiltInMappingName } from '../dataflow/environments/built-in';
 import { resolveIdToValue } from '../dataflow/eval/resolve/alias-tracking';
 import { valueSetGuard } from '../dataflow/eval/values/general';
 import { isValue } from '../dataflow/eval/values/r-value';
 import type { DataflowGraph } from '../dataflow/graph/graph';
+import type { DataflowGraphVertexFunctionCall } from '../dataflow/graph/vertex';
 import { VertexType } from '../dataflow/graph/vertex';
+import { happensInEveryBranch } from '../dataflow/info';
 import { EmptyArgument } from '../r-bridge/lang-4.x/ast/model/nodes/r-function-call';
+import type { NormalizedAst } from '../r-bridge/lang-4.x/ast/model/processing/decorate';
 import type { NodeId } from '../r-bridge/lang-4.x/ast/model/processing/node-id';
 import { guard } from '../util/assert';
 import type { ControlFlowInformation } from './control-flow-graph';
+import type { SemanticCfgGuidedVisitorConfiguration } from './semantic-cfg-guided-visitor';
 import { SemanticCfgGuidedVisitor } from './semantic-cfg-guided-visitor';
 
 
@@ -22,9 +26,11 @@ const loopyFunctions = new Set<BuiltInMappingName>(['builtin:for-loop', 'builtin
  * @param loop        - nodeid of the loop to analyse
  * @param dataflow    - dataflow graph
  * @param controlflow - controlflow graph
+ * @param ast         - normalized ast
+ * @param config      - current flowr config
  * @returns true if the given loop only iterates once
  */
-export function onlyLoopsOnce(loop: NodeId, dataflow: DataflowGraph, controlflow: ControlFlowInformation, resolveConfig: VariableResolve): boolean | undefined {
+export function onlyLoopsOnce(loop: NodeId, dataflow: DataflowGraph, controlflow: ControlFlowInformation, ast: NormalizedAst, config: FlowrConfigOptions): boolean | undefined {
 	const vertex = dataflow.getVertex(loop);
 	if(!vertex) {
 		return undefined;
@@ -33,7 +39,7 @@ export function onlyLoopsOnce(loop: NodeId, dataflow: DataflowGraph, controlflow
 	guard(vertex.tag === VertexType.FunctionCall, 'invalid vertex type for onlyLoopsOnce');
 	guard(vertex.origin !== 'unnamed' && loopyFunctions.has(vertex.origin[0] as BuiltInMappingName), 'onlyLoopsOnce can only be called with loops');
 
-	// 1.  In case of for loop, check if vector has only one element
+	// 1. In case of for loop, check if vector has only one element
 	if(vertex.origin[0] === 'builtin:for-loop') {
 		if(vertex.args.length < 2) {
 			return undefined;
@@ -44,7 +50,7 @@ export function onlyLoopsOnce(loop: NodeId, dataflow: DataflowGraph, controlflow
 			return undefined;
 		}
 
-		const values = valueSetGuard(resolveIdToValue(vectorOfLoop.nodeId, { graph: dataflow, idMap: dataflow.idMap, resolve: resolveConfig }));
+		const values = valueSetGuard(resolveIdToValue(vectorOfLoop.nodeId, { graph: dataflow, idMap: dataflow.idMap, resolve: config.solver.variables }));
 		if(values === undefined || values.elements.length !== 1 || values.elements[0].type !== 'vector' || !isValue(values.elements[0].elements)) {
 			return undefined;
 		}
@@ -55,12 +61,75 @@ export function onlyLoopsOnce(loop: NodeId, dataflow: DataflowGraph, controlflow
 	}
 
 	// 2. Use CFG Visitor to determine if loop always exits after the first iteration
+	const visitor = new CfgSingleIterationLoopDetector(loop, {
+		controlFlow:          controlflow,
+		normalizedAst:        ast,
+		dfg:                  dataflow,
+		flowrConfig:          config,
+		defaultVisitingOrder: 'forward'
+	});
 
+	return visitor.loopsOnlyOnce();
 }
 
-
 class CfgSingleIterationLoopDetector extends SemanticCfgGuidedVisitor {
-	protected override startVisitor(start: readonly NodeId[]): void {
-		
+	
+	private onlyLoopyOnce = false;
+
+	private loopToCheck: NodeId;
+
+
+	constructor(loop: NodeId, config: SemanticCfgGuidedVisitorConfiguration) {
+		super(config);
+		this.loopToCheck = loop;
+	}
+
+	private getBoolArgValue(data: { call: DataflowGraphVertexFunctionCall }): boolean | undefined {
+		if(data.call.args.length !== 1 || data.call.args[0] === EmptyArgument) {
+			return undefined;
+		}
+
+		const values = valueSetGuard(resolveIdToValue(data.call.args[0].nodeId, { graph: this.config.dfg, full: true, idMap: this.config.normalizedAst.idMap, resolve: this.config.flowrConfig.solver.variables  }));
+		if(values === undefined || values.elements.length !== 1 || values.elements[0].type != 'logical'  || !isValue(values.elements[0].value)) {
+			return undefined;
+		}
+
+		return Boolean(values.elements[0].value);
+	}
+
+	protected onDefaultFunctionCall(data: { call: DataflowGraphVertexFunctionCall; }): void {
+		let stopsLoop = false;
+
+		const alwaysHappens = () => {
+			if(!data.call.cds || 
+				(data.call.cds.length === 1 && data.call.cds[0].id === this.loopToCheck)) {
+				return true;
+			}
+
+			return happensInEveryBranch(data.call.cds);
+		};
+
+		switch(data.call.origin[0]) {
+			case 'builtin:return':
+			case 'builtin:stop':
+			case 'builtin:break':
+				stopsLoop = alwaysHappens();
+				break;
+			case 'builtin:stopifnot': {
+				const arg = this.getBoolArgValue(data);
+				if(arg !== undefined) {
+					stopsLoop = !arg;
+				}
+				break;
+			}
+		}
+			
+			
+		this.onlyLoopyOnce = this.onlyLoopyOnce || stopsLoop;
+	}
+
+	public loopsOnlyOnce(): boolean {
+		this.startVisitor([this.loopToCheck]);
+		return this.onlyLoopyOnce;
 	}
 }
--- test/functionality/control-flow/useless-loop.test.ts
+++ test/functionality/control-flow/useless-loop.test.ts
@@ -17,15 +17,43 @@ describe('One Iteration Loop Detection', withTreeSitter(shell => {
 			}, defaultConfigOptions).allRemainingSteps();
 			const cfg = extractCfg(result.normalize, defaultConfigOptions, result.dataflow.graph);
 
-			const actual = onlyLoopsOnce(slicingCriterionToId(node, result.normalize.idMap), result.dataflow.graph, cfg, defaultConfigOptions.solver.variables);
+			const actual = onlyLoopsOnce(slicingCriterionToId(node, result.normalize.idMap), result.dataflow.graph, cfg, result.normalize, defaultConfigOptions);
 			assert(actual === expectedLoopsOnlyOnce, `Expected to ${expectedLoopsOnlyOnce ? 'loop only once' : 'loop multiple times'}`);
 		});
 	}
 
+	describe('Simple Cases', () => {
+		checkLoop('Simple For',                'for(i in c(1)) { print(i) }',                          '1@for',    true);	
+		
+		// works after #1858 is merged
+		// checkLoop('Simple For with Alias', 'x <- c(1); for(i in x) { print(i) }', '1@for', true);
+		
+		checkLoop('Always Break',              'repeat { print(42); break; }',                         '1@repeat', true);
+	});
 
-	checkLoop('Simple For',            'for(i in c(1)) { print(i) }',         '1@for', true);
+
+	describe('Generated', () => {
+		const loopVariants = ['while (TRUE)', 'repeat', 'for (i in 1:10)', 'for (i in c(1,2))'];
+		const stopVariants = ['break', 'return(42)', 'stop(42)', 'stopifnot(FALSE)'];
+
+		for(const loop of loopVariants) {
+			for(const stop of stopVariants) {
+				const code = `${loop} { print(42); ${stop} }`;
+				checkLoop(code, code, `1@${loop.split(' ')[0]}`, true);
+			}
+		}
+	});
+
+	// Negative Tests
+	describe('Negative', () => {
+		checkLoop('Normal For',    'for (i in c(1,2)) { print(42); }',                                    '1@for',    false);
+		checkLoop('repeat',        'repeat { print(42); }',                                               '1@repeat', false);
+		checkLoop('while',         'while(TRUE) { print(42) }',                                           '1@while',  false);
+		checkLoop('unknown while', 'while(x) { print(42) }',                                              '1@while',  false);
 	
-	// works after #1858 is merged
-	// checkLoop('Simple For with Alias', 'x <- c(1); for(i in x) { print(i) }', '1@for', true);
+		checkLoop('Useful Loop before uselss', 'for (i in c(1,2)) { print(42); }\nrepeat { break; }',     '1@for',    false);
+
+		checkLoop('false break', 'for (i in c(1,2)) { if (FALSE) { break } }',                            '1@for',    false);
+	});
 
 }));

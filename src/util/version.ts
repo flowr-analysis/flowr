4b90535cf7c52b62784265e42235301a1dff6d35
GitHub Action <action@github.com>
2025-08-20 08:52:12 +0000

    [skip ci] Release v2.4.3

--- src/util/version.ts
+++ src/util/version.ts
@@ -1,7 +1,7 @@
 import { SemVer } from 'semver';
 
 // this is automatically replaced with the current version by release-it
-const version = '2.4.2';
+const version = '2.4.3';
 
 export function flowrVersion(): SemVer {
 	return new SemVer(version);

cbc22a549f2c2e50b8b23e3a8e4f6661d3566cad
snh21 <lars.pfrenger@uni-ulm.de>
2025-08-22 13:54:49 +0200

    test(lintr): add test for error handling - #1876

--- src/linter/linter-executor.ts
+++ src/linter/linter-executor.ts
@@ -9,17 +9,16 @@ import { deepMergeObject } from '../util/objects';
 import type { FlowrConfigOptions } from '../config';
 
 export function executeLintingRule<Name extends LintingRuleNames>(ruleName: Name, input: { normalize: NormalizedAst, dataflow: DataflowInformation, config: FlowrConfigOptions }, lintingRuleConfig?: DeepPartial<LintingRuleConfig<Name>>): LintingResults<Name> {
-	const rule = LintingRules[ruleName] as unknown as LintingRule<LintingRuleResult<Name>, LintingRuleMetadata<Name>, LintingRuleConfig<Name>>;
-	const fullConfig = deepMergeObject<LintingRuleConfig<Name>>(rule.info.defaultConfig, lintingRuleConfig);
-
-	const ruleSearch = rule.createSearch(fullConfig, input);
-
-	const searchStart = Date.now();
-	const searchResult = runSearch(ruleSearch, input);
-	const searchTime = Date.now() - searchStart;
-
-
 	try {
+		const rule = LintingRules[ruleName] as unknown as LintingRule<LintingRuleResult<Name>, LintingRuleMetadata<Name>, LintingRuleConfig<Name>>;
+		const fullConfig = deepMergeObject<LintingRuleConfig<Name>>(rule.info.defaultConfig, lintingRuleConfig);
+
+		const ruleSearch = rule.createSearch(fullConfig, input);
+
+		const searchStart = Date.now();
+		const searchResult = runSearch(ruleSearch, input);
+		const searchTime = Date.now() - searchStart;
+
 		const processStart = Date.now();
 		const result = rule.processSearchResult(searchResult, fullConfig, input);
 		const processTime = Date.now() - processStart;

c42e9427fa1d59a23cf203d071f4f5b0db75436f
Johanna <johanna.scheck@uni-ulm.de>
2025-09-09 20:09:54 +0200

    test(linter): added network functions linter tests - #1813

--- test/functionality/linter/lint-network-functions.test.ts
+++ test/functionality/linter/lint-network-functions.test.ts
@@ -38,11 +38,78 @@ describe('flowR linter', withTreeSitter(parser => {
 				{ totalCalls: 1, totalFunctionDefinitions: 1 },
 				{ fns: ['read.csv'] }
 			);
+
+			assertLinter(`network funcion with multiple arguments: ${prefix}`, parser, `download.file("${prefix}foo.org/bar.csv", "local.csv")`,
+				'network-functions',
+				[ 
+					{ certainty: LintingResultCertainty.Certain, function: 'download.file', range: [1,1,1,prefix.length+45] }
+				],
+				{ totalCalls: 1, totalFunctionDefinitions: 1 },
+				{ fns: ['download.file'] }
+			);
+
 		}
+		assertLinter('namespace call', parser, 'httr::GET("http://example.com")',
+			'network-functions',
+			[ 
+				{ certainty: LintingResultCertainty.Certain, function: 'httr::GET', range: [1,1,1,31] }
+			],
+			{ totalCalls: 1, totalFunctionDefinitions: 1 },
+			{ fns: ['GET'] }
+		);
+
 		assertLinter('do not trigger without url prefix', parser, 'read.csv("www.example.com")',
 			'network-functions',
 			[],
 			{ totalCalls: 0, totalFunctionDefinitions: 0 },
+			{ fns: ['read.csv'] }
+		);
+
+		assertLinter('do not trigger with multiple arguments', parser, 'download.file("data/local.csv", "local.csv")',
+			'network-functions',
+			[],
+			{ totalCalls: 0, totalFunctionDefinitions: 0 },
+			{ fns: ['read.csv'] }
+		);
+
+		assertLinter('not in list test', parser, 'file("data/local.csv")',
+			'network-functions',
+			[],
+			{ totalCalls: 0, totalFunctionDefinitions: 0 },
+			{ fns: NETWORK_FUNCTIONS.info.defaultConfig.fns }
+		);
+
+		assertLinter('nor in list but prefix in string', parser, 'print("http://example.com")',
+			'network-functions',
+			[],
+			{ totalCalls: 0, totalFunctionDefinitions: 0 },
+			{ fns: NETWORK_FUNCTIONS.info.defaultConfig.fns }
+		);
+
+		assertLinter('Named argument', parser, 'read.csv(file = "http://example.com/data.csv")',
+			'network-functions',
+			[ 
+				{ certainty: LintingResultCertainty.Certain, function: 'read.csv', range: [1,1,1,46] }
+			],
+			{ totalCalls: 1, totalFunctionDefinitions: 1 },
+			{ fns: ['read.csv'] }
+		);
+
+		assertLinter('Resolve value', parser, 'url <- "http://example.com/data.csv"; read.csv(url)',
+			'network-functions',
+			[ 
+				{ certainty: LintingResultCertainty.Certain, function: 'read.csv', range: [1,39,1,51] }
+			],
+			{ totalCalls: 1, totalFunctionDefinitions: 1 },
+			{ fns: ['read.csv'] }
+		);
+
+		assertLinter('Alias detections', parser, 'f <- read.csv; f("http://example.com/data.csv")',
+			'network-functions',
+			[ 
+				{ certainty: LintingResultCertainty.Certain, function: 'read.csv', range: [1,16,1,47] }
+			],
+			{ totalCalls: 1, totalFunctionDefinitions: 1 },
 			{ fns: ['read.csv'] }
 		);
 	});

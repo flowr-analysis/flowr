59c21c1bea7c07b7d9a84d317bb9d95756dda9b7
snh21 <lars.pfrenger@uni-ulm.de>
2025-09-16 14:23:19 +0200

    feat(formats): parse options and inline code blocks - #1931

--- src/util/formats/adapters/rmd-adapter.ts
+++ src/util/formats/adapters/rmd-adapter.ts
@@ -13,21 +13,19 @@ export interface RmdInfo {
 	options: object
 }
 
-// TODO: Also parse inline code 
-// TODO: Parse Options
-// TODO: Parse InBody Options
-
 export const RmdAdapter = {
 	readFile: (p: string) => {
+		// Read and Parse Markdown
 		const raw = fs.readFileSync(p).toString();
 		const parser = new Parser();
 		const ast = parser.parse(raw);
 		
+		// Parse Frontmatter
 		const frontmatter = matter(raw);
 
-		const blocks: CodeBlock[] = [];
-	
+		// Parse Codeblocks
 		const walker = ast.walker();
+		const blocks: CodeBlock[] = [];
 		let e;
 		while((e = walker.next())) {
 			const node = e.node;
@@ -40,17 +38,44 @@ export const RmdAdapter = {
 				}
 
 				blocks.push({
-					code:    node.literal ?? '',
-					options: node.info ?? ''
+					code:    node.literal,
+					options: parseOptions(node.info, node.literal)
 				});
+			} else if(node.type === 'code') {
+				if(node.literal && node.literal.startsWith('r ')) {
+					blocks.push({
+						code:    node.literal.substring(2),
+						options: ''
+					});
+				}
 			}
 		}
 
 		return {
 			type:    '.Rmd',
-			code:    blocks.map(b => b.code).join(),
+			code:    blocks.map(b => b.code).join('\n'),
 			blocks:  blocks,
 			options: frontmatter.data
 		};
 	}
 } as FileAdapter<RmdInfo>;
+
+
+function parseOptions(header: string, content: string): string {
+	let opts = header.length === 3 
+		? '' 
+		: header.substring(3, header.length-1).trim();
+	
+	const lines = content.split('\n');
+	for(const line of lines) {
+		if(!line.trim().startsWith('#|')) {
+			break;
+		}
+
+		const opt = line.substring(3);
+
+		opts += opts.length === 0 ? `${opt}` : `, ${opt}`;
+	}	
+
+	return opts;
+}
--- test/functionality/util/formats/rmd-format.test.ts
+++ test/functionality/util/formats/rmd-format.test.ts
@@ -1,9 +1,26 @@
-import { describe, test } from 'vitest';
+import { assert, describe, test } from 'vitest';
 import { readFile } from '../../../../src/util/formats/adapter';
+import type { RmdInfo } from '../../../../src/util/formats/adapters/rmd-adapter';
+import type { FileData } from '../../../../src/util/formats/adapter-format';
 
 describe('rmd', () => { 
 	test('simple', () => {
-		const out = readFile('test/testfiles/example.Rmd');
-		console.log(out);
+		const data = readFile('test/testfiles/example.Rmd');
+
+		assert.deepEqual(data, {
+			type:   '.Rmd',
+			code:   'test <- 42\n\ntest\nx <- "Hello World"\n\ncat("Hi")\n\n#| cache=FALSE\ncat("hi!")\n',
+			blocks: [
+				{ code: 'test <- 42\n', options: '' },
+				{ code: 'test', options: '' },
+				{ code: 'x <- "Hello World"\n', options: 'abc' },
+				{ code: 'cat("Hi")\n', options: 'ops, echo=FALSE' },
+				{
+					code:    '#| cache=FALSE\ncat("hi!")\n',
+					options: 'echo=FALSE, cache=FALSE'
+				}
+			],
+			options: { title: 'Sample Document', output: 'pdf_document' }
+		} as FileData<RmdInfo>);
 	});
 });

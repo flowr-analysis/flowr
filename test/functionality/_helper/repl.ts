dcbc0f26608723a568387b007b04c97651257eea
MaxAtoms <7847075+MaxAtoms@users.noreply.github.com>
2025-10-28 20:53:57 +0100

    test(repl): add config query completion tests

--- test/functionality/_helper/repl.ts
+++ test/functionality/_helper/repl.ts
@@ -3,15 +3,18 @@ import { defaultConfigOptions } from '../../../src/config';
 import type { CommandCompletions } from '../../../src/cli/repl/core';
 import { expect, test } from 'vitest';
 
-export function assertReplCompletions(
-	completer: (splitLine: readonly string[], startingNewArg: boolean, config: FlowrConfigOptions) => CommandCompletions,
-	label: string,
-	startingNewArg: boolean,
-	splitLine: readonly string[],
+export interface ReplCompletionTestCase {
+	completer:           (splitLine: readonly string[], startingNewArg: boolean, config: FlowrConfigOptions) => CommandCompletions,
+	label:               string,
+	startingNewArg:      boolean,
+	config?:             object,
+	splitLine:           readonly string[],
 	expectedCompletions: readonly string[]
-) {
+}
+
+export function assertReplCompletions({ completer, label, startingNewArg, splitLine, config = defaultConfigOptions, expectedCompletions }: ReplCompletionTestCase) {
 	test(label, () => {
-		const result = completer(splitLine, startingNewArg, defaultConfigOptions);
+		const result = completer(splitLine, startingNewArg, config as FlowrConfigOptions);
 		expect(result.completions).toEqual(expectedCompletions);
 	});
 }
--- test/functionality/cli/repl/linter-query-repl.test.ts
+++ test/functionality/cli/repl/linter-query-repl.test.ts
@@ -5,16 +5,72 @@ import { fileProtocol } from '../../../../src/r-bridge/retriever';
 import { LintingRules } from '../../../../src/linter/linter-rules';
 
 describe('Linter Query REPL Completions', () => {
-	const c = SupportedQueries['linter'].completer;
-	const all = Object.keys(LintingRules);
-	assertReplCompletions(c, 'empty arguments',                  true,   [''],                                    ['rules:']);
-	assertReplCompletions(c, 'partial prefix',                   false,  ['r'],                                   ['rules:']);
-	assertReplCompletions(c, 'no rules',                         false,  ['rules:'],                              all);
-	assertReplCompletions(c, 'partial rule',                     false,  ['rules:d'],                             all);
-	assertReplCompletions(c, 'partial unique rule',              false,  ['rules:dead'],                          all);
-	assertReplCompletions(c, 'multiple rules, one partial',      false,  ['rules:dead-code,file-path-val'],       all.filter(l => !(l === 'dead-code')));
-	assertReplCompletions(c, 'multiple rules, no new one',       false,  ['rules:dead-code,file-path-validity'],  [',']);
-	assertReplCompletions(c, 'multiple rules, starting new one', false,  ['rules:dead-code,file-path-validity,'], all.filter(l => !['dead-code','file-path-validity'].includes(l)));
-	assertReplCompletions(c, 'all rules used',                   false,  [`rules:${all.join(',')}`],              [' ']);
-	assertReplCompletions(c, 'rules finished', 		     true,   ['rules:dead'],                          [fileProtocol]);
+	const completer = SupportedQueries['linter'].completer;
+	const allRules = Object.keys(LintingRules);
+	assertReplCompletions({ completer,
+		label:               'empty arguments',
+		startingNewArg:      true,
+		splitLine:           [''],
+		expectedCompletions: ['rules:']
+	});
+	assertReplCompletions({ completer,
+		label:               'partial prefix',
+		startingNewArg:      false,
+		splitLine:           ['r'],
+		expectedCompletions: ['rules:']
+	});
+	assertReplCompletions({ completer,
+		label:               'no rules',
+		startingNewArg:      false,
+		splitLine:           ['rules:'],
+		expectedCompletions: allRules
+	});
+	assertReplCompletions({ completer,
+		label:               'partial rule',
+		startingNewArg:      false,
+		splitLine:           ['rules:d'],
+		expectedCompletions: allRules
+	});
+	assertReplCompletions({ completer,
+		label:               'partial unique rule',
+		startingNewArg:      false,
+		splitLine:           ['rules:dead'],
+		expectedCompletions: allRules
+	});
+	assertReplCompletions({ completer,
+		label:               'multiple rules, one partial',
+		startingNewArg:      false,
+		splitLine:           ['rules:dead-code,file-path-val'],
+		expectedCompletions: allRules.filter(l => !(l === 'dead-code'))
+	});
+	assertReplCompletions({ completer,
+		label:               'multiple rules, no new one',
+		startingNewArg:      false,
+		splitLine:           ['rules:dead-code,file-path-validity'],
+		expectedCompletions: [',']
+	});
+	assertReplCompletions({ completer,
+		label:               'multiple rules, starting new one',
+		startingNewArg:      false,
+		splitLine:           ['rules:dead-code,file-path-validity,'],
+		expectedCompletions: allRules.filter(l => !['dead-code','file-path-validity'].includes(l))
+	});
+	assertReplCompletions({ completer,
+		label:               'all rules used',
+		startingNewArg:      false,
+		splitLine:           [`rules:${allRules.join(',')}`],
+		expectedCompletions: [' ']
+	});
+	assertReplCompletions({ completer,
+		label:               'rules finished',
+		startingNewArg:      true,
+		splitLine:           ['rules:dead'],
+		expectedCompletions: [fileProtocol]
+	});
+	assertReplCompletions({ completer,
+		label:               'rules finished',
+		startingNewArg:      true,
+		splitLine:           ['rules:dead'],
+		expectedCompletions: [fileProtocol]
+	});
 });

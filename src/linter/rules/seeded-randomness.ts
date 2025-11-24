c89e33447d59dead8b47ce85c301c15e3740878c
Ell (Julian) <me@ellpeck.de>
2025-11-24 15:53:00 +0100

    Improve unseeded randomness rule to include potential unreachability check (#2019)
    * feat: also give enrichers access to the flowr config
    
    * refactor: reuse the existing cfg for last-call
    
    * feat-fix: account for branch coverage in seeded randomness rule
    
    * test: add a test that doesn't work lol
    
    * feat-fix: this seems even worse ngl
    
    * refactor: some improvements based on flos opinions
    
    * feat-fix: property test for control dependency overlap
    
    * feat: demote unclear branch dependencies to unclear certainty
    
    * test: clean up and additional seeded randomness tests
    
    * refactor: style cleanup

--- src/linter/rules/seeded-randomness.ts
+++ src/linter/rules/seeded-randomness.ts
@@ -18,6 +18,7 @@ import { VariableResolve } from '../../config';
 import type { DataflowGraphVertexFunctionCall } from '../../dataflow/graph/vertex';
 import { EmptyArgument } from '../../r-bridge/lang-4.x/ast/model/nodes/r-function-call';
 import { asValue } from '../../dataflow/eval/values/r-value';
+import { happensInEveryBranch } from '../../dataflow/info';
 
 export interface SeededRandomnessResult extends LintingResult {
 	function: string
@@ -42,6 +43,7 @@ export interface SeededRandomnessMeta extends MergeableRecord {
 	callsWithFunctionProducers:    number
 	callsWithAssignmentProducers:  number
 	callsWithNonConstantProducers: number
+	callsWithOtherBranchProducers: number
 }
 
 export const SEEDED_RANDOMNESS = {
@@ -65,7 +67,8 @@ export const SEEDED_RANDOMNESS = {
 			consumerCalls:                 0,
 			callsWithFunctionProducers:    0,
 			callsWithAssignmentProducers:  0,
-			callsWithNonConstantProducers: 0
+			callsWithNonConstantProducers: 0,
+			callsWithOtherBranchProducers: 0
 		};
 		return {
 			results: elements.getElements()
@@ -79,17 +82,25 @@ export const SEEDED_RANDOMNESS = {
 					};
 				}))
 				// filter by calls that aren't preceded by a randomness producer
-				.filter(element => {
+				.flatMap(element => {
+					const dfgElement = dataflow.graph.getVertex(element.searchElement.node.info.id);
+					const cds = dfgElement ? new Set(dfgElement.cds) : new Set();
 					const producers = enrichmentContent(element.searchElement, Enrichment.LastCall).linkedIds
 						.map(e => dataflow.graph.getVertex(e.node.info.id) as DataflowGraphVertexFunctionCall);
 					const { assignment, func } = Object.groupBy(producers, f => assignmentArgIndexes.has(f.name) ? 'assignment' : 'func');
 					let nonConstant = false;
+					let otherBranch = false;
 
 					// function calls are already taken care of through the LastCall enrichment itself
 					for(const f of func ?? []) {
 						if(isConstantArgument(dataflow.graph, f, 0)) {
-							metadata.callsWithFunctionProducers++;
-							return false;
+							const fCds = new Set(f.cds).difference(cds);
+							if(fCds.size <= 0 || happensInEveryBranch([...fCds])){
+								metadata.callsWithFunctionProducers++;
+								return [];
+							} else {
+								otherBranch = true;
+							}
 						} else {
 							nonConstant = true;
 						}
@@ -100,9 +111,15 @@ export const SEEDED_RANDOMNESS = {
 						const argIdx = assignmentArgIndexes.get(a.name) as number;
 						const dest = getReferenceOfArgument(a.args[argIdx]);
 						if(dest !== undefined && assignmentProducers.has(recoverName(dest, dataflow.graph.idMap) as string)){
+							// we either have arg index 0 or 1 for the assignmentProducers destination, so we select the assignment value as 1-argIdx here
 							if(isConstantArgument(dataflow.graph, a, 1-argIdx)) {
-								metadata.callsWithAssignmentProducers++;
-								return false;
+								const aCds = new Set(a.cds).difference(cds);
+								if(aCds.size <= 0 || happensInEveryBranch([...aCds])) {
+									metadata.callsWithAssignmentProducers++;
+									return [];
+								} else {
+									otherBranch = true;
+								}
 							} else {
 								nonConstant = true;
 							}
@@ -112,14 +129,15 @@ export const SEEDED_RANDOMNESS = {
 					if(nonConstant) {
 						metadata.callsWithNonConstantProducers++;
 					}
-					return true;
-				})
-
-				.map(element => ({
-					certainty: LintingResultCertainty.Certain,
-					function:  element.target,
-					range:     element.range
-				})),
+					if(otherBranch) {
+						metadata.callsWithOtherBranchProducers++;
+					}
+					return [{
+						certainty: otherBranch ? LintingResultCertainty.Uncertain : LintingResultCertainty.Certain,
+						function:  element.target,
+						range:     element.range
+					}];
+				}),
 			'.meta': metadata
 		};
 	},

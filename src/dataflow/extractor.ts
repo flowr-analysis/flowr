a4796f3131a1e2a0e43fef8cca2ce5811b8c2cb7
jonas kruger <jonas.kruger@uni-ulm.de>
2025-11-27 22:55:51 +0100

    feat(parallel-dataflow): implemented basic usage of threadpool
    basic dispatch of all files to analyze to threadpool.
    currently fails, because path to worker file is not correctly resolved
    diff --git a/src/dataflow/extractor.ts b/src/dataflow/extractor.ts
    index 34dfe000..83f6be32 100644
    --- a/src/dataflow/extractor.ts
    +++ b/src/dataflow/extractor.ts
    @@ -26,6 +26,8 @@ import type { ControlFlowInformation } from '../control-flow/control-flow-graph'
     import { getBuiltInDefinitions } from './environments/built-in-config';
     import type { FlowrAnalyzerContext } from '../project/context/flowr-analyzer-context';
     import { FlowrFile } from '../project/context/flowr-file';
    +import { Threadpool } from './parallel/threadpool';
    +import { SourceFilePayload } from './parallel/task-registry';
    
     /**
      * The best friend of {@link produceDataFlowGraph} and {@link processDataflowFor}.
    @@ -118,6 +120,20 @@ export function produceDataFlowGraph<OtherInfo>(
     	};
     	let df = processDataflowFor<OtherInfo>(files[0].root, dfData);
    
    +	// first call with threadpool
    +	const pool = new Threadpool();
    +
    +	// submit all files
    +	const result = pool.submitTasks<SourceFilePayload<OtherInfo>, void>(
    +		"testPool",
    +		files.map((file, i) => ({
    +			index: i,
    +			file,
    +			data: dfData,
    +			dataflowInfo: df,
    +		}))
    +	)
    +
     	for(let i = 1; i < files.length; i++) {
     		/* source requests register automatically */
     		df = standaloneSourceFile(i, files[i], dfData, df);

--- src/dataflow/extractor.ts
+++ src/dataflow/extractor.ts
@@ -26,6 +26,8 @@ import type { ControlFlowInformation } from '../control-flow/control-flow-graph'
 import { getBuiltInDefinitions } from './environments/built-in-config';
 import type { FlowrAnalyzerContext } from '../project/context/flowr-analyzer-context';
 import { FlowrFile } from '../project/context/flowr-file';
+import { Threadpool } from './parallel/threadpool';
+import { SourceFilePayload } from './parallel/task-registry';
 
 /**
  * The best friend of {@link produceDataFlowGraph} and {@link processDataflowFor}.
@@ -117,6 +119,20 @@ export function produceDataFlowGraph<OtherInfo>(
 		ctx
 	};
 	let df = processDataflowFor<OtherInfo>(files[0].root, dfData);
+
+	// first call with threadpool
+	const pool = new Threadpool();
+
+	// submit all files
+	const result = pool.submitTasks<SourceFilePayload<OtherInfo>, void>(
+		"testPool",
+		files.map((file, i) => ({
+			index: i,
+			file,
+			data: dfData,
+			dataflowInfo: df,
+		}))
+	)
 
 	for(let i = 1; i < files.length; i++) {
 		/* source requests register automatically */

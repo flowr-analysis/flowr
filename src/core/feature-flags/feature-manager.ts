d219eebb57ddedc8f931d3a865643b6f3c40c549
jonas kruger <jonas.kruger@uni-ulm.de>
2025-12-08 21:54:24 +0100

    refactor(parallel-dataflow): linter fixes

--- src/core/feature-flags/feature-manager.ts
+++ src/core/feature-flags/feature-manager.ts
@@ -11,27 +11,27 @@ const mutableFlags = { ...featureFlags };
  *  Feature Manager object to handle interacting with Feature Flags
  */
 export const FeatureManager = {
-    /**
-     * Checks the provided feature Flag
-     * @param flag - feature to check for
-     * @returns status of the feature selection
-     */
+	/**
+	 * Checks the provided feature Flag
+	 * @param flag - feature to check for
+	 * @returns status of the feature selection
+	 */
 	isEnabled(flag: FeatureFlag): boolean {
 		return mutableFlags[flag];
 	},
 
-    /**
-     * Sets the status selection flag of a feature
-     * @param flag - feature to set status for
-     * @param value - value to set
-     */
+	/**
+	 * Sets the status selection flag of a feature
+	 * @param flag - feature to set status for
+	 * @param value - value to set
+	 */
 	setFlag(flag: FeatureFlag, value: boolean): void {
 		mutableFlags[flag] = value;
 	},
 
-    /**
-     * Tries to load the state of each feature flag from the enviroment. If present, the default status is overwritten.
-     */
+	/**
+	 * Tries to load the state of each feature flag from the enviroment. If present, the default status is overwritten.
+	 */
 	loadFromEnv(){
 		(Object.keys(mutableFlags) as FeatureFlag[]).forEach(key => {
 			const envValue = process.env[`FEATURE_${key.toUpperCase()}`];

5dacc81353030a2eaf2e0d00b244cd358a247d0d
jonas kruger <jonas.kruger@uni-ulm.de>
2025-12-10 15:17:02 +0100

    refactor(parallel-dataflow): feature manager now own class
    feature manager is now a class and not an global object

--- src/core/feature-flags/feature-manager.ts
+++ src/core/feature-flags/feature-manager.ts
@@ -1,24 +1,29 @@
 import type { FeatureFlag } from './feature-def';
 import { featureFlags } from './feature-def';
 
-/**
- * Mutable version of the feature Flags for type safety
- */
-const mutableFlags = { ...featureFlags };
 
 
 /**
  *  Feature Manager object to handle interacting with Feature Flags
- */
-export const FeatureManager = {
+*/
+export class FeatureManager {
+	/**
+	 * Mutable version of the feature Flags for type safety
+	 */
+	private mutableFlags;
+
+	constructor(flags?: typeof featureFlags){
+		this.mutableFlags = {...(flags ?? featureFlags)};
+	}
+
 	/**
 	 * Checks the provided feature Flag
 	 * @param flag - feature to check for
 	 * @returns status of the feature selection
 	 */
 	isEnabled(flag: FeatureFlag): boolean {
-		return mutableFlags[flag];
-	},
+		return this.mutableFlags[flag];
+	}
 
 	/**
 	 * Sets the status selection flag of a feature
@@ -26,17 +31,17 @@ export const FeatureManager = {
 	 * @param value - value to set
 	 */
 	setFlag(flag: FeatureFlag, value: boolean): void {
-		mutableFlags[flag] = value;
-	},
+		this.mutableFlags[flag] = value;
+	}
 
 	/**
 	 * Tries to load the state of each feature flag from the enviroment. If present, the default status is overwritten.
 	 */
 	loadFromEnv(){
-		(Object.keys(mutableFlags) as FeatureFlag[]).forEach(key => {
+		(Object.keys(this.mutableFlags) as FeatureFlag[]).forEach(key => {
 			const envValue = process.env[`FEATURE_${key.toUpperCase()}`];
 			if( envValue !== undefined){
-				mutableFlags[key] = envValue === 'true';
+				this.mutableFlags[key] = envValue === 'true';
 			}
 		});
 	}

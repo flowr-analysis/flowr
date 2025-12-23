31e230d0a94fee9ab13fc316d3b33eacc58cd8c5
Florian Sihler <florian.sihler@uni-ulm.de>
2025-12-23 15:37:27 +0100

    [Fix] improve subselection of cfg serialization (#2116)
    feat-fix: improve subselection of cfg serialization

--- src/util/mermaid/cfg.ts
+++ src/util/mermaid/cfg.ts
@@ -2,17 +2,17 @@ import { escapeMarkdown, mermaidCodeToUrl } from './mermaid';
 import type { NormalizedAst, RNodeWithParent } from '../../r-bridge/lang-4.x/ast/model/processing/decorate';
 import { RType } from '../../r-bridge/lang-4.x/ast/model/type';
 import {
-	type CfgSimpleVertex,
-	type ControlFlowInformation
-	,
 	CfgEdgeType,
+	type CfgSimpleVertex,
 	CfgVertexType,
+	type ControlFlowInformation,
 	edgeTypeToString
 } from '../../control-flow/control-flow-graph';
 import type { NodeId } from '../../r-bridge/lang-4.x/ast/model/processing/node-id';
 import { reconstructToCode } from '../../reconstruct/reconstruct';
 import { doNotAutoSelect } from '../../reconstruct/auto-select/auto-select-defaults';
 import { MermaidDefaultMarkStyle, type MermaidGraphPrinterInfo } from './info';
+import { collectAllIds } from '../../r-bridge/lang-4.x/ast/model/collect';
 
 function getLexeme(n?: RNodeWithParent) {
 	return n ? n.info.fullLexeme ?? n.lexeme ?? '' : undefined;
@@ -40,9 +40,33 @@ const getDirRegex = /flowchart\s+([A-Za-z]+)/;
  * @param normalizedAst    - The normalized AST to use for the vertex content.
  * @param prefix           - The prefix to use for the mermaid string.
  * @param simplify         - Whether to simplify the control flow graph (especially in the context of basic blocks).
+ * @param markStyle        - The style to use for marked vertices and edges.
+ * @param includeOnlyIds   - If provided, only include the vertices with the given IDs.
+ * @param mark             - If provided, mark the given vertices and edges.
  */
 export function cfgToMermaid(cfg: ControlFlowInformation, normalizedAst: NormalizedAst, { prefix = 'flowchart BT\n', simplify = false, markStyle = MermaidDefaultMarkStyle, includeOnlyIds, mark }: MermaidGraphPrinterInfo = {}): string {
 	let output = prefix;
+	if(includeOnlyIds) {
+		const completed = new Set(includeOnlyIds);
+		// foreach nast id we add all children
+		for(const id of includeOnlyIds.values()) {
+			const nastNode = normalizedAst.idMap.get(id);
+			if(!nastNode) {
+				continue;
+			}
+			const ids = collectAllIds(nastNode);
+			for(const childId of ids) {
+				completed.add(childId);
+			}
+		}
+		// if we have a filter, we automatically add all vertices in the cfg that are *markers* for these ids and
+		for(const [id,v] of cfg.graph.vertices()) {
+			if(v.type === CfgVertexType.EndMarker && completed.has(v.root)) {
+				completed.add(id);
+			}
+		}
+		includeOnlyIds = completed;
+	}
 
 	const dirIs: string = getDirRegex.exec(prefix)?.at(1) ?? 'LR';
 
@@ -91,7 +115,6 @@ export function cfgToMermaid(cfg: ControlFlowInformation, normalizedAst: Normali
 		if(includeOnlyIds && !includeOnlyIds.has(from)) {
 			continue;
 		}
-
 		for(const [to, edge] of targets) {
 			if(includeOnlyIds && !includeOnlyIds.has(to)) {
 				continue;

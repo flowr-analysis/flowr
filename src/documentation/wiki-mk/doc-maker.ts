2f68f44e953ebfc2a94a848ca13e7bee5d398281
Florian Sihler <florian.sihler@uni-ulm.de>
2025-12-30 10:26:22 +0100

    refactor: wiki update detection make quotes optional

--- src/documentation/wiki-mk/doc-maker.ts
+++ src/documentation/wiki-mk/doc-maker.ts
@@ -44,7 +44,7 @@ const DefaultReplacementPatterns: Array<[RegExp, string]> = [
 	// eslint-disable-next-line no-irregular-whitespace -- we may produce it in output
 	[/[0-9]+(\.[0-9]+)?( |\s*)?ms/g, ''],
 	[/tmp[%A-Za-z0-9-]+/g, ''],
-	[/"(timing|searchTimeMs|processTimeMs|id|treeSitterId)":\s*[0-9]+(\.[0-9])?,?/g, ''],
+	[/"?(timing|searchTimeMs|processTimeMs|id|treeSitterId)"?:\s*[0-9]+(\.[0-9])?,?/g, ''],
 	[/"format":"compact".+/gmius, ''],
 	[/%%\s*\d*-+/g, ''],
 	[/"[rR]": "\d+\.\d+\.\d+.*?"/g, ''],

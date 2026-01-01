c426547ffe490ccb91fce4e33bd72aa5bdcfe9e9
Florian Sihler <florian.sihler@uni-ulm.de>
2026-01-01 14:23:57 +0100

    feat: improved stopifnot handling

--- test/functionality/dataflow/processing-of-elements/exceptions/dataflow-exceptions.test.ts
+++ test/functionality/dataflow/processing-of-elements/exceptions/dataflow-exceptions.test.ts
@@ -5,6 +5,7 @@ import { FlowrAnalyzerBuilder } from '../../../../../src/project/flowr-analyzer-
 import { requestFromInput } from '../../../../../src/r-bridge/retriever';
 import type { SingleSlicingCriterion } from '../../../../../src/slicing/criterion/parse';
 import { Q } from '../../../../../src/search/flowr-search-builder';
+import { graphToMermaidUrl } from '../../../../../src/util/mermaid/dfg';
 
 interface DFConstraints {
 	hasVertices:         SingleSlicingCriterion[];
@@ -19,19 +20,35 @@ describe('Dataflow, Handle Exceptions', withTreeSitter(ts  => {
 			const analyzer = await new FlowrAnalyzerBuilder().setParser(ts).build();
 			analyzer.addRequest(requestFromInput(code));
 			const df = await analyzer.dataflow();
-			for(const [coll, shouldHave] of [[constraints.hasVertices, true], [constraints.doesNotHaveVertices, false]] as const) {
-				for(const v of coll) {
-					const resolved = (await analyzer.runSearch(Q.criterion(v))).getElements();
-					assert.lengthOf(resolved, 1);
-					const id = resolved[0].node.info.id;
-					const hasVertex = df.graph.hasVertex(id);
-					assert.strictEqual(hasVertex, shouldHave);
+			try {
+				for(const [coll, shouldHave] of [[constraints.hasVertices, true], [constraints.doesNotHaveVertices, false]] as const) {
+					for(const v of coll) {
+						const resolved = (await analyzer.runSearch(Q.criterion(v))).getElements();
+						assert.lengthOf(resolved, 1);
+						const id = resolved[0].node.info.id;
+						const hasVertex = df.graph.hasVertex(id);
+						assert.strictEqual(hasVertex, shouldHave);
+					}
 				}
+			} catch(e: unknown) {
+				console.error('DF', graphToMermaidUrl(df.graph));
+				throw e;
 			}
 		});
 	}
-	describe('Simple stops', () =>  {
-		checkDfContains('x <- 1\nstopifnot(TRUE)\n3',  { hasVertices: ['1@1', '3@3'],  doesNotHaveVertices: [] });
-		checkDfContains('x <- 1\nstopifnot(FALSE)\n3',  { hasVertices: ['1@1'],  doesNotHaveVertices: ['3@3'] });
+	describe('Simple stops', () => {
+		const trueVariants = ['stopifnot(TRUE)', 'if(FALSE) stop()', 'stopifnot(u)', 'stopifnot(TRUE, u)', 'stopifnot(TRUE, TRUE)', 'stopifnot(TRUE, T, TRUE)', 'stopifnot(t, t)', 'stopifnot(exprs={TRUE; TRUE})'];
+		const falseVariants = ['if(TRUE) stop()', 'stop()', 'stop(msg)', 'stopifnot(FALSE)', 'stopifnot(FALSE, u)', 'stopifnot(FALSE, TRUE)', 'stopifnot(FALSE, F, FALSE)', 'stopifnot(f, f)', 'stopifnot(exprs={FALSE; FALSE})'];
+		for(const [variant, exp] of [[trueVariants, true], [falseVariants, false]] as const) {
+			describe(exp ? 'Reachable' : 'Unreachable', () => {
+				for(const v of variant) {
+					checkDfContains(`x <- 1
+t <- TRUE
+f <- FALSE
+${v}
+3`, { hasVertices: exp ? ['1@1', '5@3'] : ['1@1'], doesNotHaveVertices: exp ? [] : ['5@3'] });
+				}
+			});
+		}
 	});
 }));

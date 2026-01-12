ac8202353f74ce6e4f52a3b7ad3feecfdb79823b
Luca Schlecker <luca@rfts.me>
2026-01-12 04:59:00 +0100

    feat(sde-query): use resolveIdToValue if string domains are disabled

--- src/queries/catalog/sde-query/sde-query-executor.ts
+++ src/queries/catalog/sde-query/sde-query-executor.ts
@@ -5,12 +5,18 @@ import type { SingleSlicingCriterion } from '../../../slicing/criterion/parse';
 import { slicingCriterionToId } from '../../../slicing/criterion/parse';
 import type { Lift, Value } from '../../../abstract-interpretation/eval/domain';
 import { Top } from '../../../abstract-interpretation/eval/domain';
+import type { ResolveResult } from '../../../dataflow/eval/resolve/alias-tracking';
+import { resolveIdToValue } from '../../../dataflow/eval/resolve/alias-tracking';
+import { isValue } from '../../../dataflow/eval/values/r-value';
+import { unescapeSpecialChars } from '../../../abstract-interpretation/data-frame/resolve-args';
+import type { RStringValue } from '../../../r-bridge/lang-4.x/convert-values';
+import type { ConstSet } from '../../../abstract-interpretation/eval/domains/constant-set';
 
 export function fingerPrintOfQuery(query: SdeQuery): string {
 	return JSON.stringify(query);
 }
 
-export function executeSdeQuery({ ast }: BasicQueryData, queries: readonly SdeQuery[]): SdeQueryResult {
+export function executeSdeQuery({ ast, config, dataflow }: BasicQueryData, queries: readonly SdeQuery[]): SdeQueryResult {
 	const start = Date.now();
 	const results = new Map<SingleSlicingCriterion, Lift<Value>>();
 
@@ -22,7 +28,18 @@ export function executeSdeQuery({ ast }: BasicQueryData, queries: readonly SdeQu
 			}
 			
 			const nodeId = slicingCriterionToId(criterion, ast.idMap);
-			const value = ast.idMap.get(nodeId)?.info.sdvalue as Lift<Value> | undefined ?? Top;
+			let value = ast.idMap.get(nodeId)?.info.sdvalue as Lift<Value> | undefined;
+			if(config.abstractInterpretation.string.enable) {
+				value ??= Top;
+			} else {
+				const resolved = resolveIdToValue(nodeId, {
+					resolve: config.solver.variables,
+					full:    true,
+					idMap:   ast.idMap,
+					graph:   dataflow.graph,
+				});
+				value ??= resolveResultToConstSet(resolved);
+			}
 			results.set(criterion, value);
 		}
 	}
@@ -33,4 +50,23 @@ export function executeSdeQuery({ ast }: BasicQueryData, queries: readonly SdeQu
 		},
 		results,
 	};
+}
+
+function resolveResultToConstSet(resolved: ResolveResult): Lift<Value> {
+	if(resolved.type !== 'set') {
+		return Top;
+	}
+	if(!resolved.elements.every(it => isValue(it) && it.type === 'string')) {
+		return Top;
+	}
+	const elements = resolved.elements;
+	if(!elements.every(it => isValue(it.value))) {
+		return Top;
+	}
+	const values = elements.map(it => it.value as RStringValue);
+	const value: ConstSet = {
+		kind:  'const-set',
+		value: values.map(it => unescapeSpecialChars(it.str))
+	};
+	return value;
 }

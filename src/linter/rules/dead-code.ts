f63a09035305c775cbe9921468d3aedf2b245e24
Ellpeck <me@ellpeck.de>
2026-01-26 15:03:05 +0100

    feat: include involved ids in dead-code linting rule

--- src/linter/rules/dead-code.ts
+++ src/linter/rules/dead-code.ts
@@ -1,5 +1,5 @@
 import { LintingResultCertainty, LintingPrettyPrintContext, type LintingResult, type LintingRule, LintingRuleCertainty } from '../linter-format';
-import { type SourceRange , combineRanges } from '../../util/range';
+import { type SourceRange, rangeIsSubsetOf } from '../../util/range';
 import type { MergeableRecord } from '../../util/objects';
 import { Q } from '../../search/flowr-search-builder';
 import { formatRange } from '../../util/mermaid/dfg';
@@ -7,9 +7,10 @@ import { LintingRuleTag } from '../linter-tags';
 import { Enrichment, enrichmentContent } from '../../search/search-executor/search-enrichers';
 import { isNotUndefined } from '../../util/assert';
 import { type CfgSimplificationPassName, DefaultCfgSimplificationOrder } from '../../control-flow/cfg-simplification';
+import type { Writable } from 'ts-essentials';
 
 export interface DeadCodeResult extends LintingResult {
-	range: SourceRange
+	readonly range: SourceRange
 }
 
 export interface DeadCodeConfig extends MergeableRecord {
@@ -34,20 +35,20 @@ export const DEAD_CODE = {
 			consideredNodes: 0
 		};
 		return {
-			results: combineRanges(
-				...new Set<SourceRange>(elements.getElements()
+			results: combineResults(
+				elements.getElements()
 					.filter(element => {
 						meta.consideredNodes++;
 						const cfgInformation = enrichmentContent(element, Enrichment.CfgInformation);
 						return cfgInformation.isRoot && !cfgInformation.isReachable;
 					})
-					.map(element => element.node.info.fullRange ?? element.node.location)
-					.filter(isNotUndefined)))
-				.map(range => ({
-					certainty:  LintingResultCertainty.Certain,
-					involvedId: undefined,
-					range
-				})),
+					.map(element => ({
+						certainty:  LintingResultCertainty.Certain,
+						involvedId: element.node.info.id,
+						range:      (element.node.info.fullRange ?? element.node.location) as SourceRange
+					}))
+					.filter(element => isNotUndefined(element.range))
+			),
 			'.meta': meta
 		};
 	},
@@ -64,3 +65,22 @@ export const DEAD_CODE = {
 		defaultConfig: {}
 	}
 } as const satisfies LintingRule<DeadCodeResult, DeadCodeMetadata, DeadCodeConfig>;
+
+function combineResults(results: Writable<DeadCodeResult>[]): DeadCodeResult[] {
+	for(let i = results.length-1; i >= 0; i--){
+		const result = results[i];
+		const other = results.find(other => result !== other && rangeIsSubsetOf(result.range, other.range));
+		if(other !== undefined) {
+			if(!Array.isArray(other.involvedId)) {
+				other.involvedId = other.involvedId !== undefined ? [other.involvedId] : [];
+			}
+			if(Array.isArray(result.involvedId)) {
+				other.involvedId.push(...result.involvedId);
+			} else if(result.involvedId !== undefined) {
+				other.involvedId.push(result.involvedId);
+			}
+			results.splice(i, 1);
+		}
+	}
+	return results;
+}

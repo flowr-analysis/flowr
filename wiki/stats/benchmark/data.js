63effeeefd2029242151437f7d50310d646aadc3
github-action-benchmark <github@users.noreply.github.com>
2026-01-27 11:06:32 +0000

    add "artificial" Benchmark Suite (customSmallerIsBetter) benchmark result for c47729a3b6133b052da8c87565bdaaddc3bfd740

--- wiki/stats/benchmark/data.js
+++ wiki/stats/benchmark/data.js
@@ -1,104 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1768736637273,
+  "lastUpdate": 1769511992215,
   "repoUrl": "https://github.com/flowr-analysis/flowr",
   "entries": {
     "\"artificial\" Benchmark Suite": [
-      {
-        "commit": {
-          "author": {
-            "email": "florian.sihler@uni-ulm.de",
-            "name": "Florian Sihler",
-            "username": "EagleoutIce"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "20612c4734312e5bbd8963132eb9d25802d2f8a8",
-          "message": "Fine tune benchmark plots with new colors, labels, and more (#338)\n\n* ci: cycle colors\r\n\r\n* ci: show commit message name start in benchmarks as well\r\n\r\n* ci-fix: npm script `performance-test` should be able to read arguments :D\r\n\r\n* ci, typo: fix wrong name for uploading of benchmark results\r\n\r\n* ci-fix: add `--` to separatae arguments in `qa.yaml`\r\n\r\n* ci: reset current benchmark data (Release v1.1.4)",
-          "timestamp": "2023-09-21T01:44:43+02:00",
-          "tree_id": "9f4b37285d7d1eb59c97e553f8fd0766c9bb1b06",
-          "url": "https://github.com/flowr-analysis/flowr/commit/20612c4734312e5bbd8963132eb9d25802d2f8a8"
-        },
-        "date": 1695255188267,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Total per-file",
-            "value": 8259.250394318182,
-            "unit": "ms",
-            "range": 4968.250512363158,
-            "extra": "median: 6847.55ms"
-          },
-          {
-            "name": "Retrieve AST from R code",
-            "value": 2117.869953409091,
-            "unit": "ms",
-            "range": 206.30641574206575,
-            "extra": "median: 2061.32ms"
-          },
-          {
-            "name": "Normalize R AST",
-            "value": 130.60619313636363,
-            "unit": "ms",
-            "range": 224.83856303552116,
-            "extra": "median: 83.93ms"
-          },
-          {
-            "name": "Produce dataflow information",
-            "value": 89.47369154545454,
-            "unit": "ms",
-            "range": 233.7696346539724,
-            "extra": "median: 24.78ms"
-          },
-          {
-            "name": "Total per-slice",
-            "value": 2.3299490711368147,
-            "unit": "ms",
-            "range": 1.788027577151099,
-            "extra": "median: 2.02ms"
-          },
-          {
-            "name": "Static slicing",
-            "value": 1.6894677939608982,
-            "unit": "ms",
-            "range": 1.6793862825524892,
-            "extra": "median: 1.33ms"
-          },
-          {
-            "name": "Reconstruct code",
-            "value": 0.6158474940073386,
-            "unit": "ms",
-            "range": 0.3802273110956727,
-            "extra": "median: 0.55ms"
-          },
-          {
-            "name": "failed to reconstruct/re-parse",
-            "value": 0,
-            "unit": "#",
-            "extra": "out of 7728 slices"
-          },
-          {
-            "name": "times hit threshold",
-            "value": 0,
-            "unit": "#"
-          },
-          {
-            "name": "reduction (characters)",
-            "value": 0.7329390759026896,
-            "unit": "#",
-            "extra": "std: 0.1494698363920617"
-          },
-          {
-            "name": "reduction (normalized tokens)",
-            "value": 0.720988345209971,
-            "unit": "#",
-            "extra": "std: 0.13987413604166843"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -10319,6 +10223,116 @@ window.BENCHMARK_DATA = {
             "range": "0.6812123226026012",
             "unit": "ms",
             "extra": "median: 0.45ms"
+          },
+          {
+            "name": "failed to reconstruct/re-parse",
+            "value": 0,
+            "unit": "#",
+            "extra": "out of 7728 slices"
+          },
+          {
+            "name": "times hit threshold",
+            "value": 0,
+            "unit": "#"
+          },
+          {
+            "name": "reduction (characters)",
+            "value": 0.7801660063880064,
+            "unit": "#",
+            "extra": "std: 0.1261285782699485"
+          },
+          {
+            "name": "reduction (normalized tokens)",
+            "value": 0.7575735855163274,
+            "unit": "#",
+            "extra": "std: 0.12942676908005035"
+          },
+          {
+            "name": "memory (df-graph)",
+            "value": 104.45432350852273,
+            "range": "266.0285210002154",
+            "unit": "KiB",
+            "extra": "median: 30.02"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "florian.sihler@uni-ulm.de",
+            "name": "Florian Sihler",
+            "username": "EagleoutIce"
+          },
+          "committer": {
+            "email": "florian.sihler@uni-ulm.de",
+            "name": "Florian Sihler",
+            "username": "EagleoutIce"
+          },
+          "distinct": true,
+          "id": "c47729a3b6133b052da8c87565bdaaddc3bfd740",
+          "message": "[release:patch] 2.8.10 Linting Improvements, Try-Calls, and Bug-Fixes",
+          "timestamp": "2026-01-27T11:48:58+01:00",
+          "tree_id": "7cfeaaaa7b524fddf3a804dbf99354d5b28a1ab5",
+          "url": "https://github.com/flowr-analysis/flowr/commit/c47729a3b6133b052da8c87565bdaaddc3bfd740"
+        },
+        "date": 1769511992206,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Retrieve AST from R code",
+            "value": 250.55679904545454,
+            "range": "113.98830480619539",
+            "unit": "ms",
+            "extra": "median: 219.02ms"
+          },
+          {
+            "name": "Normalize R AST",
+            "value": 19.517977954545454,
+            "range": "33.93518939584622",
+            "unit": "ms",
+            "extra": "median: 10.18ms"
+          },
+          {
+            "name": "Produce dataflow information",
+            "value": 50.40629504545455,
+            "range": "95.36836441423976",
+            "unit": "ms",
+            "extra": "median: 24.62ms"
+          },
+          {
+            "name": "Total per-file",
+            "value": 672.2245021363636,
+            "range": "1210.6187547653801",
+            "unit": "ms",
+            "extra": "median: 326.21ms"
+          },
+          {
+            "name": "Extract control flow graph",
+            "value": 8.865571636363637,
+            "range": "18.888133663779097",
+            "unit": "ms",
+            "extra": "median: 2.98ms"
+          },
+          {
+            "name": "Static slicing",
+            "value": 0.47530915200192736,
+            "range": "0.5437575076987039",
+            "unit": "ms",
+            "extra": "median: 0.19ms"
+          },
+          {
+            "name": "Reconstruct code",
+            "value": 0.2597652980503904,
+            "range": "0.19839129913122402",
+            "unit": "ms",
+            "extra": "median: 0.12ms"
+          },
+          {
+            "name": "Total per-slice",
+            "value": 0.7550596420002517,
+            "range": "0.7048013357977345",
+            "unit": "ms",
+            "extra": "median: 0.41ms"
           },
           {
             "name": "failed to reconstruct/re-parse",

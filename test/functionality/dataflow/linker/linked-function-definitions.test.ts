b1cfc01fb1c76991978c0afce6e80da272f62021
Florian Sihler <florian.sihler@uni-ulm.de>
2026-01-29 09:41:42 +0100

    test: improve explicit tests for call retrieval

--- test/functionality/dataflow/linker/linked-function-definitions.test.ts
+++ test/functionality/dataflow/linker/linked-function-definitions.test.ts
@@ -5,10 +5,12 @@ import { requestFromInput } from '../../../../src/r-bridge/retriever';
 import { tryResolveSliceCriterionToId } from '../../../../src/slicing/criterion/parse';
 import { graphToMermaidUrl } from '../../../../src/util/mermaid/dfg';
 import { getAllLinkedFunctionDefinitions } from '../../../../src/dataflow/internal/linker';
+import { builtInId } from '../../../../src/dataflow/environments/built-in';
+import { label } from '../../_helper/label';
 
 describe('Linked Function Definitions', withTreeSitter(ts => {
 	function expectLinkedFns(lab: string, code: string, expect: Record<string, { fns?: string[], bi?: string[] }>) {
-		it(lab, async() => {
+		it(label(lab, [], ['dataflow']), async() => {
 			const a = await new FlowrAnalyzerBuilder().setParser(ts).build();
 			a.addRequest(requestFromInput(code));
 			const df = await a.dataflow();
@@ -35,8 +37,23 @@ describe('Linked Function Definitions', withTreeSitter(ts => {
 	expectLinkedFns('one definition', 'x <- function(a) { a + 1 }', {
 		'1@x': { fns: ['1@function'] }
 	});
+	expectLinkedFns('alias bi', 'x <- c', {
+		'1@x': { bi: [builtInId('c')] }
+	});
+	expectLinkedFns('alias bi with sconst', 'if(u) c <- 3\nx <- c', {
+		'2@x': { bi: [builtInId('c')] }
+	});
 	expectLinkedFns('one definition ho', 'x <- function(a) { a + 1 }\ny <- function() { x } \nh <- y()', {
 		'3@h': { fns: ['1@function'] }
+	});
+	expectLinkedFns('hidden with get', 'x <- function(a) { a + 1 }\ny <- function() { get("x") } \nh <- y()', {
+		'3@h': { fns: ['1@function'] }
+	});
+	expectLinkedFns('hidden with side-effect', 'y <- function() {\nx <<- function(a) { a + 1 } }\ny()\nh <- x', {
+		'4@h': { fns: ['2@function'] }
+	});
+	expectLinkedFns('unreturned fn', 'y <- function() {\nfunction(); 2 }\nh <- y()', {
+		'4@h': { fns: [] }
 	});
 	expectLinkedFns('multiple defs', 'x <- function(a) { a + 1 }\nk <- function() function() 2\ny <- function() { if(u) { x } else { k() } } \nh <- y()', {
 		'4@h': { fns: ['1@function', '2@function'] }

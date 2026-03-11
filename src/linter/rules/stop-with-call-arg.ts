cac1d77298d321ace52672a3bdba57c7bf6e6564
Naomi Panda <naomi.panda@uni-ulm.de>
2026-03-11 18:12:28 +0100

    feat: find stop() where not call.=FALSE

--- src/linter/rules/stop-with-call-arg.ts
+++ src/linter/rules/stop-with-call-arg.ts
@@ -11,7 +11,12 @@ import { Q } from '../../search/flowr-search-builder';
 import { LintingRuleTag } from '../linter-tags';
 import { isNotUndefined } from '../../util/assert';
 import type { Writable } from 'ts-essentials';
+import type { DataflowGraphVertexFunctionCall } from '../../dataflow/graph/vertex';
 import { VertexType } from '../../dataflow/graph/vertex';
+import { FunctionArgument } from '../../dataflow/graph/graph';
+import { resolveIdToValue } from '../../dataflow/eval/resolve/alias-tracking';
+import type { NodeId } from '../../r-bridge/lang-4.x/ast/model/processing/node-id';
+import { getOriginInDfg, OriginType } from '../../dataflow/origin/dfg-get-origin';
 
 export interface StopWithCallResult extends LintingResult {
 	readonly loc: SourceLocation
@@ -34,6 +39,37 @@ export const STOP_WITH_CALL_ARG = {
 		return {
 			results:
 				elements.getElements()
+					.filter(element => {
+						//only built-in functions
+						const origins = getOriginInDfg(_data.dataflow.graph, element.node.info.id);
+						if(isNotUndefined(origins)){
+							let builtIn = true;
+							for(let i = 0; i < origins.length; i++){
+								if(origins[i].type !== OriginType.BuiltInFunctionOrigin){
+									builtIn = false;
+								}
+							}
+							if(!builtIn){
+								return false;
+							}
+						}
+
+						const fCall = _data.dataflow.graph.getVertex(element.node.info.id) as DataflowGraphVertexFunctionCall;
+
+						//filter out function calls with argument "call." set to false
+						for(let i = 0; i < fCall.args.length; i++){
+							if(FunctionArgument.hasName(fCall.args[i], 'call.')){
+								const argId = FunctionArgument.getReference(fCall.args[i]) as NodeId;
+								const res = resolveIdToValue(argId, { graph: _data.dataflow.graph, environment: fCall.environment, ctx: _data.analyzer.inspectContext() });
+								if(res.type === 'set' && res.elements.length != 0){
+									if(res.elements[0].type === 'logical'){
+										return res.elements[0].value;
+									}
+								}
+							}
+						}
+						return true;
+					})
 					.map(element => ({
 						certainty:  LintingResultCertainty.Uncertain,
 						involvedId: element.node.info.id,
--- test/functionality/linter/lint-stop-call.test.ts
+++ test/functionality/linter/lint-stop-call.test.ts
@@ -14,5 +14,13 @@ describe('flowR linter', withTreeSitter(parser => {
 			}
 		]);
 		assertLinter('none', parser, 'stop(x, call.=FALSE)', 'stop-call', []);
+		assertLinter('none', parser, 'stop <- function(x, call.){return 0}\nstop(3, call.=TRUE)', 'stop-call', []);
+		assertLinter('none', parser, 'stop(y, call.=TRUE)', 'stop-call', [
+			{
+				certainty: LintingResultCertainty.Uncertain,
+				loc:       SourceRange.from(1, 1, 1, 19)
+			}
+		]);
+		assertLinter('none', parser, 'x <- FALSE\nstop(y, call.=x)', 'stop-call', []);
 	});
 }));

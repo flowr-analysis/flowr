7ea08812d00727464ad8efbb31415f8cf3fe0b9b
Jonathan Riesland <jonathan.riesland@uni-ulm.de>
2026-04-01 12:04:44 +0200

    feat: extend coarseCheckWhetherToInvalidate

--- src/project/incremental/incremental-parse/incremental-parse.ts
+++ src/project/incremental/incremental-parse/incremental-parse.ts
@@ -1,8 +1,7 @@
 import type { FlowrAnalyzerContext } from '../../context/flowr-analyzer-context';
-import type { FileContentInvalidateEvent, InvalidationEvent } from '../../cache/flowr-cache';
+import type { InvalidationEvent } from '../../cache/flowr-cache';
 import { InvalidationEventType } from '../../cache/flowr-cache';
-import type { FlowrFileProvider } from '../../context/flowr-file';
-import type Parser from 'web-tree-sitter';
+import { FileRole } from '../../context/flowr-file';
 
 /**
  * Is this file even relevant to us?
@@ -11,93 +10,14 @@ export function coarseCheckWhetherToInvalidate(ctx: FlowrAnalyzerContext, event:
 	if(event?.type === InvalidationEventType.Full) {
 		return true;
 	}
-	// const path = event.file.path();
-	// if the file has not been considered by the analysis we do not have to continue
-	// TODO: make sure this also works for descriptions/other meta files maybe we have to check for the role
-	/*
-	if(event.file.roles?.includes(FileRole.Source) && !ctx.files.consideredFilesList().includes(path)) {
-		return false;
-	}
-	*/
-	const newContent = event.file.content();
-	if(event.oldContent === newContent) {
-		// TODO: maybe we want to allow a 'force' flag?
-		// nothing changed
-		console.debug('File content did not change, skipping invalidation');
+
+	if(!event.file.roles?.includes(FileRole.Source) && !event.file.roles?.includes(FileRole.Description)) {
 		return false;
 	}
 
-	return true;
-}
-
-export interface ReparseAction {
-	file: FlowrFileProvider,
-	edit: Parser.Edit;
-}
-
-/**
- * Compute the reparse action for the given file.
- * @param event - The invalidation event.
- * @returns The reparse action.
- */
-export function computeReparseAction(event: FileContentInvalidateEvent): ReparseAction {
-	const oldContent = event.oldContent?.toString() ?? '';
-	const newContent = event.file.content().toString();
-
-	const oldLen = oldContent.length;
-	const newLen = newContent.length;
-
-	// 1) Longest common prefix
-	let startIndex = 0;
-	while(
-		startIndex < oldLen &&
-		startIndex < newLen &&
-		oldContent[startIndex] === newContent[startIndex]
-	) {
-		startIndex++;
+	if(!ctx.files.consideredFilesList().includes(event.file.path())) {
+		return false;
 	}
 
-	// 2) Longest common suffix, without overlapping the prefix
-	let oldSuffixIndex = oldLen;
-	let newSuffixIndex = newLen;
-	while(
-		oldSuffixIndex > startIndex &&
-		newSuffixIndex > startIndex &&
-		oldContent[oldSuffixIndex - 1] === newContent[newSuffixIndex - 1]
-	) {
-		oldSuffixIndex--;
-		newSuffixIndex--;
-	}
-
-	const oldEndIndex = oldSuffixIndex;
-	const newEndIndex = newSuffixIndex;
-
-	return {
-		file: event.file,
-		edit: {
-			startIndex,
-			oldEndIndex,
-			newEndIndex,
-			startPosition:  indexToPoint(oldContent, startIndex),
-			oldEndPosition: indexToPoint(oldContent, oldEndIndex),
-			newEndPosition: indexToPoint(newContent, newEndIndex),
-		}
-	};
-}
-
-
-function indexToPoint(text: string, index: number): Parser.Point {
-	let row = 0;
-	let column = 0;
-
-	for(let i = 0; i < index; i++) {
-		if(text[i] === '\n') {
-			row++;
-			column = 0;
-		} else {
-			column++;
-		}
-	}
-
-	return { row, column };
+	return event.oldContent !== event.file.content();
 }

77605d81c4fa98c7d10f697e5532526a4f2e0ba3
Naomi Panda <naomi.panda@uni-ulm.de>
2026-04-23 15:11:09 +0200

    refactor: tests in new file

--- test/functionality/slicing/backward/static-backward-program-slices/eval.test.ts
+++ test/functionality/slicing/backward/static-backward-program-slices/eval.test.ts
@@ -1,9 +1,7 @@
-import { assertDataflow, assertSliced, withShell, withTreeSitter } from '../../../_helper/shell';
+import { assertSliced, withShell } from '../../../_helper/shell';
 import { label } from '../../../_helper/label';
 import { OperatorDatabase } from '../../../../../src/r-bridge/lang-4.x/ast/model/operators';
 import { describe } from 'vitest';
-import { emptyGraph } from '../../../../../src/dataflow/graph/dataflowgraph-builder';
-import { EdgeType } from '../../../../../src/dataflow/graph/edge';
 
 describe.sequential('eval', withShell(shell => {
 	assertSliced(label('simple eval use', ['name-normal', ...OperatorDatabase['<-'].capabilities, 'numbers', 'unnamed-arguments', 'strings', 'built-in-evaluation', 'newlines']),
@@ -32,35 +30,6 @@ print(y)`);
 		shell, `for(v in c("x", "y")) eval(parse(text=paste(k, "<- 2")))
 print(y)`, ['2@print'], `for(v in c("x", "y")) eval(parse(text=paste(k, "<- 2")))
 print(y)`);
-}));
-
-describe.sequential('eval', withTreeSitter(tr => {
-	assertDataflow(label('simple evalText use for dataflow', ['name-normal', ...OperatorDatabase['<-'].capabilities, 'numbers', 'unnamed-arguments', 'strings', 'built-in-evaluation', 'newlines']),
-		tr, 'a <- "1+1"\nx <- "1"\nb <- "3"\nz <- eval(parse(text=x))', emptyGraph()
-			.defineVariable('2@x')
-			.defineVariable('4@z')
-			.definedBy('4@z', '4@eval')
-			.addEdge('4@eval', '4@parse', EdgeType.Argument | EdgeType.Reads | EdgeType.Returns)
-			.addEdge('4@parse', '4@x', EdgeType.Reads)
-			.addEdge('4@x', '2@x', EdgeType.Reads),
-		{
-			expectIsSubgraph:      true,
-			resolveIdsAsCriterion: true,
-			context:               'dataflow'
-		});
-	assertDataflow(label('simple eval use for dataflow', ['name-normal', ...OperatorDatabase['<-'].capabilities, 'numbers', 'unnamed-arguments', 'strings', 'built-in-evaluation', 'newlines']),
-		tr, 'x <- 1\ny <- 1\na <- 2\nz <- eval(parse(text="x+y"))', emptyGraph()
-			.definedBy('4@z', '4@eval')
-			.addEdge(17, 'eval::17-1:27-1:30-2', EdgeType.Reads | EdgeType.Calls)
-			.addEdge('eval::17-1:27-1:30-2', 'eval::17-1:27-1:30-0', EdgeType.Reads | EdgeType.Argument)
-			.addEdge('eval::17-1:27-1:30-2', 'eval::17-1:27-1:30-1', EdgeType.Reads | EdgeType.Argument)
-			.addEdge('eval::17-1:27-1:30-0', '1@x', EdgeType.Reads)
-			.addEdge('eval::17-1:27-1:30-1', '2@y', EdgeType.Reads),
-		{
-			expectIsSubgraph:      true,
-			resolveIdsAsCriterion: true,
-			context:               'dataflow'
-		});
 }));
 
 describe.sequential('evalText', withShell(shell => {
@@ -96,32 +65,4 @@ for(i in 1:4){
 }
 if(u >= 4) y <- 2
 t <- evalText("1+y")`, ['4@z'], '');
-}));
-
-describe.sequential('evalText', withTreeSitter(tr => {
-	assertDataflow(label('simple evalText use for dataflow', ['name-normal', ...OperatorDatabase['<-'].capabilities, 'numbers', 'unnamed-arguments', 'strings', 'built-in-evaluation', 'newlines']),
-		tr, 'a <- "1+1"\nx <- "1"\nb <- "3"\nz <- evalText(x)', emptyGraph()
-			.defineVariable('2@x')
-			.defineVariable('4@z')
-			.definedBy('4@z', '4@evalText')
-			.addEdge('4@evalText', '4@x', EdgeType.Argument | EdgeType.Reads | EdgeType.Returns)
-			.addEdge('4@x', '2@x', EdgeType.Reads),
-		{
-			expectIsSubgraph:      true,
-			resolveIdsAsCriterion: true,
-			context:               'dataflow'
-		});
-	assertDataflow(label('simple evalText use for dataflow', ['name-normal', ...OperatorDatabase['<-'].capabilities, 'numbers', 'unnamed-arguments', 'strings', 'built-in-evaluation', 'newlines']),
-		tr, 'x <- 1\ny <- 1\na <- 2\nz <- evalText("x+y")', emptyGraph()
-			.definedBy('4@z', '4@evalText')
-			.addEdge(13, 'evalText::13-1:27-1:34-2', EdgeType.Returns)
-			.addEdge('evalText::13-1:27-1:34-2', 'evalText::13-1:27-1:34-0', EdgeType.Reads | EdgeType.Argument)
-			.addEdge('evalText::13-1:27-1:34-2', 'evalText::13-1:27-1:34-1', EdgeType.Reads | EdgeType.Argument)
-			.addEdge('evalText::13-1:27-1:34-0', '1@x', EdgeType.Reads)
-			.addEdge('evalText::13-1:27-1:34-1', '2@y', EdgeType.Reads),
-		{
-			expectIsSubgraph:      true,
-			resolveIdsAsCriterion: true,
-			context:               'dataflow'
-		});
 }));

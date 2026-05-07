9ed75c396fc9c9a1326c40986ab684a77b67623a
MaxAtoms <7847075+MaxAtoms@users.noreply.github.com>
2026-05-07 15:21:11 +0200

    feat: add taint analysis query

---
+++ src/queries/catalog/taint-query/taint-query-executor.ts
@@ -0,0 +1,29 @@
+import { log } from '../../../util/log';
+import type { BasicQueryData } from '../../base-query-format';
+import type { TaintQuery, TaintQueryResult } from './taint-query-format';
+
+/**
+ * Executes the given taint queries using the provided analyzer.
+ */
+export async function executeTaintQuery({ analyzer }: BasicQueryData, queries: readonly TaintQuery[]): Promise<TaintQueryResult> {
+	const flattened = queries.flatMap(q => q.defs);
+
+	if(flattened.length == 0) {
+		log.warn('Missing taint query definition');
+	}
+
+	const start = Date.now();
+	const analysis = analyzer.taint();
+	for(const def of flattened) {
+		analysis.addPredefined(def);
+	}
+
+	const results = await analysis.run();
+
+	return {
+		results: results,
+		'.meta': {
+			timing: Date.now() - start
+		},
+	};
+}

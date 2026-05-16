f36ad891757843493a041933ac63ce1319746d77
Naomi Panda <naomi.panda@uni-ulm.de>
2026-05-16 18:13:15 +0200

    feat: wip identify name of package

--- src/dataflow/internal/process/functions/call/built-in/built-in-library.ts
+++ src/dataflow/internal/process/functions/call/built-in/built-in-library.ts
@@ -15,6 +15,11 @@ import { BuiltInProcName } from '../../../../../environments/built-in-proc-name'
 import { Environment } from '../../../../../environments/environment';
 import { EdgeType } from '../../../../../graph/edge';
 import { isUndefined } from '../../../../../../util/assert';
+import type { RArgument } from '../../../../../../r-bridge/lang-4.x/ast/model/nodes/r-argument';
+import { resolveIdToValue } from '../../../../../eval/resolve/alias-tracking';
+import { valueSetGuard } from '../../../../../eval/values/general';
+import type { Package } from '../../../../../../project/plugins/package-version-plugins/package';
+import { isValue } from '../../../../../eval/values/r-value';
 
 /**
  * Process a library call like `library` or `require`
@@ -26,22 +31,24 @@ export function processLibrary<OtherInfo>(
 	data: DataflowProcessorInformation<OtherInfo & ParentInformation>
 ): DataflowInformation {
 	/* we do not really know what loading the library does and what side effects it causes, hence we mark it as an unknown side effect */
-	if(args.length !== 1) {
+	const characterOnlyArg = args.map(v => v as RArgument).find(v => v.lexeme === 'character.only');
+	const characterOnly = characterOnlyArg?.value?.type === 'RLogical' && characterOnlyArg?.value?.content === true;
+	if(args.length > 2 || args.length === 2 && !characterOnly ){
 		dataflowLogger.warn(`Currently only one-arg library-likes are allows (for ${Identifier.toString(name.content)}), skipping`);
 		return processKnownFunctionCall({ name, args, rootId, data, hasUnknownSideEffect: true, origin: 'default' }).information;
 	}
 	const nameToLoad = unpackNonameArg(args[0]);
 
-	if(nameToLoad === undefined || nameToLoad.type !== RType.Symbol) {
+	if(nameToLoad === undefined || nameToLoad.type !== RType.Symbol && nameToLoad.type !== RType.String){
+	//if(nameToLoad === undefined || nameToLoad.type !== RType.Symbol && !characterOnly || characterOnly && nameToLoad.type !== RType.Symbol && nameToLoad.type !== RType.String) {
 		dataflowLogger.warn('No library name provided, skipping');
 		return processKnownFunctionCall({ name, args, rootId, data, hasUnknownSideEffect: true, origin: 'default' }).information;
 	}
-	if(Identifier.getNamespace(nameToLoad.content) !== undefined) {
+	if(Identifier.getNamespace(nameToLoad.type === RType.String ? nameToLoad.content.str : nameToLoad.content) !== undefined) {
 		dataflowLogger.warn('Namespaced library names are not supported, ignoring namespace');
 	}
-
 	// treat as a function call but convert the first argument to a string
-	const newArg: RString<OtherInfo & ParentInformation> = {
+	const newArg: RString<OtherInfo & ParentInformation> = nameToLoad.type === RType.String ? nameToLoad : {
 		type:     RType.String,
 		info:     nameToLoad.info,
 		lexeme:   nameToLoad.lexeme,
@@ -56,17 +63,50 @@ export function processLibrary<OtherInfo>(
 		hasUnknownSideEffect: true,
 		origin:               BuiltInProcName.Library
 	}).information;
-
-	const dependency = data.ctx.deps.getDependency(nameToLoad.lexeme);
-	/*if(dependency && dependency.name === 'ggplot2'){
-		if(dependency.namespaceInfo?.exportedSymbols.find(v => v === 'ggplot')){
-			linkLibraryGlob('ggplot', dependency.name, info, rootId);
+	let packetName = nameToLoad?.lexeme;
+	//case: character.only = TRUE
+	if(characterOnly){
+		const resolveArgs = { environment: data.environment, idMap: data.completeAst.idMap, resolve: data.ctx.config.solver.variables, ctx: data.ctx };
+		const resolved = valueSetGuard(resolveIdToValue(nameToLoad.info.id, resolveArgs));
+		let t = undefined;
+		if(resolved?.elements.length === 1 && resolved.elements[0].type === 'string') {
+			const r = resolved.elements[0];
+			if(isValue(r.value)){
+				t = r.value.str;
+			}
 		}
-	}*/
-	if(dependency){
-		dependency.namespaceInfo?.exportedSymbols.forEach(v => linkLibraryGlob(v, dependency.name, info, rootId));
+		if(t){
+			packetName = t;
+		} else {
+			dataflowLogger.warn('Package argument must be character only, skipping');
+			return processKnownFunctionCall({ name, args, rootId, data, hasUnknownSideEffect: true, origin: 'default' }).information;
+		}
 	}
+	const dependency = data.ctx.deps.getDependency(packetName);
+	linkDependency(dependency, info, rootId, data);
 	return info;
+}
+
+function linkDependency<OtherInfo>(dependency: Package | undefined, info: DataflowInformation, rootId: NodeId, data: DataflowProcessorInformation<OtherInfo & ParentInformation>){
+	if(dependency && dependency.namespaceInfo){
+		for(const v of dependency.namespaceInfo.exportedSymbols){
+			linkLibraryGlob(v, dependency.name, info, rootId);
+		}
+		for(const v of dependency.namespaceInfo.importedPackages){
+			//problem! we shouldn't always import everything
+			const importedDependency = data.ctx.deps.getDependency(v[0]);
+			//check whether already loaded, problem also applies here
+			//better?: define set loadedDep? in REnv and add to it whenever defining new env for pack
+			//const env = info.environment.current;
+			/*while(env !== null){
+				if(env.n === importedDependency){
+					continue;
+				}
+				env = env.parent;
+			}*/
+			linkDependency(importedDependency, info, rootId, data);
+		}
+	}
 }
 
 function linkLibraryGlob(func: string, pack: string, info: DataflowInformation, rootId: NodeId): void{
@@ -75,15 +115,17 @@ function linkLibraryGlob(func: string, pack: string, info: DataflowInformation, 
 		return;
 	}
 	const oldGlobParent = globalEnv.parent;
-	let ggplotEnv = new Environment(oldGlobParent);
-	ggplotEnv.n = pack;
-	ggplotEnv = ggplotEnv.define({
+	let packetEnv = new Environment(oldGlobParent);
+	packetEnv.n = pack;
+	console.log('before define', packetEnv);
+	packetEnv = packetEnv.define({
 		name:      Identifier.make(func, pack),
 		type:      ReferenceType.Function,
 		nodeId:    NodeId.toBuiltIn(func),
 		definedAt: NodeId.toBuiltIn(pack),
 	});
-	globalEnv.parent = ggplotEnv;
+	console.log('after define', packetEnv);
+	globalEnv.parent = packetEnv;
 	info.environment = {
 		level:   info.environment.level + 1,
 		current: info.environment.current
@@ -96,27 +138,8 @@ function getGlobalEnv(info: DataflowInformation){
 		return undefined;
 	}
 	let env = info.environment.current;
-	for(let i = 0; i < info.environment.level; i++){
+	for(let i = 1; i < info.environment.level; i++){
 		env = env.parent;
 	}
 	return env;
 }
-/*function linkLibrary(func: string, pack: string, info: DataflowInformation, rootId: NodeId): void{
-	// console.log(nameToLoad?.lexeme);g
-	// console.log(data.ctx.deps.getDependency(nameToLoad?.lexeme ?? ''))
-	const oldParent = info.environment.current.parent;
-	let ggplotEnv = new Environment(oldParent);
-	ggplotEnv.n = pack;
-	ggplotEnv = ggplotEnv.define({
-		name:      Identifier.make(func, pack),
-		type:      ReferenceType.Function,
-		nodeId:    NodeId.toBuiltIn(func),
-		definedAt: NodeId.toBuiltIn(pack),
-	});
-	info.environment.current.parent = ggplotEnv;
-	info.environment = {
-		level:   info.environment.level + 1,
-		current: info.environment.current
-	};
-	info.graph.addEdge(NodeId.toBuiltIn(func), rootId, EdgeType.Reads);
-}*/
--- test/functionality/dataflow/main/libraries/link-libraries.test.ts
+++ test/functionality/dataflow/main/libraries/link-libraries.test.ts
@@ -54,6 +54,7 @@ importFrom(stats,setNames)`)).content().current
 		}));
 		analyzer.addRequest('library(ggplot2)\nggplot()\nggplot()');
 		const df = await analyzer.dataflow();
+		console.log(Dataflow.visualize.mermaid.url(df.graph));
 		expect(df.graph.outgoingEdges(5)?.get('built-in:ggplot')?.types === EdgeType.Reads + EdgeType.Calls).toBeTruthy();
 		expect(df.graph.outgoingEdges(7)?.get('built-in:ggplot')?.types === EdgeType.Reads + EdgeType.Calls).toBeTruthy();
 		expect(df.graph.outgoingEdges('built-in:ggplot')?.get(3)?.types === EdgeType.Reads + EdgeType.Calls).toBeTruthy();
@@ -150,3 +151,151 @@ importFrom(stats,setNames)`)).content().current
 		expect(df.graph.outgoingEdges(7)?.get(5)?.types === EdgeType.Argument).toBeTruthy();
 	});
 }));
+
+describe('Linked library imports libraries', withTreeSitter(ts => {
+	test('Linked library loads imported (unloaded) library', async() => {
+		const analyzer = await new FlowrAnalyzerBuilder()
+			.setParser(ts)
+			.build();
+		analyzer.context().deps.addDependency(new Package({
+			name:          'ggplot2',
+			namespaceInfo: FlowrNamespaceFile.from(new FlowrInlineTextFile('NAMESPACE', `S3method(fortify,data.frame)
+S3method(fortify,lm)
+S3method(fortify,default)
+S3method(fortify,map)
+S3method(scale_type,Date)
+S3method(scale_type,POSIXt)
+S3method(scale_type,character)
+S3method(scale_type,numeric)
+S3method(scale_type,factor)
+S3method(scale_type,default)
+S3method(ggplot,default)
+S3method(print,ggproto)
+S3method(print,rel)
+export("+")
+export(ggplot)
+export(aes)
+export(geom_point)
+export(geom_line)
+export(theme_bw)
+export(coord_cartesian)
+export(ggsave)
+export(fortify)
+export(scale_type)
+exportPattern("^[^\\\\.]\\\\.*$")
+import(grid)
+import(rlang)
+importFrom(scales,alpha)
+importFrom(stats,setNames)`)).content().current
+		}));
+		analyzer.context().deps.addDependency(new Package({
+			name:          'dplyr',
+			namespaceInfo: FlowrNamespaceFile.from(new FlowrInlineTextFile('NAMESPACE', 'export(across)\nexport(test)')).content().current
+		}));
+		analyzer.addRequest('library(ggplot2)\nlibrary(dplyr)\nggplot(data = NULL, mapping = aes())');
+		const df = await analyzer.dataflow();
+		//console.log(analyzer.context().env);
+		console.log(df.environment.current);
+
+	});
+}));
+
+
+describe('Link libraries with character.only', withTreeSitter(ts => {
+	test('link with variable', async() => {
+		const analyzer = await new FlowrAnalyzerBuilder()
+			.setParser(ts)
+			.build();
+		analyzer.context().deps.addDependency(new Package({
+			name:          'ggplot2',
+			namespaceInfo: FlowrNamespaceFile.from(new FlowrInlineTextFile('NAMESPACE', `S3method(fortify,data.frame)
+S3method(fortify,lm)
+S3method(fortify,default)
+S3method(fortify,map)
+S3method(scale_type,Date)
+S3method(scale_type,POSIXt)
+S3method(scale_type,character)
+S3method(scale_type,numeric)
+S3method(scale_type,factor)
+S3method(scale_type,default)
+S3method(ggplot,default)
+S3method(print,ggproto)
+S3method(print,rel)
+export("+")
+export(ggplot)
+export(aes)
+export(geom_point)
+export(geom_line)
+export(theme_bw)
+export(coord_cartesian)
+export(ggsave)
+export(fortify)
+export(scale_type)
+exportPattern("^[^\\\\.]\\\\.*$")
+import(grid)
+import(rlang)
+importFrom(scales,alpha)
+importFrom(stats,setNames)`)).content().current
+		}));
+		analyzer.addRequest('x <- "ggplot2"; library(x, character.only = TRUE)\nggplot()\nggplot()');
+		const df = await analyzer.dataflow();
+		console.log(Dataflow.visualize.mermaid.url(df.graph));
+		//correctly reads character.only
+		expect(df.graph.outgoingEdges(9)?.get(8)?.types === EdgeType.Argument).toBeTruthy();
+		expect(df.graph.outgoingEdges(8)?.get(7)?.types === EdgeType.Reads).toBeTruthy();
+		expect(df.graph.idMap?.get(7)?.lexeme).toBeTruthy();
+		//ggplot links to library
+		expect(df.graph.outgoingEdges(11)?.get('built-in:ggplot')?.types === EdgeType.Reads + EdgeType.Calls).toBeTruthy();
+		expect(df.graph.outgoingEdges(13)?.get('built-in:ggplot')?.types === EdgeType.Reads + EdgeType.Calls).toBeTruthy();
+		expect(df.graph.outgoingEdges('built-in:ggplot')?.get(6)?.types === EdgeType.Reads + EdgeType.Calls).toBeTruthy();
+	});
+	test('link with string value', async() => {
+		const analyzer = await new FlowrAnalyzerBuilder()
+			.setParser(ts)
+			.build();
+		analyzer.context().deps.addDependency(new Package({
+			name:          'ggplot2',
+			namespaceInfo: FlowrNamespaceFile.from(new FlowrInlineTextFile('NAMESPACE', `S3method(fortify,data.frame)
+S3method(fortify,lm)
+S3method(fortify,default)
+S3method(fortify,map)
+S3method(scale_type,Date)
+S3method(scale_type,POSIXt)
+S3method(scale_type,character)
+S3method(scale_type,numeric)
+S3method(scale_type,factor)
+S3method(scale_type,default)
+S3method(ggplot,default)
+S3method(print,ggproto)
+S3method(print,rel)
+export("+")
+export(ggplot)
+export(aes)
+export(geom_point)
+export(geom_line)
+export(theme_bw)
+export(coord_cartesian)
+export(ggsave)
+export(fortify)
+export(scale_type)
+exportPattern("^[^\\\\.]\\\\.*$")
+import(grid)
+import(rlang)
+importFrom(scales,alpha)
+importFrom(stats,setNames)`)).content().current
+		}));
+		analyzer.addRequest('library("ggplot2", character.only = TRUE)\nggplot()\nggplot()');
+		const df = await analyzer.dataflow();
+		console.log(Dataflow.visualize.mermaid.url(df.graph));
+		//correctly reads character.only
+		console.log(df.graph);
+		expect(df.graph.outgoingEdges(6)?.get(5)?.types === EdgeType.Argument).toBeTruthy();
+		expect(df.graph.outgoingEdges(5)?.get(4)?.types === EdgeType.Reads).toBeTruthy();
+		expect(df.graph.idMap?.get(4)?.lexeme).toBeTruthy();
+		//ggplot2 links to library
+		expect(df.graph.outgoingEdges(8)?.get('built-in:ggplot')?.types === EdgeType.Reads + EdgeType.Calls).toBeTruthy();
+		expect(df.graph.outgoingEdges(10)?.get('built-in:ggplot')?.types === EdgeType.Reads + EdgeType.Calls).toBeTruthy();
+		expect(df.graph.outgoingEdges('built-in:ggplot')?.get(6)?.types === EdgeType.Reads + EdgeType.Calls).toBeTruthy();
+	});
+}));
+

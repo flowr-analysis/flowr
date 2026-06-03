4c24f74ad903811d926beeb6b7fea4ad8505f478
Ellpeck <me@ellpeck.de>
2026-06-03 14:18:19 +0200

    test: update search benchmarks!

--- test/functionality/search/search.bench.ts
+++ test/functionality/search/search.bench.ts
@@ -11,19 +11,25 @@ describe('flowR search', withTreeSitter(parser => {
 		benchmarkSearch('none', parser, "cat('hello')\nprint('world')",
 			Q.all().filter({ name: FlowrFilter.MatchesEnrichment, args: {
 				enrichment: Enrichment.CallTargets,
-				test:       /print/
+				test:       {
+					targets: /library/
+				}
 			} })
 		);
 		benchmarkSearch('other', parser, "cat('hello')\nprint('world')",
 			Q.all().with(Enrichment.CallTargets).filter({ name: FlowrFilter.MatchesEnrichment, args: {
 				enrichment: Enrichment.CallTargets,
-				test:       /library/
+				test:       {
+					targets: /library/
+				}
 			} })
 		);
 		benchmarkSearch('match', parser, "cat('hello')\nprint('world')",
 			Q.all().with(Enrichment.CallTargets).filter({ name: FlowrFilter.MatchesEnrichment, args: {
 				enrichment: Enrichment.CallTargets,
-				test:       /print/
+				test:       {
+					targets: /print/
+				}
 			} })
 		);
 		const cfgArgs: CfgInformationArguments = {
@@ -33,25 +39,33 @@ describe('flowR search', withTreeSitter(parser => {
 		benchmarkSearch('reachable always', parser, 'if(TRUE) 1 else 2', Q.all().with(Enrichment.CfgInformation, cfgArgs).filter({
 			name: FlowrFilter.MatchesEnrichment, args: {
 				enrichment: Enrichment.CfgInformation,
-				test:       /"isReachable":true/
+				test:       {
+					isReachable: true
+				}
 			}
 		}));
 		benchmarkSearch('reachable never', parser, 'if(FALSE) 1 else 2', Q.all().with(Enrichment.CfgInformation, cfgArgs).filter({
 			name: FlowrFilter.MatchesEnrichment, args: {
 				enrichment: Enrichment.CfgInformation,
-				test:       /"isReachable":true/
+				test:       {
+					isReachable: /true/
+				}
 			}
 		}));
 		benchmarkSearch('reachable no dead code', parser, 'if(FALSE) 1 else 2', Q.all().with(Enrichment.CfgInformation).filter({
 			name: FlowrFilter.MatchesEnrichment, args: {
 				enrichment: Enrichment.CfgInformation,
-				test:       /"isReachable":false/
+				test:       {
+					isReachable: false
+				}
 			}
 		}));
 		benchmarkSearch('reachable no reachable', parser, 'if(FALSE) 1 else 2', Q.all().with(Enrichment.CfgInformation).filter({
 			name: FlowrFilter.MatchesEnrichment, args: {
 				enrichment: Enrichment.CfgInformation,
-				test:       /"isReachable":false/
+				test:       {
+					isReachable: /false/
+				}
 			}
 		}));
 	});

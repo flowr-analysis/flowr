5edf6cd0a623a5166bc8d4774c5ceeb429d2beef
Henry <henryschuler@outlook.de>
2026-06-04 15:33:50 +0200

    feat-fix: first batch of pr fixes

--- src/abstract-interpretation/benchmarking.ts
+++ src/abstract-interpretation/benchmarking.ts
@@ -7,22 +7,21 @@ import { VertexType } from '../dataflow/graph/vertex';
 import { RType } from '../r-bridge/lang-4.x/ast/model/type';
 import { IntervalExpressionSemanticsMapper } from './interval/expression-semantics';
 import { Identifier } from '../dataflow/environments/identifier';
-import { isNotUndefined, isUndefined } from '../util/assert';
 import { PentagonExpressionSemanticsMapper } from './pentagon/expression-semantics';
 import { IntervalSemanticsMaper } from './interval/condition-semantics';
 import { UpperBoundsSemanticsMapper } from './pentagon/upper-bounds/upper-bounds-condition-semantics';
 import { NumericIntervalInferenceVisitor } from './interval/numeric-interval-inference';
-import { SourceLocation } from '../util/range';
+import { SourceRange } from '../util/range';
 import { NumericPentagonInferenceVisitor } from './pentagon/numeric-pentagon-inference';
 import path from 'path';
+import type { AbsintVisitorConfiguration } from './absint-visitor';
 
 if(process.argv.length < 4) {
 	console.error('Usage: ts-node src/abstract-interpretation/benchmarking.ts <file-to-analyze> <output-folder>');
 	process.exit(1);
 }
 
-const filePath = process.argv[2];
-const outputDirectory = process.argv[3];
+const [_, __, filePath, outputDirectory] = process.argv;
 
 interface FileMetadata {
 	fileName:                               string;
@@ -40,6 +39,9 @@ interface FileMetadata {
 	numOfConditions:                        number;
 	metadataGatheringInMs:                  number;
 	baselineInMs:                           number;
+	astInMs:                                number;
+	dfgInMs:                                number;
+	cfgInMs:                                number;
 	intervalAnalysisInMs:                   number;
 	intervalResultGatheringInMs:            number;
 	pentagonAnalysisInMs:                   number;
@@ -51,30 +53,34 @@ interface InferredValue {
 	inferredValue:      string;
 	significantFigures: string;
 	sourceLocation:     string;
+	lexeme:             string;
+	dfgTag:             VertexType | 'unknown';
 }
 
 void async function() {
 	fs.mkdirSync(outputDirectory, { recursive: true });
-	const fileName = filePath.split('/').reverse()[0];
-	const metadataOutputFile = fileName.slice(0, -2) + '.csv';
-	const intervalResultFile = fileName.slice(0, -2) + '-interval.csv';
-	const pentagonResultFile = fileName.slice(0, -2) + '-pentagon.csv';
+	const fileName = path.basename(filePath);
+	const fileNameWithoutEnding = path.parse(fileName).name;
+	const metadataOutputFile = fileNameWithoutEnding + '.csv';
+	const intervalResultFile = fileNameWithoutEnding + '-interval.csv';
+	const pentagonResultFile = fileNameWithoutEnding + '-pentagon.csv';
 
 	const baselineStart = performance.now();
 	const analyzer = await new FlowrAnalyzerBuilder()
 		.setEngine('tree-sitter')
 		.build();
 	analyzer.addRequest(fileProtocol + filePath);
-	const dfg = (await analyzer.dataflow()).graph;
+	const astStart = performance.now();
 	const ast = await analyzer.normalize();
-
-	const visitorContext = {
-		normalizedAst: ast,
-		dfg:           dfg,
-		controlFlow:   await analyzer.controlflow(),
-		ctx:           analyzer.inspectContext()
-	};
+	const dfgStart = performance.now();
+	const dfg = (await analyzer.dataflow()).graph;
+	const cfgStart = performance.now();
+	const cfg = await analyzer.controlflow();
+	const cfgEnd = performance.now();
+	const ctx = analyzer.context();
 	const baselineEnd = performance.now();
+
+	const visitorContext: AbsintVisitorConfiguration = { normalizedAst: ast, dfg, controlFlow: cfg, ctx };
 
 	// Run Interval Analysis
 	const intervalVisitor = new NumericIntervalInferenceVisitor(visitorContext);
@@ -87,15 +93,16 @@ void async function() {
 	const intervalResults: InferredValue[] = [];
 	for(const nodeId of intervalVisitor.getAbstractTrace().keys()) {
 		const value = intervalVisitor.getAbstractValue(nodeId);
-		if(isNotUndefined(value)) {
+		if(value !== undefined) {
 			const node = ast.idMap.get(nodeId);
-			const location = node ? SourceLocation.fromNode(node) : undefined;
 
 			intervalResults.push({
 				nodeId:             nodeId,
-				inferredValue:      value?.toString() ?? 'undefined',
-				significantFigures: value?.significantFigures?.toString() ?? 'unknown',
-				sourceLocation:     isUndefined(location) ? 'unknown' : SourceLocation.getRange(location).slice(0, 4).toString(),
+				inferredValue:      value.toString() ?? 'undefined',
+				significantFigures: value.significantFigures?.toString() ?? 'unknown',
+				sourceLocation:     SourceRange.fromNode(node)?.toString() ?? 'unknown',
+				lexeme:             node?.lexeme ?? 'unknown',
+				dfgTag:             dfg.getVertex(nodeId)?.tag ?? 'unknown',
 			});
 		}
 	}
@@ -103,9 +110,7 @@ void async function() {
 
 	// Dump Interval Results
 	const intervalPath = path.join(outputDirectory, intervalResultFile);
-	if(fs.existsSync(intervalPath)) {
-		fs.rmSync(intervalPath, { recursive: true, force: true });
-	}
+	fs.rmSync(intervalPath, { recursive: true, force: true });
 	fs.writeFileSync(intervalPath, ['nodeId', 'inferredValue', 'significantFigures', 'sourceLocation'].join(',') + '\n');
 	for(const result of intervalResults) {
 		fs.appendFileSync(intervalPath, Object.values(result).map(value => '"' + value + '"').join(',') + '\n');
@@ -122,15 +127,16 @@ void async function() {
 	const pentagonResults: InferredValue[] = [];
 	for(const nodeId of pentagonVisitor.getAbstractTrace().keys()) {
 		const value = pentagonVisitor.getAbstractValue(nodeId);
-		if(isNotUndefined(value)) {
+		if(value !== undefined) {
 			const node = ast.idMap.get(nodeId);
-			const location = node ? SourceLocation.fromNode(node) : undefined;
 
 			pentagonResults.push({
 				nodeId:             nodeId,
-				inferredValue:      value?.toString() ?? 'undefined',
-				significantFigures: value?.value.interval.significantFigures?.toString() ?? 'unknown',
-				sourceLocation:     isUndefined(location) ? 'unknown' : SourceLocation.getRange(location).slice(0, 4).toString(),
+				inferredValue:      value.toString() ?? 'undefined',
+				significantFigures: value.value.interval.significantFigures?.toString() ?? 'unknown',
+				sourceLocation:     SourceRange.fromNode(node)?.toString() ?? 'unknown',
+				lexeme:             node?.lexeme ?? 'unknown',
+				dfgTag:             dfg.getVertex(nodeId)?.tag ?? 'unknown',
 			});
 		}
 	}
@@ -138,9 +144,7 @@ void async function() {
 
 	// Dump Pentagon Results
 	const pentagonPath = path.join(outputDirectory, pentagonResultFile);
-	if(fs.existsSync(pentagonPath)) {
-		fs.rmSync(pentagonPath, { recursive: true, force: true });
-	}
+	fs.rmSync(pentagonPath, { recursive: true, force: true });
 	fs.writeFileSync(pentagonPath, ['nodeId', 'inferredValue', 'significantFigures', 'sourceLocation'].join(',') + '\n');
 	for(const result of pentagonResults) {
 		fs.appendFileSync(pentagonPath, Object.values(result).map(value => '"' + value + '"').join(',') + '\n');
@@ -153,12 +157,12 @@ void async function() {
 
 	const functionCalls = dfg.verticesOfType(VertexType.FunctionCall).toArray();
 	const supportedExpressionFunctionCalls = functionCalls.filter(([_, dfgCall]) =>
-		isNotUndefined(IntervalExpressionSemanticsMapper().find(([id]) => Identifier.matches(id, dfgCall.name))) ||
-		isNotUndefined(PentagonExpressionSemanticsMapper.find(([id]) => Identifier.matches(id, dfgCall.name)))
+		IntervalExpressionSemanticsMapper().find(([id]) => Identifier.matches(id, dfgCall.name)) !== undefined ||
+		PentagonExpressionSemanticsMapper.find(([id]) => Identifier.matches(id, dfgCall.name)) !== undefined
 	);
 	const supportedConditionFunctionCalls = functionCalls.filter(([_, dfgCall]) =>
-		isNotUndefined(IntervalSemanticsMaper().find(([id]) => Identifier.matches(id, dfgCall.name))) ||
-		isNotUndefined(UpperBoundsSemanticsMapper().find(([id]) => Identifier.matches(id, dfgCall.name))) ||
+		IntervalSemanticsMaper().find(([id]) => Identifier.matches(id, dfgCall.name)) !== undefined ||
+		UpperBoundsSemanticsMapper().find(([id]) => Identifier.matches(id, dfgCall.name)) !== undefined ||
 		['&&', '!', '||'].some(id => Identifier.matches(id, dfgCall.name))
 	);
 	const conditions = functionCalls.filter(([_, dfgCall]) =>
@@ -182,6 +186,9 @@ void async function() {
 		numOfConditions:                        conditions.length,
 		metadataGatheringInMs:                  metadataEnd - metadataStart,
 		baselineInMs:                           baselineEnd - baselineStart,
+		astInMs:                                dfgStart - astStart,
+		dfgInMs:                                cfgStart - dfgStart,
+		cfgInMs:                                cfgEnd - cfgStart,
 		intervalAnalysisInMs:                   intervalEnd - intervalStart,
 		intervalResultGatheringInMs:            intervalResultEnd - intervalResultStart,
 		pentagonAnalysisInMs:                   pentagonEnd - pentagonStart,
@@ -190,9 +197,7 @@ void async function() {
 
 	// Dump File and Runtime Metadata
 	const fileMetadataPath = path.join(outputDirectory, metadataOutputFile);
-	if(fs.existsSync(fileMetadataPath)) {
-		fs.rmSync(fileMetadataPath, { recursive: true, force: true });
-	}
+	fs.rmSync(fileMetadataPath, { recursive: true, force: true });
 	fs.writeFileSync(fileMetadataPath, Object.keys(fileMetadata).join(',') + '\n' +
 		Object.values(fileMetadata).map(value => '"' + value + '"').join(',') + '\n');
 }();

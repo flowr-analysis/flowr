235748caf9c9df43813e9516d72c532609f5321d
MaxAtoms <7847075+MaxAtoms@users.noreply.github.com>
2026-07-09 23:06:53 +0200

    feat: add namespaces to randomness analysis

--- src/taint-analysis/predefined/randomness-analysis.ts
+++ src/taint-analysis/predefined/randomness-analysis.ts
@@ -13,18 +13,39 @@ export const randomnessDomain = new FiniteDomainBuilder<Top, Bottom, [Top, Botto
 	.build();
 
 export const randomnessAnalysis = new TaintAnalysisDefinition('randomness', randomnessDomain)
-	.through([ {
-		identifier: Identifier.make('c'),
+	.from([ {
+		identifier: Identifier.make('c', 'base'),
 		taint:      Deterministic
 	},
 	{
 		identifier: [
-			'jitter', 'sample', 'sample.int', 'arima.sim', 'kmeans', 'princomp', 'rcauchy', 'rchisq', 'rexp',
-			'rgamma', 'rgeom', 'rlnorm', 'rlogis', 'rmultinom', 'rnbinom', 'rnorm', 'rpois', 'runif', 'pointLabel',
-			'some', 'rbernoulli', 'rdunif', 'generateSeedVectors',
-			'rbeta', 'rf', 'rhyper', 'rweibull', 'rt', 'rvonmises', 'rwilcox', 'rxor', 'rhyper', 'rmvnorm',
-			'rsignrank', 'randomForest',
-			'permuted', 'permute', 'shuffle', 'shuffleSet', 'data_shuffle', 'sample_frac', 'sample_n', 'slice_sample',
+			Identifier.make('jitter', 'base'),
+			Identifier.make('sample', 'base'),
+			Identifier.make('sample.int', 'base'),
+
+			// Distribution samplers and stochastic algorithms
+			Identifier.make('arima.sim', 'stats'),
+			Identifier.make('kmeans', 'stats'),
+			Identifier.make('princomp', 'stats'),
+			Identifier.make('rcauchy', 'stats'),
+			Identifier.make('rchisq', 'stats'),
+			Identifier.make('rexp', 'stats'),
+			Identifier.make('rgamma', 'stats'),
+			Identifier.make('rgeom', 'stats'),
+			Identifier.make('rlnorm', 'stats'),
+			Identifier.make('rlogis', 'stats'),
+			Identifier.make('rmultinom', 'stats'),
+			Identifier.make('rnbinom', 'stats'),
+			Identifier.make('rnorm', 'stats'),
+			Identifier.make('rpois', 'stats'),
+			Identifier.make('runif', 'stats'),
+			Identifier.make('rbeta', 'stats'),
+			Identifier.make('rf', 'stats'),
+			Identifier.make('rhyper', 'stats'),
+			Identifier.make('rweibull', 'stats'),
+			Identifier.make('rt', 'stats'),
+			Identifier.make('rwilcox', 'stats'),
+			Identifier.make('rsignrank', 'stats'),
 		],
 		taint: Random
 	}]);

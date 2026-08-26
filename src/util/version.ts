70934c7094a1d4c3a53189a8e9561d177644017b
GitHub Action <action@github.com>
2026-08-26 17:56:42 +0000

    [skip ci] Release v2.15.4

--- src/util/version.ts
+++ src/util/version.ts
@@ -8,9 +8,9 @@ import { sigDbRemoteRelease } from '../project/sigdb/sigdb-download';
 import { FlowrWikiBaseRef } from '../documentation/doc-util/doc-files';
 
 // this is automatically replaced with the current version by release-it
-const version = '2.15.3';
+const version = '2.15.4';
 // this is automatically replaced with the release date by release-it (regex-bumper, see package.json)
-const versionDate = '2026-08-26T12:04:46Z';
+const versionDate = '2026-08-26T17:54:03Z';
 
 /**
  * Retrieves the current flowR version as a new {@link SemVer} object.

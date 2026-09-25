74616233957e6461f1aa94c0686ee3f4920a3f4e
Ellpeck <me@ellpeck.de>
2026-09-25 19:02:01 +0200

    wip: transitive analysis setup

--- src/linter/rules/autoload-files.ts
+++ src/linter/rules/autoload-files.ts
@@ -1,8 +1,12 @@
 import type { LintingResult, LintingRule } from '../linter-format';
 import { LintingPrettyPrintContext, LintingResultCertainty, LintingRuleCertainty } from '../linter-format';
+import type { FlowrFileProvider } from '../../project/context/flowr-file';
 import { FileRole } from '../../project/context/flowr-file';
 import { LintingRuleTag } from '../linter-tags';
 import type { MergeableRecord } from '../../util/objects';
+import { Q } from '../../search/flowr-search-builder';
+import { Enrichment } from '../../search/search-executor/search-enrichers';
+import type { NodeId } from '../../r-bridge/lang-4.x/ast/model/processing/node-id';
 
 export interface AutoloadResult extends LintingResult {
 	readonly filePath: string;
@@ -10,23 +14,33 @@ export interface AutoloadResult extends LintingResult {
 
 export interface AutoloadConfig extends MergeableRecord {
 	readonly allowEmptyFiles:     boolean
+	readonly allowInvalidFiles:   boolean
 	readonly allowedFilePatterns: (string | RegExp)[]
 }
 
 export const AUTOLOAD_FILES = {
-	createSearch:        () => undefined as never,
-	processSearchResult: (_elements, config, data) => {
+	createSearch:        () => Q.fromQuery([{ type: 'dependencies', enabledCategories: ['source'] }]),
+	processSearchResult: (elements, config, data) => {
 		const results: AutoloadResult[] = [];
-		const ctx = data.inspectContext();
 		const patterns = config.allowedFilePatterns.map(p => typeof p == 'string' ? new RegExp(p) : p);
-		for(const file of ctx.files.getFilesByRole(FileRole.Startup)) {
+		const sourced = new Map(elements.enrichmentContent(Enrichment.QueryData).queries['dependencies'].source
+			.filter(s => s.nodeId !== undefined).map(s => [s.nodeId as NodeId, s]));
+		const files = data.inspectContext().files;
+		for(const file of files.getFilesByRole(FileRole.Startup)) {
+			analyzeFile(file);
+		}
+		return { results, '.meta': {} };
+
+		function analyzeFile(file: FlowrFileProvider) {
 			const path = file.path();
 			if(patterns.some(p => p.exec(path))) {
-				continue;
+				return;
 			}
-			if(config.allowEmptyFiles && file.content().toString().trim().length <= 0) {
-				continue;
+			const content = file.content().toString();
+			if(config.allowEmptyFiles && content.trim().length <= 0) {
+				return;
 			}
+
 			results.push({
 				certainty:  LintingResultCertainty.Certain,
 				filePath:   path,
@@ -34,9 +48,26 @@ export const AUTOLOAD_FILES = {
 				loc:        undefined
 			});
 
-			// TODO see if files source *further* files (-> dependencies query?) and report those as well (recursively!)
+			// TODO this doesn't work yet because the dependency query doesn't include files that are just "added on" through addFile, it only looks at parse requests! -> how solve :(
+			const sourcedInFile = elements.getElements().filter(e => e.node.info.file === path).map(e => sourced.get(e.node.info.id)?.value);
+			console.log(path, elements.getElements().map(e => e.node.info.file), sourced, sourcedInFile);
+			for(const sourced of sourcedInFile) {
+				if(sourced !== undefined) {
+					const otherFile = files.getFileByPath(sourced);
+					if(otherFile !== undefined) {
+						// TODO check if the file is already in our results list, otherwise we may go into an endless loop!
+						analyzeFile(otherFile);
+					} else if(!config.allowInvalidFiles) {
+						results.push({
+							certainty:  LintingResultCertainty.Uncertain,
+							filePath:   sourced,
+							involvedId: undefined,
+							loc:        undefined
+						});
+					}
+				}
+			}
 		}
-		return { results, '.meta': {} };
 	},
 	prettyPrint: {
 		[LintingPrettyPrintContext.Query]: (result) => `File \`${result.filePath}\``,
@@ -49,7 +80,8 @@ export const AUTOLOAD_FILES = {
 		certainty:     LintingRuleCertainty.OverApproximative,
 		defaultConfig: () => ({
 			allowEmptyFiles:     true,
+			allowInvalidFiles:   false,
 			allowedFilePatterns: []
 		})
 	}
-} as const satisfies LintingRule<AutoloadResult, MergeableRecord, AutoloadConfig, never, never>;
+} as const satisfies LintingRule<AutoloadResult, MergeableRecord, AutoloadConfig>;
--- test/functionality/linter/autoload-files.test.ts
+++ test/functionality/linter/autoload-files.test.ts
@@ -2,24 +2,30 @@ import { describe } from 'vitest';
 import { assertLinter } from '../_helper/linter';
 import { FlowrInlineTextFile } from '../../../src/project/context/flowr-file';
 import { LintingResultCertainty } from '../../../src/linter/linter-format';
+import { withTreeSitter } from '../_helper/shell';
 
-describe('flowR linter', () => {
+describe('flowR linter', withTreeSitter(parser => {
 	describe('autoload-files', () => {
-		assertLinter('no autoload files', undefined, '', 'autoload-files', []);
-		assertLinter('basic autoload file', undefined, '', 'autoload-files',
+		assertLinter('no autoload files', parser, '', 'autoload-files', []);
+		assertLinter('basic autoload file', parser, '', 'autoload-files',
 			[{ certainty: LintingResultCertainty.Certain, filePath: '/project/.RProfile', loc: undefined }],
 			{}, { addFiles: [new FlowrInlineTextFile('/project/.RProfile', 'system("rm -rf /")')] });
 
-		assertLinter('empty autoload file allowed', undefined, '', 'autoload-files', [], {},
+		assertLinter('empty autoload file allowed', parser, '', 'autoload-files', [], {},
 			{ addFiles: [new FlowrInlineTextFile('/project/.RProfile', '')] });
-		assertLinter('empty autoload file disallowed', undefined, '', 'autoload-files',
+		assertLinter('empty autoload file disallowed', parser, '', 'autoload-files',
 			[{ certainty: LintingResultCertainty.Certain, filePath: '/project/.RProfile', loc: undefined }],
 			{}, { allowEmptyFiles: false, addFiles: [new FlowrInlineTextFile('/project/.RProfile', '')] });
 
-		assertLinter('allowed file patterns allowed', undefined, '', 'autoload-files', [], {},
+		assertLinter('allowed file patterns allowed', parser, '', 'autoload-files', [], {},
 			{ allowedFilePatterns: ['/project/'], addFiles: [new FlowrInlineTextFile('/project/.RProfile', 'system("rm -rf /")')] });
-		assertLinter('allowed file patterns disallowed', undefined, '', 'autoload-files',
+		assertLinter('allowed file patterns disallowed', parser, '', 'autoload-files',
 			[{ certainty: LintingResultCertainty.Certain, filePath: '/project/.RProfile', loc: undefined }],
 			{}, { allowedFilePatterns: ['/projects/'], addFiles: [new FlowrInlineTextFile('/project/.RProfile', 'system("rm -rf /")')] });
+
+		assertLinter('autoload file with source', parser, '', 'autoload-files', [
+			{ certainty: LintingResultCertainty.Certain, filePath: '/project/.RProfile', loc: undefined },
+			{ certainty: LintingResultCertainty.Certain, filePath: '/project/test.R', loc: undefined }
+		], {}, { addFiles: [new FlowrInlineTextFile('/project/.RProfile', 'source("/project/test.R")')] });
 	});
-});
+}));

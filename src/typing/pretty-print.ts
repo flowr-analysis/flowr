1ec6d8694e4f6a565b4b85cd5352b918cdb5ea47
Mark <>
2026-09-25 18:26:14 +0200

    feat: pretty-printing DataTypes places smarter braces if shorten = true

--- src/typing/pretty-print.ts
+++ src/typing/pretty-print.ts
@@ -6,7 +6,13 @@ import { assertUnreachable } from '../util/assert';
  * Visualize a {@link DataType} as a string.
  * The `shorten` parameter will do sever simplification so should not be used for debugging but just for display purposes.
  */
-export function prettyPrintDataType(type: DataType, shorten = true): string {
+export function prettyPrintDataType(type: DataType, shorten = true, precedence: number = 0): string {
+	// usual precedence of set operations may not be obvious to everyone -> always add brackets
+	const PREC_UNION = 6; // A ⋃ B
+	const PREC_INTERSECTION = 6; // A ⋂ B
+	const PREC_FUNCTION = 3; // (a: A, b: B) -> C
+	/** If precedence is not high enough, returns parentheses. Otherwise, returns two empty strings. **/
+	const prec_parens = (p: number): [string, string] => p < precedence ? ["(", ")"] : ["", ""];
 	const tag = type.tag;
 	switch(tag) {
 		case DataTypeTag.String:
@@ -20,17 +26,20 @@ export function prettyPrintDataType(type: DataType, shorten = true): string {
 		case DataTypeTag.Language:
 		case DataTypeTag.S4:
 			return shorten && tag.startsWith('R') && tag.endsWith('Type') ? tag.slice(1, -4).toLowerCase() : tag.toLowerCase();
-		case DataTypeTag.Function:
-			return `(${[...type.parameterTypes.entries()].map(([k, v]) =>
+		case DataTypeTag.Function: {
+			const [popen, pclose] = prec_parens(PREC_FUNCTION);
+			return `${popen}(${[...type.parameterTypes.entries()].map(([k, v]) =>
 				`${k}: ${prettyPrintDataType(v, shorten)}`
-			).join(', ')}) ${shorten ? '→' : '->'} ${prettyPrintDataType(type.returnType, shorten)}`;
+			).join(', ')}) ${shorten ? '→' : '->'} ${prettyPrintDataType(type.returnType, shorten, PREC_FUNCTION)}${pclose}`;
+		}
 		case DataTypeTag.List: {
 			const elementType = prettyPrintDataType(type.elementType, shorten);
 			const indexedElementTypes = `${[...type.indexedElementTypes.entries()].map(([k, v]) =>
 				`${k}: ${prettyPrintDataType(v, shorten)}`
 			).join(', ')}`;
 			return `list[${shorten || indexedElementTypes === '' ? elementType : indexedElementTypes}]`;
-		} case DataTypeTag.Variable: {
+		}
+		case DataTypeTag.Variable: {
 			const prefix = shorten ? '?' : 'var';
 			const lower = prettyPrintDataType(type.lowerBound, shorten);
 			const upper = prettyPrintDataType(type.upperBound, shorten);
@@ -44,20 +53,22 @@ export function prettyPrintDataType(type: DataType, shorten = true): string {
 				return shorten ? '⊥' : 'none';
 			}
 			if(shorten && type.types.size === 1) {
-				return prettyPrintDataType([...type.types][0], shorten);
+				return prettyPrintDataType([...type.types][0], shorten, precedence);
 			}
-			const types = [...type.types].map(d => prettyPrintDataType(d, shorten));
-			return shorten ? `${types.join(' ⋃ ')})` : `union(${types.join(', ')})`;
+			const [popen, pclose] = prec_parens(PREC_UNION);
+			const types = [...type.types].map(d => prettyPrintDataType(d, shorten, PREC_UNION));
+			return shorten ? `${popen}${types.join(' ⋃ ')}${pclose}` : `union(${types.join(', ')})`;
 		}
 		case DataTypeTag.Intersection: {
 			if(type.types.size === 0) {
 				return shorten ? '⊤' : 'any';
 			}
 			if(shorten && type.types.size === 1) {
-				return prettyPrintDataType([...type.types][0], shorten);
+				return prettyPrintDataType([...type.types][0], shorten, precedence);
 			}
-			const types = [...type.types].map(d => prettyPrintDataType(d, shorten));
-			return shorten ? `${types.join(' ⋂ ')})` : `intersection(${types.join(', ')})`;
+			const [popen, pclose] = prec_parens(PREC_UNION);
+			const types = [...type.types].map(d => prettyPrintDataType(d, shorten, PREC_INTERSECTION));
+			return shorten ? `${popen}${types.join(' ⋂ ')}${pclose}` : `intersection(${types.join(', ')})`;
 		}
 		case DataTypeTag.AtomicVector:
 			return `vector[${prettyPrintDataType(type.elementType, shorten)}]`;
